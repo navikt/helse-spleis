66969b664849c2d358531cbd71407669898ae36b
Knut Nygaard <knutnyg@gmail.com>
2019-09-30 14:31:37 +0200

    Visitor: Test av sammensatte tidslinjer

--- test/src/test/kotlin/no/nav/helse/sykdomstidlinje/test/VisitorTest.kt
+++ test/src/test/kotlin/no/nav/helse/sykdomstidlinje/test/VisitorTest.kt
@@ -1,6 +1,8 @@
 package no.nav.helse.sykdomstidlinje.test
 
+import no.nav.helse.sykdomstidslinje.Arbeidsdag
 import no.nav.helse.sykdomstidslinje.CompositeSykdomstidslinje
+import no.nav.helse.sykdomstidslinje.SykHelgedag
 import no.nav.helse.sykdomstidslinje.Sykdomstidslinje
 import no.nav.helse.sykdomstidslinje.SykdomstidslinjeVisitor
 import no.nav.helse.sykdomstidslinje.Sykedag
@@ -14,7 +16,21 @@ class VisitorTest {
         private val uke1Mandag = LocalDate.of(2019, 9, 23)
         private val uke1Onsdag = LocalDate.of(2019, 9, 25)
 
+        private val uke1Fredag = LocalDate.of(2019, 9, 27)
+        private val uke2Mandag = LocalDate.of(2019, 9, 30)
+
         private val rapporteringshendelse = Testhendelse(LocalDateTime.of(2019, 10, 14, 20, 0))
+    }
+
+    @Test
+    fun flereSykdomstilfellerSlåttSammen() {
+        val visitor = VisitorCounter()
+        (Sykdomstidslinje.sykedager(uke1Mandag, uke1Onsdag, rapporteringshendelse) +
+                Sykdomstidslinje.sykedager(uke1Fredag, uke2Mandag, rapporteringshendelse)).accept(visitor)
+
+        assertEquals(2, visitor.compositeCount)
+        assertEquals(7, visitor.sykedagerCount)
+        assertEquals(1, visitor.arbeidsdagerCount)
     }
 
     @Test
@@ -29,6 +45,7 @@ class VisitorTest {
     private class VisitorCounter : SykdomstidslinjeVisitor {
         var compositeCount = 0
         var sykedagerCount = 0
+        var arbeidsdagerCount = 0
 
         override fun preVisitComposite(compositeSykdomstidslinje: CompositeSykdomstidslinje) {
             compositeCount += 1
@@ -39,6 +56,14 @@ class VisitorTest {
         }
 
         override fun visitSykedag(sykedag: Sykedag) {
+            sykedagerCount += 1
+        }
+
+        override fun visitArbeidsdag(arbeidsdag: Arbeidsdag) {
+            arbeidsdagerCount += 1
+        }
+
+        override fun visitSykHelgedag(sykHelgedag: SykHelgedag) {
             sykedagerCount += 1
         }
     }
