9c8f4a14a7405b45e7bf8773be9691c3e89f51f6
Knut Nygaard <knutnyg@gmail.com>
2019-10-10 14:52:08 +0200

    Uferdig test av slåssmetoden
    (Anders, Håkon og Knut)

--- sykdomstidslinje/src/main/kotlin/no/nav/helse/tournament/DagTurnering.kt
+++ sykdomstidslinje/src/main/kotlin/no/nav/helse/tournament/DagTurnering.kt
@@ -7,6 +7,16 @@ class DagTurnering(val source: String = "/dagturnering.csv") {
 
     internal val strategies: Map<Dag.Nøkkel, Map<Dag.Nøkkel, Strategy>> = readStrategies()
 
+    fun slåss(left: Dag, right: Dag): Dag {
+        val leftKey = left.nøkkel()
+        val rightKey = right.nøkkel()
+
+        return strategies[leftKey]?.get(rightKey)?.decide(left, right)
+            ?: strategies[rightKey]?.get(leftKey)?.decide(right, left)
+            ?: throw RuntimeException("Fant ikke strategi for $leftKey + $rightKey")
+
+    }
+
     private fun readStrategies(): Map<Dag.Nøkkel, Map<Dag.Nøkkel, Strategy>> {
         val reader = this::class.java.getResourceAsStream(source)
             .bufferedReader(Charsets.UTF_8)
@@ -21,20 +31,20 @@ class DagTurnering(val source: String = "/dagturnering.csv") {
                 row.drop(1)
                     .forEachIndexed { columnNumber, cell ->
 
-                    if (cell != "") {
-                        val strategy = strategyFor(cell)
-                        val rowKey = row[0]
-                        val columnKey = columnHeaders[columnNumber]
-                        result.getOrPut(enumValueOf(rowKey)) { mutableMapOf() }[enumValueOf(columnKey)] = strategy
+                        if (cell != "") {
+                            val strategy = strategyFor(cell)
+                            val rowKey = row[0]
+                            val columnKey = columnHeaders[columnNumber]
+                            result.getOrPut(enumValueOf(rowKey)) { mutableMapOf() }[enumValueOf(columnKey)] = strategy
+                        }
                     }
-                }
             }
 
         return result
     }
 
-    private fun strategyFor(cellValue:String) =
-        when(cellValue) {
+    private fun strategyFor(cellValue: String) =
+        when (cellValue) {
             "U" -> Undecided
             "R" -> Row
             "C" -> Column
@@ -44,26 +54,27 @@ class DagTurnering(val source: String = "/dagturnering.csv") {
         }
 }
 
-internal sealed class  Strategy {
+internal sealed class Strategy {
     abstract fun decide(row: Dag, column: Dag): Dag
 }
 
-internal object Undecided: Strategy() {
+internal object Undecided : Strategy() {
     override fun decide(row: Dag, column: Dag): Dag = Ubestemtdag(row, column)
 }
 
-internal object Row: Strategy() {
+internal object Row : Strategy() {
     override fun decide(row: Dag, column: Dag): Dag = row.erstatter(column)
 }
 
-internal object Column: Strategy() {
+internal object Column : Strategy() {
     override fun decide(row: Dag, column: Dag): Dag = column.erstatter(row)
 }
 
-internal object Latest: Strategy() {
-    override fun decide(row: Dag, column: Dag): Dag = if (row.sisteHendelse() > (column.sisteHendelse())) row else column
+internal object Latest : Strategy() {
+    override fun decide(row: Dag, column: Dag): Dag =
+        if (row.sisteHendelse() > (column.sisteHendelse())) row else column
 }
 
-internal object Impossible: Strategy() {
+internal object Impossible : Strategy() {
     override fun decide(row: Dag, column: Dag): Dag = throw RuntimeException("there was never a thing here.")
 }
--- sykdomstidslinje/src/test/kotlin/no/nav/helse/tournament/DagTurneringTest.kt
+++ sykdomstidslinje/src/test/kotlin/no/nav/helse/tournament/DagTurneringTest.kt
@@ -1,18 +1,35 @@
 package no.nav.helse.tournament
 
+import no.nav.helse.hendelse.TestHendelser.nySøknad
+import no.nav.helse.hendelse.TestHendelser.sendtSøknad
+import no.nav.helse.sykdomstidslinje.dag.Arbeidsdag
 import no.nav.helse.sykdomstidslinje.dag.Dag
+import no.nav.helse.sykdomstidslinje.dag.Sykedag
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class DagTurneringTest {
+
     @Test
     fun `Turneringen skal inneholde riktige strategier basert på en csv-fil`() {
         val turnering = DagTurnering("/microturnering.csv")
-
         assertEquals(Latest::class, turnering.strategies.getValue(Dag.Nøkkel.WD_I).getValue(Dag.Nøkkel.WD_I)::class)
         assertEquals(Row::class, turnering.strategies.getValue(Dag.Nøkkel.WD_A).getValue(Dag.Nøkkel.WD_I)::class)
         assertEquals(Undecided::class, turnering.strategies.getValue(Dag.Nøkkel.WD_A).getValue(Dag.Nøkkel.WD_A)::class)
         assertEquals(Column::class, turnering.strategies.getValue(Dag.Nøkkel.S).getValue(Dag.Nøkkel.WD_A)::class)
         assertEquals(Impossible::class, turnering.strategies.getValue(Dag.Nøkkel.S).getValue(Dag.Nøkkel.S)::class)
+    }
+
+    @Disabled("YOLO: Knut skal på ferie")
+    @Test
+    fun `Arbeidsdag fra søknad vinner over sykedag fra sykmelding`() {
+        val turnering = DagTurnering()
+        val arbeidsdag = Arbeidsdag(LocalDate.of(2019,10,10), sendtSøknad())
+        val sykedag = Sykedag(LocalDate.of(2019,10,10), nySøknad())
+        val vinner = turnering.slåss(arbeidsdag, sykedag)
+
+        assertEquals(vinner, arbeidsdag)
     }
 }
