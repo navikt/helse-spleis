38bfd850d74550f17dc2f16cb0e1035fa57b9089
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-10-10 14:39:57 +0200

    bumper sykdomstidslinjebiblioteket
    @trulsjor

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("no.nav:vault-jdbc:$vaultJdbcVersion")
     implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
 
-    implementation("no.nav.helse:sykdomstidslinje:1.2a0e901")
+    implementation("no.nav.helse:sykdomstidslinje:1.263d6f1")
 
     testImplementation("no.nav.sykepenger.kontrakter:inntektsmelding-kontrakt:2019.09.06-09-24-0426e")
     testImplementation("no.nav.syfo.kafka:sykepengesoknad:191a7a91c115dab0038a7063be52dfae34f76c3a")
