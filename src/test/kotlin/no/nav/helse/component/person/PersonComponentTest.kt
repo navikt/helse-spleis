f38a98b0683e6fe70271a8bd6dc492373a6fee50
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-10-17 09:06:00 +0200

    tester at rekkefølge på inntektsmelding og sendt søknad ikke betyr noe
    @Mort12lander

--- src/test/kotlin/no/nav/helse/component/person/PersonComponentTest.kt
+++ src/test/kotlin/no/nav/helse/component/person/PersonComponentTest.kt
@@ -2,25 +2,19 @@ package no.nav.helse.component.person
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.opentable.db.postgres.embedded.EmbeddedPostgres
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.ktor.util.KtorExperimentalAPI
-import no.nav.common.JAASCredential
 import no.nav.common.KafkaEnvironment
 import no.nav.helse.TestConstants.inntektsmeldingDTO
 import no.nav.helse.TestConstants.søknadDTO
 import no.nav.helse.Topics.behovTopic
 import no.nav.helse.Topics.inntektsmeldingTopic
 import no.nav.helse.Topics.søknadTopic
-import no.nav.helse.behov.BehovProducer
 import no.nav.helse.createHikariConfig
 import no.nav.helse.inntektsmelding.InntektsmeldingConsumer.Companion.inntektsmeldingObjectMapper
 import no.nav.helse.sakskompleks.db.runMigration
-import no.nav.helse.serde.JsonNodeDeserializer
 import no.nav.helse.serde.JsonNodeSerializer
 import no.nav.helse.testServer
 import no.nav.helse.toJsonNode
@@ -28,7 +22,6 @@ import no.nav.syfo.kafka.sykepengesoknad.dto.ArbeidsgiverDTO
 import no.nav.syfo.kafka.sykepengesoknad.dto.SoknadsstatusDTO
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
-import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
@@ -37,7 +30,6 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -66,10 +58,6 @@ internal class PersonComponentTest {
 
         private lateinit var hikariConfig: HikariConfig
 
-        private val objectMapper = jacksonObjectMapper()
-            .registerModule(JavaTimeModule())
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-
         @BeforeAll
         @JvmStatic
         internal fun `start embedded environment`() {
@@ -92,34 +80,8 @@ internal class PersonComponentTest {
 
     }
 
-
     @Test
-    fun `sender ny søknad til kafka`() {
-        testServer(config = mapOf(
-                "KAFKA_BOOTSTRAP_SERVERS" to embeddedKafkaEnvironment.brokersURL,
-                "KAFKA_USERNAME" to username,
-                "KAFKA_PASSWORD" to password,
-                "DATABASE_JDBC_URL" to embeddedPostgres.getJdbcUrl("postgres", "postgres")
-        )) {
-
-            val nySøknad = søknadDTO(status = SoknadsstatusDTO.NY)
-            sendKafkaMessage(søknadTopic, nySøknad.id!!, nySøknad.toJsonNode())
-            val resultConsumer = KafkaConsumer<String, String>(consumerProperties(), StringDeserializer(), StringDeserializer())
-            resultConsumer.subscribe(listOf(søknadTopic))
-
-            await()
-                    .atMost(30, TimeUnit.SECONDS)
-                    .untilAsserted {
-                        val record = resultConsumer.poll(Duration.ofSeconds(1))
-                        Assertions.assertFalse(record!!.isEmpty)
-                    }
-            resultConsumer.unsubscribe()
-        }
-    }
-
-
-    @Test
-    fun `komplett sak fører til at sykepengehistorikk blir etterspurt`() {
+    fun `nySøknad-sendtSøknad-inntektsmelding fører til at sykepengehistorikk blir etterspurt`() {
         testServer(config = mapOf(
                 "KAFKA_BOOTSTRAP_SERVERS" to embeddedKafkaEnvironment.brokersURL,
                 "KAFKA_USERNAME" to username,
@@ -145,6 +107,40 @@ internal class PersonComponentTest {
 
             sendKafkaMessage(inntektsmeldingTopic, inntektsMelding.inntektsmeldingId, inntektsMelding.toJsonNode())
             assertMessageReadyToBeConsumed(resultConsumer, inntektsmeldingTopic)
+
+            assertMessageReadyToBeConsumed(resultConsumer, behovTopic)
+
+            resultConsumer.unsubscribe()
+        }
+    }
+
+    @Test
+    fun `nySøknad-inntektsmelding-sendtSøknad sak fører til at sykepengehistorikk blir etterspurt`() {
+        testServer(config = mapOf(
+                "KAFKA_BOOTSTRAP_SERVERS" to embeddedKafkaEnvironment.brokersURL,
+                "KAFKA_USERNAME" to username,
+                "KAFKA_PASSWORD" to password,
+                "DATABASE_JDBC_URL" to embeddedPostgres.getJdbcUrl("postgres", "postgres")
+        )) {
+
+            val aktorID = "1234567890123"
+            val virksomhetsnummer = "123456789"
+
+            val nySøknad = søknadDTO(aktørId = aktorID, arbeidsgiver = ArbeidsgiverDTO(orgnummer = virksomhetsnummer), status = SoknadsstatusDTO.NY)
+            val sendtSøknad = søknadDTO(aktørId = aktorID, arbeidsgiver = ArbeidsgiverDTO(orgnummer = virksomhetsnummer), status = SoknadsstatusDTO.SENDT)
+            val inntektsMelding = inntektsmeldingDTO(aktørId = aktorID, virksomhetsnummer = virksomhetsnummer)
+
+            val resultConsumer = KafkaConsumer<String, String>(consumerProperties(), StringDeserializer(), StringDeserializer())
+            resultConsumer.subscribe(listOf(behovTopic, søknadTopic, inntektsmeldingTopic))
+
+            sendKafkaMessage(søknadTopic, nySøknad.id!!, nySøknad.toJsonNode())
+            assertMessageReadyToBeConsumed(resultConsumer, søknadTopic)
+
+            sendKafkaMessage(inntektsmeldingTopic, inntektsMelding.inntektsmeldingId, inntektsMelding.toJsonNode())
+            assertMessageReadyToBeConsumed(resultConsumer, inntektsmeldingTopic)
+
+            sendKafkaMessage(søknadTopic, sendtSøknad.id!!, sendtSøknad.toJsonNode())
+            assertMessageReadyToBeConsumed(resultConsumer, søknadTopic)
 
             assertMessageReadyToBeConsumed(resultConsumer, behovTopic)
 
