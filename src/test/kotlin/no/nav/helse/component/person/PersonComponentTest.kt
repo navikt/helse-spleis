36052e7f6d567246f4de1c6fa4b4dfed15944ba4
Stefan Andreas Engelien <stefan.andreas.engelien@nav.no>
2019-10-18 16:28:32 +0200

    Teste begge veier til komplett sak
    Co-authored-by: Truls Jørgensen <truls.jorgensen@nav.no>

--- src/test/kotlin/no/nav/helse/component/person/PersonComponentTest.kt
+++ src/test/kotlin/no/nav/helse/component/person/PersonComponentTest.kt
@@ -52,17 +52,17 @@ internal class PersonComponentTest {
     companion object {
 
         private val objectMapper = jacksonObjectMapper()
-                .registerModule(JavaTimeModule())
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
         private const val username = "srvkafkaclient"
         private const val password = "kafkaclient"
 
         private val embeddedKafkaEnvironment = KafkaEnvironment(
-                autoStart = false,
-                withSchemaRegistry = false,
-                withSecurity = false,
-                topicNames = listOf(søknadTopic, inntektsmeldingTopic, behovTopic)
+            autoStart = false,
+            withSchemaRegistry = false,
+            withSecurity = false,
+            topicNames = listOf(søknadTopic, inntektsmeldingTopic, behovTopic)
         )
 
         private val kafkaProducer = KafkaProducer<String, JsonNode>(producerProperties(), StringSerializer(), JsonNodeSerializer(objectMapper))
@@ -91,13 +91,13 @@ internal class PersonComponentTest {
         }
 
         private fun producerProperties() =
-                Properties().apply {
-                    put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaEnvironment.brokersURL)
-                    put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT")
-                    put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1")
-                    put(ProducerConfig.RETRIES_CONFIG, "0")
-                    put(SaslConfigs.SASL_MECHANISM, "PLAIN")
-                }
+            Properties().apply {
+                put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaEnvironment.brokersURL)
+                put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT")
+                put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1")
+                put(ProducerConfig.RETRIES_CONFIG, "0")
+                put(SaslConfigs.SASL_MECHANISM, "PLAIN")
+            }
 
 
         private fun consumerProperties(): MutableMap<String, Any>? {
@@ -112,88 +112,101 @@ internal class PersonComponentTest {
     }
 
     @Test
-    fun `innsendt Nysøknad, Søknad og Inntekstmelding fører til at sykepengehistorikk blir etterspurt`() {
-        testServer(config = mapOf(
-                "KAFKA_BOOTSTRAP_SERVERS" to embeddedKafkaEnvironment.brokersURL,
-                "KAFKA_USERNAME" to username,
-                "KAFKA_PASSWORD" to password,
-                "DATABASE_JDBC_URL" to embeddedPostgres.getJdbcUrl("postgres", "postgres")
+    fun `innsendt Nysøknad, Søknad og Inntektmelding fører til at sykepengehistorikk blir etterspurt`() {
+
+        testServer(shutdownTimeoutMs = 40000, config = mapOf(
+            "KAFKA_BOOTSTRAP_SERVERS" to embeddedKafkaEnvironment.brokersURL,
+            "KAFKA_USERNAME" to username,
+            "KAFKA_PASSWORD" to password,
+            "DATABASE_JDBC_URL" to embeddedPostgres.getJdbcUrl("postgres", "postgres")
         )) {
 
+            // Alt 1: Inntektsmelding kommer sist
             val aktørID = "1234567890123"
             val virksomhetsnummer = "123456789"
 
-            sendNySøknad(aktørID, virksomhetsnummer)
-            ventTilSøknadErSendt(antall = 1)
-            sendSøknad(aktørID, virksomhetsnummer)
-            sendInnteksmelding(aktørID, virksomhetsnummer)
+            ventTilMeldingErSendt(id = sendNySøknad(aktørID, virksomhetsnummer), topic = søknadTopic)
+            ventTilMeldingErSendt(id = sendSøknad(aktørID, virksomhetsnummer), topic = søknadTopic)
+            ventTilMeldingErSendt(id = sendInnteksmelding(aktørID, virksomhetsnummer), topic = inntektsmeldingTopic)
 
-            val behov = ventPåBehov(antall = 2).groupBy(Behov::behovType)
+            assertBehov(aktørId = aktørID, virksomhetsnummer = virksomhetsnummer, typer = listOf(Sykepengehistorikk.name, Personopplysninger.name))
 
-            behov.getValue(Sykepengehistorikk.name).first().also {
-                assertEquals(aktørID, it["aktørId"])
-                assertEquals(virksomhetsnummer, it["organisasjonsnummer"])
-                assertTrue(it.get<String>("sakskompleksId")?.isNotBlank() ?: false)
+            // Alt 2: Sendt søknad kommer sist
+            val aktørId2 = "0123456789012"
+            val virksomhetsnummer2 = "012345678"
 
-            }
+            ventTilMeldingErSendt(id = sendNySøknad(aktørId2, virksomhetsnummer2), topic = søknadTopic)
+            ventTilMeldingErSendt(id = sendInnteksmelding(aktørId2, virksomhetsnummer2), topic = inntektsmeldingTopic)
+            ventTilMeldingErSendt(id = sendSøknad(aktørId2, virksomhetsnummer2), topic = søknadTopic)
 
-            behov.getValue(Personopplysninger.name).first().also {
-                assertEquals(aktørID, it["aktørId"])
-                assertEquals(virksomhetsnummer, it["organisasjonsnummer"])
-                assertTrue(it.get<String>("sakskompleksId")?.isNotBlank() ?: false)
-            }
-
+            assertBehov(aktørId = aktørId2, virksomhetsnummer = virksomhetsnummer2, typer = listOf(Sykepengehistorikk.name, Personopplysninger.name))
         }
     }
 
-    private fun ventTilSøknadErSendt(antall: Int) {
+
+
+    private fun ventTilMeldingErSendt(topic: String, id: String) {
         val resultConsumer = KafkaConsumer(consumerProperties(), StringDeserializer(), StringDeserializer())
-        resultConsumer.subscribe(listOf(søknadTopic))
-        assertMessageConsumedCount(resultConsumer, søknadTopic, antall)
+        resultConsumer.subscribe(listOf(topic))
+        assertMessageConsumed(resultConsumer, topic, id)
         resultConsumer.unsubscribe()
     }
 
-    private fun ventPåBehov(antall: Int): List<Behov> {
-        val resultConsumer = KafkaConsumer(consumerProperties(), StringDeserializer(), StringDeserializer())
-        resultConsumer.subscribe(listOf(behovTopic))
-        val messages = assertMessageConsumedCount(resultConsumer, behovTopic, antall)
-        return messages.map {
-            Behov.fromJson(it.value())
-        }.also {
-            resultConsumer.unsubscribe()
-        }
-    }
-
-    private fun sendInnteksmelding(aktorID: String, virksomhetsnummer: String) {
+    private fun sendInnteksmelding(aktorID: String, virksomhetsnummer: String): String {
         val inntektsMelding = inntektsmeldingDTO(aktørId = aktorID, virksomhetsnummer = virksomhetsnummer)
         sendKafkaMessage(inntektsmeldingTopic, inntektsMelding.inntektsmeldingId, inntektsMelding.toJsonNode())
+        return inntektsMelding.inntektsmeldingId
     }
 
-    private fun sendSøknad(aktorID: String, virksomhetsnummer: String) {
+    private fun sendSøknad(aktorID: String, virksomhetsnummer: String): String {
         val sendtSøknad = søknadDTO(aktørId = aktorID, arbeidsgiver = ArbeidsgiverDTO(orgnummer = virksomhetsnummer), status = SoknadsstatusDTO.SENDT)
         sendKafkaMessage(søknadTopic, sendtSøknad.id!!, sendtSøknad.toJsonNode())
+        return sendtSøknad.id!!
     }
 
-    private fun sendNySøknad(aktorID: String, virksomhetsnummer: String) {
+    private fun sendNySøknad(aktorID: String, virksomhetsnummer: String): String {
         val nySøknad = søknadDTO(aktørId = aktorID, arbeidsgiver = ArbeidsgiverDTO(orgnummer = virksomhetsnummer), status = SoknadsstatusDTO.NY)
         sendKafkaMessage(søknadTopic, nySøknad.id!!, nySøknad.toJsonNode())
+        return nySøknad.id!!
     }
 
-    private fun <K, V> assertMessageConsumedCount(resultConsumer: KafkaConsumer<K, V>, topic: String, noOfExpectedMessages: Int): List<ConsumerRecord<K, V>> {
+    private fun <K, V> assertMessageConsumed(resultConsumer: KafkaConsumer<K, V>, topic: String, id: String): List<ConsumerRecord<K, V>> {
         var record: ConsumerRecords<K, V>? = null
         await()
-                .atMost(30, TimeUnit.SECONDS)
-                .untilAsserted {
-                    record = resultConsumer.poll(Duration.ofSeconds(1))
-                    assertEquals(noOfExpectedMessages, record?.records(topic)?.count(), "Antall meldinger på topic $topic skulle vært 1, men var ${record?.records(topic)?.count()}")
+            .atMost(30, TimeUnit.SECONDS)
+            .untilAsserted {
+                record = resultConsumer.poll(Duration.ofSeconds(1))
+                assertTrue(record?.records(topic)?.any {
+                    it.key()?.equals(id) ?: false
                 }
+                    ?: false, "Antall meldinger på topic $topic skulle vært 1, men var ${record?.records(topic)?.count()}")
+            }
         return record?.records(topic)?.toList() ?: emptyList()
     }
 
-    private fun sendKafkaMessage(topic: String, key: String, message: JsonNode) {
-        kafkaProducer.send(ProducerRecord(topic, key, message))
-        kafkaProducer.flush()
+    private fun assertBehov(virksomhetsnummer: String, aktørId: String, typer: List<String>) {
+        val resultConsumer = KafkaConsumer(consumerProperties(), StringDeserializer(), StringDeserializer())
+        resultConsumer.subscribe(listOf(behovTopic))
+
+        await()
+            .atMost(30, TimeUnit.SECONDS)
+            .untilAsserted {
+                val records = resultConsumer.poll(Duration.ofSeconds(1))
+                val behov = records.records(behovTopic).map { Behov.fromJson(it.value()) }.filter { it.get<String>("aktørId").equals(aktørId) }
+
+                assertEquals(typer.size, behov.size, "Antall meldinger på topic $behovTopic skulle vært ${typer.size}, men var ${records?.records(behovTopic)?.count()}")
+                assertTrue(behov.all { aktørId.equals(it.get("aktørId")) })
+                assertTrue(behov.all { virksomhetsnummer.equals(it.get("organisasjonsnummer")) })
+                assertTrue(behov.all { typer.contains(it.behovType()) })
+
+            }
+
+        resultConsumer.unsubscribe()
     }
 
+    private fun sendKafkaMessage(topic: String, key: String, message: JsonNode) {
+        kafkaProducer.send(ProducerRecord(topic, key, message)).get()
+        kafkaProducer.flush()
+    }
 
 }
