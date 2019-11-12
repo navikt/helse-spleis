22fcc4a7225e55a5bf864b9cc4c54c60871d0ca2
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-11-12 21:56:15 +0100

    test en hel gjennomgang av en sak i personMediator

--- src/main/kotlin/no/nav/helse/behov/BehovProducer.kt
+++ src/main/kotlin/no/nav/helse/behov/BehovProducer.kt
@@ -5,13 +5,14 @@ import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
 
 class BehovProducer(private val topic: String, private val producer: KafkaProducer<String, String>) {
-    companion object {
+    private companion object {
         private val log = LoggerFactory.getLogger(BehovProducer::class.java)
     }
 
-    fun sendNyttBehov(behov: Behov) =
-            producer.send(ProducerRecord(topic, behov.id().toString(), behov.toJson()))
-                    .get().also {
-                        log.info("produserte behov=$behov, recordMetadata=$it")
-                    }
+    fun sendNyttBehov(behov: Behov) {
+        producer.send(ProducerRecord(topic, behov.id().toString(), behov.toJson()))
+                .get().also {
+                    log.info("produserte behov=$behov, recordMetadata=$it")
+                }
+    }
 }
