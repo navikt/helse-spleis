d2f125d4a6f5d98b01ca03df9d9393f3ad157bc8
Knut Nygaard <knut.nygaard@nav.no>
2019-12-06 11:06:25 +0100

    Lagt inn builderpattern for å gjøre testene mer lesbare
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Truls Jørgensen <truls.jorgensen@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/tournament/DagTurneringTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/tournament/DagTurneringTest.kt
@@ -3,46 +3,69 @@ package no.nav.helse.tournament
 
 import no.nav.helse.hendelser.Testhendelse
 import no.nav.helse.sykdomstidslinje.Sykdomstidslinje
-import no.nav.helse.sykdomstidslinje.dag.*
+import no.nav.helse.sykdomstidslinje.SykdomstidslinjeHendelse
+import no.nav.helse.sykdomstidslinje.dag.Arbeidsdag
+import no.nav.helse.sykdomstidslinje.dag.Dag
+import no.nav.helse.sykdomstidslinje.dag.Sykedag
 import no.nav.helse.testhelpers.Uke
 import no.nav.helse.testhelpers.get
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 internal class DagTurneringTest {
 
-    @Test
-    fun `Turneringen skal inneholde riktige strategier basert på en csv-fil`() {
-        val turnering = DagTurnering("/microturnering.csv")
-        assertEquals(LatestOrRow::class, turnering.strategies.getValue(Dag.Nøkkel.I).getValue(Dag.Nøkkel.I)::class)
-        assertEquals(LatestOrColumn::class, turnering.strategies.getValue(Dag.Nøkkel.S_A).getValue(Dag.Nøkkel.I)::class)
-        assertEquals(Row::class, turnering.strategies.getValue(Dag.Nøkkel.WD_A).getValue(Dag.Nøkkel.I)::class)
-        assertEquals(Undecided::class, turnering.strategies.getValue(Dag.Nøkkel.WD_A).getValue(Dag.Nøkkel.WD_A)::class)
-        assertEquals(Column::class, turnering.strategies.getValue(Dag.Nøkkel.S_A).getValue(Dag.Nøkkel.WD_A)::class)
-        assertEquals(Impossible::class, turnering.strategies.getValue(Dag.Nøkkel.S_A).getValue(Dag.Nøkkel.S_A)::class)
+    companion object {
+        val turnering = DagTurnering()
+        val microturnering = DagTurnering("/microturnering.csv")
+        val testhendelse = { dato: LocalDate ->
+            Testhendelse(dato.atStartOfDay())
+        }
+
+        private val mandag get() = TestHendelseBuilder(LocalDate.of(2018, 1, 1))
+        private val søndag get() = TestHendelseBuilder(LocalDate.of(2018, 1, 7))
     }
 
     @Test
-    fun `Arbeidsdag fra søknad vinner over sykedag fra sykmelding`() {
-        val turnering = DagTurnering()
-        val sykedag = Sykedag(
-            Uke(1).mandag, Testhendelse(
-                rapportertdato = Uke(1).mandag.atTime(9, 0)
-            )
+    internal fun `Turneringen skal inneholde riktige strategier basert på en csv-fil`() {
+
+        assertEquals(
+            LatestOrRow::class,
+            microturnering.strategies.getValue(Dag.Nøkkel.I).getValue(Dag.Nøkkel.I)::class
         )
-        val arbeidsdag = Arbeidsdag(
-            Uke(1).mandag, Testhendelse(
-                rapportertdato = Uke(1).mandag.atTime(12, 0)
-            )
+        assertEquals(
+            LatestOrColumn::class,
+            microturnering.strategies.getValue(Dag.Nøkkel.S_A).getValue(Dag.Nøkkel.I)::class
         )
+        assertEquals(Row::class, microturnering.strategies.getValue(Dag.Nøkkel.WD_A).getValue(Dag.Nøkkel.I)::class)
+        assertEquals(
+            Undecided::class,
+            microturnering.strategies.getValue(Dag.Nøkkel.WD_A).getValue(Dag.Nøkkel.WD_A)::class
+        )
+        assertEquals(
+            Column::class,
+            microturnering.strategies.getValue(Dag.Nøkkel.S_A).getValue(Dag.Nøkkel.WD_A)::class
+        )
+        assertEquals(
+            Impossible::class,
+            microturnering.strategies.getValue(Dag.Nøkkel.S_A).getValue(Dag.Nøkkel.S_A)::class
+        )
+    }
+
+
+    @Test
+    internal fun `Arbeidsdag fra søknad vinner over sykedag fra sykmelding`() {
+        val sykedag = mandag.sykedag.fraSykmelding.rapportertTidlig
+        val arbeidsdag = mandag.arbeidsdag.fraSøknad.rapportertSent
+
         val vinner = turnering.slåss(arbeidsdag, sykedag)
 
         assertEquals(vinner, arbeidsdag)
     }
 
     @Test
-    fun `kombinering av tidslinjer fører til at dagsturnering slår sammen dagene`() {
+    internal fun `kombinering av tidslinjer fører til at dagsturnering slår sammen dagene`() {
         val nySøknad = Sykdomstidslinje.sykedager(
             Uke(1).mandag, Uke(1).fredag, Testhendelse(
                 rapportertdato = Uke(1).mandag.atTime(9, 0)
@@ -66,22 +89,68 @@ internal class DagTurneringTest {
     }
 
     @Test
-    fun `sykedag fra arbeidsgiver taper mot syk helgedag fra sykmeldingen`() {
-        val turnering = DagTurnering()
-        val egenmeldingsdagFraArbeidsgiver = Egenmeldingsdag(
-            Uke(1).søndag, Testhendelse(
-                rapportertdato = Uke(1).søndag.atTime(9, 0),
-                hendelsetype = Dag.NøkkelHendelseType.Inntektsmelding
-            )
-        )
-        val sykhelgedag = SykHelgedag(
-            Uke(1).søndag, Testhendelse(
-                rapportertdato = Uke(1).søndag.atTime(8, 0),
-                hendelsetype = Dag.NøkkelHendelseType.Sykmelding
-            )
-        )
-        val vinner = turnering.slåss(egenmeldingsdagFraArbeidsgiver, sykhelgedag)
+    internal fun `sykedag fra arbeidsgiver taper mot syk helgedag fra sykmeldingen`() {
+        val egenmeldingsdagFraArbeidsgiver = søndag.egenmeldingsdag.fraInntektsmelding.rapportertSent
+        val sykHelgedag = søndag.sykedag.fraSykmelding.rapportertTidlig
 
-        assertEquals(sykhelgedag, vinner)
+        val vinner = turnering.slåss(egenmeldingsdagFraArbeidsgiver, sykHelgedag)
+        assertEquals(sykHelgedag, vinner)
+    }
+
+    private class TestHendelseBuilder(private val dato: LocalDate) {
+        private var dagbuilder: ((LocalDate, SykdomstidslinjeHendelse) -> Dag)? = null
+        private var hendelsetype: Dag.NøkkelHendelseType? = null
+
+        val sykedag: TestHendelseBuilder
+            get() {
+                dagbuilder = { dato, hendelse -> Sykdomstidslinje.sykedag(dato, hendelse) }
+                return this
+            }
+
+        val arbeidsdag: TestHendelseBuilder
+            get() {
+                dagbuilder = { dato, hendelse -> Sykdomstidslinje.ikkeSykedag(dato, hendelse) }
+                return this
+            }
+
+        val egenmeldingsdag: TestHendelseBuilder
+            get() {
+                dagbuilder = { dato, hendelse -> Sykdomstidslinje.egenmeldingsdag(dato, hendelse) }
+                return this
+            }
+
+        val fraSykmelding
+            get(): TestHendelseBuilder {
+                hendelsetype = Dag.NøkkelHendelseType.Sykmelding
+                return this
+            }
+
+        val fraSøknad
+            get(): TestHendelseBuilder {
+                hendelsetype = Dag.NøkkelHendelseType.Søknad
+                return this
+            }
+
+        val fraInntektsmelding
+            get(): TestHendelseBuilder {
+                hendelsetype = Dag.NøkkelHendelseType.Inntektsmelding
+                return this
+            }
+        val rapportertTidlig
+            get() = dagbuilder!!(
+                dato, Testhendelse(
+                    dato.atStartOfDay(),
+                    Dag.NøkkelHendelseType.Søknad
+                )
+            )
+        val rapportertSent
+            get() = dagbuilder!!(
+                dato, Testhendelse(
+                    dato.atTime(18, 0),
+                    Dag.NøkkelHendelseType.Søknad
+                )
+            )
+
     }
 }
+
