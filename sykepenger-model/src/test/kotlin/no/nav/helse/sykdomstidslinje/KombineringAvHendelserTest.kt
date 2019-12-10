0f749ef4304126afc4113796f8d5e0b204198355
Anders Østby <anders.ostby@nav.no>
2019-12-10 08:36:26 +0100

    Søknad DSL tar inn parameter som angir returtypen

--- sykepenger-model/src/test/kotlin/no/nav/helse/sykdomstidslinje/KombineringAvHendelserTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/sykdomstidslinje/KombineringAvHendelserTest.kt
@@ -1,11 +1,7 @@
 package no.nav.helse.sykdomstidslinje
 
-import no.nav.helse.hendelser.søknad.SendtSøknadHendelse
 import no.nav.helse.sykdomstidslinje.dag.Egenmeldingsdag
-import no.nav.helse.testhelpers.SøknadBuilder.Companion.søknad
-import no.nav.helse.testhelpers.Uke
-import no.nav.helse.testhelpers.asHendelse
-import no.nav.helse.testhelpers.inntektsmelding
+import no.nav.helse.testhelpers.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -14,14 +10,19 @@ internal class KombineringAvHendelserTest {
 
     @Test
     internal fun `egenmelding oppgitt i søknad overskrives av arbeidsdager fra inntektsmelding`() {
-        val søknadHendelse: SendtSøknadHendelse = søknad {
-            fom = Uke(1).torsdag
-            tom = Uke(4).mandag
-            egenmelding {
-                fom = Uke(1).mandag
-                tom = Uke(1).onsdag
+        val søknadHendelse =
+            søknad(NySøknadHendelseWrapper) {
+                fom = Uke(1).torsdag
+                tom = Uke(4).mandag
+                søknadsperiode {
+                    fom = Uke(1).torsdag
+                    tom = Uke(4).mandag
+                }
+                egenmelding {
+                    fom = Uke(1).mandag
+                    tom = Uke(1).onsdag
+                }
             }
-        }.asHendelse()
 
         val inntektsmeldingHendelse = inntektsmelding {
             arbeidsgiverperiode {
--- sykepenger-model/src/test/kotlin/no/nav/helse/testhelpers/Søknad.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/testhelpers/Søknad.kt
@@ -1,61 +1,149 @@
 package no.nav.helse.testhelpers
 
+import no.nav.helse.hendelser.søknad.NySøknadHendelse
 import no.nav.helse.hendelser.søknad.SendtSøknadHendelse
 import no.nav.helse.hendelser.søknad.Sykepengesøknad
 import no.nav.helse.toJsonNode
 import no.nav.syfo.kafka.sykepengesoknad.dto.PeriodeDTO
 import no.nav.syfo.kafka.sykepengesoknad.dto.SoknadsperiodeDTO
+import no.nav.syfo.kafka.sykepengesoknad.dto.SoknadsstatusDTO
 import no.nav.syfo.kafka.sykepengesoknad.dto.SykepengesoknadDTO
 import java.time.LocalDate
 
 /* Dette er en hjelpeklasse for å generere testdata. Det er viktig at defaults her holdes til absolutt minimum, slik at
  * en ikke ender opp med tester som er avhengig av sære defaults i søknaden
  */
-internal class SøknadBuilder {
+internal fun <Type, Builder> søknad(søknadtype: Søknadtype<Type, Builder>, block: Builder.() -> Unit) =
+    søknadtype.søknad(block)
+
+internal interface Søknadtype<Type, Builder> {
+    fun søknad(block: Builder.() -> Unit): Type
+}
+
+internal object NySøknadHendelseWrapper : Søknadtype<NySøknadHendelse, SykepengesoknadDTOBuilder> {
+    override fun søknad(block: SykepengesoknadDTOBuilder.() -> Unit) =
+        NySøknadHendelseBuilder.søknad {
+            sykepengesøknad {
+                søknad {
+                    status = SoknadsstatusDTO.NY
+                    apply(block)
+                }
+            }
+        }
+}
+
+internal object SendtSøknadHendelseWrapper : Søknadtype<SendtSøknadHendelse, SykepengesoknadDTOBuilder> {
+    override fun søknad(block: SykepengesoknadDTOBuilder.() -> Unit) =
+        SendtSøknadHendelseBuilder.søknad {
+            sykepengesøknad {
+                søknad {
+                    status = SoknadsstatusDTO.SENDT
+                    apply(block)
+                }
+            }
+        }
+
+}
+
+internal object SykepengesøknadWrapper : Søknadtype<Sykepengesøknad, SykepengesoknadDTOBuilder> {
+    override fun søknad(block: SykepengesoknadDTOBuilder.() -> Unit) =
+        SykepengesøknadBuilder.søknad { søknad(block) }
+}
+
+internal class NySøknadHendelseBuilder {
+    private lateinit var sykepengesøknad: Sykepengesøknad
+
+    internal fun sykepengesøknad(block: SykepengesøknadBuilder.() -> Unit) {
+        sykepengesøknad = SykepengesøknadBuilder.søknad(block)
+    }
+
+    private fun build() = NySøknadHendelse(sykepengesøknad)
+
+    internal companion object Type : Søknadtype<NySøknadHendelse, NySøknadHendelseBuilder> {
+        override fun søknad(block: NySøknadHendelseBuilder.() -> Unit) =
+            NySøknadHendelseBuilder().apply(block).build()
+    }
+}
+
+internal class SendtSøknadHendelseBuilder {
+    private lateinit var sykepengesøknad: Sykepengesøknad
+
+    internal fun sykepengesøknad(block: SykepengesøknadBuilder.() -> Unit) {
+        sykepengesøknad = SykepengesøknadBuilder.søknad(block)
+    }
+
+    private fun build() = SendtSøknadHendelse(sykepengesøknad)
+
+    internal companion object Type : Søknadtype<SendtSøknadHendelse, SendtSøknadHendelseBuilder> {
+        override fun søknad(block: SendtSøknadHendelseBuilder.() -> Unit) =
+            SendtSøknadHendelseBuilder().apply(block).build()
+    }
+}
+
+internal class SykepengesøknadBuilder {
+    private lateinit var søknad: SykepengesoknadDTO
+
+    internal fun søknad(block: SykepengesoknadDTOBuilder.() -> Unit) {
+        søknad = SykepengesoknadDTOBuilder.søknad(block)
+    }
+
+    private fun build() = Sykepengesøknad(søknad.toJsonNode())
+
+    internal companion object Type : Søknadtype<Sykepengesøknad, SykepengesøknadBuilder> {
+        override fun søknad(block: SykepengesøknadBuilder.() -> Unit) =
+            SykepengesøknadBuilder().apply(block).build()
+    }
+}
+
+internal class SykepengesoknadDTOBuilder {
     internal lateinit var fom: LocalDate
     internal lateinit var tom: LocalDate
+    internal lateinit var status: SoknadsstatusDTO
     private val perioder = mutableListOf<SoknadsperiodeDTO>()
     private val egenmeldinger = mutableListOf<PeriodeDTO>()
 
     internal fun søknadsperiode(block: SøknadsperiodeBuilder.() -> Unit) {
-        perioder.add(SøknadsperiodeBuilder().apply(block).build())
+        perioder.add(SøknadsperiodeBuilder.søknadsperiode(block))
     }
 
     internal fun egenmelding(block: PeriodeBuilder.() -> Unit) {
-        egenmeldinger.add(PeriodeBuilder().apply(block).build())
+        egenmeldinger.add(PeriodeBuilder.periode(block))
     }
 
-    internal fun build(): SykepengesoknadDTO {
-        perioder.takeIf { it.isEmpty() }
-            ?.add(
-                SoknadsperiodeDTO(
-                    fom = this@SøknadBuilder.fom,
-                    tom = this@SøknadBuilder.tom
-                )
-            )
+    private fun build(): SykepengesoknadDTO {
+        perioder.takeIf { it.isEmpty() }?.add(SoknadsperiodeDTO(fom = fom, tom = tom))
         return SykepengesoknadDTO(
             fom = fom,
             tom = tom,
             soknadsperioder = perioder,
-            egenmeldinger = egenmeldinger
+            egenmeldinger = egenmeldinger,
+            status = status
         )
     }
 
-    internal companion object {
-        internal fun søknad(block: SøknadBuilder.() -> Unit) = SøknadBuilder().apply(block).build()
+    internal companion object Type : Søknadtype<SykepengesoknadDTO, SykepengesoknadDTOBuilder> {
+        override fun søknad(block: SykepengesoknadDTOBuilder.() -> Unit) =
+            SykepengesoknadDTOBuilder().apply(block).build()
     }
 }
 
 internal class SøknadsperiodeBuilder {
-    internal var fom: LocalDate? = null
-    internal var tom: LocalDate? = null
-    internal fun build() = SoknadsperiodeDTO(fom, tom)
+    internal lateinit var fom: LocalDate
+    internal lateinit var tom: LocalDate
+    private fun build() = SoknadsperiodeDTO(fom, tom)
+
+    internal companion object {
+        internal fun søknadsperiode(block: SøknadsperiodeBuilder.() -> Unit) =
+            SøknadsperiodeBuilder().apply(block).build()
+    }
 }
 
 internal class PeriodeBuilder {
     internal lateinit var fom: LocalDate
     internal lateinit var tom: LocalDate
-    internal fun build() = PeriodeDTO(fom, tom)
-}
+    private fun build() = PeriodeDTO(fom, tom)
 
-internal fun SykepengesoknadDTO.asHendelse() = SendtSøknadHendelse(Sykepengesøknad(this.toJsonNode()))
+    internal companion object {
+        fun periode(block: PeriodeBuilder.() -> Unit) = PeriodeBuilder().apply(block).build()
+    }
+}
