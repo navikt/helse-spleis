406aba853c929d2edf5d977bdd72156044e3a087
Fred George <fredgeorge@acm.org>
2019-12-17 08:23:58 +0100

    Updated Sykdomstidslinjer to generate real Sykdomstidslinje. Tests still needed.

--- sykepenger-model/src/test/kotlin/no/nav/helse/fixtures/Sykdomstidslinjer.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/fixtures/Sykdomstidslinjer.kt
@@ -1,14 +1,70 @@
 package no.nav.helse.fixtures
 
+import no.nav.helse.hendelser.Testhendelse
+import no.nav.helse.sykdomstidslinje.ConcreteSykdomstidslinje
 import java.time.LocalDate
 
-private val seed = LocalDate.of(2018, 1, 1)
-private var currentDate = seed
-private val nextDate get() = currentDate.also { currentDate = currentDate.plusDays(1) }
+private val frøDato = LocalDate.of(2018, 1, 1)
+private var dagensDato = frøDato
+private val nesteDato get() = dagensDato.also { dagensDato = dagensDato.plusDays(1) }
 
-fun resetSeed() { currentDate = seed }
+internal fun resetSeed() {
+    dagensDato = frøDato
+}
 
-val Int.S get() = (1..this).map { Sykdomsdag('S', nextDate) }
-val Int.F get() = (1..this).map { Sykdomsdag('F', nextDate) }
+internal val sendtSykmelding =
+    Testhendelse(3.mandag.atStartOfDay())
 
-data class Sykdomsdag(val type: Any, val dato: LocalDate)
+internal val Int.S
+    get() = ConcreteSykdomstidslinje.sykedager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.F
+    get() = ConcreteSykdomstidslinje.ferie(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.A
+    get() = ConcreteSykdomstidslinje.ikkeSykedager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.I
+    get() = ConcreteSykdomstidslinje.implisittdager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.E
+    get() = ConcreteSykdomstidslinje.egenmeldingsdager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.P
+    get() = ConcreteSykdomstidslinje.permisjonsdager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.EDU
+    get() = ConcreteSykdomstidslinje.studiedager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.UT
+    get() = ConcreteSykdomstidslinje.utenlandsdager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
+
+internal val Int.U
+    get() = ConcreteSykdomstidslinje.ubestemtdager(
+        nesteDato, nesteDato.plusDays(this.toLong() - 1),
+        sendtSykmelding
+    )
