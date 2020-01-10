743ffa0ab2a1116a993fa7a4bb8ccfa200de7f65
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2020-01-10 19:21:20 +0100

    kan bare hente data fra nøkler som er angitt på forhånd

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/hendelser/JsonMessage.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/hendelser/JsonMessage.kt
@@ -15,6 +15,7 @@ internal open class JsonMessage(private val originalMessage: String, private val
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
     private val json: JsonNode
+    private val recognizedKeys = mutableMapOf<String, JsonNode>()
 
     init {
         json = try {
@@ -30,18 +31,37 @@ internal open class JsonMessage(private val originalMessage: String, private val
     }
 
     fun requiredKey(key: String) {
-        if (json.path(key).isMissingNode) {
-            problems.error("Missing required key $key")
-        } else if (json[key].isNull) {
-            problems.error("Missing required key $key; value is null")
-        }
+        if (isKeyMissing(key)) return problems.error("Missing required key $key")
+        if (json.path(key).isNull) return problems.error("Missing required key $key; value is null")
+        accessor(key)
     }
 
     fun requiredValue(key: String, value: Boolean) {
-        if (!json.path(key).isBoolean || json.path(key).booleanValue() != value) {
-            problems.error("Required $key is not boolean $value")
+        if (isKeyMissing(key) || !json.path(key).isBoolean || json.path(key).booleanValue() != value) {
+            return problems.error("Required $key is not boolean $value")
         }
+        accessor(key)
     }
+
+    fun requiredValue(key: String, value: String) {
+        if (isKeyMissing(key) || !json.path(key).isTextual || json.path(key).asText() != value) {
+            return problems.error("Required $key is not string $value")
+        }
+        accessor(key)
+    }
+
+    fun interestedIn(key: String) {
+        accessor(key)
+    }
+
+    private fun accessor(key: String) {
+        recognizedKeys.computeIfAbsent(key) { json.path(key) }
+    }
+
+    private fun isKeyMissing(key: String) = json.path(key).isMissingNode
+
+    operator fun get(key: String): JsonNode =
+        requireNotNull(recognizedKeys[key]) { "$key is unknown; keys must be declared as required, forbidden, or interesting" }
 
     fun toJson() = originalMessage
 }
--- sykepenger-mediators/src/test/kotlin/no/nav/helse/unit/spleis/hendelser/JsonMessageTest.kt
+++ sykepenger-mediators/src/test/kotlin/no/nav/helse/unit/spleis/hendelser/JsonMessageTest.kt
@@ -2,9 +2,9 @@ package no.nav.helse.unit.spleis.hendelser
 
 import no.nav.helse.spleis.hendelser.JsonMessage
 import no.nav.helse.spleis.hendelser.MessageProblems
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class JsonMessageTest {
 
@@ -46,7 +46,103 @@ internal class JsonMessageTest {
         }
     }
 
-    class ExtendedMessage(originalMessage: String, problems: MessageProblems) : JsonMessage(originalMessage, problems) {
+    @Test
+    internal fun `keys must be declared before use`() {
+        "{\"foo\": \"bar\"}".also { json ->
+            message(json).also {
+                assertThrows<IllegalArgumentException> { it["foo"] }
+                it.requiredKey("foo")
+                assertEquals("bar", it["foo"].textValue())
+            }
+            message(json).also {
+                assertThrows<IllegalArgumentException> { it["foo"] }
+                it.interestedIn("foo")
+                assertEquals("bar", it["foo"].textValue())
+            }
+        }
+    }
+
+    @Test
+    internal fun requiredValue() {
+        "{}".also {
+            assertThrows(it, "foo", "bar")
+            assertThrows(it, "foo", false)
+        }
+        "{\"foo\": null}".also {
+            assertThrows(it, "foo", "bar")
+            assertThrows(it, "foo", false)
+        }
+        assertThrows("{\"foo\": \"baz\"}", "foo", "bar")
+        assertThrows("{\"foo\": true}", "foo", false)
+
+        assertEquals("{\"foo\": \"bar\"}", "foo", "bar")
+        assertEquals("{\"foo\": false}", "foo", false)
+    }
+
+    @Test
+    internal fun `requiredKey can not return null`() {
+        val message = message("{\"foo\": null}").apply {
+            requiredKey("foo")
+        }
+
+        assertThrows<IllegalArgumentException> {
+            message["foo"]
+        }
+    }
+
+    @Test
+    internal fun `interestedIn can return null`() {
+        val message = message("{\"foo\": null}").apply {
+            interestedIn("foo")
+        }
+        assertNull(message["foo"].textValue())
+    }
+
+    private fun assertEquals(msg: String, key: String, expectedValue: String) {
+        val problems = MessageProblems(msg)
+        JsonMessage(msg, problems).also {
+            it.requiredValue(key, expectedValue)
+            assertFalse(problems.hasErrors())
+            assertEquals(expectedValue, it[key].textValue())
+        }
+    }
+
+    private fun assertEquals(msg: String, key: String, expectedValue: Boolean) {
+        val problems = MessageProblems(msg)
+        JsonMessage(msg, problems).also {
+            it.requiredValue(key, expectedValue)
+            assertFalse(problems.hasErrors())
+            assertEquals(expectedValue, it[key].booleanValue())
+        }
+    }
+
+    private fun assertThrows(msg: String, key: String, expectedValue: Boolean) {
+        val problems = MessageProblems(msg)
+        JsonMessage(msg, problems).also {
+            it.requiredValue(key, expectedValue)
+            assertTrue(problems.hasErrors())
+            assertThrows(it, key)
+        }
+    }
+
+    private fun assertThrows(msg: String, key: String, expectedValue: String) {
+        val problems = MessageProblems(msg)
+        JsonMessage(msg, problems).also {
+            it.requiredValue(key, expectedValue)
+            assertTrue(problems.hasErrors())
+            assertThrows(it, key)
+        }
+    }
+
+    private fun assertThrows(message: JsonMessage, key: String) {
+        assertThrows<IllegalArgumentException>({ "should throw exception, instead returned ${message[key]}" }) {
+            message[key]
+        }
+    }
+
+    private fun message(json: String) = JsonMessage(json, MessageProblems(json))
+
+    private class ExtendedMessage(originalMessage: String, problems: MessageProblems) : JsonMessage(originalMessage, problems) {
         init {
             requiredKey("required_key")
         }
