a688cadf54bb6a8e2bda0e4c112f8a4f35cd41e6
royag-nav <roy.andre.gundersen@nav.no>
2020-01-22 16:33:25 +0100

    Laget skall for ModelBuilder som er en JsonVisitor
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/ModelBuilder.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/ModelBuilder.kt
@@ -1,6 +1,103 @@
 package no.nav.helse.serde
 
+import no.nav.helse.person.Arbeidsgiver
+import no.nav.helse.person.Person
+import java.util.*
+
 internal class ModelBuilder : StructureVisitor {
+
+    private var personResult:Person? = null
+
+    internal val result:Person get() = personResult!!
+
+    private val stack: Stack<ModelState> = Stack()
+
+    init {
+        stack.push(Root())
+    }
+
+    private val currentState: ModelState
+        get() = stack.peek()
+
+    override fun toString() = currentState.toString()
+
+    private fun pushState(state: ModelState) {
+        currentState.leaving(this)
+        stack.push(state)
+        currentState.entering(this)
+    }
+
+    private fun popState() {
+        currentState.leaving(this)
+        stack.pop()
+        currentState.entering(this)
+    }
+
+    override fun preVisitArrayField(name: String) { currentState.preVisitArrayField(this, name) }
+    override fun postVisitArrayField() { currentState.postVisitArrayField(this) }
+    override fun preVisitObjectField(name: String) { currentState.preVisitObjectField(this, name) }
+    override fun postVisitObjectField() { currentState.postVisitObjectField(this) }
+    override fun visitStringField(name: String, value: String) { currentState.visitStringField(this, name, value) }
+    override fun visitBooleanField(name: String, value: Boolean) { currentState.visitBooleanField(this, name, value) }
+    override fun visitNumberField(name: String, value: Number) { currentState.visitNumberField(this, name, value) }
+
+    override fun preVisitArray() { currentState.preVisitArray(this) }
+    override fun postVisitArray() { currentState.postVisitArray(this) }
+    override fun preVisitObject() { currentState.preVisitObject(this) }
+    override fun postVisitObject() { currentState.postVisitObject(this) }
+    override fun visitString(value: String) { currentState.visitString(this, value) }
+    override fun visitBoolean(value: Boolean) { currentState.visitBoolean(this, value) }
+    override fun visitNumber(value: Number) { currentState.visitNumber(this, value) }
+
+    private interface ModelState {
+        fun entering(builder: ModelBuilder) {}
+        fun leaving(builder: ModelBuilder) {}
+
+        fun preVisitArrayField(builder: ModelBuilder, name: String) {}
+        fun postVisitArrayField(builder: ModelBuilder) {}
+        fun preVisitObjectField(builder: ModelBuilder, name: String) {}
+        fun postVisitObjectField(builder: ModelBuilder) {}
+        fun visitStringField(builder: ModelBuilder, name: String, value: String) {}
+        fun visitBooleanField(builder: ModelBuilder, name: String, value: Boolean) {}
+        fun visitNumberField(builder: ModelBuilder, name: String, value: Number) {}
+        fun preVisitArray(builder: ModelBuilder) {}
+        fun postVisitArray(builder: ModelBuilder) {}
+        fun preVisitObject(builder: ModelBuilder) {}
+        fun postVisitObject(builder: ModelBuilder) {}
+        fun visitString(builder: ModelBuilder, value: String) {}
+        fun visitBoolean(builder: ModelBuilder, value: Boolean) {}
+        fun visitNumber(builder: ModelBuilder, value: Number) {}
+    }
+
+    private class Root : ModelState {
+        override fun preVisitObject(builder: ModelBuilder) {
+            builder.stack.push(PersonState {
+                builder.personResult = it
+            })
+        }
+    }
+
+    private class PersonState(private val personSetter : (Person) -> Unit) : ModelState {
+        val arbeidsgivere = mutableListOf<Arbeidsgiver>()
+        var aktørId: String? = null
+        var fødselsnummer: String? = null
+
+        override fun visitStringField(builder: ModelBuilder, name: String, value: String) {
+            when (name) {
+                "aktørId" -> aktørId = value
+                "fødselsnummer" -> fødselsnummer = value
+            }
+        }
+
+        override fun postVisitObject(builder: ModelBuilder) {
+            personSetter(Person(aktørId!!, fødselsnummer!!))
+        }
+
+        override fun preVisitArrayField(builder: ModelBuilder, name: String) {
+            //if (name == "arbeidsgivere")
+        }
+    }
+
 
 
 }
--- sykepenger-model/src/test/kotlin/no/nav/helse/serde/JsonDeserializerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/serde/JsonDeserializerTest.kt
@@ -3,38 +3,97 @@ package no.nav.helse.serde
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.helse.person.Arbeidsgiver
 import no.nav.helse.person.InntektHistorie
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 
 internal class JsonDeserializerTest {
 
     @Test
     fun testPrivateConstructorReflectionStuff() {
-        val c = Arbeidsgiver::class.java.getDeclaredConstructor(String::class.java, UUID::class.java, InntektHistorie::class.java)
+        val c = Arbeidsgiver::class.java.getDeclaredConstructor(
+            String::class.java,
+            UUID::class.java,
+            InntektHistorie::class.java
+        )
         c.isAccessible = true
         val uuid = UUID.randomUUID()
         val ih = InntektHistorie()
-        val arb:Arbeidsgiver = c.newInstance("999888777", uuid, ih)
+        val arb: Arbeidsgiver = c.newInstance("999888777", uuid, ih)
         println(arb)
     }
 
+    private val aktørId = "AKTID"
+    private val fødselsnummer = "FNR"
+
+    private val uuid1 = UUID.randomUUID().toString()
+
     @Test
     fun test1() {
-        val json = enkelTidslinjeJson()
+        val json = enkelPersonJson()
         val visitableJson = JsonVisitable(json)
         val modelBuilder = ModelBuilder()
         visitableJson.accept(modelBuilder)
-        //println(modelBuilder.result)
+        val result = modelBuilder.result
+
+        assertEquals(aktørId, result.privatProp("aktørId"))
+        assertEquals(fødselsnummer, result.privatProp("fødselsnummer"))
     }
 
-    private fun enkelTidslinjeJson() =
+
+    private fun enkelPersonJson() =
         jacksonObjectMapper().writeValueAsString(
-        mapOf( "tidslinje" to listOf( mapOf(
-            "type" to "FERIEDAG",
-            "dato" to "2020-01-01"
-        ))))
-
-
-
-
+            mapOf(
+                "aktørId" to aktørId,
+                "fødselsnummer" to fødselsnummer,
+                "arbeidsgivere" to emptyList<Map<String,Any?>>(),
+                "hendelser" to listOf(
+                    mapOf(uuid1 to mapOf<String, Any?>(
+                        "id" to uuid1,
+                        "type" to "henning"
+                    ))
+                )
+            )
+        )
 }
+
+/*
+    private fun enkelPersonJson() =
+        jacksonObjectMapper().writeValueAsString(
+            listOf(
+                mapOf(
+                    hendelseId to mapOf<String, Any?>(
+                        "id" to hendelseId,
+                        "type" to "ArbeidstakerHendelse"
+                    )
+                ),
+                mapOf(
+                    "aktørId" to aktørId,
+                    "fødselsnummer" to fødselsnummer,
+                    "arbeidsgivere" to listOf(
+                        mapOf(
+                            "organisasjonsnummer" to organisasjonsnummer,
+                            "id" to arbeidsgiverId,
+                            "inntekter" to listOf(
+                                mapOf(
+                                    "fom" to "2020-01-01",
+                                    "hendelse" to hendelseId,
+                                    "beløp" to 30000.0
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+ */
+
+private fun Any.privatProp(fieldName: String): Any? =
+    this::class.memberProperties.first { it.name == fieldName }.apply {
+        isAccessible = true
+    }.call(this)
+
+
+
