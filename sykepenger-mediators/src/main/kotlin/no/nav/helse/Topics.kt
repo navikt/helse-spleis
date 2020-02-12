cab7a282d1372f236b8b5083c00cd3d5bfc5755a
David Steinsland <david@steinsland.email>
2020-02-12 11:06:18 +0100

    endrer navn på topic og reduserer antall partisjoner

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/Topics.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/Topics.kt
@@ -3,7 +3,7 @@ package no.nav.helse
 internal object Topics {
     const val søknadTopic = "syfo-soknad-v2"
     const val inntektsmeldingTopic = "privat-sykepenger-inntektsmelding"
-    const val rapidTopic = "privat-helse-sykepenger-rapid-v1"
+    const val rapidTopic = "helse-rapid-v1"
 
     val hendelseKildeTopics = listOf(
         inntektsmeldingTopic,
