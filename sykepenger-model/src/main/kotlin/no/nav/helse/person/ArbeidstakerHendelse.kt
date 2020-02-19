d1981b307194500c10db0564b3921ce9ef9cdbb7
David Steinsland <david@steinsland.email>
2020-02-19 11:58:06 +0100

    fjerner kode som henger igjen fra epic-1
    Co-authored-by: Stian Douzette <stian.douzette@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/ArbeidstakerHendelse.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/ArbeidstakerHendelse.kt
@@ -9,7 +9,7 @@ abstract class ArbeidstakerHendelse protected constructor(
     private val hendelseId: UUID,
     internal val aktivitetslogger: Aktivitetslogger,
     private val aktivitetslogg: Aktivitetslogg
-) : Comparable<ArbeidstakerHendelse>, IAktivitetslogger by aktivitetslogger, IAktivitetslogg by aktivitetslogg {
+) : IAktivitetslogger by aktivitetslogger, IAktivitetslogg by aktivitetslogg {
     private val hendelseObservers = mutableListOf<HendelseObserver>()
 
     fun addObserver(hendelseObserver: HendelseObserver) = hendelseObservers.add(hendelseObserver)
@@ -21,16 +21,6 @@ abstract class ArbeidstakerHendelse protected constructor(
     abstract fun aktørId(): String
     abstract fun fødselsnummer(): String
     abstract fun organisasjonsnummer(): String
-
-    @Deprecated("Henger igjen fra Epic-1")
-    override fun compareTo(other: ArbeidstakerHendelse) = this.rapportertdato().compareTo(other.rapportertdato())
-
-    @Deprecated("Henger igjen fra Epic-1")
-    override fun equals(other: Any?) =
-        other is ArbeidstakerHendelse && other.hendelseId == this.hendelseId
-
-    @Deprecated("Henger igjen fra Epic-1")
-    override fun hashCode() = hendelseId.hashCode()
 
     internal fun need(behov: BehovType) {
         aktivitetslogg.need(melding = behov.navn)
