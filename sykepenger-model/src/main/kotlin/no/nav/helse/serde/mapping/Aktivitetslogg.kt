5391deb044523023ec8f024a49b0e790b844fcb8
Fred George <fredgeorge@acm.org>
2020-02-28 12:04:54 +0100

    Bruker konstruktør direkte
    Co-authored-by: David Steinsland <david.steinsland@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/mapping/Aktivitetslogg.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/mapping/Aktivitetslogg.kt
@@ -3,40 +3,37 @@ package no.nav.helse.serde.mapping
 import no.nav.helse.person.Aktivitetslogg
 import no.nav.helse.person.SpesifikkKontekst
 import no.nav.helse.serde.PersonData.AktivitetsloggData
-import no.nav.helse.serde.reflection.ReflectClass
 import no.nav.helse.serde.reflection.ReflectInstance.Companion.get
 
 internal fun konverterTilAktivitetslogg(aktivitetsloggData: AktivitetsloggData): Aktivitetslogg {
     val aktivitetslogg = Aktivitetslogg()
 
-    val aktivitetClass: ReflectClass = ReflectClass.getNestedClass<Aktivitetslogg>("Aktivitet")
-
     val aktiviteter = aktivitetslogg.get<Aktivitetslogg, MutableList<Any>>("aktiviteter")
     aktivitetsloggData.aktiviteter.forEach {
         val kontekster = it.kontekster.map { SpesifikkKontekst(it.kontekstType, it.kontekstMap) }
         aktiviteter.add(when (it.alvorlighetsgrad) {
-            AktivitetsloggData.Alvorlighetsgrad.INFO -> aktivitetClass.getNestedClass("Info").getInstance(
+            AktivitetsloggData.Alvorlighetsgrad.INFO -> Aktivitetslogg.Aktivitet.Info(
                 kontekster,
                 it.melding,
                 it.tidsstempel
             )
-            AktivitetsloggData.Alvorlighetsgrad.WARN -> aktivitetClass.getNestedClass("Warn").getInstance(
+            AktivitetsloggData.Alvorlighetsgrad.WARN -> Aktivitetslogg.Aktivitet.Warn(
                 kontekster,
                 it.melding,
                 it.tidsstempel
             )
-            AktivitetsloggData.Alvorlighetsgrad.BEHOV -> aktivitetClass.getNestedClass("Behov").getInstance(
+            AktivitetsloggData.Alvorlighetsgrad.BEHOV -> Aktivitetslogg.Aktivitet.Behov(
                 Aktivitetslogg.Aktivitet.Behov.Behovtype.valueOf(it.behovtype!!),
                 kontekster,
                 it.melding,
                 it.tidsstempel
             )
-            AktivitetsloggData.Alvorlighetsgrad.ERROR -> aktivitetClass.getNestedClass("Error").getInstance(
+            AktivitetsloggData.Alvorlighetsgrad.ERROR -> Aktivitetslogg.Aktivitet.Error(
                 kontekster,
                 it.melding,
                 it.tidsstempel
             )
-            AktivitetsloggData.Alvorlighetsgrad.SEVERE -> aktivitetClass.getNestedClass("Severe").getInstance(
+            AktivitetsloggData.Alvorlighetsgrad.SEVERE -> Aktivitetslogg.Aktivitet.Severe(
                 kontekster,
                 it.melding,
                 it.tidsstempel
