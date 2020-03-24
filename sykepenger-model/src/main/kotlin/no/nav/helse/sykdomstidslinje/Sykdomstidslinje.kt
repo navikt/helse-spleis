c17555dd6f607294b7e18ddf30109b183f7ab59e
Fred George <fredgeorge@acm.org>
2020-03-24 10:04:17 +0100

    Migrated all the CompositeSykdomstidslinjeTest tests to SykdomstidslinjeTest.

--- sykepenger-model/src/main/kotlin/no/nav/helse/sykdomstidslinje/Sykdomstidslinje.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/sykdomstidslinje/Sykdomstidslinje.kt
@@ -1,5 +1,7 @@
 package no.nav.helse.sykdomstidslinje
 
+import no.nav.helse.hendelser.Sykmelding.SykmeldingDagFactory
+import no.nav.helse.person.IAktivitetslogg
 import no.nav.helse.sykdomstidslinje.dag.*
 import no.nav.helse.tournament.Dagturnering
 import no.nav.helse.tournament.historiskDagturnering
@@ -12,43 +14,38 @@ internal class Sykdomstidslinje private constructor(private val dager: List<Dag>
 
     internal constructor(): this(mutableListOf<Dag>())
 
-    companion object {
-        internal fun sykedager(fra: LocalDate, til: LocalDate, grad: Double, factory: DagFactory): Sykdomstidslinje {
-            require(!fra.isAfter(til)) { "fra må være før eller lik til" }
-            return Sykdomstidslinje(fra.datesUntil(til.plusDays(1))
-                .map {sykedag(it, grad, factory) }
-                .toList())
-        }
-
-        private fun sykedag(gjelder: LocalDate, grad: Double, factory: DagFactory): Dag =
-            if (!gjelder.erHelg()) factory.sykedag(gjelder, grad) else factory.sykHelgedag(gjelder, grad)
-
-        internal fun ikkeSykedag(gjelder: LocalDate, factory: DagFactory) =
-            if (!gjelder.erHelg()) factory.arbeidsdag(gjelder) else factory.implisittDag(gjelder)
-
-        private fun beste(dagturnering: Dagturnering, a: Dag?, b: Dag?): Dag? {
-            if (a == null) return b
-            if (b == null) return a
-            return dagturnering.beste(a, b)
-        }
-    }
-
     internal val size get() = dager.size
 
+    internal fun førsteDag() = dager.first().dagen
+
+    internal fun sisteDag() = dager.last().dagen
+
+    operator fun get(dato: LocalDate) = dag(dato)
+
+    private fun dag(dato: LocalDate) = dager.find { it.dagen == dato }
+
+    internal operator fun plus(other: Sykdomstidslinje) = merge(other, historiskDagturnering)
+
     internal fun merge(other: Sykdomstidslinje, dagturnering: Dagturnering, gapDayCreator: (LocalDate) -> Dag = ::ImplisittDag): Sykdomstidslinje {
-        return kobleSammen(other) {
-            beste(dagturnering, this.dag(it), other.dag(it)) ?: gapDayCreator(it)
+        return merge(other) {
+            beste(dagturnering, dag(it), other.dag(it)) ?: gapDayCreator(it)
         }
     }
 
-    operator fun plus(other: Sykdomstidslinje) = merge(other, historiskDagturnering)
-
-    private fun kobleSammen(other: Sykdomstidslinje, mapper: (LocalDate) -> Dag): Sykdomstidslinje {
+    private fun merge(other: Sykdomstidslinje, mapper: (LocalDate) -> Dag): Sykdomstidslinje {
         if (other.size == 0) return this
         if (this.size == 0) return other
         val førsteDag = this.førsteStartdato(other)
         val sisteDag = this.sisteSluttdato(other).plusDays(1)
         return Sykdomstidslinje(førsteDag.datesUntil(sisteDag).map(mapper).toList())
+    }
+
+    internal fun valider(aktivitetslogg: IAktivitetslogg): Boolean {
+        val ugyldigeDager = listOf(Permisjonsdag.Søknad::class, Permisjonsdag.Aareg::class, Ubestemtdag::class)
+        return dager.filter { it::class in ugyldigeDager }
+            .distinctBy { it::class.simpleName }
+            .onEach { aktivitetslogg.error("Sykdomstidslinjen inneholder ustøttet dag: %s", it::class.simpleName) }
+            .isEmpty()
     }
 
     private fun førsteStartdato(other: Sykdomstidslinje) =
@@ -57,15 +54,9 @@ internal class Sykdomstidslinje private constructor(private val dager: List<Dag>
     private fun sisteSluttdato(other: Sykdomstidslinje) =
         if (this.sisteDag().isAfter(other.sisteDag())) this.sisteDag() else other.sisteDag()
 
-    private fun førsteDag() = dager.first().dagen
+    override fun toString() = toShortString()
 
-    private fun sisteDag() = dager.last().dagen
-
-    private fun dag(dato: LocalDate) = dager.find { it.dagen == dato }
-
-    operator fun get(dato: LocalDate) = dag(dato)
-
-    override fun toString(): String {
+    internal fun toDetailedString(): String {
         if (dager.isEmpty()) return "<empty>"
         return dager.joinToString(separator = "\n")
     }
@@ -79,8 +70,77 @@ internal class Sykdomstidslinje private constructor(private val dager: List<Dag>
                 Arbeidsdag.Inntektsmelding::class -> "A"
                 Arbeidsdag.Søknad::class -> "A"
                 ImplisittDag::class -> "I"
+                SykHelgedag.Sykmelding::class -> "H"
+                SykHelgedag.Søknad::class -> "H"
                 else -> "?"
             }
         }
     }
+
+    companion object {
+        internal fun sykedager(fra: LocalDate, til: LocalDate, grad: Double, factory: DagFactory): Sykdomstidslinje =
+            dag(fra, til, grad, factory, ::sykedag)
+
+        internal fun ikkeSykedager(fra: LocalDate, til: LocalDate, factory: DagFactory): Sykdomstidslinje =
+            dag(fra, til, factory, ::ikkeSykedag)
+
+        internal fun ferie(fra: LocalDate, til: LocalDate, factory: DagFactory): Sykdomstidslinje =
+            dag(fra, til, factory, ::ferie)
+
+        internal fun ubestemtdager(fra: LocalDate, til: LocalDate, factory: DagFactory): Sykdomstidslinje =
+            dag(fra, til, factory, ::ubestemtdag)
+
+        internal fun permisjonsdager(fra: LocalDate, til: LocalDate, factory: DagFactory): Sykdomstidslinje =
+            dag(fra, til, factory, ::ubestemtdag)
+
+        private fun dag(fra: LocalDate, til: LocalDate, factory: DagFactory, enDag: EnDag) =
+            dag(fra, til, Double.NaN, factory, enDag)
+
+        private fun dag(
+            fra: LocalDate,
+            til: LocalDate,
+            grad: Double,
+            factory: DagFactory,
+            enDag: EnDag
+        ): Sykdomstidslinje {
+            require(!fra.isAfter(til)) { "fra må være før eller lik til" }
+            return Sykdomstidslinje(fra.datesUntil(til.plusDays(1))
+                .map { enDag(it, grad, factory) }
+                .toList())
+        }
+
+        private fun sykedag(dato: LocalDate, grad: Double, factory: DagFactory): Dag =
+            if (!dato.erHelg()) factory.sykedag(dato, grad) else factory.sykHelgedag(dato, grad)
+
+        private fun kunArbeidsgiverSykedag(dato: LocalDate, grad: Double, factory: DagFactory): Dag =
+            if (!dato.erHelg()) factory.kunArbeidsgiverSykedag(dato, grad) else factory.sykHelgedag(dato, grad)
+
+        private fun utenlandsdag(dato: LocalDate, grad_ignored: Double, factory: DagFactory) =
+            if (!dato.erHelg()) factory.utenlandsdag(dato) else factory.implisittDag(dato)
+
+        private fun ikkeSykedag(dato: LocalDate, grad_ignored: Double, factory: DagFactory) =
+            if (!dato.erHelg()) factory.arbeidsdag(dato) else factory.implisittDag(dato)
+
+        internal fun ikkeSykedag(dato: LocalDate, factory: DagFactory) =  // For gap days
+            ikkeSykedag(dato, Double.NaN, factory)
+
+        private fun ferie(dato: LocalDate, grad_ignored: Double, factory: DagFactory) =
+            factory.feriedag(dato)
+
+        private fun egenmeldingsdag(dato: LocalDate, grad_ignored: Double, factory: DagFactory) =
+            factory.egenmeldingsdag(dato)
+
+        private fun ubestemtdag(dato: LocalDate, grad_ignored: Double, factory_ignored: DagFactory) =
+            Ubestemtdag(dato)
+
+        internal fun ubestemtdag(dato: LocalDate) = ubestemtdag(dato, Double.NaN, SykmeldingDagFactory)
+
+        private fun beste(dagturnering: Dagturnering, a: Dag?, b: Dag?): Dag? {
+            if (a == null) return b
+            if (b == null) return a
+            return dagturnering.beste(a, b)
+        }
+    }
 }
+
+private typealias EnDag = (LocalDate, Double, DagFactory) -> Dag
--- sykepenger-model/src/test/kotlin/no/nav/helse/sykdomstidslinje/SykdomstidslinjeTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/sykdomstidslinje/SykdomstidslinjeTest.kt
@@ -2,14 +2,16 @@ package no.nav.helse.sykdomstidslinje
 
 import no.nav.helse.hendelser.Inntektsmelding
 import no.nav.helse.hendelser.Søknad
+import no.nav.helse.person.Aktivitetslogg
 import no.nav.helse.sykdomstidslinje.dag.Dag
 import no.nav.helse.sykdomstidslinje.dag.ImplisittDag
 import no.nav.helse.sykdomstidslinje.dag.Ubestemtdag
 import no.nav.helse.testhelpers.fredag
 import no.nav.helse.testhelpers.mandag
 import no.nav.helse.testhelpers.onsdag
+import no.nav.helse.testhelpers.tirsdag
 import no.nav.helse.tournament.Dagturnering
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class SykdomstidslinjeTest {
@@ -19,17 +21,46 @@ internal class SykdomstidslinjeTest {
     }
 
     @Test internal fun `kan bestemme hvilken type dager mellom to perioder skal ha`() {
-        val arbeidsgiverperiode1 = Sykdomstidslinje.sykedager(
+        val tidslinje1 = Sykdomstidslinje.sykedager(
             1.mandag, 1.onsdag, 100.0, Søknad.SøknadDagFactory)
-        val arbeidsgiverperiode2 = Sykdomstidslinje.sykedager(
+        val tidslinje2 = Sykdomstidslinje.sykedager(
             2.onsdag, 2.fredag, 100.0, Søknad.SøknadDagFactory)
-
-        val arbeidsgiverperiode = arbeidsgiverperiode1.merge(arbeidsgiverperiode2, KonfliktskyDagturnering) {
+        val tidslinje = tidslinje1.merge(tidslinje2, KonfliktskyDagturnering) {
             Sykdomstidslinje.ikkeSykedag(it, Inntektsmelding.InntektsmeldingDagFactory)
         }
-
-        assertEquals(" SSSAAII AASSS", arbeidsgiverperiode.toShortString())
+        assertEquals(" SSSAAII AASSS", tidslinje.toShortString())
     }
+
+    @Test
+    internal fun `to sykeperioder med mellomrom får riktig slutt og start dato`() {
+        val tidslinje1 = Sykdomstidslinje.sykedager(1.mandag, 1.tirsdag, 100.0, Søknad.SøknadDagFactory)
+        val tidslinje2 = Sykdomstidslinje.sykedager(1.fredag, 2.mandag, 100.0, Søknad.SøknadDagFactory)
+
+        val tidslinje = tidslinje2 + tidslinje1
+
+        assertEquals(1.mandag, tidslinje.førsteDag())
+        assertEquals(2.mandag, tidslinje.sisteDag())
+        assertEquals(8, tidslinje.size)
+        assertEquals(" SSIISHH S", tidslinje.toShortString())
+    }
+
+    @Test
+    internal fun `tidslinje med ubestemt dag er utenfor omfang`() {
+        val tidslinje = Sykdomstidslinje.ubestemtdager(1.mandag, 1.mandag, Søknad.SøknadDagFactory)
+        val aktivitetslogg = Aktivitetslogg()
+        assertFalse(tidslinje.valider(aktivitetslogg))
+        assertTrue(aktivitetslogg.hasErrors())
+    }
+
+
+    @Test
+    internal fun `tidslinje med permisjonsdag er utenfor omfang`() {
+        val tidslinje = Sykdomstidslinje.permisjonsdager(1.mandag, 1.mandag, Søknad.SøknadDagFactory)
+        val aktivitetslogg = Aktivitetslogg()
+        assertFalse(tidslinje.valider(aktivitetslogg))
+        assertTrue(aktivitetslogg.hasErrors())
+    }
+
 
     private object KonfliktskyDagturnering : Dagturnering {
         override fun beste(venstre: Dag, høyre: Dag): Dag {
