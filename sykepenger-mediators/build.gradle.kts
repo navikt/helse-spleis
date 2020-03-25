d9f647077f549ea0ec9f3c65965b4d6356ae4abb
David Steinsland <david@steinsland.email>
2020-03-25 20:22:45 +0100

    oppdaterer rapids-and-rivers

--- sykepenger-mediators/build.gradle.kts
+++ sykepenger-mediators/build.gradle.kts
@@ -8,7 +8,7 @@ val mockkVersion = "1.9.3"
 val mainClass = "no.nav.helse.AppKt"
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.1822995")
+    implementation("com.github.navikt:rapids-and-rivers:1.39bea05")
     implementation(project(":sykepenger-model"))
 
     implementation("org.flywaydb:flyway-core:$flywayVersion")
