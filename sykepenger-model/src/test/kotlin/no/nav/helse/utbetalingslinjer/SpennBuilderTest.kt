b14d16c703b5ea179ab66de2cde3709c2bce502a
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2020-04-01 22:12:17 +0200

    Retting av testbeskrivelse + hjelpefunksjon for å kontrollere linje

--- sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingslinjer/SpennBuilderTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingslinjer/SpennBuilderTest.kt
@@ -13,103 +13,91 @@ internal class SpennBuilderTest {
 
     private lateinit var linjer: List<Utbetalingslinje>
 
-    @Test internal fun `convert simple Utbetalingstidslinje to Utbetalingslinjer`() {
+    @Test
+    internal fun `konverter enkel Utbetalingstidslinje til Utbetalingslinjer`() {
         opprett(5.NAV, 2.HELG, 3.NAV)
+
         assertEquals(1, linjer.size)
-        assertEquals(1.januar, linjer.first().fom)
-        assertEquals(10.januar, linjer.first().tom)
+        assertLinje(0, 1.januar, 10.januar)
     }
 
-    @Test internal fun `helgene er utelatt`() {
-        opprett(5.NAV, 2.HELG)
+    @Test
+    internal fun `helg ved start og slutt i perioden utelates`() {
+        opprett(2.HELG, 5.NAV, 2.HELG)
+
         assertEquals(1, linjer.size)
-        assertEquals(1.januar, linjer.first().fom)
-        assertEquals(5.januar, linjer.first().tom)
+        assertLinje(0, 3.januar, 7.januar)
     }
 
-    @Test internal fun `gap vil opprette en ny betalingslinje`() {
-        assertGapBehavior(1.ARB)
-        assertGapBehavior(1.FRI)
-        assertGapBehavior(1.AVV)
-        assertGapBehavior(1.FOR)
+    @Test
+    internal fun `gap i vedtaksperiode`() {
+        assertNyLinjeVedGap(1.ARB)
+        assertNyLinjeVedGap(1.FRI)
+        assertNyLinjeVedGap(1.AVV)
+        assertNyLinjeVedGap(1.FOR)
     }
 
-    @Test internal fun `Utbetalingslinjer generation starts the day after the last employer-paid day`() {
+    @Test
+    internal fun `Utbetalingslinjer genereres kun fra dagen etter siste AGP-dag`() {
         opprett(2.NAV, 1.AP, 2.NAV, 2.HELG, 3.NAV)
+
         assertEquals(1, linjer.size)
-        assertEquals(4.januar, linjer.first().fom)
-        assertEquals(10.januar, linjer.first().tom)
+        assertLinje(0, 4.januar, 10.januar)
     }
 
-    @Test internal fun `Endring i utbetaling`() {
+    @Test
+    internal fun `Endring i sats`() {
         opprett(3.NAV(1200.0), 2.NAV(1500.0), 2.HELG, 2.NAV(1500.0))
 
         assertEquals(2, linjer.size)
-
-        assertEquals(1.januar, linjer.first().fom)
-        assertEquals(3.januar, linjer.first().tom)
-        assertEquals(1200, linjer.first().dagsats)
-
-        assertEquals(4.januar, linjer.last().fom)
-        assertEquals(9.januar, linjer.last().tom)
-        assertEquals(1500, linjer.last().dagsats)
+        assertLinje(0, 1.januar, 3.januar, 1200)
+        assertLinje(1, 4.januar, 9.januar, 1500)
     }
 
-    @Test internal fun `Endring i utbetaling pga grad`() {
+    @Test
+    internal fun `Endring i utbetaling pga grad`() {
         opprett(3.NAV(1500.0, 100.0), 2.NAV(1500.0, 60.0), 2.HELG, 2.NAV(1500.0, 60.0))
 
         assertEquals(2, linjer.size)
-
-        assertEquals(1.januar, linjer.first().fom)
-        assertEquals(3.januar, linjer.first().tom)
-        assertEquals(1500, linjer.first().dagsats)
-        assertEquals(100.0, linjer.first().grad)
-
-        assertEquals(4.januar, linjer.last().fom)
-        assertEquals(9.januar, linjer.last().tom)
-        assertEquals((1500 * 0.6).toInt(), linjer.last().dagsats)
-        assertEquals(60.0, linjer.last().grad)
+        assertLinje(0, 1.januar, 3.januar, 1500, 100.0)
+        assertLinje(1, 4.januar, 9.januar, (1500 * 0.6).toInt(), 60.0)
     }
 
-    @Test internal fun `Endring i utbetaling pga grad og inntekt, der utbetalingsbeløpet blir likt`() {
+    @Test
+    internal fun `Endring i utbetaling pga grad og inntekt, der utbetalingsbeløpet blir likt`() {
         opprett(3.NAV(1500.0, 100.0), 2.NAV(1875.0, 80.0), 2.HELG, 2.NAV(1500.0, 80.0))
 
         assertEquals(3, linjer.size)
-
-        assertEquals(1.januar, linjer.first().fom)
-        assertEquals(3.januar, linjer.first().tom)
-        assertEquals(1500, linjer.first().dagsats)
-        assertEquals(100.0, linjer.first().grad)
-
-        assertEquals(4.januar, linjer[1].fom)
-        assertEquals(5.januar, linjer[1].tom)
-        assertEquals(1500, linjer[1].dagsats)
-        assertEquals(80.0, linjer[1].grad)
-
-        assertEquals(8.januar, linjer.last().fom)
-        assertEquals(9.januar, linjer.last().tom)
-        assertEquals((1500 * 0.8).toInt(), linjer.last().dagsats)
-        assertEquals(80.0, linjer.last().grad)
+        assertLinje(0, 1.januar, 3.januar, 1500, 100.0)
+        assertLinje(1, 4.januar, 5.januar, 1500, 80.0)
+        assertLinje(2, 8.januar, 9.januar, (1500 * 0.8).toInt(), 80.0)
     }
 
-    @Test internal fun `Sickness grade changes`() {
+    @Test
+    internal fun `Endring i sykdomsgrad`() {
         opprett(3.NAV(1500.0, 100.0), 2.NAV(1500.0, 80.0), 2.HELG, 2.NAV(1500.0, 80.0))
 
         assertEquals(2, linjer.size)
-
-        assertEquals(1.januar, linjer.first().fom)
-        assertEquals(3.januar, linjer.first().tom)
-        assertEquals(1500, linjer.first().dagsats)
-        assertEquals(100.0, linjer.first().grad)
-
-        assertEquals(4.januar, linjer[1].fom)
-        assertEquals(9.januar, linjer.last().tom)
-        assertEquals((1500 * 0.8).toInt(), linjer.last().dagsats)
-        assertEquals(80.0, linjer.last().grad)
+        assertLinje(0, 1.januar, 3.januar, 1500, 100.0)
+        assertLinje(1, 4.januar, 9.januar, (1500 * 0.8).toInt(), 80.0)
     }
 
-    private fun assertGapBehavior(gapDay: Utbetalingsdager) {
+    private fun assertLinje(
+        index: Int,
+        fom: LocalDate,
+        tom: LocalDate,
+        sats: Int = linjer[index].dagsats,
+        grad: Double = linjer[index].grad
+    ) {
+        assertEquals(fom, linjer[index].fom)
+        assertEquals(tom, linjer[index].tom)
+        assertEquals(grad, linjer[index].grad)
+        assertEquals(sats, linjer[index].dagsats)
+    }
+
+    private fun assertNyLinjeVedGap(gapDay: Utbetalingsdager) {
         opprett(2.NAV, gapDay, 2.NAV, 2.HELG, 3.NAV)
+
         assertEquals(2, linjer.size)
         assertEquals(1.januar, linjer.first().fom)
         assertEquals(2.januar, linjer.first().tom)
