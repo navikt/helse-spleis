f7f2906a6a6916a04a3cf429cd990cc03675d362
David Steinsland <david@steinsland.email>
2020-04-20 08:59:51 +0200

    ordner avhengigheter i modellen og endrer logging til implementation
    `api` er en compile-avhengighet: artifaktet blir med i JAR
    `implementation` er en runtime-avhengighet som andre moduler må stille med

--- sykepenger-model/build.gradle.kts
+++ sykepenger-model/build.gradle.kts
@@ -1,13 +1,10 @@
 val jacksonVersion = "2.10.0"
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:1.7.30")
     implementation("commons-codec:commons-codec:1.11")
-    api("ch.qos.logback:logback-classic:1.2.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
-
-    testImplementation("no.nav.sykepenger.kontrakter:inntektsmelding-kontrakt:2019.11.08-09-49-c3234")
-    testImplementation("no.nav.syfo.kafka:sykepengesoknad:0b2a259676f7a78da70d65838851b05925d6de6f")
 }
 
 tasks.withType<Test> {
