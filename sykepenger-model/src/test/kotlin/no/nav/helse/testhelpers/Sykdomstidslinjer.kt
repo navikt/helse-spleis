ca5064964738db7e30f8ca00bfc51c064d9554e6
Fred George <fredgeorge@acm.org>
2020-04-23 13:59:10 +0200

    Adding helper methods to generate NySykdomstidslinjer for tests.

--- sykepenger-model/src/test/kotlin/no/nav/helse/testhelpers/Sykdomstidslinjer.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/testhelpers/Sykdomstidslinjer.kt
@@ -1,8 +1,14 @@
 package no.nav.helse.testhelpers
 
+import no.nav.helse.hendelser.Periode
 import no.nav.helse.hendelser.Søknad
+import no.nav.helse.person.Aktivitetslogg
+import no.nav.helse.person.Arbeidsgiver
+import no.nav.helse.sykdomstidslinje.NySykdomstidslinje
 import no.nav.helse.sykdomstidslinje.Sykdomstidslinje
+import no.nav.helse.sykdomstidslinje.SykdomstidslinjeHendelse
 import java.time.LocalDate
+import java.util.*
 
 private var dagensDato = LocalDate.of(2018, 1, 1)
 
@@ -71,3 +77,60 @@ internal val Int.FO
         100.0,
         Søknad.SøknadDagFactory
     ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+internal val Int.nS
+    get() = NySykdomstidslinje.sykedager(
+        dagensDato,
+        dagensDato.plusDays(this.toLong() - 1),
+        100.0,
+        TestHendelse.kilde
+    ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+internal val Int.nU
+    get() = NySykdomstidslinje.arbeidsgiverdager(
+        dagensDato,
+        dagensDato.plusDays(this.toLong() - 1),
+        100.0,
+        TestHendelse.kilde
+    ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+internal val Int.nK
+    get() = NySykdomstidslinje.kunArbeidsgiverSykedager(
+        dagensDato,
+        dagensDato.plusDays(this.toLong() - 1),
+        100.0,
+        TestHendelse.kilde
+    ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+internal val Int.nA
+    get() = NySykdomstidslinje.arbeidsdager(
+        dagensDato,
+        dagensDato.plusDays(this.toLong() - 1),
+        TestHendelse.kilde
+    ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+internal val Int.n_
+    get() = NySykdomstidslinje(
+    ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+internal val Int.nF
+    get() = NySykdomstidslinje.feriedager(
+        dagensDato,
+        dagensDato.plusDays(this.toLong() - 1),
+        TestHendelse.kilde
+    ).also { dagensDato = dagensDato.plusDays(this.toLong()) }
+
+private object TestHendelse : SykdomstidslinjeHendelse(UUID.randomUUID()) {
+    private const val UNG_PERSON_FNR_2018 = "12020052345"
+    private const val AKTØRID = "42"
+    private const val ORGNUMMER = "987654321"
+
+    override fun sykdomstidslinje(tom: LocalDate) = Sykdomstidslinje()
+    override fun sykdomstidslinje() = Sykdomstidslinje()
+    override fun nySykdomstidslinje() = NySykdomstidslinje()
+    override fun valider(periode: Periode) = Aktivitetslogg()
+    override fun fortsettÅBehandle(arbeidsgiver: Arbeidsgiver) = Unit
+    override fun aktørId() = AKTØRID
+    override fun fødselsnummer() = UNG_PERSON_FNR_2018
+    override fun organisasjonsnummer() = ORGNUMMER
+}
