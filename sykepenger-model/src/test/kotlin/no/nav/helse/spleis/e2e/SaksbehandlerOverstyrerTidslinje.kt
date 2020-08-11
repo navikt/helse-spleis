480a7f90ea0e7516a0cef813ed21e23825d9a488
Knut Nygaard <knut.nygaard@nav.no>
2020-08-11 14:08:27 +0200

    Ny test hvor saksbehandler brekker opp en forlengelse
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/SaksbehandlerOverstyrerTidslinje.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/SaksbehandlerOverstyrerTidslinje.kt
@@ -5,6 +5,7 @@ import no.nav.helse.person.TilstandType
 import no.nav.helse.testhelpers.januar
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -53,6 +54,48 @@ internal class SaksbehandlerOverstyrerTidslinje : AbstractEndToEndTest() {
             TilstandType.AVVENTER_GODKJENNING
         )
         assertEquals(19.januar, inspektør.utbetalinger.last().utbetalingstidslinje().førsteSykepengedag())
+    }
+
+    @Test
+    @Disabled
+    fun `saksbehandler oppretter gap mellom to tidligere sammenhengede perioder`() {
+        håndterSykmelding(Sykmeldingsperiode(2.januar, 25.januar, 100))
+        håndterInntektsmeldingMedValidering(0, listOf(Periode(2.januar, 17.januar)), førsteFraværsdag = 2.januar)
+        håndterSøknadMedValidering(0, Søknad.Søknadsperiode.Sykdom(2.januar, 25.januar, 100))
+        håndterVilkårsgrunnlag(0, INNTEKT)
+        håndterYtelser(0)   // No history
+        håndterSimulering(0)
+        håndterUtbetalingsgodkjenning(0, true)
+        håndterUtbetalt(0, UtbetalingHendelse.Oppdragstatus.AKSEPTERT)
+
+        assertTilstander(
+            0,
+            TilstandType.START,
+            TilstandType.MOTTATT_SYKMELDING_FERDIG_GAP,
+            TilstandType.AVVENTER_SØKNAD_FERDIG_GAP,
+            TilstandType.AVVENTER_VILKÅRSPRØVING_GAP,
+            TilstandType.AVVENTER_HISTORIKK,
+            TilstandType.AVVENTER_SIMULERING,
+            TilstandType.AVVENTER_GODKJENNING,
+            TilstandType.TIL_UTBETALING,
+            TilstandType.AVSLUTTET
+        )
+
+        håndterSykmelding(Sykmeldingsperiode(26.januar, 30.januar, 100))
+        håndterSøknad(Søknad.Søknadsperiode.Sykdom(26.januar, 30.januar, 100))
+        håndterYtelser(1)   // No history
+        håndterSimulering(1)
+        håndterOverstyring(listOf(manuellArbeidsdag(26.januar)))
+
+        assertEquals(2.januar, inspektør.førsteFraværsdag(0))
+        assertEquals(27.januar, inspektør.førsteFraværsdag(1))
+
+        assertEquals(2, inspektør.arbeidsgiverOppdrag.size)
+        assertEquals(18.januar, inspektør.arbeidsgiverOppdrag.first().førstedato)
+        assertEquals(25.januar, inspektør.arbeidsgiverOppdrag.first().sistedato)
+        assertEquals(27.januar, inspektør.arbeidsgiverOppdrag.last().førstedato)
+        assertEquals(30, inspektør.arbeidsgiverOppdrag.last().sistedato)
+
     }
 
     private fun manuellFeriedag(dato: LocalDate) = ManuellOverskrivingDag(dato, Dagtype.Feriedag)
