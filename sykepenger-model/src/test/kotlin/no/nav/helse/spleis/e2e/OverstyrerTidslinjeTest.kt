8f9d0d7c9b8392d3db69c6611a9b4c8724c206d8
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-08-12 14:10:28 +0200

    Lager tester for å verifisere at vi kan sette grad høyere og lavere enn 20% på enkeltdager
    Co-authored-by: Knut Nygaard <knut.nygaard@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/OverstyrerTidslinjeTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/OverstyrerTidslinjeTest.kt
@@ -55,8 +55,68 @@ internal class OverstyrerTidslinjeTest : AbstractEndToEndTest() {
         assertEquals(19.januar, inspektør.utbetalinger.last().utbetalingstidslinje().førsteSykepengedag())
     }
 
+    @Test
+    fun `grad over grensen endres på enkeltdag`() {
+        håndterSykmelding(Sykmeldingsperiode(2.januar, 25.januar, 100))
+        håndterInntektsmeldingMedValidering(0, listOf(Periode(2.januar, 17.januar)), førsteFraværsdag = 2.januar)
+        håndterSøknadMedValidering(0, Søknad.Søknadsperiode.Sykdom(2.januar, 25.januar, 100))
+        håndterVilkårsgrunnlag(0, INNTEKT)
+        håndterYtelser(0)
+        håndterSimulering(0)
+        håndterOverstyring(listOf(manuellSykedag(22.januar, 30)))
+
+        assertNotEquals(TilstandType.AVVENTER_GODKJENNING, inspektør.sisteTilstand(0))
+
+        håndterYtelser(0)
+        håndterSimulering(0)
+
+        assertEquals(3, inspektør.utbetalinger.last().arbeidsgiverOppdrag().size)
+        assertEquals(21.januar, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[0].tom)
+        assertEquals(30.0, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[1].grad)
+        assertEquals(23.januar, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[2].fom)
+    }
+
+    @Test
+    fun `grad under grensen blir ikke utbetalt`() {
+        håndterSykmelding(Sykmeldingsperiode(2.januar, 25.januar, 100))
+        håndterInntektsmeldingMedValidering(0, listOf(Periode(2.januar, 17.januar)), førsteFraværsdag = 2.januar)
+        håndterSøknadMedValidering(0, Søknad.Søknadsperiode.Sykdom(2.januar, 25.januar, 100))
+        håndterVilkårsgrunnlag(0, INNTEKT)
+        håndterYtelser(0)   // No history
+        håndterSimulering(0)
+        håndterOverstyring(listOf(manuellSykedag(22.januar, 0)))
+
+        assertNotEquals(TilstandType.AVVENTER_GODKJENNING, inspektør.sisteTilstand(0))
+
+        håndterYtelser(0)   // No history
+        håndterSimulering(0)
+
+        assertEquals(2, inspektør.utbetalinger.last().arbeidsgiverOppdrag().size)
+        assertEquals(21.januar, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[0].tom)
+        assertEquals(23.januar, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[1].fom)
+    }
+
+    @Test
+    fun `feriedag i midten av en periode blir ikke utbetalt`() {
+        håndterSykmelding(Sykmeldingsperiode(2.januar, 25.januar, 100))
+        håndterInntektsmeldingMedValidering(0, listOf(Periode(2.januar, 17.januar)), førsteFraværsdag = 2.januar)
+        håndterSøknadMedValidering(0, Søknad.Søknadsperiode.Sykdom(2.januar, 25.januar, 100))
+        håndterVilkårsgrunnlag(0, INNTEKT)
+        håndterYtelser(0)
+        håndterSimulering(0)
+        håndterOverstyring(listOf(manuellFeriedag(22.januar)))
+
+        assertNotEquals(TilstandType.AVVENTER_GODKJENNING, inspektør.sisteTilstand(0))
+
+        håndterYtelser(0)
+        håndterSimulering(0)
+
+        assertEquals(2, inspektør.utbetalinger.last().arbeidsgiverOppdrag().size)
+        assertEquals(21.januar, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[0].tom)
+        assertEquals(23.januar, inspektør.utbetalinger.last().arbeidsgiverOppdrag()[1].fom)
+    }
 
     private fun manuellFeriedag(dato: LocalDate) = ManuellOverskrivingDag(dato, Dagtype.Feriedag)
-    private fun manuellSykedag(dato: LocalDate) = ManuellOverskrivingDag(dato, Dagtype.Sykedag, 100)
+    private fun manuellSykedag(dato: LocalDate, grad: Int = 100) = ManuellOverskrivingDag(dato, Dagtype.Sykedag, grad)
     private fun manuellArbeidsgiverdag(dato: LocalDate) = ManuellOverskrivingDag(dato, Dagtype.Egenmeldingsdag)
 }
