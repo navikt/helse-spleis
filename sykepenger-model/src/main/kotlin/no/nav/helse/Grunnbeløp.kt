617387cd344825750cbe44416307cde47ff4af3d
David Steinsland <david@steinsland.email>
2020-10-14 08:52:21 +0200

    Sørge for at vi kan hente Grunnbeløp basert på virkningsdato også
    Grunnbeløp hentes ut ved at Grunnbeløpet må være gyldig fra og med beregningsdato, og virkningsdato til grunnbeløpet må være passert.
    
    Perioder som ennå ikke er G-justert skal fortsette å bruke 2019-satsen frem til de har blitt justert.
    I så tilfelle vil vi oppdatere en dato i perioden slik at man da kan få oppdatert Grunnbeløp neste gang man spør.
    
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Aleksander Andresen <aleksander.andresen@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/Grunnbeløp.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Grunnbeløp.kt
@@ -4,35 +4,56 @@ import no.nav.helse.økonomi.Inntekt
 import no.nav.helse.økonomi.Inntekt.Companion.årlig
 import java.time.LocalDate
 
-
 internal class Grunnbeløp private constructor(private val multiplier: Double) {
-    private val grunnbeløp = mapOf<LocalDate, Inntekt>(
-        LocalDate.of(2020, 9, 21) to 101351.årlig,
-        LocalDate.of(2019, 5, 1) to 99858.årlig,
-        LocalDate.of(2018, 5, 1) to 96883.årlig,
-        LocalDate.of(2017, 5, 1) to 93634.årlig,
-        LocalDate.of(2016, 5, 1) to 92576.årlig,
-        LocalDate.of(2015, 5, 1) to 90068.årlig,
-        LocalDate.of(2014, 5, 1) to 88370.årlig,
-        LocalDate.of(2013, 5, 1) to 85245.årlig,
-        LocalDate.of(2012, 5, 1) to 82122.årlig,
-        LocalDate.of(2011, 5, 1) to 79216.årlig,
-        LocalDate.of(2010, 5, 1) to 75641.årlig
+    private val grunnbeløp = listOf(
+        101351.årlig.gyldigFra(1.mai(2020), virkningsdato = 21.september(2020)),
+        99858.årlig.gyldigFra(1.mai(2019)),
+        96883.årlig.gyldigFra(1.mai(2018)),
+        93634.årlig.gyldigFra(1.mai(2017)),
+        92576.årlig.gyldigFra(1.mai(2016)),
+        90068.årlig.gyldigFra(1.mai(2015)),
+        88370.årlig.gyldigFra(1.mai(2014)),
+        85245.årlig.gyldigFra(1.mai(2013)),
+        82122.årlig.gyldigFra(1.mai(2012)),
+        79216.årlig.gyldigFra(1.mai(2011)),
+        75641.årlig.gyldigFra(1.mai(2010))
     )
 
-    internal fun beløp(dato: LocalDate) =
-        grunnbeløp.entries
-            .filter { dato >= it.key }
-            .maxBy { it.key }
-            ?.let { it.value * multiplier }
-            ?: throw NoSuchElementException("Finner ingen grunnbeløp etter $dato")
+    internal fun beløp(dato: LocalDate) = gjeldende(dato).beløp(multiplier)
+    internal fun beløp(dato: LocalDate, virkningFra: LocalDate) = gjeldende(dato, virkningFra).beløp(multiplier)
 
-    internal fun dagsats(dato: LocalDate) = (beløp(dato)).rundTilDaglig()
+    internal fun dagsats(dato: LocalDate) = beløp(dato).rundTilDaglig()
+    internal fun dagsats(dato: LocalDate, virkningFra: LocalDate) = beløp(dato, virkningFra).rundTilDaglig()
+
+    private fun gjeldende(dato: LocalDate, virkningFra: LocalDate? = null) =
+        HistoriskGrunnbeløp.gjeldendeGrunnbeløp(grunnbeløp, dato, virkningFra ?: dato)
 
     companion object {
         val `6G` = Grunnbeløp(6.0)
         val halvG = Grunnbeløp(0.5)
         val `2G` = Grunnbeløp(2.0)
         val `1G` = Grunnbeløp(1.0)
+
+        private fun Inntekt.gyldigFra(gyldigFra: LocalDate, virkningsdato: LocalDate = gyldigFra) = HistoriskGrunnbeløp(this, gyldigFra, virkningsdato)
+        private fun Int.mai(år: Int) = LocalDate.of(år, 5, this)
+        private fun Int.september(år: Int) = LocalDate.of(år, 9, this)
+    }
+
+    private class HistoriskGrunnbeløp(private val beløp: Inntekt, private val gyldigFra: LocalDate, private val virkningsdato: LocalDate = gyldigFra) {
+        init {
+            require(virkningsdato >= gyldigFra) { "Virkningsdato må være nyere eller lik gyldighetstidspunktet" }
+        }
+
+        companion object {
+            fun gjeldendeGrunnbeløp(grunnbeløper: List<HistoriskGrunnbeløp>, dato: LocalDate, virkningFra: LocalDate): HistoriskGrunnbeløp {
+                require(virkningFra >= dato) { "Virkningsdato må være nyere eller lik beregningsdato" }
+                return grunnbeløper
+                    .filter { virkningFra >= it.virkningsdato }
+                    .maxBy { dato >= it.gyldigFra }
+                    ?: throw NoSuchElementException("Finner ingen grunnbeløp etter $dato")
+            }
+        }
+
+        fun beløp(multiplier: Double) = beløp * multiplier
     }
 }
--- sykepenger-model/src/test/kotlin/no/nav/helse/GrunnbeløpTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/GrunnbeløpTest.kt
@@ -2,10 +2,12 @@ package no.nav.helse
 
 import no.nav.helse.testhelpers.april
 import no.nav.helse.testhelpers.mai
+import no.nav.helse.testhelpers.september
 import no.nav.helse.økonomi.Inntekt.Companion.daglig
 import no.nav.helse.økonomi.Inntekt.Companion.årlig
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class GrunnbeløpTest {
 
@@ -19,5 +21,24 @@ internal class GrunnbeløpTest {
     fun grunnbeløp() {
         assertEquals(599148.årlig, Grunnbeløp.`6G`.beløp(1.mai(2019)))
         assertEquals(581298.årlig, Grunnbeløp.`6G`.beløp(30.april(2019)))
+    }
+
+    @Test
+    fun `grunnbeløp før virkingdato`() {
+        assertEquals(99858.årlig, Grunnbeløp.`1G`.beløp(1.mai(2020)))
+        assertEquals(99858.årlig, Grunnbeløp.`1G`.beløp(30.april(2020), 20.september(2020)))
+        assertEquals(99858.årlig, Grunnbeløp.`1G`.beløp(1.mai(2020), 20.september(2020)))
+    }
+
+    @Test
+    fun `grunnbeløp etter virkingdato`() {
+        assertEquals(99858.årlig, Grunnbeløp.`1G`.beløp(30.april(2020), 21.september(2020)))
+        assertEquals(101351.årlig, Grunnbeløp.`1G`.beløp(1.mai(2020), 21.september(2020)))
+        assertEquals(101351.årlig, Grunnbeløp.`1G`.beløp(21.september(2020)))
+    }
+
+    @Test
+    fun `virkingsdato må være nyere enn beregningsdato`() {
+        assertThrows<IllegalArgumentException> { Grunnbeløp.`1G`.beløp(30.april(2020), 29.april(2020)) }
     }
 }
