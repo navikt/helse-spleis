bc7dfe15212a3aa5a2b84001e0d296295a99e378
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2020-10-16 14:30:21 +0200

    Dokumentasjon av periodetype
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: David Steinsland <david@steinsland.email>
    Co-authored-by: Aleksander Andresen <caeses@gmail.com>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Vedtaksperiode.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Vedtaksperiode.kt
@@ -1744,8 +1744,15 @@ enum class ForlengelseFraInfotrygd {
 }
 
 enum class Periodetype {
+    /** Perioden er første periode i et sykdomstilfelle */
     FØRSTEGANGSBEHANDLING,
+
+    /** Perioden en en forlengelse av en Spleis-periode */
     FORLENGELSE,
+
+    /** Perioden en en umiddelbar forlengelse av en periode som er utbetalt i Infotrygd */
     OVERGANG_FRA_IT,
+
+    /** Perioden er en direkte eller indirekte forlengelse av en OVERGANG_FRA_IT-periode */
     INFOTRYGDFORLENGELSE
 }
