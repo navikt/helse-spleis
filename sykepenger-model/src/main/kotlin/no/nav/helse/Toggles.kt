33d25f7d8b7a0450caa7825aa78a4ace20fb420a
Stephen Ramthun <stephenramthun@gmail.com>
2020-10-21 14:39:08 +0200

    Prøver å fikse deploy i dev
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggles.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggles.kt
@@ -3,5 +3,5 @@ package no.nav.helse
 object Toggles {
     var replayEnabled = false
     // TODO: Ikke send ut EgenAnsatt-behovet som en del av vilkårsprøvingen når featuren er ferdig testet og togglen kan fjernes
-    val vilkårshåndteringInfotrygd = System.getenv()["VILKÅRSHÅNDTERING_INFOTRYGD_FEATURE_TOGGLE"]?.toBoolean() ?: false
+    val vilkårshåndteringInfotrygd = System.getenv()["VILKARSHANDTERING_INFOTRYGD_FEATURE_TOGGLE"]?.toBoolean() ?: false
 }
