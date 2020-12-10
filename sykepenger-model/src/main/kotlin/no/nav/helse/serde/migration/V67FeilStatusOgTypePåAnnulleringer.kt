463db37989f2611c722e21fafd1d213ce41350d1
Knut Nygaard <knut.nygaard@nav.no>
2020-12-10 13:01:09 +0100

    Fjerner migrering av feil status og type på annullering
    Den var for inngripende og traff caser som ikke skulle treffes. Prøver igjen senere..

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V67FeilStatusOgTypePåAnnulleringer.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V67FeilStatusOgTypePåAnnulleringer.kt
@@ -1,31 +1,11 @@
 package no.nav.helse.serde.migration
 
 import com.fasterxml.jackson.databind.node.ObjectNode
-import org.slf4j.LoggerFactory
 
 internal class V67FeilStatusOgTypePåAnnulleringer : JsonMigration(version = 67) {
     override val description: String = "Endrer status og type på automatisk genererte utbetalte annulleringer"
-    private val log = LoggerFactory.getLogger(this.javaClass.name)
 
     override fun doMigration(jsonNode: ObjectNode) {
-        jsonNode
-            .path("arbeidsgivere")
-            .forEach { arbeidsgiver ->
-                arbeidsgiver
-                    .path("utbetalinger")
-                    .filter { utbetaling -> utbetaling.hasNonNull("arbeidsgiverOppdrag") }
-                    .filter { utbetaling -> !utbetaling["arbeidsgiverOppdrag"]["linjer"].isEmpty }
-                    .filter { utbetaling -> utbetaling["arbeidsgiverOppdrag"]["linjer"][0]["statuskode"].textValue() == "OPPH" }
-                    .filter { utbetaling -> utbetaling["status"].textValue() == "UTBETALT" && utbetaling["type"].textValue() == "UTBETALING" }
-                    .forEach { utbetaling ->
-                        utbetaling as ObjectNode
-                        utbetaling.put("status", "FORKASTET")
-                        utbetaling.put("type", "ANNULLERING")
-                        log.info(
-                            "Endret utbetaling: ${utbetaling["id"].textValue()} med fagsystemId: ${utbetaling["arbeidsgiverOppdrag"]["fagsystemId"].textValue()} " +
-                                "fra UTBETALT:UTBETALING til FORKASTET:ANNULLERING"
-                        )
-                    }
-            }
+        // Denne var for inngripende. Vi må komme igjen sterkere om det enda er et problem
     }
 }
