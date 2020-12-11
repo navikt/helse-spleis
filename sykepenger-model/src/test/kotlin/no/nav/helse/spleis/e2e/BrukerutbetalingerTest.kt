bb4d08d7c195cdf7ff62829f298febf5376fb7a9
David Steinsland <david@steinsland.email>
2020-12-11 12:41:12 +0100

    fikser setup for testen

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/BrukerutbetalingerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/BrukerutbetalingerTest.kt
@@ -17,7 +17,7 @@ internal class BrukerutbetalingerTest : AbstractEndToEndTest() {
     @Test
     fun `maksdato blir riktig når person har brukerutbetaling`() {
         val historikk = listOf(
-            RefusjonTilArbeidsgiver(16.januar, 30.april, INNTEKT, 100.prosent, ORGNUMMER),
+            RefusjonTilArbeidsgiver(17.januar, 30.april, INNTEKT, 100.prosent, ORGNUMMER),
             Utbetalingshistorikk.Infotrygdperiode.Utbetaling(1.mai, 30.mai, INNTEKT, 100.prosent, UNG_PERSON_FNR_2018)
         )
         val inntektsopplysning = listOf(
@@ -26,7 +26,7 @@ internal class BrukerutbetalingerTest : AbstractEndToEndTest() {
 
         håndterSykmelding(Sykmeldingsperiode(1.juni, 30.juni, 100.prosent))
         håndterSøknad(Sykdom(1.juni, 30.juni, 100.prosent))
-        håndterInntektsmelding(listOf(1.januar til 15.januar), førsteFraværsdag = 1.juni)
+        håndterInntektsmelding(listOf(1.januar til 16.januar), førsteFraværsdag = 1.juni)
         håndterVilkårsgrunnlag(1.vedtaksperiode)
         håndterUtbetalingshistorikk(1.vedtaksperiode, utbetalinger = historikk.toTypedArray(), inntektshistorikk = inntektsopplysning)
         håndterYtelser(1.vedtaksperiode, utbetalinger = historikk.toTypedArray(), inntektshistorikk = inntektsopplysning)
@@ -34,6 +34,6 @@ internal class BrukerutbetalingerTest : AbstractEndToEndTest() {
         håndterUtbetalingsgodkjenning(1.vedtaksperiode, true)
         håndterUtbetalt(1.vedtaksperiode, UtbetalingHendelse.Oppdragstatus.AKSEPTERT)
 
-        assertEquals(28.desember, inspektør.maksdato(1.vedtaksperiode))
+        assertEquals(31.desember, inspektør.maksdato(1.vedtaksperiode))
     }
 }
