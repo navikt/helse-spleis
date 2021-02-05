77b2bf9310b932eed62be008ef630c2d562d5a24
David Steinsland <david@steinsland.email>
2021-02-05 16:48:20 +0100

    kjører VACUUM FULL hver lørdag kl 01:00
    Gjør gc-job i stand til å kjøre ulike oppgaver basert på kommandolinjeargumenter

--- gc-job/src/main/kotlin/no/nav/helse/spleis/gc/App.kt
+++ gc-job/src/main/kotlin/no/nav/helse/spleis/gc/App.kt
@@ -12,15 +12,24 @@ import kotlin.time.measureTime
 private val log = LoggerFactory.getLogger("no.nav.helse.spleis.gc.App")
 
 @ExperimentalTime
-fun main() {
+fun main(args: Array<String>) {
     Thread.setDefaultUncaughtExceptionHandler { thread, err -> log.error("Uncaught exception in thread ${thread.name}: {}", err.message, err) }
-    val ds = HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(hikariConfig, System.getenv("VAULT_MOUNTPATH"), System.getenv("DB_NAME") + "-admin")
 
+    if (args.isEmpty()) return log.error("Provide a task name as CLI argument")
+
+    when (val task = args[0].trim().toLowerCase()) {
+        "vacuum" -> vacuumTask()
+        else -> log.error("Unknown task $task")
+    }
+}
+
+@ExperimentalTime
+private fun vacuumTask() {
+    val ds = HikariCPVaultUtil.createHikariDataSourceWithVaultIntegration(hikariConfig, System.getenv("VAULT_MOUNTPATH"), System.getenv("DB_NAME") + "-admin")
     log.info("Commencing VACUUM FULL")
     val duration = measureTime {
         using(sessionOf(ds)) { session ->
             session.run(queryOf(("VACUUM FULL person")).asExecute)
-            session.run(queryOf(("VACUUM FULL melding")).asExecute)
         }
     }
     log.info("VACUUM FULL completed after {} hour(s), {} minute(s) and {} second(s)", duration.inHours.toInt(), duration.inMinutes.toInt() % 60, duration.inSeconds.toInt() % 60)
