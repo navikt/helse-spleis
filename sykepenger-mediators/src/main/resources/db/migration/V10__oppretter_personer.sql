65226b779dd73f0c09ba5e182f4ca228e2b37563
David Steinsland <david@steinsland.email>
2021-02-05 15:39:11 +0100

    forsøker å ignorere duplikater

--- sykepenger-mediators/src/main/resources/db/migration/V10__oppretter_personer.sql
+++ sykepenger-mediators/src/main/resources/db/migration/V10__oppretter_personer.sql
@@ -1,4 +1,5 @@
 insert into unike_person (fnr,aktor_id)
 select p.fnr, p.aktor_id from person p
 left join unike_person up on up.fnr = p.fnr
-where up.fnr is null;
+where up.fnr is null
+on conflict do nothing;
