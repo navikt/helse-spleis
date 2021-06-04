61c5b8e979cda47cb1c63d1af9ab0afc6c300c31
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-06-04 10:13:23 +0200

    Bump snapshot-versjon

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/api/SpeilBuilder.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/api/SpeilBuilder.kt
@@ -18,7 +18,7 @@ internal class SpeilBuilder(private val hendelser: List<HendelseDTO>) : Abstract
         /* Økes for å signalisere til spesialist at strukturen i snapshot'et
          * på et eller annet vis har endret seg, og at spesialist derfor må oppdatere cachede snapshots løpende
          */
-        const val SNAPSHOT_VERSJON = 4
+        const val SNAPSHOT_VERSJON = 5
     }
 
     private lateinit var personBuilder: PersonBuilder
