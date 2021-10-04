b341ce0a285261388c854bff18c6b8112bdd9245
Håkon <hakon.a.holmstedt@gmail.com>
2021-10-04 11:59:09 +0200

    Fødselsnummer kan bare instansieres fra en elvesiffret streng.

--- sykepenger-model/src/main/kotlin/no/nav/helse/Fødselsnummer.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Fødselsnummer.kt
@@ -1,10 +1,18 @@
 package no.nav.helse
 
-class Fødselsnummer(private val value: String) {
+class Fødselsnummer private constructor(private val value: String) {
     override fun toString() = value
     override fun hashCode(): Int = value.hashCode()
     override fun equals(other: Any?) = other is Fødselsnummer && this.value == other.value
     fun somLong() = value.toLong()
+
+    companion object {
+        fun tilFødselsnummer(fnr: String): Fødselsnummer {
+            if (fnr.length == 11 && alleTegnErSiffer(fnr)) return Fødselsnummer(fnr)
+            else throw RuntimeException("$fnr er ikke et gyldig fødselsnummer")
+        }
+        private fun alleTegnErSiffer(string: String) = string.matches(Regex("\\d*"))
+    }
 }
 
-fun String.somFødselsnummer() = Fødselsnummer(this)
+fun String.somFødselsnummer() = Fødselsnummer.tilFødselsnummer(this)
--- sykepenger-model/src/test/kotlin/no/nav/helse/FødselsnummerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/FødselsnummerTest.kt
@@ -1,7 +1,9 @@
 package no.nav.helse
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class FødselsnummerTest {
 
@@ -12,5 +14,27 @@ class FødselsnummerTest {
             assertEquals(it.somFødselsnummer().somLong(), it.somFødselsnummer().somLong())
             assertEquals(it.somFødselsnummer().hashCode(), it.somFødselsnummer().hashCode())
         }
+    }
+
+    @Test
+    fun `ulike fnr er ulike`() {
+        val a = "01010112345"
+        val b = "01010112346"
+        assertNotEquals(a.somFødselsnummer(), b.somFødselsnummer())
+        assertNotEquals(a.somFødselsnummer().somLong(), b.somFødselsnummer().somLong())
+        assertNotEquals(a.somFødselsnummer().hashCode(), b.somFødselsnummer().hashCode())
+    }
+
+    @Test
+    fun `ting som ikke er fnr`() {
+        assertThrows<RuntimeException> { "010101123456".somFødselsnummer() }
+        assertThrows<RuntimeException> { "0101011234".somFødselsnummer() }
+        assertThrows<RuntimeException> { "1010112345".somFødselsnummer() }
+        assertThrows<RuntimeException> { "1o101123456".somFødselsnummer() }
+        assertThrows<RuntimeException> { "l0101123456".somFødselsnummer() }
+        assertThrows<RuntimeException> { "1010112345s".somFødselsnummer() }
+        assertThrows<RuntimeException> { "Jørgen Hattemaker".somFødselsnummer() }
+        assertThrows<RuntimeException> { "".somFødselsnummer() }
+        assertThrows<RuntimeException> { "1".somFødselsnummer() }
     }
 }
