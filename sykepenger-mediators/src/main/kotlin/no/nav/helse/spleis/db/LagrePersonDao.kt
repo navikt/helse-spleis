636d56cd45026cf49febac169dc38bdd173c85e7
Håkon <hakon.a.holmstedt@gmail.com>
2021-10-11 13:38:38 +0200

    LagrePersonDao bruker Fødselsnummer

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/db/LagrePersonDao.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/db/LagrePersonDao.kt
@@ -1,10 +1,12 @@
 package no.nav.helse.spleis.db
 
 import kotliquery.*
+import no.nav.helse.Fødselsnummer
 import no.nav.helse.hendelser.Avstemming
 import no.nav.helse.person.Person
 import no.nav.helse.person.PersonHendelse
 import no.nav.helse.serde.serialize
+import no.nav.helse.somFødselsnummer
 import no.nav.helse.spleis.PostgresProbe
 import no.nav.helse.spleis.meldinger.model.HendelseMessage
 import org.intellij.lang.annotations.Language
@@ -16,7 +18,7 @@ internal class LagrePersonDao(private val dataSource: DataSource) {
         val serialisering = person.serialize()
         lagrePerson(
             aktørId = hendelse.aktørId(),
-            fødselsnummer = hendelse.fødselsnummer(),
+            fødselsnummer = hendelse.fødselsnummer().somFødselsnummer(),
             skjemaVersjon = serialisering.skjemaVersjon,
             meldingId = message.id,
             personJson = serialisering.json,
@@ -34,7 +36,7 @@ internal class LagrePersonDao(private val dataSource: DataSource) {
         }
     }
 
-    private fun lagrePerson(aktørId: String, fødselsnummer: String, skjemaVersjon: Int, meldingId: UUID, personJson: String, vedtak: Boolean) {
+    private fun lagrePerson(aktørId: String, fødselsnummer: Fødselsnummer, skjemaVersjon: Int, meldingId: UUID, personJson: String, vedtak: Boolean) {
         using(sessionOf(dataSource)) { session ->
             session.transaction {
                 opprettNyPerson(it, fødselsnummer, aktørId, skjemaVersjon, meldingId, personJson, vedtak)
@@ -44,7 +46,7 @@ internal class LagrePersonDao(private val dataSource: DataSource) {
         }
     }
 
-    private fun opprettNyPerson(session: TransactionalSession, fødselsnummer: String, aktørId: String, skjemaVersjon: Int, meldingId: UUID, personJson: String, vedtak: Boolean) {
+    private fun opprettNyPerson(session: TransactionalSession, fødselsnummer: Fødselsnummer, aktørId: String, skjemaVersjon: Int, meldingId: UUID, personJson: String, vedtak: Boolean) {
         @Language("PostreSQL")
         val statement = "INSERT INTO unike_person (fnr, aktor_id) VALUES (:fnr, :aktor) ON CONFLICT DO NOTHING"
         session.run(queryOf(statement, mapOf(
@@ -55,7 +57,7 @@ internal class LagrePersonDao(private val dataSource: DataSource) {
         opprettNyPersonversjon(session, fødselsnummer, aktørId, skjemaVersjon, meldingId, personJson, vedtak)
     }
 
-    private fun opprettNyPersonversjon(session: Session, fødselsnummer: String, aktørId: String, skjemaVersjon: Int, meldingId: UUID, personJson: String, vedtak: Boolean) {
+    private fun opprettNyPersonversjon(session: Session, fødselsnummer: Fødselsnummer, aktørId: String, skjemaVersjon: Int, meldingId: UUID, personJson: String, vedtak: Boolean) {
         @Language("PostreSQL")
         val statement = """
             INSERT INTO person (aktor_id, fnr, skjema_versjon, melding_id, data, vedtak)
@@ -64,7 +66,7 @@ internal class LagrePersonDao(private val dataSource: DataSource) {
         session.run(queryOf(statement, aktørId.toLong(), fødselsnummer.toLong(), skjemaVersjon, meldingId, personJson, vedtak).asExecute)
     }
 
-    private fun slettEldrePersonversjon(session: Session, fødselsnummer: String) {
+    private fun slettEldrePersonversjon(session: Session, fødselsnummer: Fødselsnummer) {
         @Language("PostreSQL")
         val statement = """
             DELETE FROM person
