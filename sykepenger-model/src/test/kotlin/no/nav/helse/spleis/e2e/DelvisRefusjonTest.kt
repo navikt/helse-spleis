85b85e7f47971eef227776d9513421ad98c118c4
Hege Haavaldsen <hege.haavaldsen@nav.no>
2021-10-13 14:56:50 +0200

    Utvide tester med å asserte refusjonsbeløpet på økonomiobjektet
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Sondre Halvorsen <sondre.halvorsen@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/DelvisRefusjonTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/DelvisRefusjonTest.kt
@@ -3,11 +3,12 @@ package no.nav.helse.spleis.e2e
 import no.nav.helse.Toggles
 import no.nav.helse.hendelser.Inntektsmelding
 import no.nav.helse.testhelpers.januar
+import no.nav.helse.utbetalingstidslinje.Utbetalingstidslinje
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-internal class DelvisRefusjonTest : AbstractEndToEndTest()  {
+internal class DelvisRefusjonTest : AbstractEndToEndTest() {
 
 
     @Test
@@ -15,10 +16,17 @@ internal class DelvisRefusjonTest : AbstractEndToEndTest()  {
         nyttVedtak(1.januar, 31.januar, refusjon = Inntektsmelding.Refusjon(INNTEKT, null, emptyList()))
 
         assertTrue(inspektør.utbetalinger.last().arbeidsgiverOppdrag().isNotEmpty())
-        inspektør.utbetalinger.last().arbeidsgiverOppdrag().forEach {
-            assertEquals(1431, it.beløp)
-        }
+        inspektør.utbetalinger.last().arbeidsgiverOppdrag().forEach { assertEquals(1431, it.beløp) }
         assertTrue(inspektør.utbetalinger.last().personOppdrag().isEmpty())
+
+        inspektør.utbetalingstidslinjer(1.vedtaksperiode).forEach {
+            it.økonomi.medAvrundetData { _, arbeidsgiverRefusjonsbeløp, _, _, arbeidsgiverbeløp, personbeløp, _ ->
+                val forventetArbeidsgiverbeløp = if (it is Utbetalingstidslinje.Utbetalingsdag.NavDag) 1431 else 0
+                assertEquals(forventetArbeidsgiverbeløp, arbeidsgiverbeløp)
+                assertEquals(1431,arbeidsgiverRefusjonsbeløp)
+                assertEquals(0, personbeløp)
+            }
+        }
     }
 
     @Test
@@ -26,9 +34,16 @@ internal class DelvisRefusjonTest : AbstractEndToEndTest()  {
         nyttVedtak(1.januar, 31.januar, refusjon = Inntektsmelding.Refusjon(INNTEKT, null, emptyList()))
 
         assertTrue(inspektør.utbetalinger.last().arbeidsgiverOppdrag().isNotEmpty())
-        inspektør.utbetalinger.last().arbeidsgiverOppdrag().forEach {
-            assertEquals(1431, it.beløp)
-        }
+        inspektør.utbetalinger.last().arbeidsgiverOppdrag().forEach { assertEquals(1431, it.beløp) }
         assertTrue(inspektør.utbetalinger.last().personOppdrag().isEmpty())
+
+        inspektør.utbetalingstidslinjer(1.vedtaksperiode).forEach {
+            it.økonomi.medAvrundetData { _, arbeidsgiverRefusjonsbeløp, _, _, arbeidsgiverbeløp, personbeløp, _ ->
+                val forventetArbeidsgiverbeløp = if (it is Utbetalingstidslinje.Utbetalingsdag.NavDag) 1431 else 0
+                assertEquals(forventetArbeidsgiverbeløp, arbeidsgiverbeløp)
+                assertEquals(null, arbeidsgiverRefusjonsbeløp)
+                assertEquals(0, personbeløp)
+            }
+        }
     }
 }
