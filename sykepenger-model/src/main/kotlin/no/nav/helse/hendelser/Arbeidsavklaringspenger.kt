135639aa515182a633afec5a0e54c7c58c1efa87
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-11-16 14:20:09 +0100

    Endre varsel for overlapp med AAP eller Dagpenger
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Cecilie Elisabeth Salvesen <cecilie.elisabeth.salvesen@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Arbeidsavklaringspenger.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Arbeidsavklaringspenger.kt
@@ -7,7 +7,7 @@ import java.time.LocalDate
 class Arbeidsavklaringspenger(private val perioder: List<Periode>) {
     internal fun valider(aktivitetslogg: IAktivitetslogg, skjæringstidspunkt: LocalDate): IAktivitetslogg {
         if (perioder.slutterEtter(skjæringstidspunkt.minusMonths(6))) {
-            aktivitetslogg.warn("Bruker har mottatt AAP innenfor 6 måneder av skjæringstidspunkt. Kontroller at brukeren har rett til sykepenger")
+            aktivitetslogg.warn("Bruker har mottatt AAP innenfor 6 måneder før skjæringstidspunktet. Kontroller at brukeren har rett til sykepenger")
         }
         return aktivitetslogg
     }
--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Dagpenger.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Dagpenger.kt
@@ -7,7 +7,7 @@ import java.time.LocalDate
 class Dagpenger(private val perioder: List<Periode>) {
     internal fun valider(aktivitetslogg: IAktivitetslogg, skjæringstidspunkt: LocalDate): IAktivitetslogg {
         if (perioder.slutterEtter(skjæringstidspunkt.minusWeeks(4))) {
-            aktivitetslogg.warn("Bruker har mottatt dagpenger innenfor 4 uker av skjæringstidspunkt. Kontroller om bruker er dagpengemottaker. Kombinerte ytelser støttes foreløpig ikke av systemet")
+            aktivitetslogg.warn("Bruker har mottatt dagpenger innenfor 4 uker før skjæringstidspunktet. Kontroller om bruker er dagpengemottaker. Kombinerte ytelser støttes foreløpig ikke av systemet")
         }
         return aktivitetslogg
     }
