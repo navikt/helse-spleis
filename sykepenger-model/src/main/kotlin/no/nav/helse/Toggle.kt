d3db120bfb382d13ca012be23053fd42d4a0a6b8
Anders Østby <anders.ostby@nav.no>
2021-11-17 09:31:04 +0100

    KDoc på Toggle

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -3,37 +3,86 @@ package no.nav.helse
 import no.nav.helse.person.IAktivitetslogg
 import no.nav.helse.utbetalingslinjer.Utbetaling
 
+/**
+ * En toggle opprettes ved å angi initiell tilstand, og alternativt om tilstanden skal låses ved å angi [force]
+ *
+ * Tilstanden til togglen kan endres ved kall til [enable] eller [disable], så fremt [force] ikke har verdien `true`
+ *
+ * For å tvinge togglen til å alltid være i initiell tilstand kan [force] settes til `true`
+ * Dette kan brukes for å finne hvilke tester som er avhengige av en gitt tilstand på togglen, og sideeffekten av å skru på en toggle kommer tydelig frem
+ *
+ * **Merk: Hvis tilstanden endres ved kall til [enable] eller [disable] uten `block`, så må tilstanden senere tilbakestilles ved kall til [pop]**
+ *
+ * @param[enabled] Initiell tilstand
+ * @param[force] Om tilstanden til toggelen skal låses
+ */
 abstract class Toggle internal constructor(enabled: Boolean = false, private val force: Boolean = false) {
+    /**
+     * Den andre konstruktøren
+     *
+     * @param[key]
+     * @param[default]
+     *
+     * @see Toggle
+     */
     private constructor(key: String, default: Boolean = false) : this(System.getenv()[key]?.toBoolean() ?: default)
 
+    /**
+     *
+     */
     private val states = mutableListOf(enabled)
 
+    /**
+     *
+     */
     val enabled get() = states.last()
+
+    /**
+     *
+     */
     val disabled get() = !enabled
 
+    /**
+     * Brukes sammen med [pop] {@link Toggles#pop() pop()}
+     * statically bound {@link ILoggerFactory} instance
+     * @link Toggles
+     * @return Unit
+     */
     fun enable() {
         if (force) return
         states.add(true)
     }
 
-    fun disable() {
-        if (force) return
-        states.add(false)
-    }
-
-    fun pop() {
-        if (states.size == 1) return
-        states.removeLast()
-    }
-
+    /**
+     * Denne bruker ikke [pop]
+     */
     fun enable(block: () -> Unit) {
         enable()
         runWith(block)
     }
 
+    /**
+     *
+     */
+    fun disable() {
+        if (force) return
+        states.add(false)
+    }
+
+    /**
+     *
+     */
     fun disable(block: () -> Unit) {
         disable()
         runWith(block)
+    }
+
+    /**
+     * Brukes av [enable]
+     */
+    fun pop() {
+        if (states.size == 1) return
+        states.removeLast()
     }
 
     private fun runWith(block: () -> Unit) {
@@ -44,9 +93,15 @@ abstract class Toggle internal constructor(enabled: Boolean = false, private val
         }
     }
 
+    /**
+     *
+     */
     internal operator fun plus(toggle: Toggle) = listOf(this, toggle)
 
     internal companion object {
+        /**
+         *
+         */
         internal fun Iterable<Toggle>.enable(block: () -> Unit) {
             forEach(Toggle::enable)
             try {
@@ -56,6 +111,9 @@ abstract class Toggle internal constructor(enabled: Boolean = false, private val
             }
         }
 
+        /**
+         *
+         */
         internal fun Iterable<Toggle>.disable(block: () -> Unit) {
             forEach(Toggle::disable)
             try {
