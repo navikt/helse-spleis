b3a72301a84b827faf8440817155a5924b5e38c5
Knut Nygaard <knutnyg@gmail.com>
2021-11-29 15:28:08 +0100

    Unngå leaky bruk av session i spleis-api
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- sykepenger-api/src/main/kotlin/no/nav/helse/spleis/dao/PersonDao.kt
+++ sykepenger-api/src/main/kotlin/no/nav/helse/spleis/dao/PersonDao.kt
@@ -12,14 +12,14 @@ internal class PersonDao(private val dataSource: DataSource) {
         hentPerson(queryOf("SELECT data FROM person WHERE fnr = ? ORDER BY opprettet DESC LIMIT 1", fødselsnummer))
 
     fun hentFødselsnummer(aktørId: Long) =
-        using(sessionOf(dataSource)) { session ->
+        sessionOf(dataSource).use { session ->
             session.run(queryOf("SELECT fnr FROM person WHERE aktor_id = ? ORDER BY opprettet DESC LIMIT 1;", aktørId)
                 .map { it.long("fnr") }
                 .asSingle)
         }
 
     private fun hentPerson(query: Query) =
-        using(sessionOf(dataSource)) { session ->
+        sessionOf(dataSource).use { session ->
             session.run(query.map {
                 SerialisertPerson(it.string("data"))
             }.asSingle)
