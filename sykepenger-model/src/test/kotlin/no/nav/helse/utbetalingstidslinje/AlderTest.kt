e2db7a0e07f30c868c97a1107c6a010f5c82d649
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-11-29 11:38:06 +0100

    flytt oppførsel inn i Grunnlagsdata

--- sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingstidslinje/AlderTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingstidslinje/AlderTest.kt
@@ -5,10 +5,14 @@ import no.nav.helse.somFødselsnummer
 import no.nav.helse.testhelpers.februar
 import no.nav.helse.testhelpers.januar
 import no.nav.helse.økonomi.Inntekt.Companion.årlig
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class AlderTest {
+
+    private companion object {
+        val FYLLER_67_1_JANUAR_2018 = "01015149945".somFødselsnummer().alder()
+    }
 
     @Test
     fun `alder på gitt dato`() {
@@ -19,25 +23,39 @@ internal class AlderTest {
 
     @Test
     fun `Minimum inntekt er en halv g hvis du akkurat har fylt 67`() {
-        val alder = "01015149945".somFødselsnummer().alder()
-        assertEquals(67, alder.alderPåDato(1.januar))
-        assertEquals(Grunnbeløp.halvG.beløp(1.januar), alder.minimumInntekt(1.januar))
-        assertEquals((93634 / 2).årlig, alder.minimumInntekt(1.januar))
+        assertEquals(67, FYLLER_67_1_JANUAR_2018.alderPåDato(1.januar))
+        assertEquals(Grunnbeløp.halvG.beløp(1.januar), FYLLER_67_1_JANUAR_2018.minimumInntekt(1.januar))
+        assertEquals((93634 / 2).årlig, FYLLER_67_1_JANUAR_2018.minimumInntekt(1.januar))
     }
 
     @Test
     fun `Minimum inntekt er 2g hvis du er en dag over 67`() {
-        val alder = "01015149945".somFødselsnummer().alder()
-        assertEquals(67, alder.alderPåDato(2.januar))
-        assertEquals(Grunnbeløp.`2G`.beløp(2.januar), alder.minimumInntekt(2.januar))
-        assertEquals((93634 * 2).årlig, alder.minimumInntekt(2.januar))
+        assertEquals(67, FYLLER_67_1_JANUAR_2018.alderPåDato(2.januar))
+        assertEquals(Grunnbeløp.`2G`.beløp(2.januar), FYLLER_67_1_JANUAR_2018.minimumInntekt(2.januar))
+        assertEquals((93634 * 2).årlig, FYLLER_67_1_JANUAR_2018.minimumInntekt(2.januar))
     }
 
     @Test
     fun `Minimum inntekt er 2g hvis du er 69`() {
-        val alder = "01014949945".somFødselsnummer().alder()
-        assertEquals(69, alder.alderPåDato(1.januar))
-        assertEquals(Grunnbeløp.`2G`.beløp(1.januar), alder.minimumInntekt(1.januar))
-        assertEquals((93634 * 2).årlig, alder.minimumInntekt(1.januar))
+        val FYLLER_69_1_JANUAR_2018 = "01014949945".somFødselsnummer().alder()
+        assertEquals(69, FYLLER_69_1_JANUAR_2018.alderPåDato(1.januar))
+        assertEquals(Grunnbeløp.`2G`.beløp(1.januar), FYLLER_69_1_JANUAR_2018.minimumInntekt(1.januar))
+        assertEquals((93634 * 2).årlig, FYLLER_69_1_JANUAR_2018.minimumInntekt(1.januar))
+    }
+
+    @Test
+    fun `forhøyet inntektskrav`() {
+        assertFalse(FYLLER_67_1_JANUAR_2018.forhøyetInntektskrav(1.januar))
+        assertTrue(FYLLER_67_1_JANUAR_2018.forhøyetInntektskrav(2.januar))
+    }
+
+    @Test
+    fun `riktig begrunnelse for minimum inntekt ved alder over 67`() {
+        assertEquals(Begrunnelse.MinimumInntektOver67, FYLLER_67_1_JANUAR_2018.begrunnelseForMinimumInntekt(2.januar))
+    }
+
+    @Test
+    fun `riktig begrunnelse for minimum inntekt ved alder 67 eller under`() {
+        assertEquals(Begrunnelse.MinimumInntekt, FYLLER_67_1_JANUAR_2018.begrunnelseForMinimumInntekt(1.januar))
     }
 }
