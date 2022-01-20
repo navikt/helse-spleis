fb1195bd3fd4ee0e5dc77397b3dd6b6f9e4abb57
Simen Ullern <simen.ullern@nav.no>
2022-01-20 13:06:46 +0100

    Privatiserer felter i OverstyrArbeidsforhold-hendelse
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/OverstyrArbeidsforhold.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/OverstyrArbeidsforhold.kt
@@ -1,10 +1,12 @@
 package no.nav.helse.hendelser
 
 import no.nav.helse.Organisasjonsnummer
+import no.nav.helse.hendelser.OverstyrArbeidsforhold.ArbeidsforholdOverstyrt.Companion.overstyringFor
 import no.nav.helse.person.Aktivitetslogg
 import no.nav.helse.person.Arbeidsforholdhistorikk
 import no.nav.helse.person.Arbeidsgiver
 import no.nav.helse.person.PersonHendelse
+import no.nav.helse.somOrganisasjonsnummer
 import java.time.LocalDate
 import java.util.*
 
@@ -13,7 +15,7 @@ class OverstyrArbeidsforhold(
     private val fødselsnummer: String,
     private val aktørId: String,
     private val skjæringstidspunkt: LocalDate,
-    internal val overstyrteArbeidsforhold: List<ArbeidsforholdOverstyrt> // TODO: Kan denne privatiseres?
+    private val overstyrteArbeidsforhold: List<ArbeidsforholdOverstyrt>
 ) : PersonHendelse(meldingsreferanseId, Aktivitetslogg()) {
     override fun fødselsnummer() = fødselsnummer
     override fun aktørId() = aktørId
@@ -21,13 +23,17 @@ class OverstyrArbeidsforhold(
     internal fun erRelevant(skjæringstidspunkt: LocalDate) = this.skjæringstidspunkt == skjæringstidspunkt
 
     internal fun lagre(arbeidsgiver: Arbeidsgiver) {
-        val overstyring = overstyrteArbeidsforhold.firstOrNull { it.orgnummer.toString() == arbeidsgiver.organisasjonsnummer() }
+        val overstyring = overstyrteArbeidsforhold.overstyringFor(arbeidsgiver.organisasjonsnummer().somOrganisasjonsnummer())
         if (overstyring != null) {
             arbeidsgiver.lagreOverstyrArbeidsforhold(skjæringstidspunkt, overstyring)
         }
     }
 
-    class ArbeidsforholdOverstyrt(internal val orgnummer: Organisasjonsnummer, private val erAktivt: Boolean) {
+    class ArbeidsforholdOverstyrt(private val orgnummer: Organisasjonsnummer, private val erAktivt: Boolean) {
+
+        companion object {
+            internal fun Iterable<ArbeidsforholdOverstyrt>.overstyringFor(orgnummer: Organisasjonsnummer) = firstOrNull { it.orgnummer == orgnummer }
+        }
 
         internal fun lagre(skjæringstidspunkt: LocalDate, arbeidsforholdhistorikk: Arbeidsforholdhistorikk) {
             if (erAktivt) {
