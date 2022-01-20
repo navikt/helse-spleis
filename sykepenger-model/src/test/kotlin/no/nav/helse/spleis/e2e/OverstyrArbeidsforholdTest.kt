fae524dfba52599ae425c5f817c20c74acd8c641
Simen Ullern <simen.ullern@nav.no>
2022-01-20 11:44:25 +0100

    Feilende test: Kan ikke overstyre arbeidsforhold ved forlengelse
    ... som allerede har tidligere utbetalinger
    
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/OverstyrArbeidsforholdTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/OverstyrArbeidsforholdTest.kt
@@ -3,6 +3,7 @@ package no.nav.helse.spleis.e2e
 import no.nav.helse.ForventetFeil
 import no.nav.helse.hendelser.*
 import no.nav.helse.testhelpers.desember
+import no.nav.helse.testhelpers.februar
 import no.nav.helse.testhelpers.januar
 import no.nav.helse.økonomi.Prosentdel.Companion.prosent
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -51,5 +52,34 @@ internal class OverstyrArbeidsforholdTest : AbstractEndToEndTest() {
         håndterSimulering(1.vedtaksperiode)
         val vilkårsgrunnlag = person.vilkårsgrunnlagFor(skjæringstidspunkt)
         assertEquals(setOf(a1.toString()), vilkårsgrunnlag?.inntektsopplysningPerArbeidsgiver()?.keys)
+    }
+
+    @ForventetFeil("Dette gjør vi også etter lunsj")
+    @Test
+    fun `Kan ikke overstyre arbeidsforhold for en forlengelse som allerede har tidligere utbetalinger`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 31.januar, 100.prosent))
+        håndterSøknad(Søknad.Søknadsperiode.Sykdom(1.januar, 31.januar, 100.prosent))
+        håndterInntektsmelding(listOf(1.januar til 16.januar))
+        håndterYtelser(1.vedtaksperiode)
+        håndterVilkårsgrunnlag(
+            1.vedtaksperiode, arbeidsforhold = listOf(
+                Vilkårsgrunnlag.Arbeidsforhold(a1.toString(), LocalDate.EPOCH, null),
+                Vilkårsgrunnlag.Arbeidsforhold(a2.toString(), 1.desember(2017), null)
+            )
+        )
+        håndterYtelser(1.vedtaksperiode)
+        håndterSimulering(1.vedtaksperiode)
+        håndterUtbetalingsgodkjenning(1.vedtaksperiode, utbetalingGodkjent = true)
+        håndterUtbetalt(1.vedtaksperiode)
+        // ny periode
+        håndterSykmelding(Sykmeldingsperiode(1.februar, 28.februar, 100.prosent))
+        håndterSøknad(Søknad.Søknadsperiode.Sykdom(1.februar, 28.februar, 100.prosent))
+        håndterYtelser(2.vedtaksperiode)
+        håndterSimulering(2.vedtaksperiode)
+        val skjæringstidspunkt = inspektør.skjæringstidspunkt(2.vedtaksperiode)
+        assertEquals(listOf(a1.toString(), a2.toString()).toList(), person.orgnummereMedRelevanteArbeidsforhold(skjæringstidspunkt).toList())
+        håndterOverstyrArbeidsforhold(skjæringstidspunkt, listOf(OverstyrArbeidsforhold.ArbeidsforholdOverstyrt(a2, false)))
+        assertEquals(listOf(a1.toString(), a2.toString()), person.orgnummereMedRelevanteArbeidsforhold(skjæringstidspunkt))
+        assertError(2.vedtaksperiode, "Kan ikke overstyre arbeidsforhold for en pågående behandling der én eller flere perioder er behandlet ferdig", a1)
     }
 }
