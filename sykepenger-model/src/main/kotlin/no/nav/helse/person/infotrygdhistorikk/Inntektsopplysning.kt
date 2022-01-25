9fefd703101d42d6f0b798c03ad3a732fd5af29e
Sindre Broch <sindre.broch@nav.no>
2022-01-25 15:43:48 +0100

    Vi endra en tekst som ikke var helt riktig.
    Per nå er det faktisk den laveste som blir lagt til grunn.
    
    Co-authored-by: Peter Stensby <peter.stensby@nav.no>
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/infotrygdhistorikk/Inntektsopplysning.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/infotrygdhistorikk/Inntektsopplysning.kt
@@ -106,7 +106,7 @@ class Inntektsopplysning private constructor(
                 .groupBy { it.orgnummer to it.sykepengerFom }
                 .any { (_, inntekter) -> inntekter.size > 1 }
             if (harFlereInntekterPåSammeAGogDato)
-                aktivitetslogg.warn("Det er lagt inn flere inntekter i Infotrygd med samme fom-dato, den seneste er lagt til grunn. Kontroller sykepengegrunnlaget.")
+                aktivitetslogg.warn("Det er lagt inn flere inntekter i Infotrygd med samme fom-dato. Kontroller sykepengegrunnlaget.")
         }
 
         internal fun List<Inntektsopplysning>.lagreVilkårsgrunnlag(
