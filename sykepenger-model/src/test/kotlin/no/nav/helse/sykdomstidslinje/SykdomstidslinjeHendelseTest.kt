c27b42ecf3d16ebea30ee089821c77c07c5bc81b
David Steinsland <david@steinsland.email>
2022-01-25 08:41:01 +0100

    oppdaterFom trekker perioder tilbake - men ikke til arbeidsdager

--- sykepenger-model/src/test/kotlin/no/nav/helse/sykdomstidslinje/SykdomstidslinjeHendelseTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/sykdomstidslinje/SykdomstidslinjeHendelseTest.kt
@@ -1,9 +1,16 @@
 package no.nav.helse.sykdomstidslinje
 
+import no.nav.helse.februar
 import no.nav.helse.hendelser.Periode
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
 import no.nav.helse.person.Aktivitetslogg
 import no.nav.helse.person.Arbeidsgiver
+import no.nav.helse.testhelpers.A
+import no.nav.helse.testhelpers.S
+import no.nav.helse.testhelpers.resetSeed
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import java.util.*
@@ -16,6 +23,11 @@ internal class SykdomstidslinjeHendelseTest {
         private const val ORGNR = "orgnr"
     }
 
+    @BeforeEach
+    fun setup() {
+        resetSeed()
+    }
+
     @Test
     fun kontekst() {
         assertEquals(mapOf(
@@ -26,11 +38,31 @@ internal class SykdomstidslinjeHendelseTest {
         ), Testhendelse().toSpesifikkKontekst().kontekstMap)
     }
 
-    private class Testhendelse : SykdomstidslinjeHendelse(MELDING, LocalDateTime.now()) {
+    @Test
+    fun oppdaterFom() {
+        val hendelse = Testhendelse(31.S)
+        assertEquals(1.januar til 10.januar, hendelse.oppdaterFom(5.januar til 10.januar))
+        hendelse.trimLeft(10.januar)
+        assertEquals(11.januar til 31.januar, hendelse.oppdaterFom(15.januar til 31.januar))
+        hendelse.trimLeft(31.januar)
+        assertEquals(1.februar til 5.februar, hendelse.oppdaterFom(1.februar til 5.februar))
+    }
+
+    @Test
+    fun `oppdaterer ikke fom til arbeidsdager`() {
+        val hendelse = Testhendelse(15.A + 16.S)
+        assertEquals(1.januar til 10.januar, hendelse.oppdaterFom(5.januar til 10.januar))
+        hendelse.trimLeft(10.januar)
+        assertEquals(15.januar til 31.januar, hendelse.oppdaterFom(15.januar til 31.januar))
+        hendelse.trimLeft(31.januar)
+        assertEquals(1.februar til 5.februar, hendelse.oppdaterFom(1.februar til 5.februar))
+    }
+
+    private class Testhendelse(private val sykdomstidslinje: Sykdomstidslinje = Sykdomstidslinje()) : SykdomstidslinjeHendelse(MELDING, LocalDateTime.now()) {
         override fun aktørId() = AKTØR
         override fun fødselsnummer() = FØDSELSNUMMER
         override fun organisasjonsnummer() = ORGNR
-        override fun sykdomstidslinje(): Sykdomstidslinje = throw RuntimeException("Brukes ikke i testene")
+        override fun sykdomstidslinje(): Sykdomstidslinje = sykdomstidslinje
         override fun valider(periode: Periode): Aktivitetslogg = throw RuntimeException("Brukes ikke i testene")
         override fun fortsettÅBehandle(arbeidsgiver: Arbeidsgiver) = throw RuntimeException("Brukes ikke i testene")
     }
