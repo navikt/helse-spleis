ff3ff6074b3f62100e0b4bc124353b47e191b380
David Steinsland <david@steinsland.email>
2022-01-28 12:06:11 +0100

    bekrefter at periode blir stuck ved overstyring og flere arbeidsgivere

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/OverstyrTidslinjeFlereAGTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/OverstyrTidslinjeFlereAGTest.kt
@@ -1,7 +1,10 @@
 package no.nav.helse.spleis.e2e
 
+import no.nav.helse.ForventetFeil
 import no.nav.helse.hendelser.til
 import no.nav.helse.januar
+import no.nav.helse.oktober
+import no.nav.helse.person.TilstandType.*
 import org.junit.jupiter.api.Test
 
 internal class OverstyrTidslinjeFlereAGTest : AbstractEndToEndTest() {
@@ -21,5 +24,29 @@ internal class OverstyrTidslinjeFlereAGTest : AbstractEndToEndTest() {
         håndterSimulering(1.vedtaksperiode, orgnummer = AG2)
         håndterOverstyrTidslinje((29.januar til 29.januar).map { manuellFeriedag(it) }, orgnummer = AG2)
         assertErrorTekst(inspektør(AG2), "Kan ikke overstyre en pågående behandling der én eller flere perioder er behandlet ferdig")
+    }
+
+    @ForventetFeil("Overstyring på arbeidsgiver 1 skal ikke medføre at arbeidsgiver 2 sin urelaterte periode påvirkes")
+    @Test
+    fun `overstyre en eldre periode hos en arbeidsgiver`() {
+        nyttVedtak(1.januar, 31.januar, orgnummer = AG1)
+        tilGodkjenning(1.oktober, 30.oktober, AG2)
+        håndterOverstyrTidslinje((29.januar til 29.januar).map { manuellFeriedag(it) }, orgnummer = AG1)
+        assertSisteTilstand(1.vedtaksperiode, AVVENTER_HISTORIKK_REVURDERING, AG1)
+        assertSisteTilstand(1.vedtaksperiode, AVVENTER_GODKJENNING, AG2)
+    }
+
+    @ForventetFeil("Dersom det er riktig at arbeidsgiver 2 påvirkes, skal uansett arbeidsgiver 2 få Gjennoppta behandling når arbeidsgiver 1 avsluttes")
+    @Test
+    fun `overstyre og utbetalte en eldre periode hos en arbeidsgiver`() {
+        nyttVedtak(1.januar, 31.januar, orgnummer = AG1)
+        tilGodkjenning(1.oktober, 30.oktober, AG2)
+        håndterOverstyrTidslinje((29.januar til 29.januar).map { manuellFeriedag(it) }, orgnummer = AG1)
+        håndterYtelser(1.vedtaksperiode, orgnummer = AG1)
+        håndterSimulering(1.vedtaksperiode, orgnummer = AG1)
+        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = AG1)
+        håndterUtbetalt(1.vedtaksperiode, orgnummer = AG1)
+        assertSisteTilstand(1.vedtaksperiode, AVSLUTTET, AG1)
+        assertSisteTilstand(1.vedtaksperiode, AVVENTER_GODKJENNING, AG2)
     }
 }
