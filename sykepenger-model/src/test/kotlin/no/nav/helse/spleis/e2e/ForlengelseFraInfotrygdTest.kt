5cd7713f5fa3b4e046ff0ab4c4209a3805a73d69
Erik M.Forsman <erik.maximilian.forsman@nav.no>
2022-02-04 10:37:02 +0100

    Flytter tester for forlengelser av forkastede perioder
    Legger også til tester for oppførsel med toggle av

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/ForlengelseFraInfotrygdTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/ForlengelseFraInfotrygdTest.kt
@@ -977,37 +977,4 @@ internal class ForlengelseFraInfotrygdTest : AbstractEndToEndTest() {
 
         assertNull(person.vilkårsgrunnlagFor(1.januar))
     }
-
-    @Test
-    fun `forkaster periode som er forlengelse av forkastet periode`() = Toggle.ForkastForlengelseAvForkastetPeriode.enable {
-        (1.januar til 15.januar).forkast()
-        assertSisteForkastetPeriodeTilstand(ORGNUMMER, 1.vedtaksperiode, TIL_INFOTRYGD)
-        håndterSykmelding(Sykmeldingsperiode(16.januar, 31.januar, 100.prosent))
-        assertSisteForkastetPeriodeTilstand(ORGNUMMER, 2.vedtaksperiode, TIL_INFOTRYGD)
-        assertError(2.vedtaksperiode, "Perioden forlenger en forkastet periode")
-
-    }
-
-    @Test
-    fun `forkaster periode som er forlengelse av forkastet periode over helg`() = Toggle.ForkastForlengelseAvForkastetPeriode.enable {
-        (1.januar til 19.januar).forkast()
-        assertSisteForkastetPeriodeTilstand(ORGNUMMER, 1.vedtaksperiode, TIL_INFOTRYGD)
-        håndterSykmelding(Sykmeldingsperiode(22.januar, 31.januar, 100.prosent))
-        assertSisteForkastetPeriodeTilstand(ORGNUMMER, 2.vedtaksperiode, TIL_INFOTRYGD)
-        assertError(2.vedtaksperiode, "Perioden forlenger en forkastet periode")
-    }
-
-    @Test
-    fun `forkaster ikke periode som har gap til forkastet periode`() = Toggle.ForkastForlengelseAvForkastetPeriode.enable {
-        (1.januar til 19.januar).forkast()
-        assertSisteForkastetPeriodeTilstand(ORGNUMMER, 1.vedtaksperiode, TIL_INFOTRYGD)
-        håndterSykmelding(Sykmeldingsperiode(23.januar, 31.januar, 100.prosent))
-        assertSisteTilstand(2.vedtaksperiode, MOTTATT_SYKMELDING_FERDIG_GAP)
-        assertNoErrors(2.vedtaksperiode)
-    }
-
-    private fun Periode.forkast() {
-        håndterSykmelding(Sykmeldingsperiode(start, endInclusive, 100.prosent))
-        person.søppelbøtte(hendelselogg, this)
-    }
 }
