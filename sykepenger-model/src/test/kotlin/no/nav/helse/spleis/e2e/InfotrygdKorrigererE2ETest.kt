8a291804ea35822ae8db60bed114bd0bcd64a6c2
David Steinsland <david@steinsland.email>
2022-02-07 08:53:45 +0100

    feilende test hvor Infotrygd korrigerer gap til sykdom

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/InfotrygdKorrigererE2ETest.kt
@@ -0,0 +1,40 @@
+package no.nav.helse.spleis.e2e
+
+import no.nav.helse.ForventetFeil
+import no.nav.helse.februar
+import no.nav.helse.hendelser.Sykmeldingsperiode
+import no.nav.helse.hendelser.Søknad.Søknadsperiode.Arbeid
+import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
+import no.nav.helse.person.TilstandType.AVSLUTTET
+import no.nav.helse.person.TilstandType.AVVENTER_HISTORIKK
+import no.nav.helse.person.infotrygdhistorikk.ArbeidsgiverUtbetalingsperiode
+import no.nav.helse.person.infotrygdhistorikk.Inntektsopplysning
+import no.nav.helse.økonomi.Prosentdel.Companion.prosent
+import org.junit.jupiter.api.Test
+
+internal class InfotrygdKorrigererE2ETest : AbstractEndToEndTest() {
+
+    @ForventetFeil("Infotrygd korrigerer arbeidsdager til sykdom")
+    @Test
+    fun `infotrygd korrigerer arbeid gjenopptatt`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 31.januar, 100.prosent))
+        håndterSøknad(Sykdom(1.januar, 31.januar, 100.prosent), Arbeid(29.januar, 31.januar))
+        håndterInntektsmelding(listOf(1.januar til 16.januar))
+        håndterYtelser(1.vedtaksperiode)
+        håndterVilkårsgrunnlag(1.vedtaksperiode)
+        håndterYtelser(1.vedtaksperiode)
+        håndterSimulering(1.vedtaksperiode)
+        håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+        håndterUtbetalt(1.vedtaksperiode)
+        håndterSøknad(Sykdom(1.januar, 31.januar, 100.prosent)) // korrigerer arbeidsdagene
+        håndterSykmelding(Sykmeldingsperiode(1.februar, 16.februar, 100.prosent))
+        håndterSøknad(Sykdom(1.februar, 16.februar, 100.prosent))
+        håndterUtbetalingshistorikk(2.vedtaksperiode, ArbeidsgiverUtbetalingsperiode(ORGNUMMER, 29.januar, 31.januar, 100.prosent, INNTEKT), inntektshistorikk = listOf(
+            Inntektsopplysning(ORGNUMMER, 29.januar, INNTEKT, true)
+        ))
+        assertSisteTilstand(1.vedtaksperiode, AVSLUTTET)
+        assertSisteTilstand(2.vedtaksperiode, AVVENTER_HISTORIKK)
+    }
+}
