141ade4ee7fa6415a679e04822d06fe827d55fa6
David Steinsland <david@steinsland.email>
2022-02-12 09:49:06 +0100

    sporer arbeidsgiverperiodeavgjørelser

---
+++ sykepenger-model/src/main/kotlin/no/nav/helse/utbetalingstidslinje/ny/Arbeidsgiverperiodesubsumsjon.kt
@@ -0,0 +1,55 @@
+package no.nav.helse.utbetalingstidslinje.ny
+
+import no.nav.helse.person.etterlevelse.SubsumsjonObserver
+import no.nav.helse.økonomi.Økonomi
+import java.time.LocalDate
+
+//TODO: Logge 8-19 første ledd når går ut av første arbeidsgiverperiode - BEREGNING
+//TODO: Logge 8-19 andre ledd for hver dag i arbeidsgiverperioden - BEREGNING
+//TODO: Logge 8-19 tredje ledd når vi teller første arbeidsgiverperiode - BEREGNING
+//TODO: Logge 8-19 fjerde ledd når vi teller neste arbeidsgiverperiode - BEREGNING
+internal class Arbeidsgiverperiodesubsumsjon(
+    private val other: ArbeidsgiverperiodeMediator,
+    private val subsumsjonObserver: SubsumsjonObserver
+) : ArbeidsgiverperiodeMediator by (other) {
+
+    private var tilstand: Tilstand = Initiell
+
+    override fun fridag(dato: LocalDate) {
+        subsumsjonObserver.`§ 8-17 ledd 2`(dato, emptyList()) // TODO trenger sykdomstidslinjedager
+        other.fridag(dato)
+    }
+
+    override fun arbeidsgiverperiodedag(dato: LocalDate, økonomi: Økonomi) {
+        tilstand.arbeidsgiverperiodedag(this, dato, økonomi)
+        subsumsjonObserver.`§ 8-17 ledd 1 bokstav a`(false, dagen = dato)
+        other.arbeidsgiverperiodedag(dato, økonomi)
+    }
+
+    override fun utbetalingsdag(dato: LocalDate, økonomi: Økonomi) {
+        // på første navdag etter fullført agp
+        tilstand.utbetalingsdag(this, dato, økonomi)
+        other.utbetalingsdag(dato, økonomi)
+    }
+
+    override fun arbeidsgiverperiodeAvbrutt() {
+        tilstand = Initiell
+        other.arbeidsgiverperiodeAvbrutt()
+    }
+
+    private interface Tilstand {
+        fun arbeidsgiverperiodedag(parent: Arbeidsgiverperiodesubsumsjon, dato: LocalDate, økonomi: Økonomi) {}
+        fun utbetalingsdag(parent: Arbeidsgiverperiodesubsumsjon, dato: LocalDate, økonomi: Økonomi) {}
+    }
+    private object Initiell : Tilstand {
+        override fun arbeidsgiverperiodedag(parent: Arbeidsgiverperiodesubsumsjon, dato: LocalDate, økonomi: Økonomi) {
+            parent.tilstand = PåbegyntArbeidsgiverperiode
+        }
+    }
+    private object PåbegyntArbeidsgiverperiode : Tilstand {
+        override fun utbetalingsdag(parent: Arbeidsgiverperiodesubsumsjon, dato: LocalDate, økonomi: Økonomi) {
+            parent.subsumsjonObserver.`§ 8-17 ledd 1 bokstav a`(true, dagen = dato)
+            parent.tilstand = Initiell
+        }
+    }
+}
---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingstidslinje/ny/ArbeidsgiverperiodesubsumsjonTest.kt
@@ -0,0 +1,135 @@
+package no.nav.helse.utbetalingstidslinje.ny
+
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
+import no.nav.helse.person.SykdomstidslinjeVisitor
+import no.nav.helse.person.etterlevelse.SubsumsjonObserver
+import no.nav.helse.sykdomstidslinje.Sykdomstidslinje
+import no.nav.helse.testhelpers.A
+import no.nav.helse.testhelpers.S
+import no.nav.helse.testhelpers.opphold
+import no.nav.helse.testhelpers.resetSeed
+import no.nav.helse.utbetalingstidslinje.Begrunnelse
+import no.nav.helse.økonomi.Økonomi
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+internal class ArbeidsgiverperiodesubsumsjonTest {
+    @Test
+    fun ingenting() {
+        undersøke(1.A + 29.opphold + 1.A)
+        assertEquals(31, observatør.dager)
+        assertEquals(23, observatør.arbeidsdager)
+        assertEquals(8, observatør.fridager)
+        assertEquals(8, jurist.subsumsjoner)
+        assertEquals(8, jurist.`§ 8-17 ledd 2`)
+    }
+
+    @Test
+    fun kurant() {
+        undersøke(31.S)
+        assertEquals(31, observatør.dager)
+        assertEquals(16, observatør.arbeidsgiverperiodedager)
+        assertEquals(15, observatør.utbetalingsdager)
+        assertEquals(17, jurist.subsumsjoner)
+        assertEquals(0, jurist.`§ 8-17 ledd 2`)
+        assertEquals(16, jurist.`§ 8-17 første ledd bokstav a - ikke oppfylt`)
+        assertEquals(1, jurist.`§ 8-17 første ledd bokstav a - oppfylt`)
+    }
+
+    @Test
+    fun infotrygd() {
+        undersøke(31.S) { teller, other ->
+            Infotrygddekoratør(teller, other, listOf(1.januar til 10.januar))
+        }
+        assertEquals(31, observatør.dager)
+        assertEquals(0, observatør.arbeidsgiverperiodedager)
+        assertEquals(31, observatør.utbetalingsdager)
+        assertEquals(0, jurist.subsumsjoner)
+    }
+
+    @Test
+    fun `infotrygd etter arbeidsgiverperiode`() {
+        undersøke(31.S) { teller, other ->
+            Infotrygddekoratør(teller, other, listOf(17.januar til 20.januar))
+        }
+        assertEquals(31, observatør.dager)
+        assertEquals(16, observatør.arbeidsgiverperiodedager)
+        assertEquals(15, observatør.utbetalingsdager)
+        assertEquals(17, jurist.subsumsjoner)
+        assertEquals(0, jurist.`§ 8-17 ledd 2`)
+        assertEquals(16, jurist.`§ 8-17 første ledd bokstav a - ikke oppfylt`)
+        assertEquals(1, jurist.`§ 8-17 første ledd bokstav a - oppfylt`)
+    }
+
+    private lateinit var jurist: Subsumsjonobservatør
+    private lateinit var teller: Arbeidsgiverperiodeteller
+    private lateinit var observatør: Dagobservatør
+
+    @BeforeEach
+    fun setup() {
+        resetSeed()
+        jurist = Subsumsjonobservatør()
+        observatør = Dagobservatør()
+        teller = Arbeidsgiverperiodeteller.NormalArbeidstaker
+    }
+
+    private fun undersøke(tidslinje: Sykdomstidslinje, delegator: ((Arbeidsgiverperiodeteller, SykdomstidslinjeVisitor) -> SykdomstidslinjeVisitor)? = null) {
+        val arbeidsgiverperiodeBuilder = ArbeidsgiverperiodeBuilder(teller, Arbeidsgiverperiodesubsumsjon(observatør, jurist))
+        tidslinje.accept(delegator?.invoke(teller, arbeidsgiverperiodeBuilder) ?: arbeidsgiverperiodeBuilder)
+    }
+
+    private class Subsumsjonobservatør : SubsumsjonObserver {
+        var subsumsjoner = 0
+        var `§ 8-17 første ledd bokstav a - ikke oppfylt` = 0
+        var `§ 8-17 første ledd bokstav a - oppfylt` = 0
+        var `§ 8-17 ledd 2` = 0
+
+        override fun `§ 8-17 ledd 1 bokstav a`(oppfylt: Boolean, dagen: LocalDate) {
+            subsumsjoner += 1
+            if (oppfylt) `§ 8-17 første ledd bokstav a - oppfylt` += 1
+            else `§ 8-17 første ledd bokstav a - ikke oppfylt` += 1
+        }
+
+        override fun `§ 8-17 ledd 2`(dato: LocalDate, sykdomstidslinje: List<SubsumsjonObserver.Tidslinjedag>) {
+            subsumsjoner += 1
+            `§ 8-17 ledd 2` += 1
+        }
+    }
+
+    private class Dagobservatør : ArbeidsgiverperiodeMediator {
+        val dager get() = fridager + arbeidsdager + arbeidsgiverperiodedager + utbetalingsdager + foreldetdager + avvistdager
+        var fridager = 0
+        var arbeidsdager = 0
+        var arbeidsgiverperiodedager = 0
+        var utbetalingsdager = 0
+        var foreldetdager = 0
+        var avvistdager = 0
+
+        override fun fridag(dato: LocalDate) {
+            fridager += 1
+        }
+
+        override fun arbeidsdag(dato: LocalDate) {
+            arbeidsdager += 1
+        }
+
+        override fun arbeidsgiverperiodedag(dato: LocalDate, økonomi: Økonomi) {
+            arbeidsgiverperiodedager += 1
+        }
+
+        override fun utbetalingsdag(dato: LocalDate, økonomi: Økonomi) {
+            utbetalingsdager += 1
+        }
+
+        override fun foreldetDag(dato: LocalDate, økonomi: Økonomi) {
+            foreldetdager += 1
+        }
+
+        override fun avvistDag(dato: LocalDate, begrunnelse: Begrunnelse) {
+            avvistdager += 1
+        }
+    }
+}
