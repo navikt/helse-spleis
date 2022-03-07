54c661773d851a66c5709425f20aa333c6749527
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-03-07 13:27:27 +0100

    Skru på lesing av rapid i gcp-dev
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/App.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/App.kt
@@ -1,8 +1,6 @@
 package no.nav.helse
 
 fun main() {
-    //val applicationBuilder = ApplicationBuilder(System.getenv())
-    //applicationBuilder.start()
-    ProbeApi().start()
-    DataSourceBuilder(System.getenv()).migrate()
+    val applicationBuilder = ApplicationBuilder(System.getenv())
+    applicationBuilder.start()
 }
