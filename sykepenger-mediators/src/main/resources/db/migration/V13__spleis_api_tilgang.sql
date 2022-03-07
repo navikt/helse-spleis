c5670f2af0f08e2e2332626ab38c2d1bbcab79a9
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-03-07 15:08:10 +0100

    Fjerner spleis-api fra superuser group
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- sykepenger-mediators/src/main/resources/db/migration/V13__spleis_api_tilgang.sql
+++ sykepenger-mediators/src/main/resources/db/migration/V13__spleis_api_tilgang.sql
@@ -3,6 +3,7 @@ $do$
 BEGIN
    IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname='spleis-api') THEN
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "spleis-api";
+ALTER GROUP cloudsqlsuperuser DROP USER "spleis-api";
 END IF;
 END
 $do$
