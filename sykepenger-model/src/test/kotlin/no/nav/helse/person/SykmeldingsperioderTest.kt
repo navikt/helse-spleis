771e43942c0b36cd2775fd64b959de9a8e072326
Kevin Sillerud <kevin.sillerud@gmail.com>
2022-03-11 11:16:16 +0100

    Tester for å lagre sykmeldingsperioder for overlapp, duplikat og gap
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/person/SykmeldingsperioderTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/person/SykmeldingsperioderTest.kt
@@ -1,9 +1,11 @@
 package no.nav.helse.person
 
+import no.nav.helse.februar
 import no.nav.helse.hendelser.Periode
 import no.nav.helse.hendelser.til
 import no.nav.helse.januar
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class SykmeldingsperioderTest() {
@@ -13,7 +15,32 @@ internal class SykmeldingsperioderTest() {
         val sykmeldingsperioder = Sykmeldingsperioder()
         sykmeldingsperioder.lagre(1.januar til 31.januar)
         assertEquals(listOf(1.januar til 31.januar), sykmeldingsperioder.perioder())
+    }
 
+    @Disabled
+    @Test
+    fun `utvider ikke perioder ved duplikate sykmeldingsperioder`() {
+        val sykmeldingsperioder = Sykmeldingsperioder()
+        sykmeldingsperioder.lagre(1.januar til 31.januar)
+        sykmeldingsperioder.lagre(1.januar til 31.januar)
+        assertEquals(listOf(1.januar til 31.januar), sykmeldingsperioder.perioder())
+    }
+
+    @Disabled
+    @Test
+    fun `utvider periode ved overlappende sykmeldingsperioder, lager ikke ny periode`() {
+        val sykmeldingsperioder = Sykmeldingsperioder()
+        sykmeldingsperioder.lagre(1.januar til 31.januar)
+        sykmeldingsperioder.lagre(20.januar til 20.februar)
+        assertEquals(listOf(1.januar til 20.februar), sykmeldingsperioder.perioder())
+    }
+
+    @Test
+    fun `gap fører til to sykmeldingsperioder`() {
+        val sykmeldingsperioder = Sykmeldingsperioder()
+        sykmeldingsperioder.lagre(1.januar til 31.januar)
+        sykmeldingsperioder.lagre(20.februar til 28.februar)
+        assertEquals(listOf(1.januar til 31.januar, 20.februar til 28.februar), sykmeldingsperioder.perioder())
     }
 
     class Inspektør() : Sykmeldingsperioder.Visitor {
