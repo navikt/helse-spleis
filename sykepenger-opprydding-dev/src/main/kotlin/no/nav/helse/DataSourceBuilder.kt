e175a453f6fcd39deb50d4b43d69977ec363a0dd
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2022-03-15 15:31:08 +0100

    Fiks gcp-config for db

--- sykepenger-opprydding-dev/src/main/kotlin/no/nav/helse/DataSourceBuilder.kt
+++ sykepenger-opprydding-dev/src/main/kotlin/no/nav/helse/DataSourceBuilder.kt
@@ -2,24 +2,41 @@ package no.nav.helse
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import org.flywaydb.core.Flyway
-import org.slf4j.LoggerFactory
 import java.time.Duration
 
 // Understands how to create a data source from environment variables
 internal class DataSourceBuilder(env: Map<String, String>) {
 
+    private val gcpProjectId: String? = env["GCP_TEAM_PROJECT_ID"]
+    private val databaseRegion: String? = env["DATABASE_REGION"]
+    private val databaseInstance: String? = env["DATABASE_INSTANCE"]
+    private val databaseUsername: String? = env["DATABASE_SPLEIS_API_USERNAME"]
+    private val databasePassword: String? = env["DATABASE_SPLEIS_API_PASSWORD"]
+    private val databaseName: String? = env["DATABASE_DATABASE"]
+
+    init {
+        requireNotNull(gcpProjectId) { "gcp project id must be set if jdbd url is not provided" }
+        requireNotNull(databaseRegion) { "database region must be set if jdbd url is not provided" }
+        requireNotNull(databaseInstance) { "database instance must be set if jdbd url is not provided" }
+        requireNotNull(databaseName) { "database name must be set if jdbc url is not provided" }
+    }
+
     private val hikariConfig = HikariConfig().apply {
-        jdbcUrl = env["DATABASE_JDBC_URL"] ?: String.format(
-            "jdbc:postgresql://%s:%s/%s",
-            requireNotNull(env["DATABASE_HOST"]) { "database host must be set if jdbc url is not provided" },
-            requireNotNull(env["DATABASE_PORT"]) { "database port must be set if jdbc url is not provided" },
-            requireNotNull(env["DATABASE_DATABASE"]) { "database name must be set if jdbc url is not provided" })
-        username = requireNotNull(env["DATABASE_USERNAME"]) { "databasebrukernavn må settes" }
-        password = requireNotNull(env["DATABASE_PASSWORD"]) { "databasepassord må settes" }
-        maximumPoolSize = 1
+        jdbcUrl = String.format(
+            "jdbc:postgresql:///%s?%s&%s",
+            databaseName,
+            "cloudSqlInstance=$gcpProjectId:$databaseRegion:$databaseInstance",
+            "socketFactory=com.google.cloud.sql.postgres.SocketFactory"
+        )
+
+        databaseUsername?.let { this.username = it }
+        databasePassword?.let { this.password = it }
+
+        maximumPoolSize = 3
+        minimumIdle = 1
         connectionTimeout = Duration.ofSeconds(5).toMillis()
         maxLifetime = Duration.ofMinutes(30).toMillis()
+        idleTimeout = Duration.ofMinutes(10).toMillis()
     }
 
     internal fun getDataSource() = HikariDataSource(hikariConfig)
