e0559dc71ba0344cd100e3058b2d9bd678d1509a
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2022-03-15 15:10:05 +0100

    Slett person ved "slett_person"-melding

--- sykepenger-opprydding-dev/src/main/kotlin/no/nav/helse/SlettPersonRiver.kt
+++ sykepenger-opprydding-dev/src/main/kotlin/no/nav/helse/SlettPersonRiver.kt
@@ -4,14 +4,19 @@ import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
+import org.slf4j.LoggerFactory
 
 internal class SlettPersonRiver(
     rapidsConnection: RapidsConnection,
     private val personRepository: PersonRepository
 ): River.PacketListener {
+
+    private companion object {
+        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+    }
+
     init {
         River(rapidsConnection).apply {
-            rapidsConnection.register(this)
             validate {
                 it.demandValue("@event_name", "slett_person")
                 it.requireKey("@id", "fødselsnummer")
@@ -20,6 +25,8 @@ internal class SlettPersonRiver(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        TODO("Not yet implemented")
+        val fødselsnummer = packet["fødselsnummer"].asText()
+        sikkerlogg.info("Sletter person med fødselsnummer: $fødselsnummer")
+        personRepository.slett(fødselsnummer)
     }
 }
