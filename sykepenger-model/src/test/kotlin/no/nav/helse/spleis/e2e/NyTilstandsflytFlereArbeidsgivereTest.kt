5d34a02dc1bf6a017673b8ec39b47cc6a59e188a
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-03-22 15:02:56 +0100

    Ny flyt: Test at kun én vedtaksperiode av gangen gjenopptas
    Om vi har to arbeidsgivere med hver sin periode i
    AvventerTidligerEllerOverlappendePerioder, må vi sørge for at det er
    kun en vedtaksperiode som går videre når den tidligere perioden blir
    ferdig
    
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/NyTilstandsflytFlereArbeidsgivereTest.kt
@@ -0,0 +1,55 @@
+package no.nav.helse.spleis.e2e
+
+import no.nav.helse.Toggle
+import no.nav.helse.hendelser.Sykmeldingsperiode
+import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
+import no.nav.helse.mars
+import no.nav.helse.person.TilstandType.AVSLUTTET
+import no.nav.helse.person.TilstandType.AVVENTER_HISTORIKK
+import no.nav.helse.person.TilstandType.AVVENTER_TIDLIGERE_ELLER_OVERLAPPENDE_PERIODER
+import no.nav.helse.økonomi.Prosentdel.Companion.prosent
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+internal class NyTilstandsflytFlereArbeidsgivereTest : AbstractEndToEndTest() {
+    @BeforeEach
+    fun setup() {
+        Toggle.NyTilstandsflyt.enable()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Toggle.NyTilstandsflyt.disable()
+    }
+
+    @Disabled
+    @Test
+    fun `En periode i AvventerTidligerEllerOverlappendePerioder for hver arbeidsgiver - kun en periode skal gå videre`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 31.januar, 100.prosent), orgnummer = a1)
+        håndterSøknad(Sykdom(1.januar, 31.januar, 100.prosent), orgnummer = a1)
+
+        håndterSykmelding(Sykmeldingsperiode(1.mars, 31.mars, 100.prosent), orgnummer = a1)
+        håndterSøknad(Sykdom(1.mars, 31.mars, 100.prosent), orgnummer = a1)
+        håndterInntektsmelding(listOf(1.mars til 16.mars), orgnummer = a1)
+
+        håndterSykmelding(Sykmeldingsperiode(1.mars, 31.mars, 100.prosent), orgnummer = a2)
+        håndterSøknad(Sykdom(1.mars, 31.mars, 100.prosent), orgnummer = a2)
+        håndterInntektsmelding(listOf(1.mars til 16.mars), orgnummer = a2)
+
+        håndterInntektsmelding(listOf(1.januar til 16.januar), orgnummer = a1)
+        håndterYtelser(1.vedtaksperiode, orgnummer = a1)
+        håndterVilkårsgrunnlag(1.vedtaksperiode, orgnummer = a1)
+        håndterYtelser(1.vedtaksperiode, orgnummer = a1)
+        håndterSimulering(1.vedtaksperiode, orgnummer = a1)
+        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = a1)
+        håndterUtbetalt()
+
+        assertTilstand(1.vedtaksperiode, AVSLUTTET, orgnummer = a1)
+        assertTilstand(2.vedtaksperiode, AVVENTER_HISTORIKK, orgnummer = a1)
+        assertTilstand(1.vedtaksperiode, AVVENTER_TIDLIGERE_ELLER_OVERLAPPENDE_PERIODER, orgnummer = a2)
+    }
+}
