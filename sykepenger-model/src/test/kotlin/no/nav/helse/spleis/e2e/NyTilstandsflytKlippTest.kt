5c5a1e69d1eb4dd80664480381ec9a4e723bff19
Simen Ullern <simenullern@gmail.com>
2022-03-25 15:40:55 +0100

    Ny flyt: Grønn test for klipp happy case - starter inni slutter etter
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/NyTilstandsflytKlippTest.kt
@@ -0,0 +1,83 @@
+package no.nav.helse.spleis.e2e
+
+import no.nav.helse.Toggle
+import no.nav.helse.februar
+import no.nav.helse.hendelser.Sykmeldingsperiode
+import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
+import no.nav.helse.person.IdInnhenter
+import no.nav.helse.person.TilstandType.AVSLUTTET
+import no.nav.helse.person.TilstandType.AVVENTER_GODKJENNING
+import no.nav.helse.person.TilstandType.AVVENTER_HISTORIKK
+import no.nav.helse.person.TilstandType.AVVENTER_INNTEKTSMELDING_ELLER_HISTORIKK
+import no.nav.helse.person.TilstandType.AVVENTER_SIMULERING
+import no.nav.helse.person.TilstandType.AVVENTER_TIDLIGERE_ELLER_OVERLAPPENDE_PERIODER
+import no.nav.helse.person.TilstandType.AVVENTER_VILKÅRSPRØVING
+import no.nav.helse.person.TilstandType.START
+import no.nav.helse.person.TilstandType.TIL_UTBETALING
+import no.nav.helse.økonomi.Prosentdel.Companion.prosent
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class NyTilstandsflytKlippTest : AbstractEndToEndTest() {
+    @BeforeEach
+    fun setup() {
+        Toggle.NyTilstandsflyt.enable()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Toggle.NyTilstandsflyt.disable()
+    }
+
+    @Test
+    fun `drawio -- HAPPY CASE`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 31.januar, 100.prosent))
+        håndterSykmelding(Sykmeldingsperiode(20.januar, 15.februar, 100.prosent))
+        håndterSøknad(Sykdom(1.januar, 19.januar, 100.prosent))
+        håndterSøknad(Sykdom(20.januar, 15.februar, 100.prosent))
+        håndterInntektsmelding(listOf(1.januar til 16.januar))
+        utbetalPeriode(1.vedtaksperiode)
+        utbetalPeriodeEtterVilkårsprøving(2.vedtaksperiode)
+        assertTilstander(
+            1.vedtaksperiode,
+            START,
+            AVVENTER_INNTEKTSMELDING_ELLER_HISTORIKK,
+            AVVENTER_TIDLIGERE_ELLER_OVERLAPPENDE_PERIODER,
+            AVVENTER_HISTORIKK,
+            AVVENTER_VILKÅRSPRØVING,
+            AVVENTER_HISTORIKK,
+            AVVENTER_SIMULERING,
+            AVVENTER_GODKJENNING,
+            TIL_UTBETALING,
+            AVSLUTTET
+        )
+        assertTilstander(
+            2.vedtaksperiode,
+            START,
+            AVVENTER_INNTEKTSMELDING_ELLER_HISTORIKK,
+            AVVENTER_TIDLIGERE_ELLER_OVERLAPPENDE_PERIODER,
+            AVVENTER_HISTORIKK,
+            AVVENTER_SIMULERING,
+            AVVENTER_GODKJENNING,
+            TIL_UTBETALING,
+            AVSLUTTET
+        )
+
+    }
+
+    private fun utbetalPeriodeEtterVilkårsprøving(vedtaksperiode: IdInnhenter) {
+        håndterYtelser(vedtaksperiode)
+        håndterSimulering(vedtaksperiode)
+        håndterUtbetalingsgodkjenning(vedtaksperiode)
+        håndterUtbetalt()
+    }
+
+    private fun utbetalPeriode(vedtaksperiode: IdInnhenter) {
+        håndterYtelser(vedtaksperiode)
+        håndterVilkårsgrunnlag(vedtaksperiode)
+        utbetalPeriodeEtterVilkårsprøving(vedtaksperiode)
+    }
+}
