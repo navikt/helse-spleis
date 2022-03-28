540b410d6da96bd8502dcb501bc549eb1de0cb1e
Kevin Sillerud <kevin.sillerud@gmail.com>
2022-03-28 12:32:19 +0200

    Ny flyt: Enkel test for infotrygdforlengelse
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/NyTilstandsflytInfotrygdTest.kt
@@ -0,0 +1,50 @@
+package no.nav.helse.spleis.e2e
+
+import no.nav.helse.Toggle
+import no.nav.helse.februar
+import no.nav.helse.hendelser.Sykmeldingsperiode
+import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
+import no.nav.helse.januar
+import no.nav.helse.person.IdInnhenter
+import no.nav.helse.person.TilstandType.AVVENTER_HISTORIKK
+import no.nav.helse.person.infotrygdhistorikk.ArbeidsgiverUtbetalingsperiode
+import no.nav.helse.person.infotrygdhistorikk.Inntektsopplysning
+import no.nav.helse.økonomi.Prosentdel.Companion.prosent
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+internal class NyTilstandsflytInfotrygdTest : AbstractEndToEndTest() {
+    @BeforeEach
+    fun setup() {
+        Toggle.NyTilstandsflyt.enable()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Toggle.NyTilstandsflyt.disable()
+    }
+
+    @Disabled
+    @Test
+    fun `enkel infotrygdforlengelse`() {
+        håndterSykmelding(Sykmeldingsperiode(1.februar, 28.februar, 100.prosent))
+
+        håndterSøknad(Sykdom(1.februar, 28.februar, 100.prosent))
+        håndterUtbetalingshistorikk(
+            vedtaksperiodeIdInnhenter = 1.vedtaksperiode,
+            utbetalinger = arrayOf(ArbeidsgiverUtbetalingsperiode(ORGNUMMER, 1.januar, 31.januar, 100.prosent, INNTEKT)),
+            inntektshistorikk = listOf(Inntektsopplysning(ORGNUMMER, 1.januar, INNTEKT, true))
+        )
+        assertTilstand(1.vedtaksperiode, AVVENTER_HISTORIKK)
+        utbetalPeriode(1.vedtaksperiode)
+    }
+
+    private fun utbetalPeriode(vedtaksperiode: IdInnhenter) {
+        håndterYtelser(vedtaksperiode)
+        håndterSimulering(vedtaksperiode)
+        håndterUtbetalingsgodkjenning(vedtaksperiode)
+        håndterUtbetalt()
+    }
+}
