642b45dfde546b8261e8f34a4fb297383422c994
David Steinsland <david@steinsland.email>
2022-03-30 21:30:30 +0200

    bumper rapids and rivers

--- sykepenger-mediators/build.gradle.kts
+++ sykepenger-mediators/build.gradle.kts
@@ -1,7 +1,7 @@
 val mainClass = "no.nav.helse.AppKt"
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2022.03.28-20.54.89f957fff8f2")
+    implementation("com.github.navikt:rapids-and-rivers:2022.03.30-21.22.dbaa41649bd3")
     implementation(project(":sykepenger-model"))
     testImplementation("org.testcontainers:postgresql:1.16.2") {
         exclude("com.fasterxml.jackson.core")
