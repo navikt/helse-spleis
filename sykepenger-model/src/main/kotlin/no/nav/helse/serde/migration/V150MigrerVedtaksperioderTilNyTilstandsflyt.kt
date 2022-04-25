654be00e746f00b1fb14155093161017a2abb34b
Simen Ullern <simenullern@gmail.com>
2022-04-25 12:57:48 +0200

    Ny flyt: Logger migrering V150 til sikkerlogg
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>
    Co-authored-by: Erik M.Forsman <erik.maximilian.forsman@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V150MigrerVedtaksperioderTilNyTilstandsflyt.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V150MigrerVedtaksperioderTilNyTilstandsflyt.kt
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import java.util.UUID
+import net.logstash.logback.argument.StructuredArguments
 import no.nav.helse.Toggle
+import org.slf4j.LoggerFactory
 
 internal class V150MigrerVedtaksperioderTilNyTilstandsflyt : JsonMigration(version = 150) {
     override val description: String = "Migrerer vedtaksperiodetilstander til ny flyt"
+    private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
 
     init {
         require(Toggle.NyTilstandsflyt.enabled) {
@@ -27,7 +30,7 @@ internal class V150MigrerVedtaksperioderTilNyTilstandsflyt : JsonMigration(versi
         "AVVENTER_ARBEIDSGIVERE"
     )
 
-    private val tilstanderUtenMottatSøknad = arrayOf(
+    private val tilstanderUtenMottattSøknad = arrayOf(
         "AVVENTER_SØKNAD_UFERDIG_FORLENGELSE",
         "AVVENTER_SØKNAD_FERDIG_FORLENGELSE",
         "AVVENTER_SØKNAD_FERDIG_GAP",
@@ -45,51 +48,87 @@ internal class V150MigrerVedtaksperioderTilNyTilstandsflyt : JsonMigration(versi
         meldingerSupplier: MeldingerSupplier,
         observer: JsonMigrationObserver
     ) {
+        val fødselsnummer = jsonNode["fødselsnummer"].asText()
         jsonNode["arbeidsgivere"]
             .forEach { arbeidsgiverNode ->
                 val vedtaksperioder = arbeidsgiverNode["vedtaksperioder"] as ArrayNode
-                vedtaksperioder.slettIkkeMottattSøknadTilstander(observer)
-                vedtaksperioder.oppdaterTilstander()
+                vedtaksperioder.slettIkkeMottattSøknadTilstander(observer, fødselsnummer)
+                vedtaksperioder.oppdaterTilstander(fødselsnummer = fødselsnummer, forkastet = false)
 
                 val forkastedeVedtaksperioder = arbeidsgiverNode["forkastede"]
                     .map { forkastetVedtaksperiodeNode -> forkastetVedtaksperiodeNode["vedtaksperiode"] }
-                forkastedeVedtaksperioder.oppdaterTilstander()
-                forkastedeVedtaksperioder.oppdaterForkastedeIkkeMottattSøknadTilstander()
+                forkastedeVedtaksperioder.oppdaterTilstander(fødselsnummer = fødselsnummer, forkastet = true)
+                forkastedeVedtaksperioder.oppdaterForkastedeIkkeMottattSøknadTilstander(fødselsnummer)
             }
     }
 
-    private fun ArrayNode.slettIkkeMottattSøknadTilstander(observer: JsonMigrationObserver) {
+    private fun ArrayNode.slettIkkeMottattSøknadTilstander(observer: JsonMigrationObserver, fødselsnummer: String) {
         val iterator = iterator()
         while (iterator.hasNext()) {
             val vedtaksperiodeNode = iterator.next()
-            if (vedtaksperiodeNode["tilstand"].asText() in tilstanderUtenMottatSøknad) {
+            if (vedtaksperiodeNode.tilstand() in tilstanderUtenMottattSøknad) {
                 iterator.remove()
-                observer.vedtaksperiodeSlettet(UUID.fromString(vedtaksperiodeNode["id"].asText()), vedtaksperiodeNode)
+                val vedtaksperiodeId = vedtaksperiodeNode.vedtaksperiodeId()
+                val gammelTilstand = vedtaksperiodeNode.tilstand()
+                sikkerLogg.info(
+                    "Slettet vedtaksperiode med {}, {}, {}",
+                    StructuredArguments.keyValue("fodselsnummer", fødselsnummer),
+                    StructuredArguments.keyValue("vedtaksperiodeId", vedtaksperiodeId),
+                    StructuredArguments.keyValue("gammelTilstand", gammelTilstand)
+                )
+                observer.vedtaksperiodeSlettet(UUID.fromString(vedtaksperiodeId), vedtaksperiodeNode)
             }
         }
     }
 
-    private fun Iterable<JsonNode>.oppdaterTilstander() = forEach { vedtaksperiodeNode ->
+    private fun Iterable<JsonNode>.oppdaterTilstander(
+        fødselsnummer: String,
+        forkastet: Boolean
+    ) = forEach { vedtaksperiodeNode ->
         val vedtaksperiode = vedtaksperiodeNode as ObjectNode
-        when (vedtaksperiodeNode["tilstand"].asText()) {
-            in inntektsmeldingTilstander -> {
-                vedtaksperiode.put("tilstand", "AVVENTER_INNTEKTSMELDING_ELLER_HISTORIKK")
-            }
-            in avventerBlokkerendeTilstander -> {
-                vedtaksperiode.put("tilstand", "AVVENTER_BLOKKERENDE_PERIODE")
-            }
+        val gammelTilstand = vedtaksperiodeNode.tilstand()
+        val vedtaksperiodeId = vedtaksperiodeNode.vedtaksperiodeId()
+
+        val nyTilstand = when (gammelTilstand) {
+            in inntektsmeldingTilstander -> "AVVENTER_INNTEKTSMELDING_ELLER_HISTORIKK"
+            in avventerBlokkerendeTilstander -> "AVVENTER_BLOKKERENDE_PERIODE"
+            else -> return@forEach
         }
+        sikkerLogg.info(
+            "Migrerte vedtaksperiode med {}, {}, {}, {}, {}",
+            StructuredArguments.keyValue("fodselsnummer", fødselsnummer),
+            StructuredArguments.keyValue("vedtaksperiodeId", vedtaksperiodeId),
+            StructuredArguments.keyValue("gammelTilstand", gammelTilstand),
+            StructuredArguments.keyValue("nyTilstand", nyTilstand),
+            StructuredArguments.keyValue("forkastet", forkastet)
+        )
+        vedtaksperiode.put("tilstand", nyTilstand)
     }
 
-    private fun Iterable<JsonNode>.oppdaterForkastedeIkkeMottattSøknadTilstander() = forEach { vedtaksperiodeNode ->
-        val vedtaksperiode = vedtaksperiodeNode as ObjectNode
-        val tilstand = vedtaksperiodeNode["tilstand"].asText()
-        if (tilstand in tilstanderUtenMottatSøknad) {
-            /*
-            For forkastede perioder ønsker vi å sette tilstanden til TIL_INFOTRYGD i tilfelle det finnes gamle
-            perioder som ikke fikk denne tilstanden da de ble forkastet.
-            */
-            vedtaksperiode.put("tilstand", "TIL_INFOTRYGD")
+    private fun Iterable<JsonNode>.oppdaterForkastedeIkkeMottattSøknadTilstander(fødselsnummer: String) =
+        forEach { vedtaksperiodeNode ->
+            val vedtaksperiode = vedtaksperiodeNode as ObjectNode
+            val tilstand = vedtaksperiodeNode.tilstand()
+            val vedtaksperiodeId = vedtaksperiodeNode.vedtaksperiodeId()
+
+            if (tilstand in tilstanderUtenMottattSøknad) {
+                /*
+                For forkastede perioder ønsker vi å sette tilstanden til TIL_INFOTRYGD i tilfelle det finnes gamle
+                perioder som ikke fikk denne tilstanden da de ble forkastet.
+                */
+                vedtaksperiode.put("tilstand", "TIL_INFOTRYGD")
+                sikkerLogg.info(
+                    "Migrerte vedtaksperiode med {}, {}, {}, {}, {}",
+                    StructuredArguments.keyValue("fodselsnummer", fødselsnummer),
+                    StructuredArguments.keyValue("vedtaksperiodeId", vedtaksperiodeId),
+                    StructuredArguments.keyValue("gammelTilstand", tilstand),
+                    StructuredArguments.keyValue("nyTilstand", "TIL_INFOTRYGD"),
+                    StructuredArguments.keyValue("forkastet", true)
+                )
+            }
         }
-    }
+
+    private fun JsonNode.tilstand() = this["tilstand"].asText()
+    private fun JsonNode.vedtaksperiodeId() = this["id"].asText()
+
 }
