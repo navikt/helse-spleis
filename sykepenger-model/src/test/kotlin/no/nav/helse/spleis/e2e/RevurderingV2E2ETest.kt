42e1e9798507c2c549899e1935d7ca075671b29c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-04-28 14:43:17 +0200

    tester at avventer uferdig går frem
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/RevurderingV2E2ETest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/RevurderingV2E2ETest.kt
@@ -541,19 +541,16 @@ internal class RevurderingV2E2ETest : AbstractEndToEndTest() {
         nyttVedtak(1.januar, 31.januar)
         tilSimulering(1.mars, 31.mars, 100.prosent, 1.mars)
 
-        nullstillTilstandsendringer()
         håndterOverstyrTidslinje(listOf(ManuellOverskrivingDag(17.januar, Feriedag)))
 
-        assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_HISTORIKK_REVURDERING)
-        assertForventetFeil(
-            forklaring = "Skal bli grønn når gjenopptaBehandlingEtter-branchen er rebaset inn",
-            ønsket = {
-                assertTilstander(2.vedtaksperiode, AVVENTER_SIMULERING, AVVENTER_BLOKKERENDE_PERIODE)
-            },
-            nå = {
-                assertTilstander(2.vedtaksperiode, AVVENTER_SIMULERING, AVVENTER_UFERDIG)
-            }
-        )
+        håndterYtelser(1.vedtaksperiode)
+        håndterSimulering(1.vedtaksperiode)
+        håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+        nullstillTilstandsendringer()
+        håndterUtbetalt()
+
+        assertTilstander(1.vedtaksperiode, TIL_UTBETALING, AVSLUTTET)
+        assertTilstander(2.vedtaksperiode, AVVENTER_UFERDIG, AVVENTER_HISTORIKK)
     }
 
     @Test
