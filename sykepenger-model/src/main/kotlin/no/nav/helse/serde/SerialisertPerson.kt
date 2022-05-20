5b4be078fba532a57f7f402e417af457f86f8f9a
David Steinsland <david@steinsland.email>
2022-05-20 11:41:41 +0200

    runder av grader på utbetalingstidslinjen
    det har oppstått rare grader pga. unøyaktigheter med doubles, og kilden (søknad, sykmelding, overstyr_tidslinje) oppgir grad som Ints uansett.
    
    dry run har vist oss at det finnes bare grader som er av typen
    57.00000000000001, 33.999999999999998, osv.
    
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/SerialisertPerson.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/SerialisertPerson.kt
@@ -80,6 +80,7 @@ import no.nav.helse.serde.migration.V158LeggerTilPersonoppdragForFeriepenger
 import no.nav.helse.serde.migration.V159None
 import no.nav.helse.serde.migration.V15ØkonomiSykdomstidslinjer
 import no.nav.helse.serde.migration.V160FikserDoubleGrad
+import no.nav.helse.serde.migration.V161FikserDoubleGrad
 import no.nav.helse.serde.migration.V16StatusIUtbetaling
 import no.nav.helse.serde.migration.V17ForkastedePerioder
 import no.nav.helse.serde.migration.V18UtbetalingstidslinjeØkonomi
@@ -344,7 +345,8 @@ class SerialisertPerson(val json: String) {
             V157ManglerDagerPåSykdomstidslinjenDryRun(),
             V158LeggerTilPersonoppdragForFeriepenger(),
             V159None(),
-            V160FikserDoubleGrad()
+            V160FikserDoubleGrad(),
+            V161FikserDoubleGrad()
         )
 
         fun gjeldendeVersjon() = JsonMigration.gjeldendeVersjon(migrations)
