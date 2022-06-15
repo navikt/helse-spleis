b9053a3ac09df2c5c310cf8d5729e25fb9ed7086
David Steinsland <david@steinsland.email>
2022-06-15 09:44:08 +0200

    feilende test for manglende inntekt

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/InfotrygdInntektE2ETest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/InfotrygdInntektE2ETest.kt
@@ -9,6 +9,8 @@ import no.nav.helse.hendelser.til
 import no.nav.helse.inspectors.inspektør
 import no.nav.helse.januar
 import no.nav.helse.mars
+import no.nav.helse.person.TilstandType.AVVENTER_GODKJENNING
+import no.nav.helse.person.TilstandType.AVVENTER_HISTORIKK
 import no.nav.helse.person.TilstandType.AVVENTER_SIMULERING
 import no.nav.helse.person.TilstandType.TIL_INFOTRYGD
 import no.nav.helse.person.infotrygdhistorikk.ArbeidsgiverUtbetalingsperiode
@@ -20,6 +22,72 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class InfotrygdInntektE2ETest : AbstractEndToEndTest() {
+
+    @Test
+    fun `krever ikke inntekt for Infotrygdperioder`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 16.januar, 100.prosent))
+        håndterSøknad(Sykdom(1.januar, 16.januar, 100.prosent))
+        håndterUtbetalingshistorikk(1.vedtaksperiode)
+        håndterSykmelding(Sykmeldingsperiode(10.februar, 28.februar, 100.prosent))
+        håndterSøknad(Sykdom(10.februar, 28.februar, 100.prosent))
+        håndterUtbetalingshistorikk(2.vedtaksperiode, ArbeidsgiverUtbetalingsperiode(ORGNUMMER, 17.januar, 31.januar, 100.prosent, INNTEKT), inntektshistorikk = listOf(
+            Inntektsopplysning(ORGNUMMER, 17.januar, INNTEKT, true)
+        ))
+        håndterInntektsmelding(listOf(1.januar til 16.januar), førsteFraværsdag = 10.februar)
+        håndterYtelser(2.vedtaksperiode)
+        håndterVilkårsgrunnlag(2.vedtaksperiode)
+        håndterYtelser(2.vedtaksperiode)
+        assertSisteTilstand(2.vedtaksperiode, AVVENTER_SIMULERING)
+    }
+
+    @Test
+    fun `krever ikke inntekt for Infotrygdperioder utbetalt etter vilkårsprøving`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 16.januar, 100.prosent))
+        håndterSøknad(Sykdom(1.januar, 16.januar, 100.prosent))
+        håndterUtbetalingshistorikk(1.vedtaksperiode)
+        håndterSykmelding(Sykmeldingsperiode(10.februar, 28.februar, 100.prosent))
+        håndterSøknad(Sykdom(10.februar, 28.februar, 100.prosent))
+        håndterInntektsmelding(listOf(1.januar til 16.januar), førsteFraværsdag = 10.februar)
+        håndterYtelser(2.vedtaksperiode)
+        håndterVilkårsgrunnlag(2.vedtaksperiode)
+        håndterYtelser(2.vedtaksperiode)
+        håndterSimulering(2.vedtaksperiode)
+        assertSisteTilstand(2.vedtaksperiode, AVVENTER_GODKJENNING)
+        håndterUtbetalingshistorikk(2.vedtaksperiode, ArbeidsgiverUtbetalingsperiode(ORGNUMMER, 17.januar, 31.januar, 100.prosent, INNTEKT), inntektshistorikk = listOf(
+            Inntektsopplysning(ORGNUMMER, 17.januar, INNTEKT, true)
+        ))
+        assertSisteTilstand(2.vedtaksperiode, AVVENTER_HISTORIKK)
+        håndterYtelser(2.vedtaksperiode)
+        assertSisteTilstand(2.vedtaksperiode, AVVENTER_SIMULERING)
+    }
+
+    @Test
+    fun `krever ikke inntekt for Infotrygdperioder utbetalt etter utbetaling i spleis`() {
+        håndterSykmelding(Sykmeldingsperiode(1.januar, 16.januar, 100.prosent))
+        håndterSøknad(Sykdom(1.januar, 16.januar, 100.prosent))
+        håndterUtbetalingshistorikk(1.vedtaksperiode)
+        håndterSykmelding(Sykmeldingsperiode(10.februar, 28.februar, 100.prosent))
+        håndterSøknad(Sykdom(10.februar, 28.februar, 100.prosent))
+        håndterInntektsmelding(listOf(1.januar til 16.januar), førsteFraværsdag = 10.februar)
+        håndterYtelser(2.vedtaksperiode)
+        håndterVilkårsgrunnlag(2.vedtaksperiode)
+        håndterYtelser(2.vedtaksperiode)
+        håndterSimulering(2.vedtaksperiode)
+        håndterUtbetalingsgodkjenning(2.vedtaksperiode)
+        håndterUtbetalt()
+
+        håndterSykmelding(Sykmeldingsperiode(1.mars, 10.mars, 100.prosent))
+        håndterSøknad(Sykdom(1.mars, 10.mars, 100.prosent))
+        håndterUtbetalingshistorikk(3.vedtaksperiode, ArbeidsgiverUtbetalingsperiode(ORGNUMMER, 17.januar, 31.januar, 100.prosent, INNTEKT), inntektshistorikk = listOf(
+            Inntektsopplysning(ORGNUMMER, 17.januar, INNTEKT, true)
+        ))
+        håndterYtelser(3.vedtaksperiode)
+        assertForventetFeil(
+            forklaring = "behøver ikke kreve inntekt for infotrygdperioder",
+            nå = { assertSisteTilstand(3.vedtaksperiode, TIL_INFOTRYGD) },
+            ønsket = { assertSisteTilstand(3.vedtaksperiode, AVVENTER_SIMULERING) }
+        )
+    }
 
     @Test
     fun `bruker laveste registrerte inntekt i infotrygd om det er fler inntekter samme dato for samme arbeidsgiver`() {
