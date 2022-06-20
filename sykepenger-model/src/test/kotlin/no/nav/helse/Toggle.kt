aa229e98fdbe022d17eee6d8d75351a5d877b639
Hege Haavaldsen <hege.haavaldsen@nav.no>
2022-06-20 10:12:17 +0200

    Lager annotation for å disable toggle for en hel klasse
    Co-authored-by: Christian Bülow Skovborg <Christian.Bulow.Skovborg@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/Toggle.kt
@@ -1,6 +1,7 @@
 package no.nav.helse
 
 import java.lang.reflect.Method
+import no.nav.helse.Toggle.Companion.disable
 import no.nav.helse.Toggle.Companion.enable
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -11,10 +12,10 @@ import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@ExtendWith(ToggleInterceptor::class)
+@ExtendWith(EnableToggleInterceptor::class)
 internal annotation class EnableToggle(vararg val toggle: KClass<out Toggle>)
 
-private class ToggleInterceptor: InvocationInterceptor {
+private class EnableToggleInterceptor: InvocationInterceptor {
     override fun interceptTestMethod(
         invocation: InvocationInterceptor.Invocation<Void>,
         invocationContext: ReflectiveInvocationContext<Method>,
@@ -22,6 +23,25 @@ private class ToggleInterceptor: InvocationInterceptor {
     ) {
         AnnotationSupport.findAnnotation(extensionContext.testClass, EnableToggle::class.java).ifPresentOrElse({ annotation ->
             annotation.toggle.mapNotNull { it.objectInstance }.enable { invocation.proceed() }
+        }) {
+            invocation.proceed()
+        }
+    }
+}
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(DisableToggleInterceptor::class)
+internal annotation class DisableToggle(vararg val toggle: KClass<out Toggle>)
+
+private class DisableToggleInterceptor: InvocationInterceptor {
+    override fun interceptTestMethod(
+        invocation: InvocationInterceptor.Invocation<Void>,
+        invocationContext: ReflectiveInvocationContext<Method>,
+        extensionContext: ExtensionContext
+    ) {
+        AnnotationSupport.findAnnotation(extensionContext.testClass, DisableToggle::class.java).ifPresentOrElse({ annotation ->
+            annotation.toggle.mapNotNull { it.objectInstance }.disable { invocation.proceed() }
         }) {
             invocation.proceed()
         }
