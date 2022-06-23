ff6090823152fd31ed5cf1034065bacfa470fa3d
Simen Ullern <simenullern@gmail.com>
2022-06-23 22:14:21 +0200

    Revert "Gjenoppliv midleritdig påminnelse-kode som løsnet stuck perioder"
    This reverts commit 32a06f1d002aa2fc2a5f859d220b9e416348fe9a.

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Vedtaksperiode.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Vedtaksperiode.kt
@@ -1453,7 +1453,6 @@ internal class Vedtaksperiode private constructor(
         }
 
         override fun håndter(vedtaksperiode: Vedtaksperiode, påminnelse: Påminnelse) {
-            vedtaksperiode.arbeidsgiver.beholdSykmeldingsperioderEtter(vedtaksperiode.periode.endInclusive)
             vedtaksperiode.trengerHistorikkFraInfotrygd(påminnelse)
         }
     }
@@ -1476,7 +1475,6 @@ internal class Vedtaksperiode private constructor(
         }
 
         override fun håndter(vedtaksperiode: Vedtaksperiode, påminnelse: Påminnelse) {
-            vedtaksperiode.arbeidsgiver.beholdSykmeldingsperioderEtter(vedtaksperiode.periode.endInclusive)
             vedtaksperiode.person.gjenopptaBehandlingNy(påminnelse)
             if (vedtaksperiode.arbeidsgiver.harSykmeldingsperiodeFør(vedtaksperiode.periode.endInclusive.plusDays(1))) {
                 sikkerlogg.warn("Har sykmeldingsperiode før eller lik tom. VedtaksperiodeId=${vedtaksperiode.id}, aktørId=${påminnelse.aktørId()}")
