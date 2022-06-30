2e278514fe15da24365c45da80641595867a53bc
Simen Ullern <simenullern@gmail.com>
2022-06-30 13:19:54 +0200

    Tester filtre på utbetalinger ved revurdering
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/RevurderingUtbetalingerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/RevurderingUtbetalingerTest.kt
@@ -1,24 +1,24 @@
 package no.nav.helse.spleis.e2e
 
-import java.time.LocalDate
 import no.nav.helse.EnableToggle
 import no.nav.helse.Toggle
 import no.nav.helse.februar
 import no.nav.helse.hendelser.Dagtype.Feriedag
+import no.nav.helse.hendelser.Dagtype.Sykedag
 import no.nav.helse.hendelser.ManuellOverskrivingDag
 import no.nav.helse.inspectors.inspektør
 import no.nav.helse.januar
 import no.nav.helse.person.Vedtaksperiode.Companion.RevurderingUtbetalinger
 import no.nav.helse.person.infotrygdhistorikk.Infotrygdhistorikk
-import no.nav.helse.sykdomstidslinje.Dag
+import no.nav.helse.testhelpers.AP
 import no.nav.helse.testhelpers.NAV
 import no.nav.helse.testhelpers.tidslinjeOf
-import no.nav.helse.utbetalingstidslinje.Utbetalingstidslinje
-import no.nav.helse.økonomi.Inntekt
+import no.nav.helse.utbetalingstidslinje.Sykdomsgradfilter
+import no.nav.helse.utbetalingstidslinje.Utbetalingstidslinje.Utbetalingsdag.AvvistDag
 import no.nav.helse.økonomi.Inntekt.Companion.INGEN
-import no.nav.helse.økonomi.Inntekt.Companion.daglig
 import no.nav.helse.økonomi.Inntekt.Companion.månedlig
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -88,15 +88,79 @@ internal class RevurderingUtbetalingerTest: AbstractEndToEndTest() {
         assertEquals(20000.månedlig, tidslinjea2[1.februar].økonomi.inspektør.arbeidsgiverRefusjonsbeløp)
     }
 
-    private inline fun <reified D: Dag, reified UD: Utbetalingstidslinje.Utbetalingsdag>assertDag(dato: LocalDate, beløp: Double) {
-        inspektør.sykdomshistorikk.sykdomstidslinje()[dato].let {
-            assertTrue(it is D) { "Forventet ${D::class.simpleName} men var ${it::class.simpleName}" }
-        }
-        inspektør.sisteUtbetalingUtbetalingstidslinje()[dato].let {
-            assertTrue(it is UD) { "Forventet ${UD::class.simpleName} men var ${it::class.simpleName}" }
-            assertEquals(beløp.daglig, it.økonomi.inspektør.arbeidsgiverbeløp)
-            assertEquals(Inntekt.INGEN, it.økonomi.inspektør.personbeløp)
-        }
+    @Test
+    fun `filter på en førstegangsbehandling`() {
+        // Gjøres kun for å få en vedtaksperiode i riktig tilstand (AvventerHistorikkRevurdering)
+        nyttVedtak(1.januar, 31.januar)
+        val overstyring = håndterOverstyrTidslinje(listOf(ManuellOverskrivingDag(17.januar, Sykedag, 10)))
+
+        val utbetalinger = RevurderingUtbetalinger(listOf(inspektør.vedtaksperioder(1.vedtaksperiode)), 1.januar, ORGNUMMER, overstyring)
+        val tidslinje = tidslinjeOf(16.AP, 1.NAV(INGEN, 10.0), 14.NAV(INGEN))
+
+        assertFalse(tidslinje[17.januar] is AvvistDag)
+        utbetalinger.filtrer(listOf(Sykdomsgradfilter), mapOf(inspektør.arbeidsgiver to tidslinje))
+        assertTrue(tidslinje[17.januar] is AvvistDag)
+    }
+
+    @Test
+    fun `filter på en forlengelse`() {
+        // Gjøres kun for å få en vedtaksperiode i riktig tilstand (AvventerHistorikkRevurdering)
+        nyttVedtak(1.januar, 31.januar)
+        forlengVedtak(1.februar, 28.februar)
+        val overstyring = håndterOverstyrTidslinje(
+            listOf(
+                ManuellOverskrivingDag(17.januar, Sykedag, 10),
+                ManuellOverskrivingDag(1.februar, Sykedag, 10)
+            )
+        )
+
+        val utbetalinger = RevurderingUtbetalinger(
+            listOf(
+                inspektør.vedtaksperioder(1.vedtaksperiode),
+                inspektør.vedtaksperioder(2.vedtaksperiode)
+            ), 1.januar, ORGNUMMER, overstyring
+        )
+        val tidslinje = tidslinjeOf(16.AP, 1.NAV(INGEN, 10.0), 14.NAV(INGEN), 1.NAV(INGEN, 10.0), 27.NAV(INGEN))
+
+        assertFalse(tidslinje[17.januar] is AvvistDag)
+        assertFalse(tidslinje[1.februar] is AvvistDag)
+        utbetalinger.filtrer(listOf(Sykdomsgradfilter), mapOf(inspektør.arbeidsgiver to tidslinje))
+        assertTrue(tidslinje[17.januar] is AvvistDag)
+        assertTrue(tidslinje[1.februar] is AvvistDag)
+    }
+
+    @Test
+    fun `filter på flere ag`() {
+        nyeVedtak(1.januar, 31.januar, a1, a2, inntekt = 20000.månedlig)
+        forlengVedtak(1.februar, 28.februar, a1, a2)
+        val overstyring = håndterOverstyrTidslinje(
+            listOf(
+                ManuellOverskrivingDag(17.januar, Sykedag, 10),
+                ManuellOverskrivingDag(1.februar, Sykedag, 10)
+            ),
+            a1
+        )
+
+        val utbetalinger = RevurderingUtbetalinger(
+            listOf(
+                inspektør(a1).vedtaksperioder(1.vedtaksperiode),
+                inspektør(a1).vedtaksperioder(2.vedtaksperiode),
+                inspektør(a2).vedtaksperioder(1.vedtaksperiode),
+                inspektør(a2).vedtaksperioder(2.vedtaksperiode)
+            ), 1.januar, ORGNUMMER, overstyring
+        )
+        val tidslinjea1 = tidslinjeOf(16.AP, 1.NAV(INGEN, 10.0), 14.NAV(INGEN), 1.NAV(INGEN, 10.0), 27.NAV(INGEN))
+        val tidslinjea2 = tidslinjeOf(16.AP, 1.NAV(INGEN, 10.0), 14.NAV(INGEN), 1.NAV(INGEN, 10.0), 27.NAV(INGEN))
+
+        assertFalse(tidslinjea1[17.januar] is AvvistDag)
+        assertFalse(tidslinjea1[1.februar] is AvvistDag)
+        assertFalse(tidslinjea2[17.januar] is AvvistDag)
+        assertFalse(tidslinjea2[1.februar] is AvvistDag)
+        utbetalinger.filtrer(listOf(Sykdomsgradfilter), mapOf(inspektør(a1).arbeidsgiver to tidslinjea1, inspektør(a2).arbeidsgiver to tidslinjea2))
+        assertTrue(tidslinjea1[17.januar] is AvvistDag)
+        assertTrue(tidslinjea1[1.februar] is AvvistDag)
+        assertTrue(tidslinjea2[17.januar] is AvvistDag)
+        assertTrue(tidslinjea2[1.februar] is AvvistDag)
     }
 
 }
