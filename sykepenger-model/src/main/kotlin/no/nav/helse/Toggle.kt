d9e14d8e1226d245e4043db065cde82ec651dbcf
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-09-09 13:48:59 +0200

    Revert "Må kjøre gjennom 256 som ikke ble med på sist kjøring :car:"
    This reverts commit b88180d15d44e5dd0f1780abe5aabbba1f607200.

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -124,7 +124,7 @@ abstract class Toggle internal constructor(enabled: Boolean = false, private val
 
     object ForkasteVilkårsgrunnlag : Toggle("ForkasteVilkarsgrunnlag", false)
     object GraphQLPlayground : Toggle("GraphQLPlayground", false)
-    object SendFeriepengeOppdrag : Toggle(true)
+    object SendFeriepengeOppdrag : Toggle(false)
     object RevurdereInntektMedFlereArbeidsgivere : Toggle("RevurdereInntektMedFlereArbeidsgivere", false)
     object RevurderKorrigertSoknad : Toggle("RevurderKorrigertSoknad", true)
     object RevurderOutOfOrder : Toggle("RevurderOutOfOrder", false)
