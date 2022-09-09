b88180d15d44e5dd0f1780abe5aabbba1f607200
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-09-09 13:34:40 +0200

    Må kjøre gjennom 256 som ikke ble med på sist kjøring :car:

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -124,7 +124,7 @@ abstract class Toggle internal constructor(enabled: Boolean = false, private val
 
     object ForkasteVilkårsgrunnlag : Toggle("ForkasteVilkarsgrunnlag", false)
     object GraphQLPlayground : Toggle("GraphQLPlayground", false)
-    object SendFeriepengeOppdrag : Toggle(false)
+    object SendFeriepengeOppdrag : Toggle(true)
     object RevurdereInntektMedFlereArbeidsgivere : Toggle("RevurdereInntektMedFlereArbeidsgivere", false)
     object RevurderKorrigertSoknad : Toggle("RevurderKorrigertSoknad", true)
     object RevurderOutOfOrder : Toggle("RevurderOutOfOrder", false)
