08991fe56bc61c8c1bcbde637d9e256ed6ea9fdf
Helene Arnesen <helene.arnesen@nav.no>
2022-09-21 12:45:38 +0200

    Hensyntar helg når vi finner sykefraværstilfeller
    Tilbyr mulighet til å finne sykefraværstilfeller fra person-json
    
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/Sykefraværstilfeller.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/Sykefraværstilfeller.kt
@@ -1,8 +1,9 @@
 package no.nav.helse.serde.migration
 
+import com.fasterxml.jackson.databind.JsonNode
 import java.time.LocalDate
 import no.nav.helse.hendelser.Periode
-import no.nav.helse.hendelser.Periode.Companion.grupperSammenhengendePerioder
+import no.nav.helse.hendelser.Periode.Companion.grupperSammenhengendePerioderMedHensynTilHelg
 import no.nav.helse.hendelser.til
 
 /*
@@ -11,13 +12,27 @@ import no.nav.helse.hendelser.til
 * for å ikke snevre inn søket for mye.
 * */
 internal object Sykefraværstilfeller {
-    internal fun sykefraværstilfeller(vedtaksperioder: List<Vedtaksperiode>): List<Periode>{
-        val sammenhengendePerioder = vedtaksperioder.map { it.periode }.grupperSammenhengendePerioder()
+
+    private val JsonNode.dato get() = LocalDate.parse(asText())
+    private val JsonNode.skjæringstidspunkt get() = path("skjæringstidspunkt").dato
+    private val JsonNode.fom get() = path("fom").dato
+    private val JsonNode.tom get() = path("tom").dato
+    private val JsonNode.tilstand get() = path("tilstand").asText()
+
+    internal fun sykefraværstilfeller(person: JsonNode): Set<Periode> {
+        val aktiveVedtaksperioder = person.path("arbeidsgivere")
+            .flatMap { it.path("vedtaksperioder") }
+            .filterNot { it.tilstand == "AVSLUTTET_UTEN_UTBETALING" }
+
+        return sykefraværstilfeller(aktiveVedtaksperioder.map { Vedtaksperiode(it.skjæringstidspunkt, it.fom til it.tom) })
+    }
+
+    internal fun sykefraværstilfeller(vedtaksperioder: List<Vedtaksperiode>): Set<Periode>{
+        val sammenhengendePerioder = vedtaksperioder.map { it.periode }.grupperSammenhengendePerioderMedHensynTilHelg()
         return sammenhengendePerioder.map { sammenhengendePeriode ->
-            val tidligsteSkjæringstidspunkt =  vedtaksperioder.tidligsteSkjæringstidspunktFor(sammenhengendePeriode)
-            val sisteTom = sammenhengendePerioder.maxOf { it.endInclusive }
-            tidligsteSkjæringstidspunkt til sisteTom
-        }
+            val tidligsteSkjæringstidspunkt = vedtaksperioder.tidligsteSkjæringstidspunktFor(sammenhengendePeriode)
+            tidligsteSkjæringstidspunkt til sammenhengendePeriode.endInclusive
+        }.toSet()
     }
 
     private fun List<Vedtaksperiode>.tidligsteSkjæringstidspunktFor(periode: Periode) =
--- sykepenger-model/src/test/kotlin/no/nav/helse/serde/migration/SykefraværstilfellerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/serde/migration/SykefraværstilfellerTest.kt
@@ -5,24 +5,88 @@ import no.nav.helse.februar
 import no.nav.helse.hendelser.til
 import no.nav.helse.januar
 import no.nav.helse.mars
+import no.nav.helse.serde.migration.Sykefraværstilfeller.Vedtaksperiode
 import no.nav.helse.serde.migration.Sykefraværstilfeller.sykefraværstilfeller
+import no.nav.helse.serde.serdeObjectMapper
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class SykefraværstilfellerTest {
 
     @Test
-    fun `sammenhengende periode hvor tidligste skjæringstidspunkt er før perioden`(){
+    fun `sammenhengende periode hvor tidligste skjæringstidspunkt er før perioden`() {
         val vedtaksperioder = listOf(
-            Sykefraværstilfeller.Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 1.januar til 31.januar),
-            Sykefraværstilfeller.Vedtaksperiode(skjæringstidspunkt = 15.desember(2017), periode = 1.februar til 28.februar),
-            Sykefraværstilfeller.Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 1.mars til 31.mars)
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 1.januar til 31.januar),
+            Vedtaksperiode(skjæringstidspunkt = 15.desember(2017), periode = 1.februar til 28.februar),
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 1.mars til 31.mars)
         )
-        assertEquals(listOf(15.desember(2017) til 31.mars), sykefraværstilfeller(vedtaksperioder))
+        assertEquals(setOf(15.desember(2017) til 31.mars), sykefraværstilfeller(vedtaksperioder))
     }
 
     @Test
-    fun `perioder med helg mellom, men i samme sykefraværstilfellet hvor tidligste skjæringstidspunkt er før perioden`(){
+    fun `fjerner duplikate sykefraværstilfeller`() {
+        val vedtaksperioder = listOf(
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 1.januar til 5.januar),
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 8.januar til 31.januar),
+        )
+        assertEquals(setOf(1.januar til 31.januar), sykefraværstilfeller(vedtaksperioder))
+    }
 
+    @Test
+    fun `perioder med helg mellom, men i samme sykefraværstilfellet hvor tidligste skjæringstidspunkt er før perioden`() {
+        val vedtaksperioder = listOf(
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 1.januar til 5.januar),
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 8.januar til 12.januar),
+            Vedtaksperiode(skjæringstidspunkt = 1.januar, periode = 15.januar til 19.januar),
+        )
+        assertEquals(setOf(1.januar til 19.januar), sykefraværstilfeller(vedtaksperioder))
+    }
+
+    @Test
+    fun `finner sykefraværstilfeller fra aktive vedtaksperioder i person-json`() {
+        assertEquals(setOf(1.januar til 31.mars), sykefraværstilfeller(serdeObjectMapper.readTree(json)))
     }
 }
+
+@Language("Json")
+private val json = """{
+  "arbeidsgivere": [
+    {
+      "vedtaksperioder": [
+        {
+          "fom": "2017-12-15",
+          "tom": "2017-12-31",
+          "tilstand": "AVSLUTTET_UTEN_UTBETALING",
+          "skjæringstidspunkt": "2017-12-15"
+        },
+        {
+          "fom": "2018-01-01",
+          "tom": "2018-01-31",
+          "tilstand": "AVSLUTTET",
+          "skjæringstidspunkt": "2018-01-01"
+        }
+      ]
+    },
+    {
+      "vedtaksperioder": [
+        {
+          "fom": "2018-02-01",
+          "tom": "2018-02-28",
+          "tilstand": "AVSLUTTET",
+          "skjæringstidspunkt": "2018-01-01"
+        }
+      ]
+    },
+    {
+      "vedtaksperioder": [
+        {
+          "fom": "2018-03-01",
+          "tom": "2018-03-31",
+          "tilstand": "AVSLUTTET",
+          "skjæringstidspunkt": "2018-01-01"
+        }
+      ]
+    }
+  ]
+}"""
