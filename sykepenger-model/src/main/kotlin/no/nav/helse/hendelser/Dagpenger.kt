cf45bcb790bc5e29217c9c7721585089cee88fcb
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2022-09-27 13:26:46 +0200

    Ta i bruk varselkode RV_AY_4
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Dagpenger.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Dagpenger.kt
@@ -1,13 +1,14 @@
 package no.nav.helse.hendelser
 
+import java.time.LocalDate
 import no.nav.helse.hendelser.Periode.Companion.slutterEtter
 import no.nav.helse.person.IAktivitetslogg
-import java.time.LocalDate
+import no.nav.helse.person.Varselkode.RV_AY_4
 
 class Dagpenger(private val perioder: List<Periode>) {
     internal fun valider(aktivitetslogg: IAktivitetslogg, skjæringstidspunkt: LocalDate): IAktivitetslogg {
         if (perioder.slutterEtter(skjæringstidspunkt.minusWeeks(4))) {
-            aktivitetslogg.varsel("Bruker har mottatt dagpenger innenfor 4 uker før skjæringstidspunktet. Kontroller om bruker er dagpengemottaker. Kombinerte ytelser støttes foreløpig ikke av systemet")
+            aktivitetslogg.varsel(RV_AY_4)
         }
         return aktivitetslogg
     }
