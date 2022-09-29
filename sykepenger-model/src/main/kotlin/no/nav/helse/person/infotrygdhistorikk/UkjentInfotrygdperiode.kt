0a3ad4905b5e50b1380bf5e94bc7df4b205496cb
Simen Ullern <simenullern@gmail.com>
2022-09-29 09:39:51 +0200

    Fjener død kode
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/infotrygdhistorikk/UkjentInfotrygdperiode.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/infotrygdhistorikk/UkjentInfotrygdperiode.kt
@@ -1,15 +1,9 @@
 package no.nav.helse.person.infotrygdhistorikk
 
-import no.nav.helse.hendelser.Periode
-import no.nav.helse.person.IAktivitetslogg
-import no.nav.helse.person.InfotrygdhistorikkVisitor
 import java.time.LocalDate
+import no.nav.helse.person.InfotrygdhistorikkVisitor
 
 class UkjentInfotrygdperiode(fom: LocalDate, tom: LocalDate) : Infotrygdperiode(fom, tom) {
-    override fun valider(aktivitetslogg: IAktivitetslogg, periode: Periode, skjæringstidspunkt: LocalDate, nødnummer: Nødnummer) {
-        if (endInclusive < periode.start.minusDays(18)) return
-        aktivitetslogg.varsel("Perioden er lagt inn i Infotrygd, men ikke utbetalt. Fjern fra Infotrygd hvis det utbetales via speil.")
-    }
 
     override fun accept(visitor: InfotrygdhistorikkVisitor) {
         visitor.visitInfotrygdhistorikkUkjentPeriode(this)
