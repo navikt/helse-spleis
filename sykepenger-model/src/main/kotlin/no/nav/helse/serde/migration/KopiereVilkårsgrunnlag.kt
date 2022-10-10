6a7349dc4bef64a31fbcf9052fab1c2c2a533885
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2022-10-10 12:46:53 +0200

    Migrering for å kopiere inn vilkårsgrunnlag på skjæringstidspunkt ©
    Inntil videre ikke i bruk, og må identifiseres hvilke som skal kopieres inn.

---
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/KopiereVilkårsgrunnlag.kt
@@ -0,0 +1,58 @@
+package no.nav.helse.serde.migration
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
+import no.nav.helse.serde.serdeObjectMapper
+
+internal abstract class KopiereVilkårsgrunnlag(
+    versjon: Int,
+    vararg vilkårsgrunnlagSomSkalKopieres: Pair<UUID, LocalDate>
+): JsonMigration(version = versjon) {
+    private val vilkårsgrunnlagSomSkalKopieres = vilkårsgrunnlagSomSkalKopieres.toList()
+
+    override val description = "Kopiere inn vilkårgrunnlag for de som mangler etter V178"
+
+    override fun doMigration(jsonNode: ObjectNode, meldingerSupplier: MeldingerSupplier) {
+        val vilkårsgrunnlagHistorikk = jsonNode.path("vilkårsgrunnlagHistorikk") as ArrayNode
+        val vilkårsgrunnlag = vilkårsgrunnlagHistorikk
+            .flatMap { it.vilkårsgrunnlag }
+            .groupBy { it.vilkårsgrunnlagId }
+            .mapValues { (_, vilkårsgrunnlag) -> vilkårsgrunnlag.first() }
+
+        val skjæringstidspunktMedVilkårsgrunnlag = vilkårsgrunnlag.values.map { it.skjæringstidspunkt }
+
+        val skalKopieresPåPerson = vilkårsgrunnlagSomSkalKopieres
+            .filter { (vilkårsgrunnlagId, _) -> vilkårsgrunnlagId in vilkårsgrunnlag.keys }
+            .filterNot { (_, skjæringstidspunkt) ->  skjæringstidspunkt in skjæringstidspunktMedVilkårsgrunnlag }
+
+        if (skalKopieresPåPerson.isEmpty()) return
+
+        val oppdaterteVilkårsgrunnlag = vilkårsgrunnlagHistorikk.firstOrNull()?.vilkårsgrunnlag?.deepCopy()
+            ?: serdeObjectMapper.createArrayNode()
+
+        skalKopieresPåPerson.forEach { (vilkårsgrunnlagId, skjæringstidspunkt) ->
+            val vilkårsgrunnlagKopi = vilkårsgrunnlag.getValue(vilkårsgrunnlagId).deepCopy<ObjectNode>().apply {
+                put("skjæringstidspunkt", "$skjæringstidspunkt")
+                put("vilkårsgrunnlagId", "${UUID.randomUUID()}")
+            }
+            oppdaterteVilkårsgrunnlag.add(vilkårsgrunnlagKopi)
+        }
+
+        vilkårsgrunnlagHistorikk.insert(0, serdeObjectMapper.createObjectNode().apply {
+            put("id", "${UUID.randomUUID()}")
+            put("opprettet", "${LocalDateTime.now()}")
+            putArray("vilkårsgrunnlag").addAll(oppdaterteVilkårsgrunnlag)
+        })
+    }
+
+    private companion object {
+        private val JsonNode.dato get() = LocalDate.parse(asText())
+        private val JsonNode.vilkårsgrunnlag get() = path("vilkårsgrunnlag") as ArrayNode
+        private val JsonNode.skjæringstidspunkt get() = path("skjæringstidspunkt").dato
+        private val JsonNode.vilkårsgrunnlagId get() = UUID.fromString(path("vilkårsgrunnlagId").asText())
+    }
+}
---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/serde/migration/KopiereVilkårsgrunnlagTest.kt
@@ -0,0 +1,50 @@
+package no.nav.helse.serde.migration
+
+import java.time.LocalDate
+import java.util.UUID
+import no.nav.helse.readResource
+import org.junit.jupiter.api.Test
+
+internal class KopiereVilkårsgrunnlagTest: MigrationTest(TestMigration) {
+
+    @Test
+    fun `ikke relevant person uten vilkårsgrunnlag`() = assertKopierteVilkårgrunnlag(
+        originalJson = "/migrations/182/ikke-relevant-uten-vilkårsgrunnlag_original.json",
+        expectedJson = "/migrations/182/ikke-relevant-uten-vilkårsgrunnlag_expected.json",
+    )
+
+    @Test
+    fun `relevant person som skal få kopiert ett vilkårsgrunnlag`() = assertKopierteVilkårgrunnlag(
+        originalJson = "/migrations/182/relevant-person_original.json",
+        expectedJson = "/migrations/182/relevant-person_expected.json",
+    )
+
+    @Test
+    fun `relevant person med flere vilkårsgrunnlag som skal kopieres`() = assertKopierteVilkårgrunnlag(
+        originalJson = "/migrations/182/relevant-person-flere-skjæringstidspunkt_original.json",
+        expectedJson = "/migrations/182/relevant-person-flere-skjæringstidspunkt_expected.json",
+    )
+
+    private fun assertKopierteVilkårgrunnlag(originalJson: String, expectedJson: String) {
+        val migrert = migrer(originalJson.readResource())
+        val sisteInnslag = migrert.path("vilkårsgrunnlagHistorikk").firstOrNull()
+        val expected = expectedJson.readResource()
+            .replace("{id}", sisteInnslag?.path("id")?.asText() ?: "")
+            .replace("{opprettet}", sisteInnslag?.path("opprettet")?.asText() ?: "")
+            .let { json ->
+                var tmpJson = json
+                sisteInnslag?.path("vilkårsgrunnlag")?.forEachIndexed { index, jsonNode ->
+                    tmpJson = tmpJson.replace("{nyVilkårsgrunnlagId${index}}", jsonNode.path("vilkårsgrunnlagId").asText())
+                }
+                tmpJson
+            }
+        assertJson(migrert.toString(), expected)
+    }
+
+    private object TestMigration: KopiereVilkårsgrunnlag(
+        versjon = 1337,
+        UUID.fromString("33fa499a-6fd2-43c2-a9cf-bdde739c546b") to LocalDate.parse("2020-05-05"),
+        UUID.fromString("491f6fb1-7fea-4fec-8095-1a0f8f7d543b") to LocalDate.parse("2020-01-01"),
+        UUID.fromString("dc3004ae-6d36-4f11-bb2c-32245540fb0e") to LocalDate.parse("2021-12-31")
+    )
+}
