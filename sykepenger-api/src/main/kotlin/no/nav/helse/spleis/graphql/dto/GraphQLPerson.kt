eb8074d0809e54ab774cb2d24a08bee2d9c70209
Stephen Ramthun <stephenramthun@gmail.com>
2022-10-26 09:54:54 +0200

    Fjern ubrukt resolver

--- sykepenger-api/src/main/kotlin/no/nav/helse/spleis/graphql/dto/GraphQLPerson.kt
+++ sykepenger-api/src/main/kotlin/no/nav/helse/spleis/graphql/dto/GraphQLPerson.kt
@@ -2,18 +2,12 @@ package no.nav.helse.spleis.graphql.dto
 
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
 import java.time.LocalDate
-import java.util.UUID
 
 internal fun SchemaBuilder.personTypes() {
     type<GraphQLPerson> {
         property<GraphQLArbeidsgiver?>("arbeidsgiver") {
             resolver { person: GraphQLPerson, organisasjonsnummer: String ->
                 person.arbeidsgivere.find { it.organisasjonsnummer == organisasjonsnummer }
-            }
-        }
-        property<List<GraphQLVilkarsgrunnlag>?>("vilkarsgrunnlag") {
-            resolver { person: GraphQLPerson, id: String ->
-                person.vilkarsgrunnlaghistorikk.find { it.id === UUID.fromString(id) }?.grunnlag
             }
         }
     }
