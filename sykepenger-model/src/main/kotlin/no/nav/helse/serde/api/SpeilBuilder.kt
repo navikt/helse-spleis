f444cd9fe9a9a6e2ccc97a270f986878c7876cac
Stephen Ramthun <stephenramthun@gmail.com>
2022-10-26 13:11:08 +0200

    Bump speilbuilder-versjon

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/api/SpeilBuilder.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/api/SpeilBuilder.kt
@@ -22,7 +22,7 @@ internal class SpeilBuilder(private val hendelser: List<HendelseDTO>) : Abstract
         /* Økes for å signalisere til spesialist at strukturen i snapshot'et
          * på et eller annet vis har endret seg, og at spesialist derfor må oppdatere cachede snapshots løpende
          */
-        const val SNAPSHOT_VERSJON = 42
+        const val SNAPSHOT_VERSJON = 43
     }
 
     private lateinit var personBuilder: PersonBuilder
