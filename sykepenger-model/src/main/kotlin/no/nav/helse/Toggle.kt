62ed9b911663d499cbc697ed6c67aceb037e794c
Simen Ullern <simenullern@gmail.com>
2022-11-28 15:56:06 +0100

    Skrur på å sende skarpe ferieoppdrag
    Co-authored-by: Erik M.Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -123,7 +123,7 @@ abstract class Toggle internal constructor(enabled: Boolean = false, private val
     }
 
     object GraphQLPlayground : Toggle("GraphQLPlayground", false)
-    object SendFeriepengeOppdrag : Toggle(false)
+    object SendFeriepengeOppdrag : Toggle(true)
     object Splarbeidsbros : Toggle("Splarbeidsbros", false)
     object InntektsmeldingKanTriggeRevurdering: Toggle("InntektsmeldingKanTriggeRevurdering", false)
 }
