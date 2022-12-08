95e82a10627baa16c04e34a6de4a44628b16db5e
David Steinsland <david@steinsland.email>
2022-12-08 13:04:57 +0100

    flytter oppførsel til objekter
    tydeliggjør dermed også at det er oppførsel knyttet til årsak
    
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Revurderingseventyr.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Revurderingseventyr.kt
@@ -3,50 +3,34 @@ package no.nav.helse.person
 import java.time.LocalDate
 import no.nav.helse.hendelser.Periode
 import no.nav.helse.hendelser.somPeriode
-import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.ARBEIDSFORHOLD
-import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.ARBEIDSGIVEROPPLYSNINGER
-import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.ARBEIDSGIVERPERIODE
-import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.KORRIGERT_SØKNAD
-import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.NY_PERIODE
-import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.SYKDOMSTIDSLINJE
+import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.Arbeidsforhold
+import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.Arbeidsgiveropplysninger
+import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.Arbeidsgiverperiode
+import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.KorrigertSøknad
+import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.NyPeriode
+import no.nav.helse.person.Revurderingseventyr.RevurderingÅrsak.Sykdomstidslinje
 
 class Revurderingseventyr private constructor(
     private val hvorfor: RevurderingÅrsak,
     private val skjæringstidspunkt: LocalDate,
-    private val periodeForEndring: Periode,
-    private val ikkeRelevant: Revurderingseventyr.(otherPeriode: Periode) -> Boolean = { false },
-    private val kanInngå: (hendelse: IAktivitetslogg) -> Boolean = { true },
-    private val dersomInngått: (hendelse: IAktivitetslogg) -> Unit = { }
+    private val periodeForEndring: Periode
 ) {
     internal companion object {
-        fun nyPeriode(skjæringstidspunkt: LocalDate, periodeForEndring: Periode, overstyrtForventerInntekt: Boolean) = Revurderingseventyr(
-            NY_PERIODE,
-            skjæringstidspunkt,
-            periodeForEndring,
-            // hvis endringen treffer en nyere nyopprettet periode, da trenger vi ikke bli med
-            ikkeRelevant = { otherPeriode -> this.periodeForEndring.starterEtter(otherPeriode) },
-            // orker ikke trigger revurdering dersom perioden er innenfor agp
-            // TODO: dersom f.eks. Spesialist godkjenner revurderinger uten endringer automatisk så ville ikke det
-            // lengre vært problematisk å opprette revurderinger i slike tilfeller
-            kanInngå = { overstyrtForventerInntekt },
-            dersomInngått = { hendelse ->
-                hendelse.varsel(Varselkode.RV_OO_2)
-                hendelse.info("Søknaden har trigget en revurdering fordi det er en tidligere eller overlappende periode")
-            }
-        )
+        fun nyPeriode(skjæringstidspunkt: LocalDate, periodeForEndring: Periode, overstyrtForventerInntekt: Boolean) =
+            Revurderingseventyr(NyPeriode(overstyrtForventerInntekt), skjæringstidspunkt, periodeForEndring)
 
-        fun arbeidsforhold(skjæringstidspunkt: LocalDate) = Revurderingseventyr(ARBEIDSFORHOLD, skjæringstidspunkt, skjæringstidspunkt.somPeriode())
-        fun korrigertSøknad(skjæringstidspunkt: LocalDate, periodeForEndring: Periode) = Revurderingseventyr(KORRIGERT_SØKNAD, skjæringstidspunkt, periodeForEndring)
-        fun sykdomstidslinje(skjæringstidspunkt: LocalDate, periodeForEndring: Periode) = Revurderingseventyr(SYKDOMSTIDSLINJE, skjæringstidspunkt, periodeForEndring)
-        fun arbeidsgiveropplysninger(skjæringstidspunkt: LocalDate) = Revurderingseventyr(ARBEIDSGIVEROPPLYSNINGER, skjæringstidspunkt, skjæringstidspunkt.somPeriode())
-        fun arbeidsgiverperiode(skjæringstidspunkt: LocalDate, periodeForEndring: Periode) = Revurderingseventyr(ARBEIDSGIVERPERIODE, skjæringstidspunkt, periodeForEndring)
+        fun arbeidsforhold(skjæringstidspunkt: LocalDate) = Revurderingseventyr(Arbeidsforhold, skjæringstidspunkt, skjæringstidspunkt.somPeriode())
+        fun korrigertSøknad(skjæringstidspunkt: LocalDate, periodeForEndring: Periode) = Revurderingseventyr(KorrigertSøknad, skjæringstidspunkt, periodeForEndring)
+        fun sykdomstidslinje(skjæringstidspunkt: LocalDate, periodeForEndring: Periode) = Revurderingseventyr(Sykdomstidslinje, skjæringstidspunkt, periodeForEndring)
+        fun arbeidsgiveropplysninger(skjæringstidspunkt: LocalDate) = Revurderingseventyr(Arbeidsgiveropplysninger, skjæringstidspunkt, skjæringstidspunkt.somPeriode())
+        fun arbeidsgiverperiode(skjæringstidspunkt: LocalDate, periodeForEndring: Periode) = Revurderingseventyr(Arbeidsgiverperiode, skjæringstidspunkt, periodeForEndring)
     }
 
     private val vedtaksperioder = mutableListOf<PersonObserver.RevurderingIgangsattEvent.VedtaksperiodeData>()
 
     internal fun inngåIRevurdering(hendelse: IAktivitetslogg, vedtaksperiode: Vedtaksperiode): Boolean {
-        if (!kanInngå(hendelse)) return false
-        dersomInngått(hendelse)
+        if (!hvorfor.kanInngå(hendelse)) return false
+        hvorfor.dersomInngått(hendelse)
         inngå(vedtaksperiode)
         return true
     }
@@ -60,14 +44,14 @@ class Revurderingseventyr private constructor(
     internal fun ikkeRelevant(periode: Periode, skjæringstidspunkt: LocalDate): Boolean {
         // om endringen gjelder et nyere skjæringstidspunkt så trenger vi ikke bryr oss
         if (this.skjæringstidspunkt > skjæringstidspunkt) return true
-        return ikkeRelevant(this, periode)
+        return hvorfor.ikkeRelevant(periodeForEndring, periode)
     }
 
     internal fun sendRevurderingIgangsattEvent(person: Person) {
         if (vedtaksperioder.isEmpty()) return
         person.emitRevurderingIgangsattEvent(
             PersonObserver.RevurderingIgangsattEvent(
-                årsak = hvorfor.name,
+                årsak = hvorfor.navn(),
                 berørtePerioder = vedtaksperioder.toList(),
                 skjæringstidspunkt = skjæringstidspunkt,
                 periodeForEndring = periodeForEndring
@@ -75,8 +59,54 @@ class Revurderingseventyr private constructor(
         )
     }
 
-    private enum class RevurderingÅrsak {
-        ARBEIDSGIVERPERIODE, ARBEIDSGIVEROPPLYSNINGER, SYKDOMSTIDSLINJE, NY_PERIODE, ARBEIDSFORHOLD, KORRIGERT_SØKNAD
+    private sealed interface RevurderingÅrsak {
+
+        fun ikkeRelevant(periodeForEndring: Periode, otherPeriode: Periode): Boolean = false
+        fun kanInngå(hendelse: IAktivitetslogg): Boolean = true
+        fun dersomInngått(hendelse: IAktivitetslogg) {}
+
+        fun navn(): String
+
+        object Arbeidsgiverperiode : RevurderingÅrsak {
+            override fun navn() = "ARBEIDSGIVERPERIODE"
+        }
+
+        object Sykdomstidslinje : RevurderingÅrsak {
+            override fun navn() = "SYKDOMSTIDSLINJE"
+        }
+
+        object Arbeidsgiveropplysninger : RevurderingÅrsak {
+            override fun navn() = "ARBEIDSGIVEROPPLYSNINGER"
+        }
+
+        object Arbeidsforhold : RevurderingÅrsak {
+            override fun navn() = "ARBEIDSFORHOLD"
+        }
+
+        object KorrigertSøknad : RevurderingÅrsak {
+            override fun navn() = "KORRIGERT_SØKNAD"
+        }
+
+        class NyPeriode(private val overstyrtForventerInntekt: Boolean) : RevurderingÅrsak {
+            override fun ikkeRelevant(periodeForEndring: Periode, otherPeriode: Periode): Boolean {
+                // hvis endringen treffer en nyere nyopprettet periode, da trenger vi ikke bli med
+                return periodeForEndring.starterEtter(otherPeriode)
+            }
+
+            override fun kanInngå(hendelse: IAktivitetslogg): Boolean {
+                // orker ikke trigger revurdering dersom perioden er innenfor agp
+                // TODO: dersom f.eks. Spesialist godkjenner revurderinger uten endringer automatisk så ville ikke det
+                // lengre vært problematisk å opprette revurderinger i slike tilfeller
+                return overstyrtForventerInntekt
+            }
+
+            override fun dersomInngått(hendelse: IAktivitetslogg) {
+                hendelse.varsel(Varselkode.RV_OO_2)
+                hendelse.info("Søknaden har trigget en revurdering fordi det er en tidligere eller overlappende periode")
+            }
+
+            override fun navn() = "NY_PERIODE"
+        }
     }
 
 }
