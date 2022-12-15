becd17cac0b90eb325d80aed06bef3306c6258a7
Helene Arnesen <helene.arnesen@nav.no>
2022-12-15 09:08:29 +0100

    Endret på varsel for flere inntektsmeldinger ⚠️
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Varselkode.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Varselkode.kt
@@ -51,7 +51,7 @@ enum class Varselkode(
     RV_IM_1("Vi har mottatt en inntektsmelding i en løpende sykmeldingsperiode med oppgitt første/bestemmende fraværsdag som er ulik tidligere fastsatt skjæringstidspunkt."),
     RV_IM_2("Første fraværsdag i inntektsmeldingen er ulik skjæringstidspunktet. Kontrollér at inntektsmeldingen er knyttet til riktig periode."),
     RV_IM_3("Inntektsmeldingen og vedtaksløsningen er uenige om beregningen av arbeidsgiverperioden. Undersøk hva som er riktig arbeidsgiverperiode."),
-    RV_IM_4("Mottatt flere inntektsmeldinger - den første inntektsmeldingen som ble mottatt er lagt til grunn. Utbetal kun hvis det blir korrekt."),
+    RV_IM_4("Det er mottatt flere inntektsmeldinger på samme skjæringstidspunkt. Undersøk at arbeidsgiverperioden, sykepengegrunnlaget og refusjonsopplysningene er riktige"),
     RV_IM_5("Sykmeldte har oppgitt ferie første dag i arbeidsgiverperioden."),
     RV_IM_6("Inntektsmelding inneholder ikke beregnet inntekt"),
     RV_IM_7("Brukeren har opphold i naturalytelser"),
