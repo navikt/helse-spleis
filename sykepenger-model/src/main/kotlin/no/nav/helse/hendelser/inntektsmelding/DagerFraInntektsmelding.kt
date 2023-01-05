61e064800579c9ba65b437cb697ee45427d06a3d
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-01-05 21:10:49 +0100

    Oppstykket håndtering av dager fra im

--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/inntektsmelding/DagerFraInntektsmelding.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/inntektsmelding/DagerFraInntektsmelding.kt
@@ -1,8 +1,14 @@
 package no.nav.helse.hendelser.inntektsmelding
 
+import java.time.LocalDate
+import java.util.UUID
 import no.nav.helse.hendelser.Inntektsmelding
 import no.nav.helse.hendelser.Periode
+import no.nav.helse.hendelser.til
 import no.nav.helse.person.Arbeidsgiver
+import no.nav.helse.person.Dokumentsporing
+import no.nav.helse.person.etterlevelse.SubsumsjonObserver
+import no.nav.helse.sykdomstidslinje.SykdomstidslinjeHendelse
 
 internal class DagerFraInntektsmelding(
     private val inntektsmelding: Inntektsmelding,
@@ -13,14 +19,34 @@ internal class DagerFraInntektsmelding(
 
 
     internal fun håndterFør(periode: Periode, arbeidsgiver: Arbeidsgiver) {
-
+        val dagerFør = gjenståendeDager.filter { it < periode.start }.takeUnless { it.isEmpty() } ?: return
+        arbeidsgiver.oppdaterSykdom(PeriodeFraInntektsmelding(inntektsmelding, dagerFør.overordnetPeriode))
+        gjenståendeDager.removeAll(dagerFør.toSet())
     }
 
     internal fun håndter(periode: Periode, arbeidsgiver: Arbeidsgiver) {
-
+        val overlappendeDager = periode.intersect(gjenståendeDager).takeUnless { it.isEmpty() } ?: return
+        arbeidsgiver.oppdaterSykdom(PeriodeFraInntektsmelding(inntektsmelding, overlappendeDager.overordnetPeriode))
+        gjenståendeDager.removeAll(overlappendeDager)
     }
 
     internal fun håndterGjenstående(arbeidsgiver: Arbeidsgiver) {
+        if (gjenståendeDager.isEmpty()) return
+        arbeidsgiver.oppdaterSykdom(PeriodeFraInntektsmelding(inntektsmelding, gjenståendeDager.overordnetPeriode))
+        gjenståendeDager.clear()
+    }
 
+    private companion object {
+        private val Iterable<LocalDate>.overordnetPeriode get() = min() til max()
+    }
+
+    private class PeriodeFraInntektsmelding(
+        private val inntektsmelding: Inntektsmelding,
+        private val periode: Periode
+    ): SykdomstidslinjeHendelse(UUID.randomUUID(), inntektsmelding) {
+        override fun sykdomstidslinje() = inntektsmelding.sykdomstidslinje().subset(periode)
+        override fun valider(periode: Periode, subsumsjonObserver: SubsumsjonObserver) = throw IllegalStateException("Ikke i bruk")
+        override fun fortsettÅBehandle(arbeidsgiver: Arbeidsgiver) = throw IllegalStateException("Ikke i bruk")
+        override fun leggTil(hendelseIder: MutableSet<Dokumentsporing>) = throw IllegalStateException("Ikke i bruk")
     }
 }
