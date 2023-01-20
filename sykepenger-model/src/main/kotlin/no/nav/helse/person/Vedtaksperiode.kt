3f8400b8ca652df7887d4c76269807bcb62bc3b4
Helene Arnesen <helene.arnesen@nav.no>
2023-01-20 11:04:16 +0100

    Fjerner unødvendig kommentar

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Vedtaksperiode.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Vedtaksperiode.kt
@@ -942,7 +942,6 @@ internal class Vedtaksperiode private constructor(
     }
 
     internal fun startRevurdering(hendelse: IAktivitetslogg, revurdering: Revurderingseventyr) {
-        // endringen gjelder fra start
         if (revurdering.ikkeRelevant(periode, skjæringstidspunkt)) return
         kontekst(hendelse)
         tilstand.startRevurdering(this, hendelse, revurdering)
