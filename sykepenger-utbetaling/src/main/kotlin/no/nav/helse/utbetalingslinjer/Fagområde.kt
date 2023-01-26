756e641c9f7651c834621870cf0438e3f7225315
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2023-01-26 09:57:40 +0100

    Flytter (endelig) Fagområde til utbetalingsmodulen

--- sykepenger-utbetaling/src/main/kotlin/no/nav/helse/utbetalingslinjer/Fagområde.kt
+++ sykepenger-utbetaling/src/main/kotlin/no/nav/helse/utbetalingslinjer/Fagområde.kt
@@ -7,32 +7,34 @@ interface Beløpkilde {
     fun personbeløp(): Int
 }
 
-internal enum class Fagområde(
-    internal val verdi: String,
+enum class Fagområde(
+    val verdi: String,
     private val beløpStrategy: (Beløpkilde) -> Int,
     private val klassekode: Klassekode
 ) {
-    SykepengerRefusjon("SPREF", { beløpkilde: Beløpkilde -> beløpkilde.arbeidsgiverbeløp() }, Klassekode.RefusjonIkkeOpplysningspliktig),
+    SykepengerRefusjon("SPREF", { beløpkilde: Beløpkilde -> beløpkilde.arbeidsgiverbeløp() },
+        Klassekode.RefusjonIkkeOpplysningspliktig
+    ),
     Sykepenger("SP", { beløpkilde: Beløpkilde -> beløpkilde.personbeløp() }, Klassekode.SykepengerArbeidstakerOrdinær);
 
     override fun toString() = verdi
 
-    internal fun linje(fagsystemId: String, økonomi: Beløpkilde, dato: LocalDate, grad: Int, beløp: Int) =
+    fun linje(fagsystemId: String, økonomi: Beløpkilde, dato: LocalDate, grad: Int, beløp: Int) =
         Utbetalingslinje(dato, dato, Satstype.Daglig, beløpStrategy(økonomi), beløp, grad, fagsystemId, klassekode = klassekode)
 
-    internal fun linje(fagsystemId: String, dato: LocalDate, grad: Int) =
+    fun linje(fagsystemId: String, dato: LocalDate, grad: Int) =
         Utbetalingslinje(dato, dato, Satstype.Daglig, null, 0, grad, fagsystemId, klassekode = klassekode)
 
-    internal fun oppdaterLinje(linje: Utbetalingslinje, dato: LocalDate, økonomi: Beløpkilde, beløp: Int) {
+    fun oppdaterLinje(linje: Utbetalingslinje, dato: LocalDate, økonomi: Beløpkilde, beløp: Int) {
         linje.beløp = beløpStrategy(økonomi)
         linje.aktuellDagsinntekt = beløp
         linje.fom = dato
     }
 
-    internal fun kanLinjeUtvides(linje: Utbetalingslinje, økonomi: Beløpkilde, grad: Int) =
+    fun kanLinjeUtvides(linje: Utbetalingslinje, økonomi: Beløpkilde, grad: Int) =
         grad == linje.grad && (linje.beløp == null || linje.beløp == beløpStrategy(økonomi))
 
-    internal companion object {
+    companion object {
         private val map = values().associateBy(Fagområde::verdi)
         fun from(verdi: String) = requireNotNull(map[verdi]) { "Støtter ikke klassekode: $verdi" }
     }
