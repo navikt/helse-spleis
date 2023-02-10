355a11fce37b92067bf5bc9083f83cd8eac0fe43
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2023-02-10 08:43:34 +0100

    Subsumsjon-hendelsen må leve i primitiver en stund

--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/Subsumsjon.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.helse.hendelser
-
-class Subsumsjon(
-    internal val paragraf: String,
-    internal val ledd: Int?,
-    internal val bokstav: String?,
-)
---
+++ sykepenger-primitiver/src/main/kotlin/no/nav/helse/hendelser/Subsumsjon.kt
@@ -0,0 +1,7 @@
+package no.nav.helse.hendelser
+
+class Subsumsjon(
+    val paragraf: String,
+    val ledd: Int?,
+    val bokstav: String?,
+)
