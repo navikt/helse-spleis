475409c487b8b7c8f8056f928a76300c8e585438
David Steinsland <david@steinsland.email>
2023-03-13 09:11:25 +0100

    tester en ny førstegangsbehandling på en flere ag-tilfelle

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/flere_arbeidsgivere/FlereUkjenteArbeidsgivereTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/flere_arbeidsgivere/FlereUkjenteArbeidsgivereTest.kt
@@ -24,6 +24,7 @@ import no.nav.helse.spleis.e2e.håndterPåminnelse
 import no.nav.helse.spleis.e2e.håndterSimulering
 import no.nav.helse.spleis.e2e.håndterYtelser
 import no.nav.helse.spleis.e2e.nyPeriode
+import no.nav.helse.spleis.e2e.nyeVedtak
 import no.nav.helse.spleis.e2e.nyttVedtak
 import no.nav.helse.testhelpers.assertNotNull
 import no.nav.helse.økonomi.Inntekt.Companion.daglig
@@ -166,5 +167,67 @@ internal class FlereUkjenteArbeidsgivereTest : AbstractEndToEndTest() {
                 assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVVENTER_BLOKKERENDE_PERIODE, orgnummer = a2)
             }
         )
+    }
+
+    @Test
+    fun `to arbeidsgivere - ny overlappende førstegangsbehandlingen hos ag2`() {
+        val inntektA1 = INNTEKT + 500.daglig
+        val inntektA2 = INNTEKT
+
+        nyeVedtak(1.januar, 31.januar, a1, a2, inntekt = INNTEKT)
+        forlengVedtak(1.februar, 28.februar, orgnummer = a1)
+        forlengVedtak(1.mars, 31.mars, orgnummer = a1)
+
+        håndterInntektsmelding(listOf(1.januar til 16.januar), beregnetInntekt = inntektA1, orgnummer = a1)
+        håndterYtelser(3.vedtaksperiode, orgnummer = a1)
+        håndterSimulering(3.vedtaksperiode, orgnummer = a1)
+
+        håndterInntektsmelding(listOf(1.mars til 16.mars), beregnetInntekt = inntektA2, orgnummer = a2)
+        nullstillTilstandsendringer()
+        nyPeriode(1.mars til 20.mars, a2)
+
+        val vilkårsgrunnlag = inspektør(a1).vilkårsgrunnlag(3.vedtaksperiode)?.inspektør ?: fail { "må ha vilkårsgrunnlag" }
+        val inntektsopplysninger = vilkårsgrunnlag.sykepengegrunnlag.inspektør.arbeidsgiverInntektsopplysningerPerArbeidsgiver
+
+        assertEquals(2, inntektsopplysninger.size)
+        val a1Inspektør = inntektsopplysninger.getValue(a1).inspektør
+        assertEquals(Inntektsmelding::class, a1Inspektør.inntektsopplysning::class)
+        assertEquals(inntektA1, a1Inspektør.inntektsopplysning.inspektør.beløp)
+        val a2Inspektør = inntektsopplysninger.getValue(a2).inspektør
+        assertEquals(Inntektsmelding::class, a2Inspektør.inntektsopplysning::class)
+        assertEquals(inntektA2, a2Inspektør.inntektsopplysning.inspektør.beløp)
+
+        val overstyringerIgangsatt = observatør.overstyringIgangsatt
+        assertEquals(3, overstyringerIgangsatt.size)
+
+        overstyringerIgangsatt[1].also { event ->
+            assertEquals(PersonObserver.OverstyringIgangsatt(
+                årsak = "NY_PERIODE",
+                skjæringstidspunkt = 1.januar,
+                periodeForEndring = 1.mars til 20.mars,
+                berørtePerioder = listOf(
+                    VedtaksperiodeData(a1, 3.vedtaksperiode.id(a1), 1.mars til 31.mars, 1.januar, "REVURDERING")
+                )
+            ), event)
+        }
+        overstyringerIgangsatt[2].also { event ->
+            assertEquals(PersonObserver.OverstyringIgangsatt(
+                årsak = "KORRIGERT_INNTEKTSMELDING",
+                skjæringstidspunkt = 1.januar,
+                periodeForEndring = 1.mars til 1.mars,
+                berørtePerioder = listOf(
+                    VedtaksperiodeData(a1, 1.vedtaksperiode.id(a1), 1.januar til 31.januar, 1.januar, "REVURDERING"),
+                    VedtaksperiodeData(a1, 2.vedtaksperiode.id(a1), 1.februar til 28.februar, 1.januar, "REVURDERING"),
+                    VedtaksperiodeData(a1, 3.vedtaksperiode.id(a1), 1.mars til 31.mars, 1.januar, "REVURDERING"),
+                    VedtaksperiodeData(a2, 1.vedtaksperiode.id(a2), 1.januar til 31.januar, 1.januar, "REVURDERING")
+                )
+            ), event)
+        }
+
+        assertTilstander(1.vedtaksperiode, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_REVURDERING, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_REVURDERING, AVVENTER_GJENNOMFØRT_REVURDERING, orgnummer = a1)
+        assertTilstander(2.vedtaksperiode, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_REVURDERING, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_REVURDERING, AVVENTER_GJENNOMFØRT_REVURDERING, orgnummer = a1)
+        assertTilstander(3.vedtaksperiode, AVVENTER_GODKJENNING_REVURDERING, AVVENTER_REVURDERING, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, AVVENTER_REVURDERING, AVVENTER_GJENNOMFØRT_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, orgnummer = a1)
+        assertTilstander(1.vedtaksperiode, AVVENTER_REVURDERING, orgnummer = a2)
+        assertTilstander(2.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVVENTER_BLOKKERENDE_PERIODE, orgnummer = a2)
     }
 }
