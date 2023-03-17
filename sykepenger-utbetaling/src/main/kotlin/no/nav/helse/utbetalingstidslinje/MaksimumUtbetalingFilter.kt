ccae198ddf433c5ce896c0b36eb90f164da355c6
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2023-03-17 09:44:16 +0100

    Flytter UtbetalingstidslinjerFilter og  MaksimumUtbetalingFilter til utbetalingsmodulen

--- sykepenger-utbetaling/src/main/kotlin/no/nav/helse/utbetalingstidslinje/MaksimumUtbetalingFilter.kt
+++ sykepenger-utbetaling/src/main/kotlin/no/nav/helse/utbetalingstidslinje/MaksimumUtbetalingFilter.kt
@@ -6,7 +6,7 @@ import no.nav.helse.etterlevelse.SubsumsjonObserver
 import no.nav.helse.økonomi.betal
 import no.nav.helse.økonomi.er6GBegrenset
 
-internal class MaksimumUtbetalingFilter : UtbetalingstidslinjerFilter {
+class MaksimumUtbetalingFilter : UtbetalingstidslinjerFilter {
     private var harRedusertUtbetaling = false
 
     override fun filter(
@@ -32,7 +32,7 @@ internal class MaksimumUtbetalingFilter : UtbetalingstidslinjerFilter {
         return tidslinjer
     }
 
-    internal fun betal(
+    fun betal(
         tidslinjer: List<Utbetalingstidslinje>,
         periode: Periode,
         aktivitetslogg: IAktivitetslogg,
--- sykepenger-utbetaling/src/main/kotlin/no/nav/helse/utbetalingstidslinje/UtbetalingstidslinjerFilter.kt
+++ sykepenger-utbetaling/src/main/kotlin/no/nav/helse/utbetalingstidslinje/UtbetalingstidslinjerFilter.kt
@@ -4,7 +4,7 @@ import no.nav.helse.hendelser.Periode
 import no.nav.helse.person.aktivitetslogg.IAktivitetslogg
 import no.nav.helse.etterlevelse.SubsumsjonObserver
 
-internal interface UtbetalingstidslinjerFilter {
+interface UtbetalingstidslinjerFilter {
     fun filter(
         tidslinjer: List<Utbetalingstidslinje>,
         periode: Periode,
