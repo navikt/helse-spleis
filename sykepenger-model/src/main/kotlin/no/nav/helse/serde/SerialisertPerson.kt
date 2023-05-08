979dc71e918be72c987e929b94725ca689a15403
David Steinsland <david@steinsland.email>
2023-05-08 08:07:08 +0200

    migrerer perioder som overlapper
    V242 avdekket at det var 3741 perioder totalt som overlappet med en annen, rundt 2740 i AUU resten i Avsluttet.

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/SerialisertPerson.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/SerialisertPerson.kt
@@ -164,7 +164,8 @@ import no.nav.helse.serde.migration.V239KompensereManglendeOriginalInntekt
 import no.nav.helse.serde.migration.V23None
 import no.nav.helse.serde.migration.V240KopiereSykdomstidslinjeTilVedtaksperiodeutbetalinger
 import no.nav.helse.serde.migration.V241DokumenttypeSomListe
-import no.nav.helse.serde.migration.V242FinneOverlappendePerioder
+import no.nav.helse.serde.migration.V242None
+import no.nav.helse.serde.migration.V243FinneOverlappendePerioder
 import no.nav.helse.serde.migration.V24None
 import no.nav.helse.serde.migration.V25ManglendeForlengelseFraInfotrygd
 import no.nav.helse.serde.migration.V26SykdomshistorikkMerge
@@ -494,7 +495,8 @@ class SerialisertPerson(val json: String) {
             V239KompensereManglendeOriginalInntekt(),
             V240KopiereSykdomstidslinjeTilVedtaksperiodeutbetalinger(),
             V241DokumenttypeSomListe(),
-            V242FinneOverlappendePerioder()
+            V242None(),
+            V243FinneOverlappendePerioder()
         )
 
         fun gjeldendeVersjon() = JsonMigration.gjeldendeVersjon(migrations)
--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V242FinneOverlappendePerioder.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V242FinneOverlappendePerioder.kt
@@ -1,13 +1,17 @@
 package no.nav.helse.serde.migration
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import java.time.LocalDate
 import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.helse.hendelser.Periode
 import no.nav.helse.hendelser.til
+import no.nav.helse.nesteDag
+import no.nav.helse.serde.serdeObjectMapper
 import org.slf4j.LoggerFactory
 
-internal class V242FinneOverlappendePerioder: JsonMigration(242) {
+internal class V243FinneOverlappendePerioder: JsonMigration(243) {
     private companion object {
         private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
     }
@@ -18,22 +22,63 @@ internal class V242FinneOverlappendePerioder: JsonMigration(242) {
         val fnr = jsonNode.path("fødselsnummer").asText()
         jsonNode.path("arbeidsgivere").forEach { arbeidsgiver ->
             val orgnummer = arbeidsgiver.path("organisasjonsnummer").asText()
+            var måJustereLåser = false
             arbeidsgiver
                 .path("vedtaksperioder")
-                .fold(emptyList<Periode>()) { perioder, vedtaksperiode ->
+                .fold(emptyList<Pair<Periode, JsonNode>>()) { perioder, vedtaksperiode ->
                     val periode = LocalDate.parse(vedtaksperiode.path("fom").asText()) til LocalDate.parse(vedtaksperiode.path("tom").asText())
-                    if (perioder.none { it.overlapperMed(periode) }) {
-                        perioder.plusElement(periode)
-                    } else {
-                        sikkerlogg.info("{} vedtaksperiode {} i {} hos {} overlapper med tidligere periode",
+                    val overlappende = perioder.firstOrNull { it.first.overlapperMed(periode) }
+                    if (overlappende == null) {
+                        perioder.plusElement(periode to vedtaksperiode)
+                    } else if (periode.count() == 1) {
+                        sikkerlogg.info("{} vedtaksperiode {} ($periode) i {} hos {} overlapper med tidligere periode ${overlappende.first} - må justeres manuelt fordi perioden består av én dag",
                             keyValue("fødselsnummer", fnr),
                             keyValue("vedtaksperiodeId", vedtaksperiode.path("id").asText()),
                             keyValue("tilstand", vedtaksperiode.path("tilstand").asText()),
                             keyValue("orgnummer", orgnummer)
                         )
                         perioder
+                    } else {
+                        måJustereLåser = true
+                        val nyFom = if (overlappende.first == periode) {
+                            sikkerlogg.info("{} vedtaksperiode {} ($periode) i {} hos {} overlapper med tidligere periode ${overlappende.first} - må justere tom på forrige og fom på gjeldende",
+                                keyValue("fødselsnummer", fnr),
+                                keyValue("vedtaksperiodeId", vedtaksperiode.path("id").asText()),
+                                keyValue("tilstand", vedtaksperiode.path("tilstand").asText()),
+                                keyValue("orgnummer", orgnummer)
+                            )
+                            (overlappende.second as ObjectNode).put("tom", overlappende.first.start.toString())
+                            overlappende.first.start.nesteDag
+                        } else {
+                            sikkerlogg.info("{} vedtaksperiode {} ($periode) i {} hos {} overlapper med tidligere periode ${overlappende.first} - endrer fom på gjeldende",
+                                keyValue("fødselsnummer", fnr),
+                                keyValue("vedtaksperiodeId", vedtaksperiode.path("id").asText()),
+                                keyValue("tilstand", vedtaksperiode.path("tilstand").asText()),
+                                keyValue("orgnummer", orgnummer)
+                            )
+                            overlappende.first.endInclusive.nesteDag
+                        }
+
+                        (vedtaksperiode as ObjectNode).put("fom", nyFom.toString())
+                        perioder.plusElement((nyFom til periode.endInclusive) to vedtaksperiode)
                     }
                 }
+            if (måJustereLåser) {
+                // sikre at alle avsluttede perioder har låste perioder
+                val låstePerioder = arbeidsgiver.path("sykdomshistorikk").path(0).path("beregnetSykdomstidslinje").path("låstePerioder") as ArrayNode
+                låstePerioder.removeAll()
+
+                val avsluttedePerioder = arbeidsgiver
+                    .path("vedtaksperioder")
+                    .filter { it.path("tilstand").asText() in setOf("AVSLUTTET", "AVSLUTTET_UTEN_UTBETALING") }
+                    .map { it.path("fom").asText() to it.path("tom").asText() }
+                    .map { (fom, tom) -> serdeObjectMapper.createObjectNode().apply {
+                        put("fom", fom)
+                        put("tom", tom)
+                    } }
+
+                låstePerioder.addAll(avsluttedePerioder)
+            }
         }
     }
 }
