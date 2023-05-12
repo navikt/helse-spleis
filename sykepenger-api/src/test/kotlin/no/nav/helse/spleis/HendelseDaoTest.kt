4d8f085876bc6561072cdbf48d5fec05f437eba0
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2023-05-12 12:40:26 +0200

    Setter opp en abstrakt klasse for å dele database i -api

--- sykepenger-api/src/test/kotlin/no/nav/helse/spleis/HendelseDaoTest.kt
+++ sykepenger-api/src/test/kotlin/no/nav/helse/spleis/HendelseDaoTest.kt
@@ -1,66 +1,32 @@
 package no.nav.helse.spleis
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
 import java.time.LocalDateTime
 import java.util.UUID
 import javax.sql.DataSource
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import no.nav.helse.spleis.dao.HendelseDao
-import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle
-import org.testcontainers.containers.PostgreSQLContainer
 
 @TestInstance(Lifecycle.PER_CLASS)
-class HendelseDaoTest {
-
-    private companion object {
-        private val postgres = PostgreSQLContainer<Nothing>("postgres:14").apply {
-            withCreateContainerCmdModifier { command -> command.withName("spleis-api3") }
-            withReuse(true)
-            withLabel("app-navn", "spleis-api")
-            start()
-        }
-    }
+internal class HendelseDaoTest: DBTest() {
 
     private val UNG_PERSON_FNR = "12029240045"
-    private lateinit var dataSource: DataSource
-    private lateinit var flyway: Flyway
     private val meldingsReferanse = UUID.randomUUID()
 
     @BeforeAll
     internal fun `start embedded environment`() {
-
-        dataSource = HikariDataSource(HikariConfig().apply {
-            jdbcUrl = postgres.jdbcUrl
-            username = postgres.username
-            password = postgres.password
-            maximumPoolSize = 3
-            minimumIdle = 1
-            initializationFailTimeout = 5000
-            idleTimeout = 10001
-            connectionTimeout = 1000
-            maxLifetime = 30001
-        })
-        flyway = Flyway
-            .configure()
-            .cleanDisabled(false)
-            .dataSource(dataSource)
-            .locations("classpath:db/migration")
-            .load()
-
     }
 
     @BeforeEach
     internal fun setup() {
-        flyway.clean()
-        flyway.migrate()
+        clean()
+        migrate()
         dataSource.lagreHendelse(meldingsReferanse)
     }
 
