6c54f0b35f95815af6671775727c2eb796f006fb
David Steinsland <david@steinsland.email>
2023-05-19 08:02:43 +0200

    oppdaterer dato for forrige infotrygd-kjøring

--- sykepenger-model/src/main/kotlin/no/nav/helse/utbetalingstidslinje/Feriepengeberegner.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/utbetalingstidslinje/Feriepengeberegner.kt
@@ -219,7 +219,7 @@ internal class Feriepengeberegner(
 
         private companion object {
             // Hardkodet dato skal være datoen Infotrygd sist kjørte feriepenger
-            private val DATO_FOR_SISTE_FERIEPENGEKJØRING_I_INFOTRYGD = LocalDate.of(2023, 1, 14)
+            private val DATO_FOR_SISTE_FERIEPENGEKJØRING_I_INFOTRYGD = LocalDate.of(2023, 5, 14)
         }
 
         private inner class InfotrygdUtbetalteDagerVisitor : FeriepengeutbetalingsperiodeVisitor {
