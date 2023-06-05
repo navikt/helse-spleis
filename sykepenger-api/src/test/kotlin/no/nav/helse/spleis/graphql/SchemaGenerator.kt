9a4a2939c12d2905537c697b1446ac86e7819369
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-06-05 16:54:05 +0200

    GraphQL tåler ikke norske bokstaver 🤮
    Da slipper vi å gjøre den feilen igjen.

--- sykepenger-api/src/test/kotlin/no/nav/helse/spleis/graphql/SchemaGenerator.kt
+++ sykepenger-api/src/test/kotlin/no/nav/helse/spleis/graphql/SchemaGenerator.kt
@@ -10,6 +10,7 @@ import java.nio.file.Paths
 import no.nav.helse.spleis.graphql.dto.GraphQLPerson
 import no.nav.helse.spleis.objectMapper
 import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
@@ -48,6 +49,10 @@ internal class SchemaGenerator {
             "Noe er galt med det nytt schema"
         }
 
+        assertFalse(nyttSchema.toString().contains(norskeBokstaver)) {
+            "Kan ikke ha norske bokstaver i GraphQL 🤷‍"
+        }
+
         if (erLik(gammeltSchema, nyttSchema.toString())) {
             println("Ingen endringer i GraphQL schema 🤡")
         } else {
@@ -56,6 +61,8 @@ internal class SchemaGenerator {
     }
 
     internal companion object {
+        private val norskeBokstaver = "[æøåÆØÅ]".toRegex()
+
         @Language("JSON")
         internal const val IntrospectionQuery = """
         {"query":"\n    query IntrospectionQuery {\n      __schema {\n        queryType { name }\n        mutationType { name }\n        subscriptionType { name }\n        types {\n          ...FullType\n        }\n        directives {\n          name\n          description\n          locations\n          args {\n            ...InputValue\n          }\n        }\n      }\n    }\n    fragment FullType on __Type {\n      kind\n      name\n      description\n      fields(includeDeprecated: true) {\n        name\n        description\n        args {\n          ...InputValue\n        }\n        type {\n          ...TypeRef\n        }\n        isDeprecated\n        deprecationReason\n      }\n      inputFields {\n        ...InputValue\n      }\n      interfaces {\n        ...TypeRef\n      }\n      enumValues(includeDeprecated: true) {\n        name\n        description\n        isDeprecated\n        deprecationReason\n      }\n      possibleTypes {\n        ...TypeRef\n      }\n    }\n    fragment InputValue on __InputValue {\n      name\n      description\n      type { ...TypeRef }\n      defaultValue\n    }\n    fragment TypeRef on __Type {\n      kind\n      name\n      ofType {\n        kind\n        name\n        ofType {\n          kind\n          name\n          ofType {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                  ofType {\n                    kind\n                    name\n                  }\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n    ","operationName":"IntrospectionQuery"}
