acca62e36ada4096560577f0045f1ebc220749e9
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2023-12-11 14:26:43 +0100

    fom og tom må være datoer, også i ForkastSykmeldingsperioderRiver
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/meldinger/ForkastSykmeldingsperioderRiver.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/meldinger/ForkastSykmeldingsperioderRiver.kt
@@ -1,7 +1,9 @@
 package no.nav.helse.spleis.meldinger
 
+import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.asLocalDate
 import no.nav.helse.spleis.IMessageMediator
 import no.nav.helse.spleis.meldinger.model.ForkastSykmeldingsperioderMessage
 
@@ -18,10 +20,10 @@ internal class ForkastSykmeldingsperioderRiver(
             "@id",
             "aktørId",
             "fødselsnummer",
-            "organisasjonsnummer",
-            "fom",
-            "tom"
+            "organisasjonsnummer"
         )
+        message.require("fom", JsonNode::asLocalDate)
+        message.require("tom", JsonNode::asLocalDate)
     }
 
     override fun createMessage(packet: JsonMessage) = ForkastSykmeldingsperioderMessage(packet)
