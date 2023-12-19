1ab42a7240960fc79c697642cc509d68396260d8
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-12-19 10:10:42 +0100

    Stryker Utbetalingsavgjørelse medhårs 💆‍

--- sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/VedtakFattet.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/hendelser/VedtakFattet.kt
@@ -4,9 +4,9 @@ import java.time.LocalDateTime
 import java.util.UUID
 import no.nav.helse.hendelser.Avsender.SAKSBEHANDLER
 import no.nav.helse.hendelser.Avsender.SYSTEM
+import no.nav.helse.hendelser.utbetaling.Saksbehandler
 import no.nav.helse.hendelser.utbetaling.Utbetalingsavgjørelse
 import no.nav.helse.person.aktivitetslogg.Aktivitetslogg
-import no.nav.helse.utbetalingslinjer.Utbetaling.Vurdering
 
 class VedtakFattet(
     meldingsreferanseId: UUID,
@@ -15,30 +15,16 @@ class VedtakFattet(
     organisasjonsnummer: String,
     private val vedtaksperiodeId: UUID,
     private val utbetalingId: UUID,
-    private val saksbehandlerIdent: String?,
-    private val saksbehandlerEpost: String?,
+    private val saksbehandlerIdent: String,
+    private val saksbehandlerEpost: String,
     private val vedtakFattetTidspunkt: LocalDateTime,
+    override val automatisert: Boolean,
+    override val godkjent: Boolean
 ) : ArbeidstakerHendelse(meldingsreferanseId, fødselsnummer, aktørId, organisasjonsnummer, Aktivitetslogg()), Utbetalingsavgjørelse {
-
-    init {
-        listOfNotNull(saksbehandlerIdent, saksbehandlerEpost).size.let {
-            check(it == 0 || it == 2) { "Enten må både ident & epost for saksebandler settes, eller ingen av delene." }
-        }
-    }
-
-    override fun godkjent() = true
-    override fun avgjørelsestidspunkt() = vedtakFattetTidspunkt
-    override fun automatisert() = saksbehandlerIdent == null
-    override fun vurdering() = Vurdering(
-        godkjent = godkjent(),
-        ident = saksbehandlerIdent ?: "Automatisk behandlet",
-        epost = saksbehandlerEpost ?: "tbd@nav.no",
-        tidspunkt = vedtakFattetTidspunkt,
-        automatiskBehandling = automatisert()
-    )
+    override val avgjørelsestidspunkt = vedtakFattetTidspunkt
+    override fun saksbehandler() = Saksbehandler(saksbehandlerIdent, saksbehandlerEpost)
     override fun relevantVedtaksperiode(id: UUID) = vedtaksperiodeId == id
     override fun relevantUtbetaling(id: UUID) = utbetalingId == id
-
     override fun innsendt() = vedtakFattetTidspunkt
-    override fun avsender() = if (automatisert()) SYSTEM else SAKSBEHANDLER
+    override fun avsender() = if (automatisert) SYSTEM else SAKSBEHANDLER
 }
--- sykepenger-utbetaling/src/main/kotlin/no/nav/helse/hendelser/utbetaling/Utbetalingsavgjørelse.kt
+++ sykepenger-utbetaling/src/main/kotlin/no/nav/helse/hendelser/utbetaling/Utbetalingsavgjørelse.kt
@@ -2,16 +2,49 @@ package no.nav.helse.hendelser.utbetaling
 
 import java.time.LocalDateTime
 import java.util.UUID
-import no.nav.helse.person.aktivitetslogg.IAktivitetslogg
+import no.nav.helse.hendelser.Hendelse
+import no.nav.helse.person.aktivitetslogg.Varselkode.RV_UT_18
+import no.nav.helse.person.aktivitetslogg.Varselkode.RV_UT_19
 import no.nav.helse.utbetalingslinjer.Utbetaling.Vurdering
 
-interface Utbetalingsavgjørelse: IAktivitetslogg {
+interface Utbetalingsavgjørelse: Hendelse {
     fun relevantUtbetaling(id: UUID): Boolean
     fun relevantVedtaksperiode(id: UUID): Boolean
-    fun godkjent(): Boolean
-    fun avgjørelsestidspunkt(): LocalDateTime
-    fun automatisert(): Boolean
-    fun vurdering(): Vurdering
-    fun valider() {}
+    fun saksbehandler(): Saksbehandler
+    val godkjent: Boolean
+    val avgjørelsestidspunkt: LocalDateTime
+    val automatisert: Boolean
 }
-fun Utbetalingsavgjørelse.avvist() = !godkjent()
+class Saksbehandler(private val ident: String, private val epost: String) {
+    override fun toString() = ident
+    internal fun vurdering(godkjent: Boolean, avgjørelsestidspunkt: LocalDateTime, automatisert: Boolean) = Vurdering(
+        godkjent = godkjent,
+        tidspunkt = avgjørelsestidspunkt,
+        automatiskBehandling = automatisert,
+        ident = ident,
+        epost = epost
+    )
+}
+val Utbetalingsavgjørelse.avvist get() = !godkjent
+val Utbetalingsavgjørelse.vurdering get() = saksbehandler().vurdering(
+    godkjent = godkjent,
+    avgjørelsestidspunkt = avgjørelsestidspunkt,
+    automatisert = automatisert
+)
+private val Utbetalingsavgjørelse.manueltBehandlet get() = !automatisert
+fun Utbetalingsavgjørelse.valider() {
+    when {
+        avvist && manueltBehandlet -> {
+            funksjonellFeil(RV_UT_19)
+            info("Utbetaling markert som ikke godkjent av saksbehandler ${saksbehandler()} $avgjørelsestidspunkt")
+        }
+        avvist && automatisert -> {
+            funksjonellFeil(RV_UT_18)
+            info("Utbetaling markert som ikke godkjent automatisk $avgjørelsestidspunkt")
+        }
+        godkjent && manueltBehandlet ->
+            info("Utbetaling markert som godkjent av saksbehandler ${saksbehandler()} $avgjørelsestidspunkt")
+        else ->
+            info("Utbetaling markert som godkjent automatisk $avgjørelsestidspunkt")
+    }
+}
