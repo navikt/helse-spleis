c99f5bb1866fd079380805554156a07f858ef9ba
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-01-02 16:09:58 +0100

    Revert "Flipper på SendFeriepengeOppdrag 🏖️"
    This reverts commit be8a09494c1929c938747b8a451f80e0bc927cae.

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -122,7 +122,7 @@ abstract class Toggle internal constructor(enabled: Boolean = false, private val
         }
     }
 
-    object SendFeriepengeOppdrag : Toggle("SEND_FERIEPENGEOPPDRAG", true)
+    object SendFeriepengeOppdrag : Toggle("SEND_FERIEPENGEOPPDRAG", false)
     object AltAvTjuefemprosentAvvikssaker : Toggle("AVVIKSAKER", false)
     object AvviksvurderingFlyttet : Toggle("AVVIKSAKER_FLYTTET", false)
 }
