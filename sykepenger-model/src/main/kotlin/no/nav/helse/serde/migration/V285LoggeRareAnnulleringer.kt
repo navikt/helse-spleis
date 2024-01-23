a7fecfc511c8bd15ceab960e05eb25c2664d0f76
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-01-23 08:24:32 +0100

    Ser på status i oppdrag istedenfor overføringstidspunkt 🤔
    Viste seg at det ble logget en del med overføringstidspunkt `null` selv om de var sendt til oppdrag og annulleringene fantes i Spenn.
    
    De reelle casene ser ut til å ha linjer på ett eller fler oppdrag, men ikke ha noe status på oppdraget

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V285LoggeRareAnnulleringer.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V285LoggeRareAnnulleringer.kt
@@ -1,10 +1,14 @@
 package no.nav.helse.serde.migration
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import java.time.LocalDateTime
 import org.slf4j.LoggerFactory
 
-internal class V285LoggeRareAnnulleringer(private val forkast: Set<String> = setOf("ec293bc9-73ce-4c5f-b7a8-18451f5f623c")): JsonMigration(version = 285) {
+internal class V285LoggeRareAnnulleringer(
+    private val forkast: Set<String> = setOf("ec293bc9-73ce-4c5f-b7a8-18451f5f623c"),
+    val forkastetTidspunkt: () -> LocalDateTime = { LocalDateTime.now() }
+): JsonMigration(version = 285) {
     override val description = "Logger rare utbetalinger"
 
     override fun doMigration(jsonNode: ObjectNode, meldingerSupplier: MeldingerSupplier) {
@@ -15,16 +19,18 @@ internal class V285LoggeRareAnnulleringer(private val forkast: Set<String> = set
             arbeidsgiver.path("utbetalinger")
                 .filter { it.path("type").asText() == "ANNULLERING" }
                 .filter { it.path("status").asText() != "FORKASTET" }
-                .filter { it.path("overføringstidspunkt").isNull }
-                .forEach { rarAnnullering ->
-                    val id = rarAnnullering.path("id").asText()
-                    val nåværendeStatus = rarAnnullering.path("status").asText()
-                    val oppdatert = LocalDateTime.parse(rarAnnullering.path("oppdatert").asText())
-                    sikkerLogg.warn("Annullering $id i Status $nåværendeStatus ser ikke ut til å være overført til Oppdrag (sist oppdatert $oppdatert). AktørId $aktørId, Organisasjonsnummer $organisasjonsnummer")
+                .filter { LocalDateTime.parse(it.path("oppdatert").asText()).year < 2024 }
+                .filter { it.path("arbeidsgiverOppdrag").oppdragManglerOverføring || it.path("personOppdrag").oppdragManglerOverføring }
+                .forEach { manglerOverføring ->
+                    val id = manglerOverføring.path("id").asText()
+                    val nåværendeStatus = manglerOverføring.path("status").asText()
+                    val oppdatert = LocalDateTime.parse(manglerOverføring.path("oppdatert").asText())
+                    sikkerLogg.warn("Annullering $id i Status $nåværendeStatus ser ikke ut til å være overført til Oppdrag (sist oppdatert $oppdatert). AktørId $aktørId, Organisasjonsnummer $organisasjonsnummer (Versjon2)")
                     if (id in forkast) {
-                        sikkerLogg.info("Setter status på $id til ")
-                        rarAnnullering as ObjectNode
-                        rarAnnullering.put("status","FORKASTET")
+                        sikkerLogg.info("Setter status på Utbetaling $id til FORKASTET og setter nytt oppdatert-tidspunkt")
+                        manglerOverføring as ObjectNode
+                        manglerOverføring.put("status","FORKASTET")
+                        manglerOverføring.put("oppdatert", "${forkastetTidspunkt()}")
                     }
                 }
         }
@@ -32,5 +38,9 @@ internal class V285LoggeRareAnnulleringer(private val forkast: Set<String> = set
 
     private companion object {
         private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
+        private val JsonNode.oppdragManglerOverføring get(): Boolean {
+            path("linjer").takeUnless { it.isEmpty } ?: return false
+            return path("status").isNull
+        }
     }
 }
--- sykepenger-model/src/test/kotlin/no/nav/helse/serde/migration/V285LoggeRareAnnulleringerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/serde/migration/V285LoggeRareAnnulleringerTest.kt
@@ -1,8 +1,12 @@
 package no.nav.helse.serde.migration
 
+import java.time.LocalDateTime
 import org.junit.jupiter.api.Test
 
-internal class V285LoggeRareAnnulleringerTest: MigrationTest(V285LoggeRareAnnulleringer(forkast = setOf("52fc8116-1757-45bc-8ca1-d333b33d3496"))) {
+internal class V285LoggeRareAnnulleringerTest: MigrationTest(V285LoggeRareAnnulleringer(
+    forkast = setOf("52fc8116-1757-45bc-8ca1-d333b33d3496"),
+    forkastetTidspunkt = { LocalDateTime.parse("2024-01-23T08:15:00.00001") }
+)) {
 
     @Test
     fun `forkaster annulleringer som ikke er sendt til oppdrag`() {
