893054ab69c7713c074fadd1f11614fc42d7c536
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-01-24 09:49:48 +0100

    Bumper snapshot-versjon for å tvinge bort bruk av UberegnetVilkårsprøvdPeriode
    e99baf43837f557035baf36dac3669e26e1b4499
    
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/api/SpeilBuilder.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/api/SpeilBuilder.kt
@@ -20,7 +20,7 @@ internal class SpeilBuilder : AbstractBuilder() {
         /* Økes for å signalisere til spesialist at strukturen i snapshot'et
          * på et eller annet vis har endret seg, og at spesialist derfor må oppdatere cachede snapshots løpende
          */
-        const val SNAPSHOT_VERSJON = 51
+        const val SNAPSHOT_VERSJON = 52
     }
 
     private lateinit var personBuilder: PersonBuilder
