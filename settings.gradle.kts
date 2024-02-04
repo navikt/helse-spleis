34136cfb630ba7cc0499908b7d3ceea1c0c60043
david steinsland <david@davidsteinsland.net>
2024-02-04 19:58:15 +0100

    setter opp prestophook
    rapids-and-rivers har en smule mer graceful shutdown med prestophook

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("testcontainers", "1.19.3")
-            version("rapids-and-rivers", "2024010209171704183456.6d035b91ffb4")
+            version("rapids-and-rivers", "2024020415331707057200.009c5dd149c5")
             version("postgres", "42.7.1")
             version("hikari", "5.1.0")
             version("kotliquery", "1.9.0")
