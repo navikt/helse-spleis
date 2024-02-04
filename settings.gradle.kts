a584f9203491d1dfb3d58869589386c475b3399a
david steinsland <david@davidsteinsland.net>
2024-02-04 22:00:57 +0100

    bumper r&r

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("testcontainers", "1.19.3")
-            version("rapids-and-rivers", "2024020421191707077994.337b557194f1")
+            version("rapids-and-rivers", "2024020421571707080262.b74e641fd6dd")
             version("postgres", "42.7.1")
             version("hikari", "5.1.0")
             version("kotliquery", "1.9.0")
