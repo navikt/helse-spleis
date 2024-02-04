4ef74dcbfcd27526d99db8ddf055aa6941f26f5e
david steinsland <david@davidsteinsland.net>
2024-02-04 20:32:49 +0100

    gjør TestHendelseMediator trådsikker

--- sykepenger-mediators/src/test/kotlin/no/nav/helse/spleis/mediator/TestHendelseMediator.kt
+++ sykepenger-mediators/src/test/kotlin/no/nav/helse/spleis/mediator/TestHendelseMediator.kt
@@ -74,108 +74,108 @@ import no.nav.helse.spleis.meldinger.model.VilkårsgrunnlagMessage
 import no.nav.helse.spleis.meldinger.model.YtelserMessage
 
 internal class TestHendelseMediator : IHendelseMediator {
+    val lestNySøknad get() = lestNySøknadVerdi.get()
+    val lestNySøknadFrilans get() = lestNySøknadFrilansVerdi.get()
+    val lestNySøknadSelvstendig get() = lestNySøknadSelvstendigVerdi.get()
+    val lestNySøknadArbeidsledig get() = lestNySøknadArbeidsledigVerdi.get()
+    val lestSendtSøknadArbeidsgiver get() = lestSendtSøknadArbeidsgiverVerdi.get()
+    val lestSendtSøknad get() = lestSendtSøknadVerdi.get()
+    val lestSendtSøknadFrilans get() = lestSendtSøknadFrilansVerdi.get()
+    val lestSendtSøknadSelvstendig get() = lestSendtSøknadSelvstendigVerdi.get()
+    val lestSendtSøknadArbeidsledig get() = lestSendtSøknadArbeidsledigVerdi.get()
+    val lestInntektsmelding get() = lestInntektsmeldingVerdi.get()
+    val lestInntektsmeldingReplay get() = lestInntektsmeldingReplayVerdi.get()
+    val lestDødsmelding get() = lestDødsmeldingVerdi.get()
+    val lestPåminnelse get() = lestPåminnelseVerdi.get()
+    val lestPersonpåminnelse get() = lestPersonpåminnelseVerdi.get()
+    val lestAnmodningOmForkasting get() = lestAnmodningOmForkastingVerdi.get()
+    val lestutbetalingpåminnelse get() = lestutbetalingpåminnelseVerdi.get()
+    val lestUtbetalingshistorikk get() = lestUtbetalingshistorikkVerdi.get()
+    val lestUtbetalingshistorikkForFeriepenger get() = lestUtbetalingshistorikkForFeriepengerVerdi.get()
+    val lestYtelser get() = lestYtelserVerdi.get()
+    val lestVilkårsgrunnlag get() = lestVilkårsgrunnlagVerdi.get()
+    val lestUtbetalingsgrunnlag get() = lestUtbetalingsgrunnlagVerdi.get()
+    val lestSimulering get() = lestSimuleringVerdi.get()
+    val lestUtbetalingsgodkjenning get() = lestUtbetalingsgodkjenningVerdi.get()
+    val lestUtbetaling get() = lestUtbetalingVerdi.get()
+    val lestAnnullerUtbetaling get() = lestAnnullerUtbetalingVerdi.get()
+    val lestAvstemming get() = lestAvstemmingVerdi.get()
+    val lestMigrate get() = lestMigrateVerdi.get()
+    val lestOverstyrTidslinje get() = lestOverstyrTidslinjeVerdi.get()
+    val lestOverstyrInntekt get() = lestOverstyrInntektVerdi.get()
+    val lestOverstyrArbeidsgiveropplysningser get() = lestOverstyrArbeidsgiveropplysningserVerdi.get()
+    val lestOverstyrArbeidsforhold get() = lestOverstyrArbeidsforholdVerdi.get()
+    val lestReplayHendelser get() = lestReplayHendelserVerdi.get()
+    val lestGrunnbeløpsregulering get() = lestGrunnbeløpsreguleringVerdi.get()
+    val lestInfotrygdendring get() = lestInfotrygdendringVerdi.get()
+    val utbetalingshistorikkEtterInfotrygdendringMessage get() = utbetalingshistorikkEtterInfotrygdendringMessageVerdi.get()
+    val lestForkastSykmeldingsperioderMessage get() = lestForkastSykmeldingsperioderMessageVerdi.get()
 
-    internal var lestNySøknad = false
-        private set
-    internal var lestNySøknadFrilans = false
-        private set
-    internal var lestNySøknadSelvstendig = false
-        private set
-    internal var lestNySøknadArbeidsledig = false
-        private set
-    internal var lestSendtSøknadArbeidsgiver = false
-        private set
-    internal var lestSendtSøknad = false
-        private set
-    internal var lestSendtSøknadFrilans = false
-        private set
-    internal var lestSendtSøknadSelvstendig = false
-        private set
-    internal var lestSendtSøknadArbeidsledig = false
-        private set
-    internal var lestInntektsmelding = false
-        private set
-    internal var lestInntektsmeldingReplay = false
-        private set
-    internal var lestDødsmelding = false
-        private set
-    internal var lestPåminnelse = false
-        private set
-    internal var lestPersonpåminnelse = false
-        private set
-    internal var lestAnmodningOmForkasting = false
-        private set
-    internal var lestutbetalingpåminnelse = false
-        private set
-    internal var lestUtbetalingshistorikk = false
-        private set
-    internal var lestUtbetalingshistorikkForFeriepenger = false
-        private set
-    internal var lestYtelser = false
-        private set
-    internal var lestVilkårsgrunnlag = false
-        private set
-    internal var lestUtbetalingsgrunnlag = false
-        private set
-    internal var lestSimulering = false
-        private set
-    internal var lestUtbetalingsgodkjenning = false
-        private set
-    internal var lestUtbetaling = false
-        private set
-    internal var lestAnnullerUtbetaling = false
-        private set
-    internal var lestAvstemming = false
-        private set
-    internal var lestMigrate = false
-        private set
-    internal var lestOverstyrTidslinje = false
-        private set
-    internal var lestOverstyrInntekt = false
-        private set
-    internal var lestOverstyrArbeidsgiveropplysningser = false
-        private set
-    internal var lestOverstyrArbeidsforhold = false
-        private set
-    internal var lestReplayHendelser = false
-        private set
-    internal var lestGrunnbeløpsregulering = false
-        private set
-    internal var lestInfotrygdendring = false
-        private set
-    internal var utbetalingshistorikkEtterInfotrygdendringMessage = false
-        private set
-    internal var lestForkastSykmeldingsperioderMessage = false
-        private set
+    private val lestNySøknadVerdi = ThreadLocal.withInitial { false }
+    private val lestNySøknadFrilansVerdi = ThreadLocal.withInitial { false }
+    private val lestNySøknadSelvstendigVerdi = ThreadLocal.withInitial { false }
+    private val lestNySøknadArbeidsledigVerdi = ThreadLocal.withInitial { false }
+    private val lestSendtSøknadArbeidsgiverVerdi = ThreadLocal.withInitial { false }
+    private val lestSendtSøknadVerdi = ThreadLocal.withInitial { false }
+    private val lestSendtSøknadFrilansVerdi = ThreadLocal.withInitial { false }
+    private val lestSendtSøknadSelvstendigVerdi = ThreadLocal.withInitial { false }
+    private val lestSendtSøknadArbeidsledigVerdi = ThreadLocal.withInitial { false }
+    private val lestInntektsmeldingVerdi = ThreadLocal.withInitial { false }
+    private val lestInntektsmeldingReplayVerdi = ThreadLocal.withInitial { false }
+    private val lestDødsmeldingVerdi = ThreadLocal.withInitial { false }
+    private val lestPåminnelseVerdi = ThreadLocal.withInitial { false }
+    private val lestPersonpåminnelseVerdi = ThreadLocal.withInitial { false }
+    private val lestAnmodningOmForkastingVerdi = ThreadLocal.withInitial { false }
+    private val lestutbetalingpåminnelseVerdi = ThreadLocal.withInitial { false }
+    private val lestUtbetalingshistorikkVerdi = ThreadLocal.withInitial { false }
+    private val lestUtbetalingshistorikkForFeriepengerVerdi = ThreadLocal.withInitial { false }
+    private val lestYtelserVerdi = ThreadLocal.withInitial { false }
+    private val lestVilkårsgrunnlagVerdi = ThreadLocal.withInitial { false }
+    private val lestUtbetalingsgrunnlagVerdi = ThreadLocal.withInitial { false }
+    private val lestSimuleringVerdi = ThreadLocal.withInitial { false }
+    private val lestUtbetalingsgodkjenningVerdi = ThreadLocal.withInitial { false }
+    private val lestUtbetalingVerdi = ThreadLocal.withInitial { false }
+    private val lestAnnullerUtbetalingVerdi = ThreadLocal.withInitial { false }
+    private val lestAvstemmingVerdi = ThreadLocal.withInitial { false }
+    private val lestMigrateVerdi = ThreadLocal.withInitial { false }
+    private val lestOverstyrTidslinjeVerdi = ThreadLocal.withInitial { false }
+    private val lestOverstyrInntektVerdi = ThreadLocal.withInitial { false }
+    private val lestOverstyrArbeidsgiveropplysningserVerdi = ThreadLocal.withInitial { false }
+    private val lestOverstyrArbeidsforholdVerdi = ThreadLocal.withInitial { false }
+    private val lestReplayHendelserVerdi = ThreadLocal.withInitial { false }
+    private val lestGrunnbeløpsreguleringVerdi = ThreadLocal.withInitial { false }
+    private val lestInfotrygdendringVerdi = ThreadLocal.withInitial { false }
+    private val utbetalingshistorikkEtterInfotrygdendringMessageVerdi = ThreadLocal.withInitial { false }
+    private val lestForkastSykmeldingsperioderMessageVerdi = ThreadLocal.withInitial { false }
 
     fun reset() {
-        lestNySøknad = false
-        lestNySøknadFrilans = false
-        lestNySøknadSelvstendig = false
-        lestNySøknadArbeidsledig = false
-        lestSendtSøknadArbeidsgiver = false
-        lestSendtSøknad = false
-        lestSendtSøknadFrilans = false
-        lestSendtSøknadSelvstendig = false
-        lestSendtSøknadArbeidsledig = false
-        lestInntektsmelding = false
-        lestPåminnelse = false
-        lestPersonpåminnelse = false
-        lestutbetalingpåminnelse = false
-        lestUtbetalingshistorikk = false
-        lestYtelser = false
-        lestVilkårsgrunnlag = false
-        lestUtbetalingsgrunnlag = false
-        lestSimulering = false
-        lestUtbetalingsgodkjenning = false
-        lestUtbetaling = false
-        lestAnnullerUtbetaling = false
-        lestAvstemming = false
-        lestOverstyrTidslinje = false
-        lestGrunnbeløpsregulering = false
-        lestInfotrygdendring = false
-        utbetalingshistorikkEtterInfotrygdendringMessage = false
-        lestAnmodningOmForkasting = false
+        lestNySøknadVerdi.remove()
+        lestNySøknadFrilansVerdi.remove()
+        lestNySøknadSelvstendigVerdi.remove()
+        lestNySøknadArbeidsledigVerdi.remove()
+        lestSendtSøknadArbeidsgiverVerdi.remove()
+        lestSendtSøknadVerdi.remove()
+        lestSendtSøknadFrilansVerdi.remove()
+        lestSendtSøknadSelvstendigVerdi.remove()
+        lestSendtSøknadArbeidsledigVerdi.remove()
+        lestInntektsmeldingVerdi.remove()
+        lestPåminnelseVerdi.remove()
+        lestPersonpåminnelseVerdi.remove()
+        lestutbetalingpåminnelseVerdi.remove()
+        lestUtbetalingshistorikkVerdi.remove()
+        lestYtelserVerdi.remove()
+        lestVilkårsgrunnlagVerdi.remove()
+        lestUtbetalingsgrunnlagVerdi.remove()
+        lestSimuleringVerdi.remove()
+        lestUtbetalingsgodkjenningVerdi.remove()
+        lestUtbetalingVerdi.remove()
+        lestAnnullerUtbetalingVerdi.remove()
+        lestAvstemmingVerdi.remove()
+        lestOverstyrTidslinjeVerdi.remove()
+        lestGrunnbeløpsreguleringVerdi.remove()
+        lestInfotrygdendringVerdi.remove()
+        utbetalingshistorikkEtterInfotrygdendringMessageVerdi.remove()
+        lestAnmodningOmForkastingVerdi.remove()
     }
 
     override fun behandle(message: HendelseMessage, context: MessageContext) {
@@ -189,7 +189,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestNySøknad = true
+        lestNySøknadVerdi.set(true)
     }
 
     override fun behandle(
@@ -199,7 +199,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestNySøknadFrilans = true
+        lestNySøknadFrilansVerdi.set(true)
     }
 
     override fun behandle(
@@ -209,7 +209,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestNySøknadSelvstendig = true
+        lestNySøknadSelvstendigVerdi.set(true)
     }
 
     override fun behandle(
@@ -219,7 +219,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestNySøknadArbeidsledig = true
+        lestNySøknadArbeidsledigVerdi.set(true)
     }
 
     override fun behandle(
@@ -229,7 +229,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestSendtSøknadFrilans = true
+        lestSendtSøknadFrilansVerdi.set(true)
     }
 
     override fun behandle(
@@ -239,7 +239,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestSendtSøknadSelvstendig = true
+        lestSendtSøknadSelvstendigVerdi.set(true)
     }
 
     override fun behandle(
@@ -249,7 +249,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestSendtSøknadArbeidsledig = true
+        lestSendtSøknadArbeidsledigVerdi.set(true)
     }
 
     override fun behandle(
@@ -259,7 +259,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestSendtSøknadArbeidsgiver = true
+        lestSendtSøknadArbeidsgiverVerdi.set(true)
     }
 
     override fun behandle(
@@ -269,19 +269,19 @@ internal class TestHendelseMediator : IHendelseMediator {
         context: MessageContext,
         historiskeFolkeregisteridenter: Set<Personidentifikator>
     ) {
-        lestSendtSøknad = true
+        lestSendtSøknadVerdi.set(true)
     }
 
     override fun behandle(personopplysninger: Personopplysninger, message: InntektsmeldingMessage, inntektsmelding: Inntektsmelding, context: MessageContext) {
-        lestInntektsmelding = true
+        lestInntektsmeldingVerdi.set(true)
     }
 
     override fun behandle(message: InntektsmeldingReplayMessage, inntektsmelding: InntektsmeldingReplay, context: MessageContext) {
-        lestInntektsmeldingReplay = true
+        lestInntektsmeldingReplayVerdi.set(true)
     }
 
     override fun behandle(message: DødsmeldingMessage, dødsmelding: Dødsmelding, context: MessageContext) {
-        lestDødsmelding = true
+        lestDødsmeldingVerdi.set(true)
     }
 
     override fun behandle(
@@ -300,15 +300,15 @@ internal class TestHendelseMediator : IHendelseMediator {
     ) {}
 
     override fun behandle(message: UtbetalingpåminnelseMessage, påminnelse: Utbetalingpåminnelse, context: MessageContext) {
-        lestutbetalingpåminnelse = true
+        lestutbetalingpåminnelseVerdi.set(true)
     }
 
     override fun behandle(message: PåminnelseMessage, påminnelse: Påminnelse, context: MessageContext) {
-        lestPåminnelse = true
+        lestPåminnelseVerdi.set(true)
     }
 
     override fun behandle(message: PersonPåminnelseMessage, påminnelse: PersonPåminnelse, context: MessageContext) {
-        lestPersonpåminnelse = true
+        lestPersonpåminnelseVerdi.set(true)
     }
 
     override fun behandle(
@@ -316,63 +316,63 @@ internal class TestHendelseMediator : IHendelseMediator {
         anmodning: AnmodningOmForkasting,
         context: MessageContext
     ) {
-        lestAnmodningOmForkasting = true
+        lestAnmodningOmForkastingVerdi.set(true)
     }
 
     override fun behandle(message: UtbetalingshistorikkMessage, utbetalingshistorikk: Utbetalingshistorikk, context: MessageContext) {
-        lestUtbetalingshistorikk = true
+        lestUtbetalingshistorikkVerdi.set(true)
     }
 
     override fun behandle(message: UtbetalingshistorikkForFeriepengerMessage, utbetalingshistorikkForFeriepenger: UtbetalingshistorikkForFeriepenger, context: MessageContext) {
-        lestUtbetalingshistorikkForFeriepenger = true
+        lestUtbetalingshistorikkForFeriepengerVerdi.set(true)
     }
 
     override fun behandle(message: YtelserMessage, ytelser: Ytelser, context: MessageContext) {
-        lestYtelser = true
+        lestYtelserVerdi.set(true)
     }
 
     override fun behandle(message: VilkårsgrunnlagMessage, vilkårsgrunnlag: Vilkårsgrunnlag, context: MessageContext) {
-        lestVilkårsgrunnlag = true
+        lestVilkårsgrunnlagVerdi.set(true)
     }
 
     override fun behandle(message: UtbetalingsgodkjenningMessage, utbetalingsgodkjenning: Utbetalingsgodkjenning, context: MessageContext) {
-        lestUtbetalingsgodkjenning = true
+        lestUtbetalingsgodkjenningVerdi.set(true)
     }
 
     override fun behandle(message: UtbetalingMessage, utbetaling: UtbetalingHendelse, context: MessageContext) {
-        lestUtbetaling = true
+        lestUtbetalingVerdi.set(true)
     }
 
     override fun behandle(message: SimuleringMessage, simulering: Simulering, context: MessageContext) {
-        lestSimulering = true
+        lestSimuleringVerdi.set(true)
     }
 
     override fun behandle(message: AnnulleringMessage, annullerUtbetaling: AnnullerUtbetaling, context: MessageContext) {
-        lestAnnullerUtbetaling = true
+        lestAnnullerUtbetalingVerdi.set(true)
     }
 
     override fun behandle(message: AvstemmingMessage, avstemming: Avstemming, context: MessageContext) {
-        lestAvstemming = true
+        lestAvstemmingVerdi.set(true)
     }
 
     override fun behandle(message: MigrateMessage, migrate: Migrate, context: MessageContext) {
-        lestMigrate = true
+        lestMigrateVerdi.set(true)
     }
 
     override fun behandle(message: OverstyrTidslinjeMessage, overstyrTidslinje: OverstyrTidslinje, context: MessageContext) {
-        lestOverstyrTidslinje = true
+        lestOverstyrTidslinjeVerdi.set(true)
     }
 
     override fun behandle(message: OverstyrArbeidsgiveropplysningerMessage, overstyrArbeidsgiveropplysninger: OverstyrArbeidsgiveropplysninger, context: MessageContext) {
-        lestOverstyrArbeidsgiveropplysningser = true
+        lestOverstyrArbeidsgiveropplysningserVerdi.set(true)
     }
 
     override fun behandle(message: OverstyrArbeidsforholdMessage, overstyrArbeidsforhold: OverstyrArbeidsforhold, context: MessageContext) {
-        lestOverstyrArbeidsforhold = true
+        lestOverstyrArbeidsforholdVerdi.set(true)
     }
 
     override fun behandle(message: GrunnbeløpsreguleringMessage, grunnbeløpsregulering: Grunnbeløpsregulering, context: MessageContext) {
-        lestGrunnbeløpsregulering = true
+        lestGrunnbeløpsreguleringVerdi.set(true)
     }
 
     override fun behandle(
@@ -380,7 +380,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         infotrygdEndring: Infotrygdendring,
         context: MessageContext
     ) {
-        lestInfotrygdendring = true
+        lestInfotrygdendringVerdi.set(true)
     }
 
     override fun behandle(
@@ -388,7 +388,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         utbetalingshistorikkEtterInfotrygdendring: UtbetalingshistorikkEtterInfotrygdendring,
         context: MessageContext
     ) {
-        utbetalingshistorikkEtterInfotrygdendringMessage = true
+        utbetalingshistorikkEtterInfotrygdendringMessageVerdi.set(true)
     }
 
     override fun behandle(
@@ -396,7 +396,7 @@ internal class TestHendelseMediator : IHendelseMediator {
         forkastSykmeldingsperioder: ForkastSykmeldingsperioder,
         context: MessageContext
     ) {
-        lestForkastSykmeldingsperioderMessage = true
+        lestForkastSykmeldingsperioderMessageVerdi.set(true)
     }
 
     override fun behandle(
