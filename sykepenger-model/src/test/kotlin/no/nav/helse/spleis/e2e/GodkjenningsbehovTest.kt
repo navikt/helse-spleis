56354966640cd5f9e016c553658ddbba65cdda01
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-02-06 10:48:28 +0100

    Tester for feil vilkårsgrunnlagId i godkjenningsbehov 🥴
    Co-authored-by: David Steinsland <david@davidsteinsland.net>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/GodkjenningsbehovTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/GodkjenningsbehovTest.kt
@@ -2,19 +2,16 @@ package no.nav.helse.spleis.e2e
 
 import java.time.LocalDate
 import java.util.UUID
+import no.nav.helse.assertForventetFeil
 import no.nav.helse.etterspurtBehov
 import no.nav.helse.februar
-import no.nav.helse.hendelser.InntektForSykepengegrunnlag
-import no.nav.helse.hendelser.Sykmeldingsperiode
+import no.nav.helse.hendelser.Inntektsmelding
 import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
-import no.nav.helse.hendelser.Vilkårsgrunnlag
-import no.nav.helse.hendelser.Vilkårsgrunnlag.Arbeidsforhold.Arbeidsforholdtype
 import no.nav.helse.hendelser.til
 import no.nav.helse.inspectors.inspektør
 import no.nav.helse.inspectors.personLogg
 import no.nav.helse.januar
 import no.nav.helse.mars
-import no.nav.helse.oktober
 import no.nav.helse.person.IdInnhenter
 import no.nav.helse.person.TilstandType
 import no.nav.helse.person.TilstandType.AVSLUTTET
@@ -24,22 +21,74 @@ import no.nav.helse.person.TilstandType.AVVENTER_GODKJENNING_REVURDERING
 import no.nav.helse.person.TilstandType.AVVENTER_HISTORIKK_REVURDERING
 import no.nav.helse.person.TilstandType.AVVENTER_INNTEKTSMELDING
 import no.nav.helse.person.TilstandType.AVVENTER_REVURDERING
+import no.nav.helse.person.TilstandType.AVVENTER_SIMULERING
 import no.nav.helse.person.TilstandType.AVVENTER_VILKÅRSPRØVING
 import no.nav.helse.person.TilstandType.AVVENTER_VILKÅRSPRØVING_REVURDERING
 import no.nav.helse.person.TilstandType.TIL_INFOTRYGD
 import no.nav.helse.person.aktivitetslogg.Aktivitet
 import no.nav.helse.person.aktivitetslogg.Varselkode.RV_UT_24
-import no.nav.helse.september
+import no.nav.helse.person.nullstillTilstandsendringer
 import no.nav.helse.sisteBehov
 import no.nav.helse.utbetalingslinjer.Utbetalingstatus.IKKE_GODKJENT
 import no.nav.helse.utbetalingslinjer.Utbetalingstatus.IKKE_UTBETALT
+import no.nav.helse.økonomi.Inntekt
 import no.nav.helse.økonomi.Prosentdel.Companion.prosent
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class GodkjenningsbehovTest : AbstractEndToEndTest() {
+
+    @Test
+    fun `sender med feil vilkårsgrunnlagId i påminnet godkjenningsbehov om det har kommet nytt vilkårsgrunnlag med endring _senere_ enn perioden`() {
+        tilGodkjenning(1.januar, 31.januar, a1)
+        val vilkårsgrunnlagId1 = vilkårsgrunnlagIdFraVilkårsgrunnlaghistorikken(1.januar)
+        assertEquals(vilkårsgrunnlagId1, vilkårsgrunnlagIdFraSisteGodkjenningsbehov(1.vedtaksperiode))
+        håndterSøknad(Sykdom(1.februar, 28.februar, 100.prosent))
+        håndterSøknad(Sykdom(1.mars, 31.mars, 100.prosent))
+        nullstillTilstandsendringer()
+
+        håndterInntektsmelding(listOf(1.januar til 16.januar), førsteFraværsdag = 1.mars, refusjon = Inntektsmelding.Refusjon(Inntekt.INGEN, null))
+        assertTilstander(1.vedtaksperiode, AVVENTER_GODKJENNING) // Reberegnes ikke ettersom endringen gjelder fra og med 1.mars
+        val vilkårsgrunnlagId2 = vilkårsgrunnlagIdFraVilkårsgrunnlaghistorikken(1.januar)
+        assertNotEquals(vilkårsgrunnlagId1, vilkårsgrunnlagId2) // IM lager nytt innslag i vilkårsgrunnlaghistorikken pga nye refusjonsopplysninger
+
+        håndterPåminnelse(1.vedtaksperiode, AVVENTER_GODKJENNING)
+
+        assertForventetFeil(
+            forklaring = "VilkårsgrunnlagId hentes fra historikken, ikke det som ligger i generasjonen",
+            nå = { assertEquals(vilkårsgrunnlagId2, vilkårsgrunnlagIdFraSisteGodkjenningsbehov(1.vedtaksperiode)) },
+            ønsket = { assertEquals(vilkårsgrunnlagId1, vilkårsgrunnlagIdFraSisteGodkjenningsbehov(1.vedtaksperiode)) }
+        )
+    }
+    @Test
+    fun `sender med feil vilkårsgrunnlagId i første godkjenningsbehov om det har kommet nytt vilkårsgrunnlag med endring _senere_ enn perioden mens den står i avventer simulering`() {
+        håndterSøknad(Sykdom(1.januar, 31.januar, 100.prosent))
+        håndterSøknad(Sykdom(1.februar, 28.februar, 100.prosent))
+        håndterSøknad(Sykdom(1.mars, 31.mars, 100.prosent))
+        håndterInntektsmelding(listOf(1.januar til 16.januar))
+        håndterVilkårsgrunnlag(1.vedtaksperiode)
+        val vilkårsgrunnlagId1 = vilkårsgrunnlagIdFraVilkårsgrunnlaghistorikken(1.januar)
+        håndterYtelser(1.vedtaksperiode)
+        nullstillTilstandsendringer()
+
+        håndterInntektsmelding(listOf(1.januar til 16.januar), førsteFraværsdag = 1.mars, refusjon = Inntektsmelding.Refusjon(Inntekt.INGEN, null))
+
+        assertTilstander(1.vedtaksperiode, AVVENTER_SIMULERING) // Reberegnes ikke ettersom endringen gjelder fra og med 1.mars
+        val vilkårsgrunnlagId2 = vilkårsgrunnlagIdFraVilkårsgrunnlaghistorikken(1.januar)
+        assertNotEquals(vilkårsgrunnlagId1, vilkårsgrunnlagId2) // IM lager nytt innslag i vilkårsgrunnlaghistorikken pga nye refusjonsopplysninger
+
+        håndterSimulering(1.vedtaksperiode)
+        assertTilstander(1.vedtaksperiode, AVVENTER_SIMULERING, AVVENTER_GODKJENNING)
+
+        assertForventetFeil(
+            forklaring = "VilkårsgrunnlagId hentes fra historikken, ikke det som ligger i generasjonen",
+            nå = { assertEquals(vilkårsgrunnlagId2, vilkårsgrunnlagIdFraSisteGodkjenningsbehov(1.vedtaksperiode)) },
+            ønsket = { assertEquals(vilkårsgrunnlagId1, vilkårsgrunnlagIdFraSisteGodkjenningsbehov(1.vedtaksperiode)) }
+        )
+    }
 
     @Test
     fun `godkjenningsbehov som ikke kan avvises blir forsøkt avvist`() {
@@ -190,4 +239,13 @@ internal class GodkjenningsbehovTest : AbstractEndToEndTest() {
         behov = Aktivitet.Behov.Behovtype.Godkjenning,
         felt = "kanAvvises"
     )!!
+
+    private fun vilkårsgrunnlagIdFraSisteGodkjenningsbehov(vedtaksperiode: IdInnhenter, orgnummer: String = a1) = hendelselogg.etterspurtBehov<String>(
+        vedtaksperiodeId = vedtaksperiode.id(orgnummer),
+        behov = Aktivitet.Behov.Behovtype.Godkjenning,
+        felt = "vilkårsgrunnlagId"
+    )!!.let { UUID.fromString(it) }
+
+    private fun vilkårsgrunnlagIdFraVilkårsgrunnlaghistorikken(skjæringstidspunkt: LocalDate, orgnummer: String = a1)
+        = inspektør(orgnummer).vilkårsgrunnlag(skjæringstidspunkt)!!.inspektør.vilkårsgrunnlagId
 }
