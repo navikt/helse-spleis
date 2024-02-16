a0b8fa84887e13ae10bc800c67fcdbb8036c86ee
david steinsland <david@davidsteinsland.net>
2024-02-16 07:50:29 +0100

    check-en slo til, da må vi først vurdere omfang før løsning

--- sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V289AvsluttetTidspunktForkastedeGenerasjoner.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/serde/migration/V289AvsluttetTidspunktForkastedeGenerasjoner.kt
@@ -11,18 +11,21 @@ internal class V289AvsluttetTidspunktForkastedeGenerasjoner: JsonMigration(versi
 
     override fun doMigration(jsonNode: ObjectNode, meldingerSupplier: MeldingerSupplier) {
         val aktørId = jsonNode.path("aktørId").asText()
+        val fnr = jsonNode.path("fødselsnummer").asText()
         jsonNode.path("arbeidsgivere").forEach { arbeidsgiver ->
             val orgnr = arbeidsgiver.path("organisasjonsnummer").asText()
             arbeidsgiver.path("forkastede").forEach { forkastet ->
-                migrerVedtaksperiode(aktørId, orgnr, forkastet.path("vedtaksperiode"))
+                migrerVedtaksperiode(fnr, aktørId, orgnr, forkastet.path("vedtaksperiode"))
             }
         }
     }
 
-    private fun migrerVedtaksperiode(aktørId: String, orgnr: String, vedtaksperiode: JsonNode) {
+    private fun migrerVedtaksperiode(fnr: String, aktørId: String, orgnr: String, vedtaksperiode: JsonNode) {
         val generasjonerNode = vedtaksperiode.path("generasjoner") as ArrayNode
         val sisteGenerasjon = generasjonerNode.last() as ObjectNode
-        check(sisteGenerasjon.path("tilstand").asText() == "TIL_INFOTRYGD")
+        if (sisteGenerasjon.path("tilstand").asText() == "TIL_INFOTRYGD")
+            return sikkerLogg.info("[V289] siste generasjon er ikke TIL_INFOTRYGD for ${vedtaksperiode.path("fom").asText()} - ${vedtaksperiode.path("tom").asText()} {}, {}, {}", kv("aktørId", aktørId), kv("orgnr", orgnr), kv("vedtaksperiodeId", vedtaksperiode.path("id").asText()), kv("fødselsnummer", fnr))
+
         if (sisteGenerasjon.hasNonNull("avsluttet")) return
         sikkerLogg.info("[V289] setter avsluttettidspunkt på generasjon ${sisteGenerasjon.path("id").asText()} for {}", kv("aktørId", aktørId))
         sisteGenerasjon.put("avsluttet", sisteGenerasjon.path("endringer").last().path("tidsstempel").asText())
