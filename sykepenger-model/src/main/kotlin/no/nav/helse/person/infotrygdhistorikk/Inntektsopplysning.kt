5f41ab403ec3b409051fa3a3bd2fd09bd9c0df93
david steinsland <david@davidsteinsland.net>
2024-03-13 08:27:10 +0100

    hensyntar orgnr i sorteringen
    får endel falske positiver på "ulikhet i json" mellom ny og gammel

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/infotrygdhistorikk/Inntektsopplysning.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/infotrygdhistorikk/Inntektsopplysning.kt
@@ -36,7 +36,7 @@ class Inntektsopplysning private constructor(
 
     internal companion object {
         internal fun sorter(inntekter: List<Inntektsopplysning>) =
-            inntekter.sortedWith(compareBy({ it.sykepengerFom }, { it.inntekt }, { it.hashCode() }))
+            inntekter.sortedWith(compareBy({ it.sykepengerFom }, { it.inntekt }, { it.orgnummer }, { it.hashCode() }))
 
         internal fun ferdigInntektsopplysning(
             orgnummer: String,
