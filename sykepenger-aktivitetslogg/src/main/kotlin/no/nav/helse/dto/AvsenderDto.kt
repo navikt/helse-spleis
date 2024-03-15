45861829598743bfa3b8bac4d3feccb73b358b47
david steinsland <david@davidsteinsland.net>
2024-03-15 12:21:26 +0100

    fjerner fil som skulle vært fjernet!

--- sykepenger-aktivitetslogg/src/main/kotlin/no/nav/helse/dto/AvsenderDto.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.helse.dto
-
-sealed class AvsenderDto {
-    data object SYKMELDT : AvsenderDto()
-    data object ARBEIDSGIVER : AvsenderDto()
-    data object SAKSBEHANDLER : AvsenderDto()
-    data object SYSTEM : AvsenderDto()
-}
