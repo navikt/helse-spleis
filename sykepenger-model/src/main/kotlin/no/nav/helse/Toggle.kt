7f2bd7d833c7f89e9b3841114a06a1b29cd13f02
Hege Haavaldsen <hege.haavaldsen@nav.no>
2024-05-14 14:28:25 +0200

    Prepper feriepengekjøring
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -41,7 +41,7 @@ class Toggle private constructor(enabled: Boolean) {
     internal operator fun plus(toggle: Toggle) = listOf(this, toggle)
 
     companion object {
-        val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", false)
+        val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", true)
         val Spekemat = fraEnv("SPEKEMAT_ENABLED", true)
 
         fun fraEnv(navn: String, defaultValue: Boolean) = Toggle(System.getenv(navn)?.lowercase()?.toBooleanStrictOrNull() ?: defaultValue)
