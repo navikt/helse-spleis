ac424548baef01565633448fab6e4afb62a31ae8
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-06-12 17:16:33 +0200

    Orker ikke ikke følge opp en IllegalStateException i kveld 🫠
    Så logger bare

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Behandlinger.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Behandlinger.kt
@@ -870,9 +870,10 @@ internal class Behandlinger private constructor(behandlinger: List<Behandling>) 
         internal fun validerFerdigBehandlet(hendelse: Hendelse) = tilstand.validerFerdigBehandlet(this, hendelse)
         private fun valideringFeilet(hendelse: Hendelse, feil: String) {
             // Om de er hendelsen vi håndterer nå som har skapt situasjonen feiler vi fremfor å gå videre.
-            if (kilde.meldingsreferanseId == hendelse.meldingsreferanseId()) error(feil)
+            //if (kilde.meldingsreferanseId == hendelse.meldingsreferanseId()) error(feil)
+            if (kilde.meldingsreferanseId == hendelse.meldingsreferanseId()) hendelse.info("Ny ugyldig behandling på en ferdig behandlet vedtaksperiode: $feil")
             // Om det er krøll fra tidligere logger vi bare
-            else hendelse.info(feil)
+            else hendelse.info("Eksisterende ugyldig behandling på en ferdig behandlet vedtaksperiode: $feil")
         }
 
         internal companion object {
