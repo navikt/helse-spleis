3e1ca87815aeddf63409e6b9884f5c9e7bdda618
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2024-06-20 08:40:45 +0200

    flertallifiserer en feilmelding

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/monitorering/Sjekk.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/monitorering/Sjekk.kt
@@ -13,7 +13,7 @@ internal class RegelmessigAvstemming(private val manglerAvstemming: () -> Int): 
     override fun sjekk(): Pair<Level, String>? {
         val mangler = manglerAvstemming()
         if (mangler == 0) return null
-        return Level.ERROR to "\nDet er $mangler personer som ikke er avstemt på over en måned!\n\n- Deres erbødig SPleis :bender_dance:"
+        return Level.ERROR to "\nDet er $mangler ${"person".flertallifiser(mangler)} som ikke er avstemt på over en måned!\n\n- Deres erbødig SPleis :bender_dance:"
     }
 
     override fun skalSjekke(nå: LocalDateTime): Boolean {
@@ -21,3 +21,5 @@ internal class RegelmessigAvstemming(private val manglerAvstemming: () -> Int): 
         return nå.hour == 8 && nå.minute == 45
     }
 }
+
+fun String.flertallifiser(antall: Int) = if (antall == 1) this else "${this}er"
