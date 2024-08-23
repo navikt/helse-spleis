c29a17bcc3833270a85503b8e47205d72887bfb9
Simen Ullern <simenullern@gmail.com>
2024-08-23 16:36:23 +0200

    Trikser inn aktivitetsloggen på et vis i OpenInSpanner

--- sykepenger-model/src/test/kotlin/no/nav/helse/OpenInSpanner.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/OpenInSpanner.kt
@@ -7,8 +7,15 @@ import java.io.File
 import java.io.IOException
 import java.net.URI
 import java.net.URISyntaxException
+import java.util.UUID
 import no.nav.helse.dto.tilSpannerPersonDto
+import no.nav.helse.inspectors.personLogg
 import no.nav.helse.person.Person
+import no.nav.helse.person.aktivitetslogg.Aktivitet
+import no.nav.helse.person.aktivitetslogg.Aktivitetslogg
+import no.nav.helse.person.aktivitetslogg.AktivitetsloggVisitor
+import no.nav.helse.person.aktivitetslogg.SpesifikkKontekst
+import no.nav.helse.person.aktivitetslogg.Varselkode
 import no.nav.helse.serde.reflection.ReflectInstance.Companion.get
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.ExtensionContext
@@ -26,6 +33,7 @@ class SpannerEtterTestInterceptor : TestWatcher {
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .registerModule(JavaTimeModule())
     }
+
     override fun testSuccessful(context: ExtensionContext?) {
         // sjekk at vi har spanner-config
         val spannerConfigJson = {}.javaClass.getResource("/spanner_config.json")?.readText(Charsets.UTF_8)
@@ -37,11 +45,16 @@ class SpannerEtterTestInterceptor : TestWatcher {
 
         // fisk ut person på et vis og opprett SpannerDto
         val person = context!!.testInstance.get().get("person") as Person
-        val dto = person.dto().tilSpannerPersonDto()
+        val spannerPerson = person.dto().tilSpannerPersonDto()
+        val personJson = objectMapper.writeValueAsString(spannerPerson)
+        // aktivitetsloggen krever litt mer greier ettersom spanner henter den fra sparsom på ekte
+        val aktivtetsloggV2Json = SugUtAlleAktivitetene(person.personLogg)
+
+        // trikser inn aktivitetsloggen. Dette burde sikkert gjøres på en bedre måte
+        val json = "{" + aktivtetsloggV2Json.drop(1).dropLast(1) + "," + personJson.drop(1)
 
         // skriver spannerDto til filsystem
         val writeLocation = map["filplassering"] + "/jsonPersonLokalFraSpleis.json"
-        val json = objectMapper.writeValueAsString(dto)
         val fil = File(writeLocation)
         if (!fil.exists()) {
             fil.createNewFile()
@@ -67,4 +80,107 @@ class SpannerEtterTestInterceptor : TestWatcher {
             Error("Desktop er ikke støttet")
         }
     }
+
+    private fun SugUtAlleAktivitetene(aktivitetslogg: Aktivitetslogg): String {
+        val liste = mutableListOf<AktivitetDto>()
+
+        aktivitetslogg.accept(object : AktivitetsloggVisitor {
+            override fun visitInfo(
+                id: UUID,
+                kontekster: List<SpesifikkKontekst>,
+                aktivitet: Aktivitet.Info,
+                melding: String,
+                tidsstempel: String
+            ) {
+                liste.add(AktivitetDto(
+                    id = 0,
+                    nivå = "INFO",
+                    tekst = melding,
+                    tidsstempel = tidsstempel,
+                    kontekster = kontekster.associateBy({ it.kontekstType }, {it.kontekstMap})
+                ))
+            }
+
+            override fun visitVarsel(
+                id: UUID,
+                kontekster: List<SpesifikkKontekst>,
+                aktivitet: Aktivitet.Varsel,
+                kode: Varselkode?,
+                melding: String,
+                tidsstempel: String
+            ) {
+                liste.add(AktivitetDto(
+                    id = 0,
+                    nivå = "VARSEL",
+                    tekst = melding,
+                    tidsstempel = tidsstempel,
+                    kontekster = kontekster.associateBy({ it.kontekstType }, {it.kontekstMap})
+                ))
+            }
+
+            override fun visitBehov(
+                id: UUID,
+                kontekster: List<SpesifikkKontekst>,
+                aktivitet: Aktivitet.Behov,
+                type: Aktivitet.Behov.Behovtype,
+                melding: String,
+                detaljer: Map<String, Any?>,
+                tidsstempel: String
+            ) {
+                liste.add(AktivitetDto(
+                    id = 0,
+                    nivå = "BEHOV",
+                    tekst = melding,
+                    tidsstempel = tidsstempel,
+                    kontekster = kontekster.associateBy({ it.kontekstType }, {it.kontekstMap})
+                ))
+            }
+
+            override fun visitLogiskFeil(
+                id: UUID,
+                kontekster: List<SpesifikkKontekst>,
+                aktivitet: Aktivitet.LogiskFeil,
+                melding: String,
+                tidsstempel: String
+            ) {
+                liste.add(AktivitetDto(
+                    id = 0,
+                    nivå = "LOGISK_FEIL",
+                    tekst = melding,
+                    tidsstempel = tidsstempel,
+                    kontekster = kontekster.associateBy({ it.kontekstType }, {it.kontekstMap})
+                ))
+            }
+
+            override fun visitFunksjonellFeil(
+                id: UUID,
+                kontekster: List<SpesifikkKontekst>,
+                aktivitet: Aktivitet.FunksjonellFeil,
+                melding: String,
+                tidsstempel: String
+            ) {
+                liste.add(AktivitetDto(
+                    id = 0,
+                    nivå = "FUNKSJONELL_FEIL",
+                    tekst = melding,
+                    tidsstempel = tidsstempel,
+                    kontekster = kontekster.associateBy({ it.kontekstType }, {it.kontekstMap})
+                ))
+            }
+        })
+
+        val aktiviteter = mapOf("aktiviteter" to liste)
+        val aktivitetsloggV2 = mapOf("aktivitetsloggV2" to aktiviteter)
+        val aktivtetsloggV2Json = objectMapper.writeValueAsString(aktivitetsloggV2)
+        return aktivtetsloggV2Json
+    }
+
+    private data class AktivitetDto(
+        val id: Int,
+        val nivå: String,
+        val tekst: String,
+        val tidsstempel: String,
+        val kontekster: Map<String, Any>
+    )
+
 }
