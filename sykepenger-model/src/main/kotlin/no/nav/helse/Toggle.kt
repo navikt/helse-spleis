cdec277674c5c73170a19577d0b2d3c9507846b3
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-08-29 10:04:44 +0200

    Revert "Noen få som manglet gitt"
    This reverts commit 0e2db041b03dc31d6176f0aaad1481e6c46af912.

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -42,7 +42,7 @@ class Toggle private constructor(enabled: Boolean) {
 
     companion object {
         val TilkommenArbeidsgiver = fraEnv("TILKOMMEN_ARBEIDSGIVER", false)
-        val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", true) // Denne MÅ settes til false når man er ferdig å kjøre feriepenger. Ref. den mystiske feriepengejobben som startet av seg selv (?) 08.08.2024
+        val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", false) // Denne MÅ settes til false når man er ferdig å kjøre feriepenger. Ref. den mystiske feriepengejobben som startet av seg selv (?) 08.08.2024
         val EgenmeldingStrekkerIkkeSykdomstidslinje = fraEnv("EGENMELDING_STREKKER_IKKE_SYKDOMSTIDSLINJE", false)
 
         fun fraEnv(navn: String, defaultValue: Boolean) = Toggle(System.getenv(navn)?.lowercase()?.toBooleanStrictOrNull() ?: defaultValue)
