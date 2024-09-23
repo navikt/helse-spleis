f83793fcb7ce8c04ad170d75dfe27c70e47aa497
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-09-23 11:56:13 +0200

    Feile test for sykmelding kommer som arbeidsledig, mens søknaden kommer på arbeidsgiver
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/ArbeidsledigTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/ArbeidsledigTest.kt
@@ -1,13 +1,39 @@
 package no.nav.helse.spleis.e2e
 
+import no.nav.helse.assertForventetFeil
 import no.nav.helse.dsl.AbstractDslTest
+import no.nav.helse.hendelser.til
 import no.nav.helse.januar
+import no.nav.helse.person.TilstandType.AVVENTER_BLOKKERENDE_PERIODE
+import no.nav.helse.person.TilstandType.AVVENTER_VILKÅRSPRØVING
 import no.nav.helse.person.TilstandType.START
 import no.nav.helse.person.TilstandType.TIL_INFOTRYGD
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class ArbeidsledigTest : AbstractDslTest() {
+
+    @Test
+    fun `håndterer at sykmelding kommer som arbeidsledig, mens søknaden kommer på arbeidsgiver`() {
+        arbeidsledig {
+            håndterSykmelding(januar)
+        }
+        a1 {
+            håndterSøknad(januar)
+            håndterInntektsmelding(listOf(1.januar til 16.januar))
+            assertForventetFeil(
+                forklaring = "Nå funker ikke dette",
+                nå = {
+                    assertSisteTilstand(1.vedtaksperiode, AVVENTER_BLOKKERENDE_PERIODE)
+                    assertEquals("SØKNAD", observatør.vedtaksperiodeVenter.last().venterPå.venteårsak.hva)
+                },
+                ønsket = {
+                    assertSisteTilstand(1.vedtaksperiode, AVVENTER_VILKÅRSPRØVING)
+                }
+            )
+        }
+    }
 
     @Test
     fun `arbeidsledigsøknad gir error`() {
