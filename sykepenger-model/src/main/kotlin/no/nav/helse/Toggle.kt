04961df972c61536d8664817887a54cfc1729f30
Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
2024-10-21 14:09:13 +0200

    Klargjør feriepengekjøring ved å skru på toggle
    Co-authored-by: Hege Haavaldsen <hege.haavaldsen@nav.no>
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Morten Tholander <morten.tholander@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/Toggle.kt
@@ -43,7 +43,7 @@ class Toggle private constructor(enabled: Boolean) {
     companion object {
         val TilkommenArbeidsgiver = fraEnv("TILKOMMEN_ARBEIDSGIVER", false)
         val InntektsmeldingSomIkkeKommer = fraEnv("INNTEKTSMELDING_SOM_IKKEKOMMER", false)
-        val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", false) // Denne MÅ settes til false når man er ferdig å kjøre feriepenger. Ref. den mystiske feriepengejobben som startet av seg selv (?) 08.08.2024
+        val SendFeriepengeOppdrag = fraEnv("SEND_FERIEPENGEOPPDRAG", true) // Denne MÅ settes til false når man er ferdig å kjøre feriepenger. Ref. den mystiske feriepengejobben som startet av seg selv (?) 08.08.2024
 
         fun fraEnv(navn: String, defaultValue: Boolean) = Toggle(System.getenv(navn)?.lowercase()?.toBooleanStrictOrNull() ?: defaultValue)
 
