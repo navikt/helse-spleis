0a6761bd9f7448b12b45d797a3ae8847bc453251
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-11-22 15:08:59 +0100

    Erstatter `if (dev)` med å hente tøyse-token fra speed-api-dev-proxy i dev

--- sykepenger-api/src/main/kotlin/no/nav/helse/spleis/config/ApplicationConfiguration.kt
+++ sykepenger-api/src/main/kotlin/no/nav/helse/spleis/config/ApplicationConfiguration.kt
@@ -2,26 +2,17 @@ package no.nav.helse.spleis.config
 
 import com.auth0.jwk.JwkProvider
 import com.auth0.jwk.JwkProviderBuilder
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.github.navikt.tbd_libs.azure.AzureToken
-import com.github.navikt.tbd_libs.azure.AzureTokenProvider
 import com.github.navikt.tbd_libs.azure.createAzureTokenClientFromEnvironment
-import com.github.navikt.tbd_libs.result_object.Result
-import com.github.navikt.tbd_libs.result_object.ok
+import com.github.navikt.tbd_libs.azure.createDefaultAzureTokenClient
 import com.github.navikt.tbd_libs.speed.SpeedClient
-import io.ktor.client.HttpClient
 import io.ktor.server.auth.jwt.JWTAuthenticationProvider
 import io.ktor.server.auth.jwt.JWTPrincipal
-import java.io.InputStream
-import java.net.HttpURLConnection
 import java.net.URI
-import java.time.Duration
 import no.nav.helse.spleis.SpekematClient
 import no.nav.helse.spleis.objectMapper
-import org.checkerframework.checker.units.qual.Speed
-import java.time.LocalDateTime
+import java.net.http.HttpClient
 
 internal class ApplicationConfiguration(env: Map<String, String> = System.getenv()) {
     internal val azureConfig = AzureAdAppConfig(
@@ -30,29 +21,19 @@ internal class ApplicationConfiguration(env: Map<String, String> = System.getenv
         jwkProvider = JwkProviderBuilder(URI(env.getValue("AZURE_OPENID_CONFIG_JWKS_URI")).toURL()).build(),
     )
 
-    internal val azureClient = createAzureTokenClientFromEnvironment(env)
     internal val speedClient = SpeedClient(
-        httpClient = java.net.http.HttpClient.newHttpClient(),
+        httpClient = HttpClient.newHttpClient(),
         objectMapper = jacksonObjectMapper().registerModule(JavaTimeModule()),
-        tokenProvider = if (System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
-            object : AzureTokenProvider {
-                override fun bearerToken(scope: String): Result<AzureToken> {
-                    return AzureToken("dummy_token_i_dev", LocalDateTime.MAX).ok()
-                }
-
-                override fun onBehalfOfToken(scope: String, token: String): Result<AzureToken> {
-                    return AzureToken("dummy_obo_token_i_dev", LocalDateTime.MAX).ok()
-                }
-            }
-        } else {
-            azureClient
-        },
-        baseUrl = if (System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
-            "http://speed-api-dev-proxy"
-        } else null
+        tokenProvider = createDefaultAzureTokenClient(
+            tokenEndpoint = URI(env.getValue(env.getValue("TOKEN_ENDPOINT_ENV_KEY"))),
+            clientId = env.getValue("AZURE_APP_CLIENT_ID"),
+            clientSecret = env.getValue("AZURE_APP_CLIENT_SECRET")
+        ),
+        baseUrl = env.getValue("SPEED_API_URL")
     )
+
     internal val spekematClient = SpekematClient(
-        tokenProvider = azureClient,
+        tokenProvider = createAzureTokenClientFromEnvironment(env),
         objectMapper = objectMapper,
         scope = env.getValue("SPEKEMAT_SCOPE")
     )
@@ -79,19 +60,5 @@ internal class AzureAdAppConfig(
             withAudience(clientId)
         }
         configuration.validate { credentials -> JWTPrincipal(credentials.payload) }
-    }
-
-    private fun String.getJson(): JsonNode {
-        val (responseCode, responseBody) = this.fetchUrl()
-        if (responseCode >= 300 || responseBody == null) throw RuntimeException("got status $responseCode from ${this}.")
-        return jacksonObjectMapper().readTree(responseBody)
-    }
-
-    private fun String.fetchUrl() = with(URI(this).toURL().openConnection() as HttpURLConnection) {
-        requestMethod = "GET"
-        connectTimeout = Duration.ofSeconds(5).toMillis().toInt()
-        readTimeout = Duration.ofSeconds(5).toMillis().toInt()
-        val stream: InputStream? = if (responseCode < 300) this.inputStream else this.errorStream
-        responseCode to stream?.bufferedReader()?.readText()
     }
 }
