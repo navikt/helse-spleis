2a27d0748aaf79ec4751abdf18c1a15b436e905d
david steinsland <david@davidsteinsland.net>
2024-11-25 10:26:06 +0100

    bumper r&r

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ include("jobs", "sykepenger-serde", "sykepenger-api", "sykepenger-api-graphql", 
     "sykepenger-mediators", "sykepenger-opprydding-dev", "sykepenger-primitiver", "sykepenger-primitiver-dto", "sykepenger-utbetaling",
     "sykepenger-utbetaling-dto", "sykepenger-aktivitetslogg", "sykepenger-aktivitetslogg-dto", "sykepenger-etterlevelse-api")
 
-val rapidsAndRiversVersion = "2024112412131732446804.1b3dcc636bed"
+val rapidsAndRiversVersion = "2024112510241732526640.8542991368ca"
 
 dependencyResolutionManagement {
     versionCatalogs {
