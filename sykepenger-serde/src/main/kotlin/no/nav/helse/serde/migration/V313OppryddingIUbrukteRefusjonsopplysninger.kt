fa9ba529fccd508f7c3cebd61ec6b3462c56b0ed
Simen Ullern <simenullern@gmail.com>
2024-12-05 11:52:33 +0100

    Lager nytt ubrukteRefusjonsopplysninger-objekt fremfor å mutere på det vi looper i migrering
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- sykepenger-serde/src/main/kotlin/no/nav/helse/serde/migration/V313OppryddingIUbrukteRefusjonsopplysninger.kt
+++ sykepenger-serde/src/main/kotlin/no/nav/helse/serde/migration/V313OppryddingIUbrukteRefusjonsopplysninger.kt
@@ -1,21 +1,23 @@
 package no.nav.helse.serde.migration
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import java.time.LocalDate
+import no.nav.helse.serde.serdeObjectMapper
 
 internal class V313OppryddingIUbrukteRefusjonsopplysninger: JsonMigration(version = 313) {
     override val description = "Fjerner ubrukte refusjonsopplysninger tidligere enn siste vedtaksperiode på arbeidsgiveren"
 
     override fun doMigration(jsonNode: ObjectNode, meldingerSupplier: MeldingerSupplier) {
         jsonNode.path("arbeidsgivere").forEach { arbeidsgiver ->
+            arbeidsgiver as ObjectNode
             val sisteVedtaksperiode = arbeidsgiver.path("vedtaksperioder").lastOrNull() ?: return@forEach
             val sisteTomPåVedtaksperiode =
                 sisteVedtaksperiode.path("behandlinger").last().path("endringer").last().path("tom").asText().let {
                     LocalDate.parse(it)
                 }
             val ubrukteRefusjonsopplysninger = arbeidsgiver.path("ubrukteRefusjonsopplysninger") as ObjectNode
+            val ubrukteRefusjonsopplysningerEtterMigrering = serdeObjectMapper.createObjectNode()
             ubrukteRefusjonsopplysninger.fields().forEach { (dato, refusjonstidslinjer) ->
                 val futiristiske = refusjonstidslinjer.path("perioder").deepCopy<JsonNode>()
                     .filter { periode -> periode.path("tom").dato().isAfter(sisteTomPåVedtaksperiode) }
@@ -23,16 +25,13 @@ internal class V313OppryddingIUbrukteRefusjonsopplysninger: JsonMigration(versio
                         val fom = periode.path("fom").dato()
                         periode.put("fom", maxOf(fom, sisteTomPåVedtaksperiode.plusDays(1)).toString())
                      }
-                if (futiristiske.isEmpty()) {
-                    ubrukteRefusjonsopplysninger.remove(dato)
-                }
-                else {
-                    (ubrukteRefusjonsopplysninger.path(dato).path("perioder") as ArrayNode).apply {
-                        this.removeAll()
+                if (futiristiske.isNotEmpty()) {
+                    ubrukteRefusjonsopplysningerEtterMigrering.putObject(dato).putArray("perioder").apply {
                         this.addAll(futiristiske)
                     }
                 }
             }
+            arbeidsgiver.replace("ubrukteRefusjonsopplysninger", ubrukteRefusjonsopplysningerEtterMigrering)
         }
     }
 
