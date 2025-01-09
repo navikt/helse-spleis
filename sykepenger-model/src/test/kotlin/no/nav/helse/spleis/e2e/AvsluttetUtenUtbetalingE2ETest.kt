f74f794afbf21f543aa5319538bb6f8a15917939
Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
2025-01-09 09:59:24 +0100

    Bytter til ny dsl

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/AvsluttetUtenUtbetalingE2ETest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/AvsluttetUtenUtbetalingE2ETest.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.spleis.e2e
 
-import no.nav.helse.dsl.INNTEKT
+import no.nav.helse.dsl.AbstractDslTest
+import no.nav.helse.dsl.a1
 import no.nav.helse.februar
 import no.nav.helse.hendelser.Periode
 import no.nav.helse.hendelser.Sykmeldingsperiode
@@ -18,11 +19,10 @@ import no.nav.helse.person.TilstandType.AVVENTER_SIMULERING
 import no.nav.helse.person.TilstandType.AVVENTER_VILKÅRSPRØVING
 import no.nav.helse.person.TilstandType.START
 import no.nav.helse.person.TilstandType.TIL_UTBETALING
-import no.nav.helse.person.nullstillTilstandsendringer
 import no.nav.helse.utbetalingslinjer.Oppdragstatus
 import org.junit.jupiter.api.Test
 
-internal class AvsluttetUtenUtbetalingE2ETest : AbstractEndToEndTest() {
+internal class AvsluttetUtenUtbetalingE2ETest : AbstractDslTest() {
     /*
         Hvis vi har en kort periode som har endt opp i AVSLUTTET_UTEN_UTBETALING vil alle etterkommende perioder
         bli stuck med å vente på den korte perioden. Da vil de aldri komme seg videre og til slutt time ut
@@ -30,37 +30,39 @@ internal class AvsluttetUtenUtbetalingE2ETest : AbstractEndToEndTest() {
 
     @Test
     fun `kort periode blokkerer neste periode i ny arbeidsgiverperiode`() {
-        håndterSykmelding(Sykmeldingsperiode(3.januar, 10.januar))
-        håndterSøknad(3.januar til 10.januar)
-        assertTilstander(
-            1.vedtaksperiode,
-            START,
-            AVVENTER_INFOTRYGDHISTORIKK,
-            AVVENTER_INNTEKTSMELDING,
-            AVSLUTTET_UTEN_UTBETALING,
-        )
+        a1 {
+            håndterSykmelding(Sykmeldingsperiode(3.januar, 10.januar))
+            håndterSøknad(3.januar til 10.januar)
+            assertTilstander(
+                1.vedtaksperiode,
+                START,
+                AVVENTER_INFOTRYGDHISTORIKK,
+                AVVENTER_INNTEKTSMELDING,
+                AVSLUTTET_UTEN_UTBETALING,
+            )
 
-        håndterSykmelding(Sykmeldingsperiode(3.mars, 26.mars))
-        håndterInntektsmelding(listOf(Periode(3.mars, 18.mars)))
-        håndterSøknad(3.mars til 26.mars)
-        håndterVilkårsgrunnlag(2.vedtaksperiode)
-        håndterYtelser(2.vedtaksperiode)
-        håndterSimulering(2.vedtaksperiode)
-        håndterUtbetalingsgodkjenning(2.vedtaksperiode, true)
-        håndterUtbetalt(Oppdragstatus.AKSEPTERT)
+            håndterSykmelding(Sykmeldingsperiode(3.mars, 26.mars))
+            håndterInntektsmelding(listOf(3.mars til 18.mars))
+            håndterSøknad(3.mars til 26.mars)
+            håndterVilkårsgrunnlag(2.vedtaksperiode)
+            håndterYtelser(2.vedtaksperiode)
+            håndterSimulering(2.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(2.vedtaksperiode, true)
+            håndterUtbetalt(Oppdragstatus.AKSEPTERT)
 
-        assertTilstander(
-            2.vedtaksperiode,
-            START,
-            AVVENTER_INNTEKTSMELDING,
-            AVVENTER_BLOKKERENDE_PERIODE,
-            AVVENTER_VILKÅRSPRØVING,
-            AVVENTER_HISTORIKK,
-            AVVENTER_SIMULERING,
-            AVVENTER_GODKJENNING,
-            TIL_UTBETALING,
-            AVSLUTTET
-        )
+            assertTilstander(
+                2.vedtaksperiode,
+                START,
+                AVVENTER_INNTEKTSMELDING,
+                AVVENTER_BLOKKERENDE_PERIODE,
+                AVVENTER_VILKÅRSPRØVING,
+                AVVENTER_HISTORIKK,
+                AVVENTER_SIMULERING,
+                AVVENTER_GODKJENNING,
+                TIL_UTBETALING,
+                AVSLUTTET
+            )
+        }
     }
 
     /*
@@ -69,74 +71,78 @@ internal class AvsluttetUtenUtbetalingE2ETest : AbstractEndToEndTest() {
      */
     @Test
     fun `kort periode setter senere periode fast i AVVENTER_HISTORIKK`() {
-        håndterSykmelding(Sykmeldingsperiode(3.januar, 10.januar))
-        håndterSøknad(3.januar til 10.januar)
-        assertTilstander(
-            1.vedtaksperiode,
-            START,
-            AVVENTER_INFOTRYGDHISTORIKK,
-            AVVENTER_INNTEKTSMELDING,
-            AVSLUTTET_UTEN_UTBETALING,
-        )
+        a1 {
+            håndterSykmelding(Sykmeldingsperiode(3.januar, 10.januar))
+            håndterSøknad(3.januar til 10.januar)
+            assertTilstander(
+                1.vedtaksperiode,
+                START,
+                AVVENTER_INFOTRYGDHISTORIKK,
+                AVVENTER_INNTEKTSMELDING,
+                AVSLUTTET_UTEN_UTBETALING,
+            )
 
-        håndterSykmelding(Sykmeldingsperiode(3.mars, 7.mars))
-        håndterSøknad(3.mars til 7.mars)
-        assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
+            håndterSykmelding(Sykmeldingsperiode(3.mars, 7.mars))
+            håndterSøknad(3.mars til 7.mars)
+            assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
 
-        håndterSykmelding(Sykmeldingsperiode(8.mars, 26.mars))
-        håndterInntektsmelding(
-            listOf(Periode(3.mars, 18.mars))
-        )
+            håndterSykmelding(Sykmeldingsperiode(8.mars, 26.mars))
+            håndterInntektsmelding(
+                listOf(Periode(3.mars, 18.mars))
+            )
 
-        assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_BLOKKERENDE_PERIODE, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_BLOKKERENDE_PERIODE, AVSLUTTET_UTEN_UTBETALING)
 
-        håndterSøknad(8.mars til 26.mars)
-        håndterVilkårsgrunnlag(3.vedtaksperiode)
-        håndterYtelser(3.vedtaksperiode)
-        håndterSimulering(3.vedtaksperiode)
-        håndterUtbetalingsgodkjenning(3.vedtaksperiode, true)
-        håndterUtbetalt(Oppdragstatus.AKSEPTERT)
+            håndterSøknad(8.mars til 26.mars)
+            håndterVilkårsgrunnlag(3.vedtaksperiode)
+            håndterYtelser(3.vedtaksperiode)
+            håndterSimulering(3.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(3.vedtaksperiode, true)
+            håndterUtbetalt(Oppdragstatus.AKSEPTERT)
 
-        assertTilstander(
-            3.vedtaksperiode,
-            START,
-            AVVENTER_INNTEKTSMELDING,
-            AVVENTER_BLOKKERENDE_PERIODE,
-            AVVENTER_VILKÅRSPRØVING,
-            AVVENTER_HISTORIKK,
-            AVVENTER_SIMULERING,
-            AVVENTER_GODKJENNING,
-            TIL_UTBETALING,
-            AVSLUTTET
-        )
+            assertTilstander(
+                3.vedtaksperiode,
+                START,
+                AVVENTER_INNTEKTSMELDING,
+                AVVENTER_BLOKKERENDE_PERIODE,
+                AVVENTER_VILKÅRSPRØVING,
+                AVVENTER_HISTORIKK,
+                AVVENTER_SIMULERING,
+                AVVENTER_GODKJENNING,
+                TIL_UTBETALING,
+                AVSLUTTET
+            )
+        }
     }
 
     @Test
     fun `arbeidsgiverperiode med brudd i helg`() {
-        håndterSøknad(4.januar til 5.januar)
-        håndterSøknad(8.januar til 12.januar)
-        håndterSøknad(13.januar til 19.januar)
-        håndterSøknad(20.januar til 1.februar)
+        a1 {
+            håndterSøknad(4.januar til 5.januar)
+            håndterSøknad(8.januar til 12.januar)
+            håndterSøknad(13.januar til 19.januar)
+            håndterSøknad(20.januar til 1.februar)
 
-        assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(3.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(4.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING)
+            assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(3.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(4.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING)
 
-        nullstillTilstandsendringer()
-        // 6. og 7. januar blir FriskHelg og medfører brudd i arbeidsgiverperioden
-        // og dermed ble også skjæringstidspunktet forskjøvet til 8. januar
-        håndterInntektsmelding(
-            listOf(
-                1.januar til 3.januar, //3
-                4.januar til 5.januar, // 2
-                // 6. og 7. januar er helg
-                8.januar til 12.januar,// 5
-                13.januar til 18.januar // 6
-            ),
-            førsteFraværsdag = 8.januar
-        )
-        assertTilstander(3.vedtaksperiode, AVSLUTTET_UTEN_UTBETALING, AVVENTER_BLOKKERENDE_PERIODE, AVVENTER_VILKÅRSPRØVING)
-        assertTilstander(4.vedtaksperiode, AVVENTER_INNTEKTSMELDING, AVVENTER_BLOKKERENDE_PERIODE)
+            nullstillTilstandsendringer()
+            // 6. og 7. januar blir FriskHelg og medfører brudd i arbeidsgiverperioden
+            // og dermed ble også skjæringstidspunktet forskjøvet til 8. januar
+            håndterInntektsmelding(
+                listOf(
+                    1.januar til 3.januar, //3
+                    4.januar til 5.januar, // 2
+                    // 6. og 7. januar er helg
+                    8.januar til 12.januar,// 5
+                    13.januar til 18.januar // 6
+                ),
+                førsteFraværsdag = 8.januar
+            )
+            assertTilstander(3.vedtaksperiode, AVSLUTTET_UTEN_UTBETALING, AVVENTER_BLOKKERENDE_PERIODE, AVVENTER_VILKÅRSPRØVING)
+            assertTilstander(4.vedtaksperiode, AVVENTER_INNTEKTSMELDING, AVVENTER_BLOKKERENDE_PERIODE)
+        }
     }
 }
