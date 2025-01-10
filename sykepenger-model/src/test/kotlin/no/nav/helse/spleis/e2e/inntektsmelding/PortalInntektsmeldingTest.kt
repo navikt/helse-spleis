f0c984b0fd039fdb0ce419d25127a16f276f4dc8
david steinsland <david@davidsteinsland.net>
2025-01-10 12:26:51 +0100

    tester hvilke opplysninger det er bedt om
    det synliggjør jo at endring av AGP kommer uventet på oss

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/PortalInntektsmeldingTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/PortalInntektsmeldingTest.kt
@@ -9,10 +9,11 @@ import no.nav.helse.februar
 import no.nav.helse.hendelser.inntektsmelding.NAV_NO_SELVBESTEMT
 import no.nav.helse.hendelser.til
 import no.nav.helse.januar
+import no.nav.helse.person.PersonObserver
 import no.nav.helse.person.TilstandType.AVVENTER_INNTEKTSMELDING
 import no.nav.helse.person.aktivitetslogg.Varselkode
-import no.nav.helse.person.aktivitetslogg.Varselkode.RV_IM_3
 import no.nav.helse.spleis.e2e.AktivitetsloggFilter.Companion.filter
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class PortalInntektsmeldingTest : AbstractDslTest() {
@@ -24,6 +25,10 @@ internal class PortalInntektsmeldingTest : AbstractDslTest() {
             håndterInntektsmeldingPortal(listOf(2.januar til 17.januar), vedtaksperiodeId = 2.vedtaksperiode)
             assertInfo("Håndterer ikke arbeidsgiverperiode i AVSLUTTET", 1.vedtaksperiode.filter())
             assertVarsel(Varselkode.RV_IM_24, 2.vedtaksperiode.filter())
+            val forespørselFebruar = observatør.trengerArbeidsgiveropplysningerVedtaksperioder.last { it.vedtaksperiodeId == 2.vedtaksperiode }
+            assertEquals(0, forespørselFebruar.forespurteOpplysninger.filterIsInstance<PersonObserver.Arbeidsgiverperiode>().size)
+            assertEquals(0, forespørselFebruar.forespurteOpplysninger.filterIsInstance<PersonObserver.Inntekt>().size)
+            assertEquals(1, forespørselFebruar.forespurteOpplysninger.filterIsInstance<PersonObserver.Refusjon>().size)
         }
     }
 
