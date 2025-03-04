3bdbc466e20d5d6829250734fd21c40e8ef07ce8
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-03-04 15:57:48 +0100

    En frekkere måte å skrive InntekterForBeregning-tester 😎

--- sykepenger-model/src/test/kotlin/no/nav/helse/person/inntekt/InntekterForBeregningTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/person/inntekt/InntekterForBeregningTest.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.person.inntekt
 
+import java.time.LocalDate
 import java.util.UUID
 import no.nav.helse.Grunnbeløp
 import no.nav.helse.dsl.INNTEKT
@@ -7,6 +8,7 @@ import no.nav.helse.dsl.a1
 import no.nav.helse.dsl.a2
 import no.nav.helse.hendelser.Avsender
 import no.nav.helse.hendelser.MeldingsreferanseId
+import no.nav.helse.hendelser.Periode
 import no.nav.helse.hendelser.somPeriode
 import no.nav.helse.hendelser.til
 import no.nav.helse.januar
@@ -17,6 +19,7 @@ import no.nav.helse.testhelpers.AP
 import no.nav.helse.testhelpers.NAV
 import no.nav.helse.testhelpers.UTELATE
 import no.nav.helse.testhelpers.tidslinjeOf
+import no.nav.helse.økonomi.Inntekt
 import no.nav.helse.økonomi.Inntekt.Companion.INGEN
 import no.nav.helse.økonomi.Inntekt.Companion.daglig
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -26,12 +29,11 @@ class InntekterForBeregningTest {
 
     @Test
     fun `lager ghosttidslinjer for dagene vi ikke har beregnet utbetalingstidslinje`() {
-        val builder = InntekterForBeregning.Builder(1.januar til 28.januar, 1.januar)
-        builder.fraInntektsgrunnlag(a1, 1000.daglig, UUID.randomUUID().arbeidsgiver)
-        builder.fraInntektsgrunnlag(a2, 1000.daglig, UUID.randomUUID().arbeidsgiver)
-        builder.medGjeldende6G(Grunnbeløp.`6G`.beløp(1.januar))
+        val inntekterForBeregning = inntekterForBeregning(1.januar til 28.januar) {
+            fraInntektsgrunnlag(a1, 1000.daglig)
+            fraInntektsgrunnlag(a2, 1000.daglig)
+        }
 
-        val inntekterForBeregning = builder.build()
         inntekterForBeregning.hensyntattAlleInntektskilder(
             mapOf(
                 "a1" to listOf(tidslinjeOf(16.AP, 8.NAV)),
@@ -46,11 +48,12 @@ class InntekterForBeregningTest {
 
     @Test
     fun `kan ikke endre inntekt på skjæringstidspunktet for en arbeidsgiver som finnes i inntektsgrunnlaget ved hjelp av en inntektsendring`() {
-        val builder = InntekterForBeregning.Builder(januar, 1.januar)
-        builder.fraInntektsgrunnlag(a1, INNTEKT, UUID.randomUUID().arbeidsgiver)
-        builder.inntektsendringer(a1, 1.januar, 31.januar, INNTEKT * 2, MeldingsreferanseId(UUID.randomUUID()))
-        builder.medGjeldende6G(Grunnbeløp.`6G`.beløp(1.januar))
-        val (_, inntektstidslinje) = builder.build().tilBeregning(a1)
+        val inntekterForBeregning = inntekterForBeregning(januar) {
+            fraInntektsgrunnlag(a1, INNTEKT)
+            inntektsendringer(a1, 1.januar, 31.januar, INNTEKT * 2)
+        }
+
+        val (_, inntektstidslinje) = inntekterForBeregning.tilBeregning(a1)
 
         val forventetTidslinje = Avsender.ARBEIDSGIVER.beløpstidslinje(1.januar.somPeriode(), INNTEKT) + Avsender.SYSTEM.beløpstidslinje(2.januar til 31.januar, INNTEKT * 2)
         assertBeløpstidslinje(forventetTidslinje, inntektstidslinje, ignoreMeldingsreferanseId = true)
@@ -58,11 +61,11 @@ class InntekterForBeregningTest {
 
     @Test
     fun `kan sette inntekt på skjæringstidspunktet for en arbeidsgiver som ikke finnes i inntektsgrunnlaget`() {
-        val builder = InntekterForBeregning.Builder(januar, 1.januar)
-        builder.fraInntektsgrunnlag(a1, INNTEKT, UUID.randomUUID().arbeidsgiver)
-        builder.inntektsendringer(a2, 1.januar, 31.januar, INNTEKT * 2, MeldingsreferanseId(UUID.randomUUID()))
-        builder.medGjeldende6G(Grunnbeløp.`6G`.beløp(1.januar))
-        val inntekterForBeregning = builder.build()
+        val inntekterForBeregning = inntekterForBeregning(januar) {
+            fraInntektsgrunnlag(a1, INNTEKT)
+            inntektsendringer(a2, 1.januar, 31.januar, INNTEKT * 2)
+        }
+
         val (fastsattÅrsinntektA1, inntektstidslinjeForA1) = inntekterForBeregning.tilBeregning(a1)
         val (fastsattÅrsinntektA2, inntektstidslinjeForA2) = inntekterForBeregning.tilBeregning(a2)
 
@@ -74,4 +77,16 @@ class InntekterForBeregningTest {
         assertEquals(INNTEKT, fastsattÅrsinntektA1)
         assertEquals(INGEN, fastsattÅrsinntektA2)
     }
+
+    private fun inntekterForBeregning(periode: Periode, skjæringstidspunkt: LocalDate = periode.start, block: InntekterForBeregning.Builder.() -> Unit) = with(InntekterForBeregning.Builder(periode, skjæringstidspunkt)) {
+        medGjeldende6G(Grunnbeløp.`6G`.beløp(skjæringstidspunkt))
+        block()
+        build()
+    }
+
+    private fun InntekterForBeregning.Builder.fraInntektsgrunnlag(organisasjonsnummer: String, fastsattÅrsinntekt: Inntekt, meldingsreferanseId: MeldingsreferanseId = MeldingsreferanseId(UUID.randomUUID())) =
+        fraInntektsgrunnlag(organisasjonsnummer, fastsattÅrsinntekt, meldingsreferanseId.id.arbeidsgiver)
+
+    private fun InntekterForBeregning.Builder.inntektsendringer(organisasjonsnummer: String, fom: LocalDate, tom: LocalDate?, inntekt: Inntekt, meldingsreferanseId: MeldingsreferanseId = MeldingsreferanseId(UUID.randomUUID())) =
+        inntektsendringer(organisasjonsnummer, fom, tom, inntekt, meldingsreferanseId)
 }
