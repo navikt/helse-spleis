d66a8e6bac0a48c7a7678ace1462f3e816878fe7
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-03-13 08:29:35 +0100

    Revert "En eller annen feil i prod vi ikke klarer å skrive en test for"
    This reverts commit 9f91c282df8485be4848f0dcc4d404053fb78b8c.

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/Behandlinger.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/Behandlinger.kt
@@ -1371,8 +1371,7 @@ internal class Behandlinger private constructor(behandlinger: List<Behandling>) 
             }
 
             fun oppdaterDokumentsporing(behandling: Behandling, dokument: Dokumentsporing): Boolean {
-                return false
-                //error("Støtter ikke å oppdatere dokumentsporing med $dokument i $this")
+                error("Støtter ikke å oppdatere dokumentsporing med $dokument i $this")
             }
 
             fun kanForkastes(behandling: Behandling, aktivitetslogg: IAktivitetslogg, arbeidsgiverUtbetalinger: List<Utbetaling>): Boolean
