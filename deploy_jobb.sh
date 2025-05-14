75da79741e1e144aea53cc949ec058a413c3dbaf
david steinsland <david@davidsteinsland.net>
2025-05-14 19:38:34 +0200

    laget et script for å hjelpe til å deploye en jobb

---
+++ deploy_jobb.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+read -p "🐳 Image: " IMAGE
+read -p "☸️ Cluster (1: dev-gcp | 2: prod-gcp): " CLUSTER_OPT
+read -s -p "🔑 API key: " APIKEY
+echo ""
+read -p "🔑 Parallelism: (30 default) " PARALLELISM
+
+read -p "🛠️ Hvilken jobb skal du kjøre? " JOBB
+read -p "🪪 Hva skal arbeidId settes til? " ARBEID_ID
+read -p "🎒 Eventuelt andre parametre til jobben? " EKSTRA_PARAMETRE
+
+if [ "$CLUSTER_OPT" = "1" ]; then
+  CLUSTER="dev-gcp"
+elif [ "$CLUSTER_OPT" = "2" ]; then
+  CLUSTER="prod-gcp"
+else
+  echo "Ugyldig valg. Avslutter."
+  exit 1
+fi
+
+if [ -z "$PARALLELISM" ]; then
+  PARALLELISM=30
+fi
+
+POOL="nav-dev"
+if [ "$CLUSTER" = "prod-gcp" ]; then
+  POOL="nav-prod"
+fi
+
+docker run --rm -it -v $(PWD)/deploy:/config \
+  -e CLUSTER=$CLUSTER \
+  -e VAR="team=tbd,app=spleis-migrate,image=$IMAGE,parallelism=$PARALLELISM,pool=$POOL,jobb=$JOBB,arbeid_id=$ARBEID_ID,ekstra_parametre=$EKSTRA_PARAMETRE" \
+  -e APIKEY="$APIKEY" \
+  ghcr.io/nais/deploy/deploy:latest /app/deploy \
+    --print-payload --resource /config/job.yml
+
+echo "Når jobben er ferdig, husk å kjøre"
+echo "  kubectl delete naisjob spleis-migrate"
---
+++ jobs/README.md
@@ -0,0 +1,19 @@
+Jobs
+====
+
+
+# Starte feriepenger
+
+```
+% ./deploy_jobb.sh
+🐳 Image: imagenavnet mitt
+☸️ Cluster (1: dev-gcp | 2: prod-gcp): 2
+🔑 API key: <hemmelig>
+🔑 Parallelism: (30 default) 
+🛠️ Hvilken jobb skal du kjøre? feriepenger
+🪪 Hva skal arbeidId settes til? fp2025
+🎒 Eventuelt andre parametre til jobben? 2024
+
+Når jobben er ferdig, husk å kjøre
+  kubectl delete naisjob spleis-migrate
+```
