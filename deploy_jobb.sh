c6065a711bd04550e05293c280a9c00a4c0c126b
david steinsland <david@davidsteinsland.net>
2025-05-21 09:29:19 +0200

    legger statiske verdier i vars filer

--- deploy_jobb.sh
+++ deploy_jobb.sh
@@ -12,9 +12,11 @@ read -p "🏜️ Dryrun? (Y/n): " DRYRUN
 read -p "🎒 Eventuelt andre parametre til jobben? " EKSTRA_PARAMETRE
 
 if [ "$CLUSTER_OPT" = "1" ]; then
+  VARS_FILE="/config/job-dev.json"
   CLUSTER="dev-gcp"
 elif [ "$CLUSTER_OPT" = "2" ]; then
   CLUSTER="prod-gcp"
+  VARS_FILE="/config/job-prod.json"
 else
   echo "Ugyldig valg. Avslutter."
   exit 1
@@ -24,28 +26,16 @@ if [ -z "$PARALLELISM" ]; then
   PARALLELISM=30
 fi
 
-POOL="nav-dev"
-DATABASE="helse-spleis"
-DATABASE_AVSTEMMING_SECRET="google-sql-helse-spleis-spleis-spleis-avstemming-038f9535"
-DATABASE_MIGRATE_SECRET="google-sql-helse-spleis-spleis-spleis-migrate-70ddf3c6"
-
-if [ "$CLUSTER" = "prod-gcp" ]; then
-  POOL="nav-prod"
-  DATABASE="spleis2"
-  DATABASE_AVSTEMMING_SECRET="google-sql-spleis-spleis-spleis-avstemming-c09f6283"
-  DATABASE_MIGRATE_SECRET="google-sql-spleis-spleis-spleis-migrate-dc6feb9b"
-fi
-
 if [[ -z $DRYRUN || "$DRYRUN" =~ ^(yes|y|true|1|YES|Y|TRUE)$ ]]; then
   DRYRUN="true"
 else
   DRYRUN="false"
 fi
 
-
 docker run --rm -it -v $(PWD)/deploy:/config \
   -e CLUSTER=$CLUSTER \
-  -e VAR="team=tbd,app=spleis-migrate,image=$IMAGE,database=$DATABASE,avstemming-secret=$DATABASE_AVSTEMMING_SECRET,migrate-secret=$DATABASE_MIGRATE_SECRET,parallelism=$PARALLELISM,pool=$POOL,jobb=$JOBB,arbeid_id=$ARBEID_ID,dryrun=$DRYRUN,ekstra_parametre=$EKSTRA_PARAMETRE" \
+  -e VAR="team=tbd,app=spleis-migrate,image=$IMAGE,parallelism=$PARALLELISM,jobb=$JOBB,arbeid_id=$ARBEID_ID,dryrun=$DRYRUN,ekstra_parametre=$EKSTRA_PARAMETRE" \
+  -e VARS=$VARS_FILE \
   -e APIKEY="$APIKEY" \
   ghcr.io/nais/deploy/deploy:latest /app/deploy \
     --print-payload --resource /config/job.yml
