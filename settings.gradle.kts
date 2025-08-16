ec997a49745a09f80d36702d2841fb1e20cdcc93
david steinsland <david@davidsteinsland.net>
2025-08-16 14:03:46 +0200

    bumper r&r

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ include(
     "sykepenger-utbetaling-dto", "sykepenger-aktivitetslogg", "sykepenger-aktivitetslogg-dto", "sykepenger-etterlevelse-api"
 )
 
-val rapidsAndRiversVersion = "2025080710011754553680.051be9b54ef9"
+val rapidsAndRiversVersion = "2025081612341755340488.ff2c2d01e04f"
 val tbdLibsVersion = "2025.08.16-09.21-71db7cad"
 
 dependencyResolutionManagement {
