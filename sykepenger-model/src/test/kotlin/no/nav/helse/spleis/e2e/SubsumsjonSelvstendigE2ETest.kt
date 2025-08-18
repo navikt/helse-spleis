157f1d95861673af19c7c249c1343fe7f1b4e5eb
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-08-18 12:55:04 +0200

    Flytter subsumsjonstester for selvstendig til egen fil
    Co-authored-by: Sivert Lunsæter <Sivert.Frang.Lunseter@nav.no>
    Co-authored-by: Linus Johansson <Linus.Sebastian.Johansson@nav.no
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Vemund Strøm <vemund.strom@nav.no>

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/SubsumsjonSelvstendigE2ETest.kt
@@ -0,0 +1,88 @@
+package no.nav.helse.spleis.e2e
+
+import java.time.Year
+import no.nav.helse.Toggle
+import no.nav.helse.desember
+import no.nav.helse.dsl.AbstractDslTest
+import no.nav.helse.dsl.selvstendig
+import no.nav.helse.etterlevelse.Ledd
+import no.nav.helse.etterlevelse.Ledd.LEDD_2
+import no.nav.helse.etterlevelse.Paragraf
+import no.nav.helse.etterlevelse.Paragraf.PARAGRAF_8_35
+import no.nav.helse.hendelser.Søknad
+import no.nav.helse.hendelser.til
+import no.nav.helse.inspectors.SubsumsjonInspektør
+import no.nav.helse.januar
+import no.nav.helse.person.aktivitetslogg.Varselkode
+import no.nav.helse.spleis.e2e.AktivitetsloggFilter.Companion.filter
+import no.nav.helse.økonomi.Inntekt.Companion.årlig
+import no.nav.helse.økonomi.Prosentdel.Companion.prosent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SubsumsjonSelvstendigE2ETest : AbstractDslTest() {
+
+    @Test
+    fun `§ 8-35 ledd 2 - selvstendignæringsdrivende sykepengegrunnlag`() = Toggle.SelvstendigNæringsdrivende.enable {
+        selvstendig {
+            håndterSøknad(
+                Søknad.Søknadsperiode.Sykdom(1.januar, 31.januar, 100.prosent),
+                Søknad.Søknadsperiode.Venteperiode(1.januar til 16.januar),
+                pensjonsgivendeInntekter = listOf(
+                    Søknad.PensjonsgivendeInntekt(Year.of(2017), 450000.årlig),
+                    Søknad.PensjonsgivendeInntekt(Year.of(2016), 450000.årlig),
+                    Søknad.PensjonsgivendeInntekt(Year.of(2015), 450000.årlig)
+                )
+            )
+            håndterVilkårsgrunnlag(1.vedtaksperiode, skatteinntekter = emptyList())
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+            SubsumsjonInspektør(jurist).assertBeregnet(
+                paragraf = PARAGRAF_8_35,
+                ledd = LEDD_2,
+                versjon = 20.desember(2018),
+                input = mapOf(
+                    "skjæringstidspunkt" to 1.januar,
+                    "pensjonsgivendeInntekter" to listOf(
+                        mapOf<String, Any>("pensjonsgivendeInntekt" to 450000.0, "årstall" to Year.of(2017), "gjennomsnittligG" to 93281.0),
+                        mapOf<String, Any>("pensjonsgivendeInntekt" to 450000.0, "årstall" to Year.of(2016), "gjennomsnittligG" to 91740.0),
+                        mapOf<String, Any>("pensjonsgivendeInntekt" to 450000.0, "årstall" to Year.of(2015), "gjennomsnittligG" to 89502.0)
+                    ),
+                    "nåværendeGrunnbeløp" to 93634.0
+                ),
+                output = mapOf("sykepengegrunnlag" to 460589.0)
+            )
+
+            assertVarsel(Varselkode.RV_SØ_45, 1.vedtaksperiode.filter())
+        }
+    }
+
+    @Test
+    fun `Subsumerer 8-34 ledd 1 for selvstendig uten forsikring`() = Toggle.SelvstendigNæringsdrivende.enable {
+        selvstendig {
+            håndterSøknad(
+                Søknad.Søknadsperiode.Sykdom(1.januar, 31.januar, 100.prosent),
+                Søknad.Søknadsperiode.Venteperiode(1.januar til 16.januar),
+                pensjonsgivendeInntekter = listOf(
+                    Søknad.PensjonsgivendeInntekt(Year.of(2017), 450000.årlig),
+                    Søknad.PensjonsgivendeInntekt(Year.of(2016), 450000.årlig),
+                    Søknad.PensjonsgivendeInntekt(Year.of(2015), 450000.årlig)
+                )
+            )
+            håndterVilkårsgrunnlag(1.vedtaksperiode, skatteinntekter = emptyList())
+            håndterYtelser(1.vedtaksperiode)
+
+            val antallSubsumsjoner = { subsumsjonInspektør: SubsumsjonInspektør ->
+                subsumsjonInspektør.antallSubsumsjoner(
+                    paragraf = Paragraf.PARAGRAF_8_34,
+                    versjon = 1.januar(2019),
+                    ledd = Ledd.LEDD_1,
+                    punktum = null,
+                    bokstav = null
+                )
+            }
+            assertSubsumsjoner { assertEquals(1, antallSubsumsjoner(this)) }
+            assertVarsel(Varselkode.RV_SØ_45, 1.vedtaksperiode.filter())
+        }
+    }
+}
