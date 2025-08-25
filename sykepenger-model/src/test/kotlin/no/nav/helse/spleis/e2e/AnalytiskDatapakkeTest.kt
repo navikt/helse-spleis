8b48bbb2cbc8e8845591803439fa887ce8687698
Amalie Mansåker <amalie.erdal.mansaker@nav.no>
2025-08-25 13:53:16 +0200

    Bytter til AbstractDslTest
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/AnalytiskDatapakkeTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/AnalytiskDatapakkeTest.kt
@@ -1,5 +1,8 @@
 package no.nav.helse.spleis.e2e
 
+import java.util.UUID
+import no.nav.helse.dsl.AbstractDslTest
+import no.nav.helse.dsl.OverstyrtArbeidsgiveropplysning
 import no.nav.helse.dsl.a1
 import no.nav.helse.hendelser.Behandlingsporing
 import no.nav.helse.hendelser.Dagtype
@@ -8,96 +11,103 @@ import no.nav.helse.inspectors.inspektør
 import no.nav.helse.januar
 import no.nav.helse.person.PersonObserver
 import no.nav.helse.person.aktivitetslogg.Varselkode
+import no.nav.helse.spleis.e2e.AktivitetsloggFilter.Companion.filter
 import no.nav.helse.økonomi.Inntekt
 import no.nav.helse.økonomi.Inntekt.Companion.månedlig
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class AnalytiskDatapakkeTest : AbstractEndToEndTest() {
-    private fun IdInnhenter.sisteBehandlingId(orgnr: String) = inspektør(orgnr).vedtaksperioder(this).inspektør.behandlinger.last().id
+internal class AnalytiskDatapakkeTest : AbstractDslTest() {
+    private fun UUID.sisteBehandlingId(orgnr: String) = inspektør(orgnr).vedtaksperioder(this).inspektør.behandlinger.last().id
 
     @Test
     fun `Standard analytisk datapakke`() {
-        nyttVedtak(januar)
+        a1 {
+            nyttVedtak(januar)
 
-        val event = observatør.analytiskDatapakkeEventer.last()
-        val expected = PersonObserver.AnalytiskDatapakkeEvent(
-            yrkesaktivitetssporing = Behandlingsporing.Yrkesaktivitet.Arbeidstaker(a1),
-            vedtaksperiodeId = 1.vedtaksperiode.id(a1),
-            behandlingId = 1.vedtaksperiode.sisteBehandlingId(a1),
-            skjæringstidspunkt = 1.januar,
-            beløpTilBruker = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
-                totalBeløp = 0.0,
-                nettoBeløp = 0.0
-            ),
-            beløpTilArbeidsgiver = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
-                totalBeløp = 15741.0,
-                nettoBeløp = 15741.0
-            ),
-            fom = 1.januar,
-            tom = 31.januar,
-            antallForbrukteSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
-                antallDager = 11,
-                nettoDager = 11
-            ),
-            antallGjenståendeSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
-                antallDager = 237,
-                nettoDager = 237
-            ),
-            harAndreInntekterIBeregning = false
-        )
+            val event = observatør.analytiskDatapakkeEventer.last()
+            val expected = PersonObserver.AnalytiskDatapakkeEvent(
+                yrkesaktivitetssporing = Behandlingsporing.Yrkesaktivitet.Arbeidstaker(a1),
+                vedtaksperiodeId = 1.vedtaksperiode,
+                behandlingId = 1.vedtaksperiode.sisteBehandlingId(a1),
+                skjæringstidspunkt = 1.januar,
+                beløpTilBruker = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
+                    totalBeløp = 0.0,
+                    nettoBeløp = 0.0
+                ),
+                beløpTilArbeidsgiver = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
+                    totalBeløp = 15741.0,
+                    nettoBeløp = 15741.0
+                ),
+                fom = 1.januar,
+                tom = 31.januar,
+                antallForbrukteSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
+                    antallDager = 11,
+                    nettoDager = 11
+                ),
+                antallGjenståendeSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
+                    antallDager = 237,
+                    nettoDager = 237
+                ),
+                harAndreInntekterIBeregning = false
+            )
 
-        assertEquals(expected, event)
+            assertEquals(expected, event)
+        }
     }
 
     @Test
     fun `Standard analytisk datapakke - revurdering`() {
-        nyttVedtak(januar)
-        håndterOverstyrArbeidsgiveropplysninger(1.januar, listOf(
-            OverstyrtArbeidsgiveropplysning(
-                orgnummer = a1,
-                inntekt = 25000.månedlig,
-                refusjonsopplysninger = listOf(
-                    Triple(1.januar, 25.januar, 25000.månedlig),
-                    Triple(26.januar, null, Inntekt.INGEN),
+        a1 {
+            nyttVedtak(januar)
+            håndterOverstyrArbeidsgiveropplysninger(
+                1.januar,
+                listOf(
+                    OverstyrtArbeidsgiveropplysning(
+                        orgnummer = a1,
+                        inntekt = 25000.månedlig,
+                        refusjonsopplysninger = listOf(
+                            Triple(1.januar, 25.januar, 25000.månedlig),
+                            Triple(26.januar, null, Inntekt.INGEN),
+                        )
+                    )
                 )
             )
-        ))
-        håndterOverstyrTidslinje(listOf(ManuellOverskrivingDag(17.januar, Dagtype.Feriedag)))
-        håndterYtelser(1.vedtaksperiode)
-        håndterSimulering(1.vedtaksperiode)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode, true)
-        håndterUtbetalt()
+            håndterOverstyrTidslinje(listOf(ManuellOverskrivingDag(17.januar, Dagtype.Feriedag)))
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode, true)
+            håndterUtbetalt()
 
-        val event = observatør.analytiskDatapakkeEventer.last()
-        val expected = PersonObserver.AnalytiskDatapakkeEvent(
-            yrkesaktivitetssporing = Behandlingsporing.Yrkesaktivitet.Arbeidstaker(a1),
-            vedtaksperiodeId = 1.vedtaksperiode.id(a1),
-            behandlingId = 1.vedtaksperiode.sisteBehandlingId(a1),
-            skjæringstidspunkt = 1.januar,
-            beløpTilBruker = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
-                totalBeløp = 4616.0,
-                nettoBeløp = 4616.0
-            ),
-            beløpTilArbeidsgiver = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
-                totalBeløp = 6924.0,
-                nettoBeløp = -8817.0
-            ),
-            fom = 1.januar,
-            tom = 31.januar,
-            antallForbrukteSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
-                antallDager = 10,
-                nettoDager = -1
-            ),
-            antallGjenståendeSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
-                antallDager = 238,
-                nettoDager = 1
-            ),
-            harAndreInntekterIBeregning = false
-        )
+            val event = observatør.analytiskDatapakkeEventer.last()
+            val expected = PersonObserver.AnalytiskDatapakkeEvent(
+                yrkesaktivitetssporing = Behandlingsporing.Yrkesaktivitet.Arbeidstaker(a1),
+                vedtaksperiodeId = 1.vedtaksperiode,
+                behandlingId = 1.vedtaksperiode.sisteBehandlingId(a1),
+                skjæringstidspunkt = 1.januar,
+                beløpTilBruker = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
+                    totalBeløp = 4616.0,
+                    nettoBeløp = 4616.0
+                ),
+                beløpTilArbeidsgiver = PersonObserver.AnalytiskDatapakkeEvent.Pengeinformasjon(
+                    totalBeløp = 6924.0,
+                    nettoBeløp = -8817.0
+                ),
+                fom = 1.januar,
+                tom = 31.januar,
+                antallForbrukteSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
+                    antallDager = 10,
+                    nettoDager = -1
+                ),
+                antallGjenståendeSykedagerEtterPeriode = PersonObserver.AnalytiskDatapakkeEvent.Daginformasjon(
+                    antallDager = 238,
+                    nettoDager = 1
+                ),
+                harAndreInntekterIBeregning = false
+            )
 
-        assertEquals(expected, event)
-        assertVarsel(Varselkode.RV_UT_23, 1.vedtaksperiode.filter())
+            assertEquals(expected, event)
+            assertVarsel(Varselkode.RV_UT_23, 1.vedtaksperiode.filter())
+        }
     }
-
 }
