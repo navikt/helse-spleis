a61d3781c26e7f8f6a237c77ec353c0c7b2505ec
david steinsland <david@davidsteinsland.net>
2025-09-01 14:38:00 +0200

    økonomi-modulen trenger ikke primitiver-avhengighet

--- sykepenger-økonomi-dto/build.gradle.kts
+++ sykepenger-økonomi-dto/build.gradle.kts
@@ -1,5 +1,1 @@
-dependencies {
-    // bruker "implementation" fremfor "api" for å unngå
-    // at avhengigheten blir transitiv, altså kopiert ut, til de som bruker denne modulen.
-    implementation(project(":sykepenger-primitiver-dto"))
-}
+
--- sykepenger-økonomi/build.gradle.kts
+++ sykepenger-økonomi/build.gradle.kts
@@ -1,7 +1,4 @@
 dependencies {
     // bruker "api" sånn at avhengigheten blir kopiert ut til konsumenter av denne modulen
     api(project(":sykepenger-økonomi-dto"))
-
-    implementation(project(":sykepenger-primitiver"))
-    testFixturesImplementation(project(":sykepenger-primitiver"))
 }
