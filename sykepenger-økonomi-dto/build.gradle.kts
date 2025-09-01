9a8d4d756ba2e563d73dc11a91ef4ebfa31403f6
david steinsland <david@davidsteinsland.net>
2025-09-01 14:46:46 +0200

    publiserer økonomi-modulen

--- sykepenger-økonomi-dto/build.gradle.kts
+++ sykepenger-økonomi-dto/build.gradle.kts
@@ -1,1 +1,26 @@
+plugins {
+    `maven-publish`
+}
 
+configure<JavaPluginExtension> {
+    withSourcesJar()
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "com.github.navikt.spleis.økonomi"
+            artifactId = project.name
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/helse-spleis")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+}
--- sykepenger-økonomi/build.gradle.kts
+++ sykepenger-økonomi/build.gradle.kts
@@ -1,4 +1,31 @@
+plugins {
+    `maven-publish`
+}
+
 dependencies {
     // bruker "api" sånn at avhengigheten blir kopiert ut til konsumenter av denne modulen
     api(project(":sykepenger-økonomi-dto"))
+}
+
+configure<JavaPluginExtension> {
+    withSourcesJar()
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = "com.github.navikt.spleis.økonomi"
+            artifactId = project.name
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/helse-spleis")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
 }
