0eaed0dda83baea5feb560410907b861493260f8
david steinsland <david@davidsteinsland.net>
2025-10-18 08:58:47 +0200

    nullstiller agp på behandlinger på ikke-arbeidstakere

--- sykepenger-serde/src/main/kotlin/no/nav/helse/serde/migration/V334ArbeidsgiverperioderKunPåArbeidstakere.kt
+++ sykepenger-serde/src/main/kotlin/no/nav/helse/serde/migration/V334ArbeidsgiverperioderKunPåArbeidstakere.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.serde.migration
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 
 internal class V334ArbeidsgiverperioderKunPåArbeidstakere : JsonMigration(334) {
@@ -9,6 +10,19 @@ internal class V334ArbeidsgiverperioderKunPåArbeidstakere : JsonMigration(334) 
         jsonNode.path("arbeidsgivere").forEach { arbeidsgiver ->
             if (arbeidsgiver.path("yrkesaktivitetstype").asText() != "ARBEIDSTAKER") {
                 (arbeidsgiver as ObjectNode).putArray("arbeidsgiverperioder")
+                arbeidsgiver.path("vedtaksperioder").forEach { migrerVedtaksperiode(it) }
+                arbeidsgiver.path("forkastede").forEach { migrerVedtaksperiode(it.path("vedtaksperiode")) }
+            }
+        }
+    }
+
+    private fun migrerVedtaksperiode(vedtaksperiode: JsonNode) {
+        vedtaksperiode.path("behandlinger").forEach { behandling ->
+            behandling.path("endringer").forEach { endring ->
+                (endring.path("arbeidsgiverperiode") as ObjectNode).apply {
+                    put("ferdigAvklart", false)
+                    putArray("dager")
+                }
             }
         }
     }
