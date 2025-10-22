d626e893816ffb3c7a3c5de40f7f2f7948cef636
david steinsland <david@davidsteinsland.net>
2025-10-22 14:17:23 +0200

    skriver tester for ventetidberegning
    basert på tester vi har lest i flex sine test caser, og § 8-34 så har forfattet det vi (for nå) tenker er ganske uttømmende for ventetid.
    
    1. ventetiden utgjør 16 dager (hovedregel)
    2. opphold i ventetiden gjør at det startes ny telling
    3. det tillates opp til 15 oppholdsdager etter en fullstendig ventetid. En fullstendig ventetid er når vi har kommet frem til 16 ventetid-dager og nav har utbetalt minst én dag. Altså betyr dette at dersom ventetidberegningen er ferdig på fredag, lørdag eller søndag, så må bruker være syk påfølgende mandag også (unntak til punkt 1 siden ventetiden da kan i praksis utgjøre 18 dager).
    4. "Implisitt helg" (altså syk fredag og syk mandag) tolkes som sykdom i ventetiden (unntak til punkt 2)
    
    Co-authored-by: Håkon Arneng Holmstedt <hakon.arneng.holmstedt@nav.no>
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>
    Co-authored-by: Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
    Co-authored-by: Sivert Lunsæter <Sivert.Frang.Lunseter@nav.no>

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingstidslinje/VentetidberegnerTest.kt
@@ -0,0 +1,258 @@
+package no.nav.helse.utbetalingstidslinje
+
+import no.nav.helse.februar
+import no.nav.helse.hendelser.Periode
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
+import no.nav.helse.sykdomstidslinje.Sykdomstidslinje
+import no.nav.helse.testhelpers.S
+import no.nav.helse.testhelpers.opphold
+import no.nav.helse.testhelpers.resetSeed
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+@Disabled
+internal class VentetidberegnerTest {
+
+    @Test
+    fun `ventetiden utgjør de første 16 dagene`() {
+        val tidslinje = 17.S
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ventetiden er ikke ferdig hvis det er nøyaktig 16 dager`() {
+        val tidslinje = 16.S
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ventetiden er ikke ferdig før det er utbetalt sykepenger - ventetiden slutter på fredag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 16.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ventetiden er ikke ferdig før det er utbetalt sykepenger - ventetiden slutter på lørdag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 17.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ventetiden er ikke ferdig før det er utbetalt sykepenger - ventetiden slutter på søndag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 18.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ventetiden er ikke ferdig før det er utbetalt sykepenger - ventetiden slutter på mandag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 18.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `starter ny ventetid hvis forrige ventetid på 16 dager slutter på fredag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 16.S + 3.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+        resultat[1].also {
+            assertEquals(23.januar til 27.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `starter ny ventetid hvis forrige ventetid på 16 dager slutter på lørdag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 17.S + 2.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+        resultat[1].also {
+            assertEquals(23.januar til 27.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `starter ny ventetid hvis forrige ventetid på 16 dager slutter på søndag`() {
+        val tidslinje = resetSeed(frøDato = 4.januar) { 18.S + 1.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(4.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+        resultat[1].also {
+            assertEquals(23.januar til 27.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `samme ventetid hvis det er utbetalt sykepenger og inntil 15 dager opphold mellom`() {
+        val tidslinje = resetSeed { 17.S + 15.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ny ventetid hvis det er utbetalt sykepenger til og med fredag og mer enn 15 dager opphold mellom`() {
+        val tidslinje = resetSeed(frøDato = 3.januar) { 17.S + 15.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(3.januar til 18.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+        resultat[1].also {
+            assertEquals(3.februar til 12.februar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ny ventetid hvis det er utbetalt sykepenger og mer enn 15 dager opphold mellom`() {
+        val tidslinje = resetSeed { 17.S + 16.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+        resultat[1].also {
+            assertEquals(3.februar til 12.februar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ny ventetid hvis det er opphold i ventetiden`() {
+        val tidslinje = resetSeed { 10.S + 1.opphold + 10.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 10.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+        resultat[1].also {
+            assertEquals(12.januar til 21.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `samme ventetid hvis det er opphold i helg mellom - lørdag og søndag mellom`() {
+        val tidslinje = resetSeed { 5.S + 2.opphold + 12.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `samme ventetid hvis det er opphold i helg mellom - lørdag mellom`() {
+        val tidslinje = resetSeed { 5.S + 1.opphold + 12.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `samme ventetid hvis det er opphold i helg mellom - søndag mellom`() {
+        val tidslinje = resetSeed { 6.S + 1.opphold + 12.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(1, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 16.januar, it.periode)
+            assertTrue(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ulik ventetid hvis det er opphold i helg og påfølgende mandag`() {
+        val tidslinje = resetSeed { 5.S + 3.opphold + 12.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 7.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+        resultat[0].also {
+            assertEquals(9.januar til 20.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    @Test
+    fun `ulik ventetid hvis det er opphold i helg og fredagen`() {
+        val tidslinje = resetSeed { 4.S + 3.opphold + 12.S }
+        val resultat = tidslinje.ventetid()
+        assertEquals(2, resultat.size)
+        resultat[0].also {
+            assertEquals(1.januar til 4.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+        resultat[0].also {
+            assertEquals(8.januar til 19.januar, it.periode)
+            assertFalse(it.ferdigAvklart)
+        }
+    }
+
+    private fun Sykdomstidslinje.ventetid(): List<Ventetidsavklaring> {
+        return emptyList()
+    }
+
+    private data class Ventetidsavklaring(
+        val periode: Periode,
+        val ferdigAvklart: Boolean
+    )
+}
