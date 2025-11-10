59209eeb4a1ed44ad913b732fb6a7034559d8257
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2025-11-10 14:26:22 +0100

    abstracte2e => abstact dsl
    another one bites the dust

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/overstyring/OverstyrInntektTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/overstyring/OverstyrInntektTest.kt
@@ -2,11 +2,14 @@ package no.nav.helse.spleis.e2e.overstyring
 
 import java.time.LocalDate
 import no.nav.helse.desember
+import no.nav.helse.dsl.AbstractDslTest
 import no.nav.helse.dsl.Arbeidstakerkilde
 import no.nav.helse.dsl.INNTEKT
+import no.nav.helse.dsl.OverstyrtArbeidsgiveropplysning
 import no.nav.helse.dsl.a1
 import no.nav.helse.dsl.a2
 import no.nav.helse.dsl.assertInntektsgrunnlag
+import no.nav.helse.dsl.tilGodkjenning
 import no.nav.helse.hendelser.Inntektsmelding.Refusjon
 import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
 import no.nav.helse.hendelser.til
@@ -18,22 +21,7 @@ import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_BLOKKERENDE_PER
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_GODKJENNING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_HISTORIKK
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_SIMULERING
-import no.nav.helse.spleis.e2e.AbstractEndToEndTest
-import no.nav.helse.spleis.e2e.OverstyrtArbeidsgiveropplysning
-import no.nav.helse.spleis.e2e.assertTilstander
-import no.nav.helse.spleis.e2e.assertVarsel
-import no.nav.helse.spleis.e2e.håndterArbeidsgiveropplysninger
-import no.nav.helse.spleis.e2e.håndterInntektsmelding
-import no.nav.helse.spleis.e2e.håndterOverstyrArbeidsgiveropplysninger
-import no.nav.helse.spleis.e2e.håndterOverstyrInntekt
-import no.nav.helse.spleis.e2e.håndterSimulering
-import no.nav.helse.spleis.e2e.håndterSykmelding
-import no.nav.helse.spleis.e2e.håndterSøknad
-import no.nav.helse.spleis.e2e.håndterUtbetalingsgodkjenning
-import no.nav.helse.spleis.e2e.håndterVilkårsgrunnlag
-import no.nav.helse.spleis.e2e.håndterYtelser
-import no.nav.helse.spleis.e2e.nullstillTilstandsendringer
-import no.nav.helse.spleis.e2e.tilGodkjenning
+import no.nav.helse.spleis.e2e.AktivitetsloggFilter.Companion.filter
 import no.nav.helse.økonomi.Inntekt.Companion.INGEN
 import no.nav.helse.økonomi.Inntekt.Companion.daglig
 import no.nav.helse.økonomi.Inntekt.Companion.månedlig
@@ -43,82 +31,85 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
-internal class OverstyrInntektTest : AbstractEndToEndTest() {
+internal class OverstyrInntektTest : AbstractDslTest() {
 
     @Test
     fun `skal kunne overstyre en inntekt i et enkelt case`() {
-        // Hva gjør vi egentlig med overstyring? Skal man kunne sette opp inntekten uten å ha mottatt en ny inntektsmelding med nye refusjonsopplysninger?
-        val fom = 1.januar(2021)
-        val overstyrtInntekt = 32000.månedlig
-        tilGodkjenning(fom til 31.januar(2021), 100.prosent)
+        a1 {
+            // Hva gjør vi egentlig med overstyring? Skal man kunne sette opp inntekten uten å ha mottatt en ny inntektsmelding med nye refusjonsopplysninger?
+            val fom = 1.januar(2021)
+            val overstyrtInntekt = 32000.månedlig
+            tilGodkjenning(fom til 31.januar(2021), 100.prosent)
 
-        håndterInntektsmelding(
-            listOf(fom til fom.plusDays(15)),
-            beregnetInntekt = overstyrtInntekt,
-            refusjon = Refusjon(overstyrtInntekt, null, emptyList())
-        )
-        håndterYtelser(1.vedtaksperiode)
-        håndterSimulering(1.vedtaksperiode)
+            håndterInntektsmelding(
+                listOf(fom til fom.plusDays(15)),
+                beregnetInntekt = overstyrtInntekt,
+                refusjon = Refusjon(overstyrtInntekt, null, emptyList())
+            )
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
 
-        nullstillTilstandsendringer()
+            nullstillTilstandsendringer()
 
-        assertVarsel(RV_IM_4, 1.vedtaksperiode.filter())
-        assertTilstander(1.vedtaksperiode, AVVENTER_GODKJENNING)
+            assertVarsel(RV_IM_4, 1.vedtaksperiode.filter())
+            assertTilstander(1.vedtaksperiode, AVVENTER_GODKJENNING)
 
-        assertInntektsgrunnlag(1.januar(2021), forventetAntallArbeidsgivere = 1) {
-            assertInntektsgrunnlag(a1, overstyrtInntekt)
+            assertInntektsgrunnlag(1.januar(2021), forventetAntallArbeidsgivere = 1) {
+                assertInntektsgrunnlag(a1, overstyrtInntekt)
+            }
         }
     }
 
     @Test
     fun `overstyre ghostinntekt`() {
-        håndterSøknad(Sykdom(1.januar, 31.januar, 100.prosent), orgnummer = a1)
-        håndterArbeidsgiveropplysninger(
-            listOf(1.januar til 16.januar),
-            orgnummer = a1,
-            vedtaksperiodeIdInnhenter = 1.vedtaksperiode
-        )
-        håndterVilkårsgrunnlag(
-            1.vedtaksperiode,
-            skatteinntekter = listOf(a1 to INNTEKT),
-            arbeidsforhold = listOf(
-                Triple(a1, LocalDate.EPOCH, null),
-                Triple(a2, 1.desember(2017), null)
-            ),
-            orgnummer = a1
-        )
-        assertVarsel(RV_VV_2, 1.vedtaksperiode.filter(orgnummer = a1))
+        a1 {
+            håndterSøknad(Sykdom(1.januar, 31.januar, 100.prosent), orgnummer = a1)
+            håndterArbeidsgiveropplysninger(
+                listOf(1.januar til 16.januar)
+            )
+            håndterVilkårsgrunnlag(
+                1.vedtaksperiode,
+                skatteinntekter = listOf(a1 to INNTEKT),
+                arbeidsforhold = listOf(
+                    Triple(a1, LocalDate.EPOCH, null),
+                    Triple(a2, 1.desember(2017), null)
+                )
+            )
+            assertVarsel(RV_VV_2, 1.vedtaksperiode.filter())
 
-        håndterYtelser(1.vedtaksperiode, orgnummer = a1)
-        håndterSimulering(1.vedtaksperiode, orgnummer = a1)
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
 
-        nullstillTilstandsendringer()
+            nullstillTilstandsendringer()
 
-        håndterOverstyrArbeidsgiveropplysninger(
-            skjæringstidspunkt = 1.januar,
-            arbeidsgiveropplysninger = listOf(OverstyrtArbeidsgiveropplysning(a2, 500.daglig, emptyList()))
-        )
-        assertInntektsgrunnlag(1.januar, forventetAntallArbeidsgivere = 2) {
-            assertInntektsgrunnlag(a1, INNTEKT, INNTEKT)
-            assertInntektsgrunnlag(a2, INGEN, 500.daglig, forventetKorrigertInntekt = 500.daglig, forventetkilde = Arbeidstakerkilde.AOrdningen)
+            håndterOverstyrArbeidsgiveropplysninger(
+                skjæringstidspunkt = 1.januar,
+                arbeidsgiveropplysninger = listOf(OverstyrtArbeidsgiveropplysning(a2, 500.daglig, emptyList()))
+            )
+            assertInntektsgrunnlag(1.januar, forventetAntallArbeidsgivere = 2) {
+                assertInntektsgrunnlag(a1, INNTEKT, INNTEKT)
+                assertInntektsgrunnlag(a2, INGEN, 500.daglig, forventetKorrigertInntekt = 500.daglig, forventetkilde = Arbeidstakerkilde.AOrdningen)
+            }
         }
     }
 
     @Test
     fun `skal ikke hente registerdata for vilkårsprøving på nytt ved overstyring av inntekt`() {
-        tilGodkjenning(januar, a1)
-        nullstillTilstandsendringer()
-        håndterOverstyrInntekt(inntekt = 19000.månedlig, orgnummer = a1, skjæringstidspunkt = 1.januar)
-        håndterYtelser(1.vedtaksperiode)
-        håndterSimulering(1.vedtaksperiode)
-        assertTilstander(
-            1.vedtaksperiode,
-            AVVENTER_GODKJENNING,
-            AVVENTER_BLOKKERENDE_PERIODE,
-            AVVENTER_HISTORIKK,
-            AVVENTER_SIMULERING,
-            AVVENTER_GODKJENNING
-        )
+        a1 {
+            tilGodkjenning(januar)
+            nullstillTilstandsendringer()
+            håndterOverstyrInntekt(inntekt = 19000.månedlig, skjæringstidspunkt = 1.januar)
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+            assertTilstander(
+                1.vedtaksperiode,
+                AVVENTER_GODKJENNING,
+                AVVENTER_BLOKKERENDE_PERIODE,
+                AVVENTER_HISTORIKK,
+                AVVENTER_SIMULERING,
+                AVVENTER_GODKJENNING
+            )
+        }
     }
 
     @Test
@@ -126,25 +117,27 @@ internal class OverstyrInntektTest : AbstractEndToEndTest() {
         val OverMinstegrense = 50000.årlig
         val UnderMinstegrense = 46000.årlig
 
-        håndterSykmelding(januar)
-        håndterSøknad(januar)
-        håndterArbeidsgiveropplysninger(
-            listOf(1.januar til 16.januar),
-            beregnetInntekt = OverMinstegrense,
-            vedtaksperiodeIdInnhenter = 1.vedtaksperiode,
-        )
-        håndterVilkårsgrunnlag(1.vedtaksperiode)
-        håndterYtelser(1.vedtaksperiode)
-        håndterSimulering(1.vedtaksperiode)
+        a1 {
+            håndterSykmelding(januar)
+            håndterSøknad(januar)
+            håndterArbeidsgiveropplysninger(
+                listOf(1.januar til 16.januar),
+                beregnetInntekt = OverMinstegrense
+            )
+            håndterVilkårsgrunnlag(1.vedtaksperiode)
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
 
-        håndterOverstyrInntekt(UnderMinstegrense, skjæringstidspunkt = 1.januar)
-        håndterYtelser(1.vedtaksperiode)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode)
-        assertVarsel(Varselkode.RV_SV_1, 1.vedtaksperiode.filter())
+            håndterOverstyrInntekt(inntekt = UnderMinstegrense, skjæringstidspunkt = 1.januar)
+            håndterYtelser(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+            assertVarsel(Varselkode.RV_SV_1, 1.vedtaksperiode.filter())
 
-        assertEquals(2, inspektør.antallUtbetalinger)
-        assertEquals(0, inspektør.utbetaling(1).arbeidsgiverOppdrag.nettoBeløp())
-        assertTrue(inspektør.utbetaling(1).erAvsluttet)
-        assertTrue(inspektør.utbetaling(0).erForkastet)
+            assertEquals(2, inspektør.antallUtbetalinger)
+            assertEquals(0, inspektør.utbetaling(1).arbeidsgiverOppdrag.nettoBeløp())
+            assertTrue(inspektør.utbetaling(1).erAvsluttet)
+            assertTrue(inspektør.utbetaling(0).erForkastet)
+
+        }
     }
 }
