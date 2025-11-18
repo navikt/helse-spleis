1fa0bdc1507b9a8537dacff9f11c1a8a5817a149
david steinsland <david@davidsteinsland.net>
2025-11-18 09:12:18 +0100

    starter på en test av SelvstendigUtbetalingstidslinjeBuilderVedtaksperiode

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/utbetalingstidslinje/SelvstendigUtbetalingstidslinjeBuilderVedtaksperiodeTest.kt
@@ -0,0 +1,110 @@
+package no.nav.helse.utbetalingstidslinje
+
+import no.nav.helse.assertForventetFeil
+import no.nav.helse.hendelser.Periode
+import no.nav.helse.hendelser.til
+import no.nav.helse.inspectors.UtbetalingstidslinjeInspektør
+import no.nav.helse.inspectors.inspektør
+import no.nav.helse.januar
+import no.nav.helse.person.beløp.Beløpstidslinje
+import no.nav.helse.sykdomstidslinje.Sykdomstidslinje
+import no.nav.helse.testhelpers.A
+import no.nav.helse.testhelpers.M
+import no.nav.helse.testhelpers.S
+import no.nav.helse.testhelpers.resetSeed
+import no.nav.helse.økonomi.Inntekt.Companion.månedlig
+import no.nav.helse.økonomi.Prosentdel.Companion.prosent
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+
+internal class SelvstendigUtbetalingstidslinjeBuilderVedtaksperiodeTest {
+    @Test
+    fun `melding til nav dager`() {
+        undersøke(16.M + 15.S, dagerNavOvertarAnsvar = listOf(1.januar til 16.januar))
+        assertEquals(31, inspektør.size)
+        assertEquals(16, inspektør.ventetidDagTeller)
+        assertEquals(1, perioder.size)
+        assertEquals(
+            PeriodeUtenNavAnsvar(
+                omsluttendePeriode = 1.januar til 31.januar,
+                dagerUtenAnsvar = listOf(1.januar til 16.januar),
+                ferdigAvklart = true
+            ), perioder.single()
+        )
+    }
+
+    @Test
+    fun `spredte melding til nav dager`() {
+        assertForventetFeil(
+            forklaring = "må håndtere spredte dager uten nav ansvar",
+            nå = {
+                assertThrows<IllegalStateException> {
+                    undersøke(4.M + 2.A + 10.M + 15.S)
+                }
+            },
+            ønsket = {
+                assertDoesNotThrow {
+                    undersøke(4.M + 2.A + 10.M + 15.S)
+                }
+                assertEquals(31, inspektør.size)
+                assertEquals(16 + 4, inspektør.ventetidDagTeller)
+                assertEquals(1, perioder.size)
+                assertEquals(
+                    PeriodeUtenNavAnsvar(
+                        omsluttendePeriode = 1.januar til 4.januar,
+                        dagerUtenAnsvar = listOf(1.januar til 4.januar),
+                        ferdigAvklart = false
+                    ), perioder[0]
+                )
+                assertEquals(
+                    PeriodeUtenNavAnsvar(
+                        omsluttendePeriode = 7.januar til 31.januar,
+                        dagerUtenAnsvar = listOf(7.januar til 22.januar),
+                        ferdigAvklart = true
+                    ), perioder[1]
+                )
+            }
+        )
+    }
+
+    @BeforeEach
+    fun setup() {
+        reset()
+    }
+
+    private lateinit var inspektør: UtbetalingstidslinjeInspektør
+    private lateinit var utbetalingstidslinje: Utbetalingstidslinje
+    private val perioder: MutableList<PeriodeUtenNavAnsvar> = mutableListOf()
+
+    private fun undersøke(tidslinje: Sykdomstidslinje, dagerNavOvertarAnsvar: List<Periode> = emptyList()) {
+        val ventetidberegner = Ventetidberegner()
+        val ventetider = ventetidberegner.result(tidslinje)
+        perioder.addAll(ventetider)
+
+        val builder = SelvstendigUtbetalingstidslinjeBuilderVedtaksperiode(
+            dekningsgrad = 80.prosent,
+            ventetid = ventetider.singleOrNull()?.dagerUtenAnsvar?.singleOrNull(),
+            dagerNavOvertarAnsvar = dagerNavOvertarAnsvar
+        )
+
+        utbetalingstidslinje = builder.result(tidslinje, 31000.månedlig, Beløpstidslinje())
+        inspektør = utbetalingstidslinje.inspektør
+    }
+
+    // undersøker forskjellige tidslinjer som skal ha samme funksjonelle betydning
+    private fun undersøkeLike(vararg tidslinje: () -> Sykdomstidslinje, dagerNavOvertarAnsvar: List<Periode> = emptyList(), assertBlock: () -> Unit) {
+        tidslinje.forEach {
+            undersøke(resetSeed(tidslinjegenerator = it), dagerNavOvertarAnsvar = dagerNavOvertarAnsvar)
+            assertBlock()
+            reset()
+        }
+    }
+
+    private fun reset() {
+        resetSeed()
+        perioder.clear()
+    }
+}
