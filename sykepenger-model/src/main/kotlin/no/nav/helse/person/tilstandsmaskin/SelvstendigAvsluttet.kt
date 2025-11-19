5a710d31a2494d5390d1899d389be7272e04210d
Linus Johansson <linus.123@hotmail.com>
2025-11-19 13:58:36 +0100

    Fjern ubrukt import

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/tilstandsmaskin/SelvstendigAvsluttet.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/tilstandsmaskin/SelvstendigAvsluttet.kt
@@ -1,6 +1,5 @@
 package no.nav.helse.person.tilstandsmaskin
 
-import no.nav.helse.hendelser.Revurderingseventyr
 import no.nav.helse.person.EventBus
 import no.nav.helse.person.Vedtaksperiode
 import no.nav.helse.person.aktivitetslogg.IAktivitetslogg
