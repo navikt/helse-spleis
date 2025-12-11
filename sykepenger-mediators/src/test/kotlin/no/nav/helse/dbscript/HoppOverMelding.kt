c588546a8bcdd4542453ef8643a6605cd42cb65e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-12-11 10:51:24 +0100

    Implementerer HoppOverMelding-scriptet

--- sykepenger-mediators/src/test/kotlin/no/nav/helse/dbscript/HoppOverMelding.kt
+++ sykepenger-mediators/src/test/kotlin/no/nav/helse/dbscript/HoppOverMelding.kt
@@ -1,9 +1,37 @@
 package no.nav.helse.dbscript
 
+import java.util.UUID
+
 internal object HoppOverMelding: DbScript() {
     override val beskrivelse = "Hopper over en melding på rapiden som Spleis typisk feiler på"
 
     override fun start(connectionInfo: ConnectionInfo) {
-        error("ÅÅÅ, det var synd! For dette scriptet er ikke laget enda.")
+        println("## Velkommen til overhopping av melding")
+        println(" - Dette blir gøyalt, men ikke like gøyalt som Personeditor 🎢")
+
+        println("## Fyll inn fødselsnummer på personen det skal endres på")
+        val fødselsnummer = Input.ventPåFødselsnummer()
+        println()
+
+        println("## Fyll inn hendelseId/meldingId/meldingsreferanseId (kjært barn har mange navn - @id UUID'en hvert fall..) på meldingen du vil hoppe over")
+        val meldingId = Input.ventPåInput { runCatching { UUID.fromString(it) }.isSuccess }
+        println()
+
+        println("## Beskriv _hvorfor_ du gjør denne endringen (for auditlog) - minst 15 makreller lang 🤏")
+        val beskrivelse = Input.ventPåBeskrivelse()
+        println()
+
+        val diff = "Meldingen med meldingId '$meldingId' har fått 'behandlet_tidspunkt' satt til now() slik at Spleis hopper over meldingen og kan prosessere andre meldinger som står i kø."
+
+        databaseTransaksjon(connectionInfo) {
+            check(1 == prepareStatement("UPDATE melding SET behandlet_tidspunkt=now() WHERE fnr=? AND melding_id=? AND behandlet_tidspunkt IS NULL").use { stmt ->
+                stmt.setLong(1, fødselsnummer.verdi.toLong())
+                stmt.setString(2, meldingId)
+                stmt.executeUpdate()
+            }) { "forventet å oppdatere nøyaktig én rad ved å hoppe over en melding" }
+
+            audit(fødselsnummer, connectionInfo.epost, diff, beskrivelse)
+        }
+        println(" - Endringene dine er live ✅")
     }
 }
