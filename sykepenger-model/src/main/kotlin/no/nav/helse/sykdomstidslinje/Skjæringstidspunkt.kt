4f8fa5dd6c7b912296e62e72f2bab26e091316e7
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2025-12-16 13:07:48 +0100

    Fjerner en alternativ implementasjon av hvordan vi finner skjæringstidspunkt
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/sykdomstidslinje/Skjæringstidspunkt.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/sykdomstidslinje/Skjæringstidspunkt.kt
@@ -4,7 +4,6 @@ import java.time.LocalDate
 import no.nav.helse.dto.PeriodeDto
 import no.nav.helse.erHelg
 import no.nav.helse.hendelser.Periode
-import no.nav.helse.hendelser.somPeriode
 import no.nav.helse.hendelser.til
 
 internal data class Skjæringstidspunkter(val skjæringstidspunkter: List<Periode>) {
@@ -31,7 +30,6 @@ internal class Skjæringstidspunkt(private val personsykdomstidslinje: Sykdomsti
 
     fun alle(): Skjæringstidspunkter {
         return Skjæringstidspunkter(finnSkjæringstidspunkt().map { it.periode })
-        //return beregnSkjærignstidspunktV1000()
     }
 
     private fun finnSkjæringstidspunkt(): List<Søkekontekst> {
@@ -103,103 +101,4 @@ internal class Skjæringstidspunkt(private val personsykdomstidslinje: Sykdomsti
         }
     }
 
-    private fun beregnSkjærignstidspunktV1000(): Skjæringstidspunkter {
-        if (personsykdomstidslinje.count() == 0) return Skjæringstidspunkter(emptyList())
-        var forrigeDag: ForrigeDag = ForrigeDag.Frisk
-        val skjæringstidspunkter = mutableListOf<Periode>()
-
-        personsykdomstidslinje.forEach { dagen ->
-            forrigeDag = when (dagen) {
-                // Syk
-                is Dag.ArbeidsgiverHelgedag,
-                is Dag.Arbeidsgiverdag,
-                is Dag.MeldingTilNavDag,
-                is Dag.MeldingTilNavHelgedag,
-                is Dag.ForeldetSykedag,
-                is Dag.SykHelgedag,
-                is Dag.Sykedag -> forrigeDag.syk(dagen.dato, skjæringstidspunkter)
-
-                // Ikke syk
-                is Dag.AndreYtelser,
-                is Dag.ArbeidIkkeGjenopptattDag,
-                is Dag.Arbeidsdag,
-                is Dag.FriskHelgedag,
-                is Dag.ProblemDag -> forrigeDag.ikkeSyk(dagen.dato, skjæringstidspunkter)
-
-                // Ferie
-                is Dag.Permisjonsdag,
-                is Dag.Feriedag -> forrigeDag.ferie(dagen.dato, skjæringstidspunkter)
-
-                // Hull i tidslinjen
-                is Dag.UkjentDag -> forrigeDag.ukjent(dagen.dato, skjæringstidspunkter)
-            }
-        }
-
-        // Når det ikke er flere dager må vi avslutte siste skjæringstidspunkt på personen
-        forrigeDag.flush(skjæringstidspunkter)
-
-        return Skjæringstidspunkter(skjæringstidspunkter)
-    }
-
-    private sealed interface ForrigeDag {
-        fun syk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>): ForrigeDag
-        fun ikkeSyk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>): ForrigeDag
-        fun ferie(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>): ForrigeDag
-        fun ukjent(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>): ForrigeDag
-        fun flush(skjæringstidspunkt: MutableList<Periode>)
-
-        object Frisk: ForrigeDag {
-            override fun syk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = Syk(dato.somPeriode())
-            override fun ikkeSyk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = Frisk
-            override fun ferie(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = Frisk
-            override fun ukjent(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = Frisk
-            override fun flush(skjæringstidspunkt: MutableList<Periode>) {}
-        }
-
-        data class Syk(private val periode: Periode): ForrigeDag {
-            override fun syk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = Syk(periode.oppdaterTom(dato))
-            override fun ikkeSyk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = IkkeSyk(periode.oppdaterTom(dato))
-            override fun ferie(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = FerieEtterSyk(periode.oppdaterTom(dato))
-            override fun ukjent(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = when (dato.erHelg()) {
-                true -> Syk(periode.oppdaterTom(dato))
-                false -> {
-                    skjæringstidspunkt.add(periode)
-                    Frisk
-                }
-            }
-            override fun flush(skjæringstidspunkt: MutableList<Periode>) { skjæringstidspunkt.add(periode) }
-        }
-
-        // Er ikke Frisk og IkkeSyk det samme?
-        // Nei, IkkeSyk er en dag vi kjenner til å sykdomstidslinjen som ikke er sykdom
-        // mens Frisk er hull i sykdomstidslinjen vi ikke kjenner til
-        // .. finn på noe bedre navn selv da vel 🤷‍♂️
-        data class IkkeSyk(private val periode: Periode): ForrigeDag {
-            override fun syk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>): ForrigeDag {
-                skjæringstidspunkt.add(periode)
-                return Syk(dato.somPeriode())
-            }
-            override fun ikkeSyk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = IkkeSyk(periode.oppdaterTom(dato))
-            override fun ferie(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = IkkeSyk(periode.oppdaterTom(dato))
-            override fun ukjent(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>): ForrigeDag {
-                skjæringstidspunkt.add(periode)
-                return Frisk
-            }
-            override fun flush(skjæringstidspunkt: MutableList<Periode>) { skjæringstidspunkt.add(periode) }
-        }
-
-        data class FerieEtterSyk(private val periode: Periode): ForrigeDag {
-            override fun syk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = Syk(periode.oppdaterTom(dato))
-            override fun ikkeSyk(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = IkkeSyk(periode.oppdaterTom(dato))
-            override fun ferie(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = FerieEtterSyk(periode.oppdaterTom(dato))
-            override fun ukjent(dato: LocalDate, skjæringstidspunkt: MutableList<Periode>) = when (dato.erHelg()) {
-                true -> FerieEtterSyk(periode.oppdaterTom(dato))
-                false -> {
-                    skjæringstidspunkt.add(periode)
-                    Frisk
-                }
-            }
-            override fun flush(skjæringstidspunkt: MutableList<Periode>) { skjæringstidspunkt.add(periode) }
-        }
-    }
 }
