518224e6a489954132336fd178718118f5262cdd
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-12-17 15:33:21 +0100

    Lirker perioder i AvventerInntektsopplysningerForAnnenArbeidsgiver ut ved påminnelse om de står i feil tilstand
    Co-authored-by: Amalie Erdal Mansåker <amalie.erdal.mansaker@nav.no>
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>
    Co-authored-by: Vemund Strom <vemund.strom@nav.no>

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/tilstandsmaskin/AvventerInntektsopplysningerForAnnenArbeidsgiver.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/tilstandsmaskin/AvventerInntektsopplysningerForAnnenArbeidsgiver.kt
@@ -1,6 +1,8 @@
 package no.nav.helse.person.tilstandsmaskin
 
 import no.nav.helse.hendelser.Hendelse
+import no.nav.helse.hendelser.Påminnelse
+import no.nav.helse.hendelser.Revurderingseventyr
 import no.nav.helse.person.EventBus
 import no.nav.helse.person.Vedtaksperiode
 import no.nav.helse.person.aktivitetslogg.IAktivitetslogg
@@ -18,5 +20,14 @@ internal data object AvventerInntektsopplysningerForAnnenArbeidsgiver : Vedtaksp
 
     override fun gjenopptaBehandling(vedtaksperiode: Vedtaksperiode, eventBus: EventBus, hendelse: Hendelse, aktivitetslogg: IAktivitetslogg) {
         vedtaksperiode.tilstand(eventBus, aktivitetslogg, nesteTilstandEtterInntekt(vedtaksperiode))
+    }
+
+    override fun håndterPåminnelse(vedtaksperiode: Vedtaksperiode, eventBus: EventBus, påminnelse: Påminnelse, aktivitetslogg: IAktivitetslogg): Revurderingseventyr? {
+        val nesteTilstandEtterInntekt = nesteTilstandEtterInntekt(vedtaksperiode)
+        if (nesteTilstandEtterInntekt is AvventerInntektsopplysningerForAnnenArbeidsgiver) return null // Er i riktig tilstand
+
+        vedtaksperiode.tilstand(eventBus, aktivitetslogg, nesteTilstandEtterInntekt)
+        vedtaksperiode.person.gjenopptaBehandling(aktivitetslogg)
+        return null
     }
 }
