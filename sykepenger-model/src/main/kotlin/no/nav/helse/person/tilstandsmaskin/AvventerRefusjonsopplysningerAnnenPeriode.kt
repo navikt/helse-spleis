4a46160488f56c41d2c7d6deee545a6e2e9a0067
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-01-26 15:51:47 +0100

    Det er visst noen perioder AvventerRefusjonsopplysningerAnnenPeriode som heller ikke er i rett tilstand
    Dette skyldes nok de som gikk inn 73dc1dc0c3ed9affde826a888591d1df3a69a225

--- sykepenger-model/src/main/kotlin/no/nav/helse/person/tilstandsmaskin/AvventerRefusjonsopplysningerAnnenPeriode.kt
+++ sykepenger-model/src/main/kotlin/no/nav/helse/person/tilstandsmaskin/AvventerRefusjonsopplysningerAnnenPeriode.kt
@@ -1,6 +1,8 @@
 package no.nav.helse.person.tilstandsmaskin
 
 import no.nav.helse.hendelser.Hendelse
+import no.nav.helse.hendelser.Påminnelse
+import no.nav.helse.hendelser.Revurderingseventyr
 import no.nav.helse.person.EventBus
 import no.nav.helse.person.Vedtaksperiode
 import no.nav.helse.person.aktivitetslogg.IAktivitetslogg
@@ -18,5 +20,12 @@ internal data object AvventerRefusjonsopplysningerAnnenPeriode : Vedtaksperiodet
 
     override fun gjenopptaBehandling(vedtaksperiode: Vedtaksperiode, eventBus: EventBus, hendelse: Hendelse, aktivitetslogg: IAktivitetslogg) {
         vedtaksperiode.tilstand(eventBus, aktivitetslogg, nesteTilstandEtterInntekt(vedtaksperiode))
+    }
+
+    override fun håndterPåminnelse(vedtaksperiode: Vedtaksperiode, eventBus: EventBus, påminnelse: Påminnelse, aktivitetslogg: IAktivitetslogg): Revurderingseventyr? {
+        val nyTilstand = nesteTilstandEtterInntekt(vedtaksperiode).takeUnless { it is AvventerRefusjonsopplysningerAnnenPeriode } ?: return null
+        aktivitetslogg.info("Endrer tilstand fra AvventerRefusjonsopplysningerAnnenPeriode til ${nyTilstand::class.simpleName} som følge av en påminnelse.")
+        vedtaksperiode.tilstand(eventBus, aktivitetslogg, nyTilstand)
+        return null
     }
 }
