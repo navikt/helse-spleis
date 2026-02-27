36aeded87a460d7e9760212b7c4bc26d8bc692b9
Sivert Lunsæter <sfl@computas.com>
2026-02-27 10:24:31 +0100

    Bruk riktig URL for graphQL spec i graphQL test
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- sykepenger-api/src/test/kotlin/no/nav/helse/spleis/graphql/GraphQLApiTest.kt
+++ sykepenger-api/src/test/kotlin/no/nav/helse/spleis/graphql/GraphQLApiTest.kt
@@ -103,7 +103,7 @@ internal class GraphQLApiTest : AbstractObservableTest() {
         spleisApiTestApplication(spekematClient = spekematClient, testdata = opprettTestdata(eventBus)) {
             every { spekematClient.hentSpekemat(UNG_PERSON_FNR, any()) } returns spekemat.resultat()
             val query =
-                URI("https://raw.githubusercontent.com/navikt/helse-spesialist/main/spesialist-client-spleis/src/main/resources/graphql/hentSnapshot.graphql").toURL()
+                URI("https://raw.githubusercontent.com/navikt/helse-spesialist/main/clients/spesialist-client-spleis/src/main/resources/graphql/hentSnapshot.graphql").toURL()
                     .readText()
 
             @Language("JSON")
