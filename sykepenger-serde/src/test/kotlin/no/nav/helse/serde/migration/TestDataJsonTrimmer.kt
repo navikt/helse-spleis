8495f3a3e0fb514e562f7a7b8341a808b558dafb
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-02-27 10:22:31 +0100

    En ørliten how to-bruke TestDataJsonTrimmer

--- sykepenger-serde/src/test/kotlin/no/nav/helse/serde/migration/TestDataJsonTrimmer.kt
+++ sykepenger-serde/src/test/kotlin/no/nav/helse/serde/migration/TestDataJsonTrimmer.kt
@@ -3,25 +3,32 @@ package no.nav.helse.serde.migration
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import no.nav.helse.serde.migration.MigrationTest.Companion.readResource
+import java.io.File
 
 fun main() {
-    val big = "<FyllMegInnMedFilenDuVilTrimme>.json".readResource()
-    val person = jacksonObjectMapper().readTree(big)
+    /**
+     * 1. Skriv din setup for migrering som en test i en 'AbstractDslTest'
+     * 2. Skriv på slutten av testen `val json = dto().tilPersonData().tilSerialisertPerson().json`
+     * 3. Debug testen og kopier innholdet av `json`
+     * 4. CMD + Shift + Space (Åpne JSON scratch fil)
+     * 5. Paste innholdet av `json`
+     * 6. Høyreklikk på scratch-fil -> Copy Path -> Absolute Path
+     * 7. Paste pathen i `path`-varabelen under
+     * 8. Tilpass felter som beholdes etter DITT behov. Defaulten er tilpassen V344
+     * 9. Kjør og du har et bra utgangspunkt for din `original.json`
+     */
+    val path = "<Du må lese det som står over!>"
+    val storPerson = File(path).readText()
+    val person = jacksonObjectMapper().readTree(storPerson)
     trimPerson(person)
     println(person)
 }
 
 private fun JsonNode.fjernAltUntatt(felter: List<String>) {
     this as ObjectNode
-    val feltNavn = mutableListOf<String>().apply {
-        fieldNames().forEach { fieldName ->
-            add(fieldName)
-        }
-    }
-    feltNavn.forEach { k ->
-        if (!felter.contains(k)) {
-            remove(k)
+    fieldNames().asSequence().toSet().forEach { key ->
+        if (key !in felter) {
+            remove(key)
         }
     }
 }
@@ -44,7 +51,7 @@ private fun trimVedtaksperiode(vedtaksperiode: JsonNode) {
     vedtaksperiode.path("behandlinger").forEach { behandling ->
         behandling.fjernAltUntatt(listOf("id", "tilstand", "endringer"))
         behandling.path("endringer").forEach { endring ->
-            endring.fjernAltUntatt(listOf("id", "utbetalingId"))
+            endring.fjernAltUntatt(listOf("id", "utbetalingId", "tidsstempel"))
         }
     }
 }
