b4779a22a254e4f7efff22c05524757be1974632
Hakon Arneng Holmstedt <hakon.a.holmstedt@gmail.com>
2026-03-04 14:32:12 +0100

    Verifiserer at vi får varsel om vi sender flere inntektsmeldinger med arbeidsforholdid, og at varselet dukker opp på replay

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/GlemArbeidsforholdIdLittTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/GlemArbeidsforholdIdLittTest.kt
@@ -4,6 +4,7 @@ import no.nav.helse.dsl.AbstractDslTest
 import no.nav.helse.dsl.a1
 import no.nav.helse.hendelser.til
 import no.nav.helse.januar
+import no.nav.helse.person.aktivitetslogg.Varselkode
 import no.nav.helse.person.tilstandsmaskin.TilstandType
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_INNTEKTSMELDING
 import org.junit.jupiter.api.Test
@@ -20,8 +21,20 @@ internal class GlemArbeidsforholdIdLittTest : AbstractDslTest() {
             håndterInntektsmelding(listOf(1.januar til 16.januar), arbeidsforholdId = "123")
             assertSisteTilstand(1.vedtaksperiode, AVVENTER_INNTEKTSMELDING)
             håndterPåminnelse(1.vedtaksperiode, AVVENTER_INNTEKTSMELDING, flagg = setOf("trengerReplay"))
-            //TODO: også bør vi verifisere at vi likevel takler å håndtere en inntektsmelding med arbeidsforholdId, i en replay-kontekst
             assertSisteTilstand(1.vedtaksperiode, TilstandType.AVVENTER_VILKÅRSPRØVING)
+        }
+    }
+
+    @Test
+    fun `flere inntektsmeldinger med arbeidsforholdId satt blir til RV_IM_4 når de replayes`() {
+        a1 {
+            håndterSøknad(1.januar til 31.januar)
+            håndterInntektsmelding(listOf(1.januar til 16.januar), arbeidsforholdId = "123")
+            håndterInntektsmelding(listOf(1.januar til 12.januar, 13.januar til 16.januar), arbeidsforholdId = "124")
+            assertSisteTilstand(1.vedtaksperiode, AVVENTER_INNTEKTSMELDING)
+            håndterPåminnelse(1.vedtaksperiode, AVVENTER_INNTEKTSMELDING, flagg = setOf("trengerReplay"))
+            assertSisteTilstand(1.vedtaksperiode, TilstandType.AVVENTER_VILKÅRSPRØVING)
+            assertVarsler(1.vedtaksperiode, Varselkode.RV_IM_4)
         }
     }
 }
