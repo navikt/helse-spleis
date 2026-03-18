39aab35ee783af4a3653a0c903f60c53f98b13c2
Linus Johansson <linus.123@hotmail.com>
2026-03-18 16:01:00 +0100

    Refaktorert fra AbstractEndToEnd -> AbstractDsl

--- sykepenger-model/src/test/kotlin/no/nav/helse/bugs_showstoppers/ManglendeVilkårsgrunnlagTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/bugs_showstoppers/ManglendeVilkårsgrunnlagTest.kt
@@ -1,5 +1,7 @@
 package no.nav.helse.bugs_showstoppers
 
+import no.nav.helse.dsl.AbstractDslTest
+import no.nav.helse.dsl.a1
 import no.nav.helse.februar
 import no.nav.helse.fredag
 import no.nav.helse.hendelser.Sykmeldingsperiode
@@ -16,87 +18,83 @@ import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_INNTEKTSMELDING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_SIMULERING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_VILKÅRSPRØVING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.START
-import no.nav.helse.spleis.e2e.AbstractEndToEndTest
-import no.nav.helse.spleis.e2e.assertTilstander
-import no.nav.helse.spleis.e2e.håndterArbeidsgiveropplysninger
-import no.nav.helse.spleis.e2e.håndterInntektsmelding
-import no.nav.helse.spleis.e2e.håndterSykmelding
-import no.nav.helse.spleis.e2e.håndterSøknad
-import no.nav.helse.spleis.e2e.håndterVilkårsgrunnlag
-import no.nav.helse.spleis.e2e.håndterYtelser
 import no.nav.helse.økonomi.Prosentdel.Companion.prosent
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class ManglendeVilkårsgrunnlagTest : AbstractEndToEndTest() {
+internal class ManglendeVilkårsgrunnlagTest : AbstractDslTest() {
 
     @Test
     fun `inntektsmelding avslutter to korte perioder og flytter nav-perioden uten å utføre vilkårsprøving`() {
-        håndterSykmelding(Sykmeldingsperiode(9.januar, 15.januar))
-        håndterSøknad(Sykdom(9.januar, 15.januar, 100.prosent))
-        håndterSykmelding(Sykmeldingsperiode(19.januar, 26.januar))
-        håndterSøknad(Sykdom(19.januar, 26.januar, 100.prosent))
-        håndterSykmelding(Sykmeldingsperiode(29.januar, 2.februar))
-        håndterSøknad(Sykdom(29.januar, 2.februar, 100.prosent))
-        // inntektsmeldingen lukker de to korte periodene og gjør samtidig at
-        // nav-perioden går fra AvventerInntektsmeldingUferdigForlengelse til AvventerUferdigForlengelse.
-        // Når Inntektsmeldingen er håndtert sendes GjenopptaBehandling ut, som medfører at
-        // NAV-perioden går videre til AvventerHistorikk uten å gjøre vilkårsvurdering først
-        håndterArbeidsgiveropplysninger(
-            listOf(
-                9.januar til 15.januar,
-                19.januar til 26.januar,
-                29.januar til 29.januar
-            ),
-            vedtaksperiodeIdInnhenter = 3.vedtaksperiode
-        )
-        håndterVilkårsgrunnlag(3.vedtaksperiode)
-        håndterYtelser(3.vedtaksperiode)
+        a1 {
+            håndterSykmelding(Sykmeldingsperiode(9.januar, 15.januar))
+            håndterSøknad(Sykdom(9.januar, 15.januar, 100.prosent))
+            håndterSykmelding(Sykmeldingsperiode(19.januar, 26.januar))
+            håndterSøknad(Sykdom(19.januar, 26.januar, 100.prosent))
+            håndterSykmelding(Sykmeldingsperiode(29.januar, 2.februar))
+            håndterSøknad(Sykdom(29.januar, 2.februar, 100.prosent))
+            // inntektsmeldingen lukker de to korte periodene og gjør samtidig at
+            // nav-perioden går fra AvventerInntektsmeldingUferdigForlengelse til AvventerUferdigForlengelse.
+            // Når Inntektsmeldingen er håndtert sendes GjenopptaBehandling ut, som medfører at
+            // NAV-perioden går videre til AvventerHistorikk uten å gjøre vilkårsvurdering først
+            håndterArbeidsgiveropplysninger(
+                listOf(
+                    9.januar til 15.januar,
+                    19.januar til 26.januar,
+                    29.januar til 29.januar
+                ),
+                vedtaksperiodeId = 3.vedtaksperiode
+            )
+            håndterVilkårsgrunnlag(3.vedtaksperiode)
+            håndterYtelser(3.vedtaksperiode)
 
-        assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(
-            3.vedtaksperiode,
-            START,
-            AVVENTER_INNTEKTSMELDING,
-            AVVENTER_BLOKKERENDE_PERIODE,
-            AVVENTER_VILKÅRSPRØVING,
-            AVVENTER_HISTORIKK,
-            AVVENTER_SIMULERING
-        )
+            assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(
+                3.vedtaksperiode,
+                START,
+                AVVENTER_INNTEKTSMELDING,
+                AVVENTER_BLOKKERENDE_PERIODE,
+                AVVENTER_VILKÅRSPRØVING,
+                AVVENTER_HISTORIKK,
+                AVVENTER_SIMULERING
+            )
+        }
     }
 
     @Test
     fun `inntektsmelding drar periode tilbake og lager tilstøtende`() {
-        håndterSykmelding(Sykmeldingsperiode(1.januar, 5.januar))
-        håndterSøknad(Sykdom(1.januar, 5.januar, 100.prosent))
+        a1 {
+            håndterSykmelding(Sykmeldingsperiode(1.januar, 5.januar))
+            håndterSøknad(Sykdom(1.januar, 5.januar, 100.prosent))
 
-        håndterSykmelding(Sykmeldingsperiode(26.januar, fredag den 2.februar))
-        håndterSøknad(Sykdom(26.januar, fredag den 2.februar, 100.prosent))
+            håndterSykmelding(Sykmeldingsperiode(26.januar, fredag den 2.februar))
+            håndterSøknad(Sykdom(26.januar, fredag den 2.februar, 100.prosent))
 
-        håndterSykmelding(Sykmeldingsperiode(mandag den 5.februar, 21.februar))
-        håndterSøknad(Sykdom(mandag den 5.februar, 21.februar, 100.prosent))
+            håndterSykmelding(Sykmeldingsperiode(mandag den 5.februar, 21.februar))
+            håndterSøknad(Sykdom(mandag den 5.februar, 21.februar, 100.prosent))
 
-        // inntektsmelding inneholder en ukjent dag — 8. januar — som vi ikke
-        // har registrert før i forbindelse med verken søknad eller sykmelding
-        // Dette medfører at perioden 26. januar - 2. februar "dras tilbake"
-        // til 6. januar, siden 8. januar er en mandag og 5. januar (forrige agp-innslag) er en fredag
-        // så regnes lørdag + søndag som del av arbeidsgiverperioden også.
-        // Dermed ble perioden 6. januar - 2. februar regnet som tilstøtende til 1.-5. januar, selv om
-        // de to har forskjellige skjæringstidspunkt.
-        håndterInntektsmelding(
-            listOf(
-                1.januar til 5.januar,
-                8.januar til 8.januar,
-                24.januar til 2.februar
+            // inntektsmelding inneholder en ukjent dag — 8. januar — som vi ikke
+            // har registrert før i forbindelse med verken søknad eller sykmelding
+            // Dette medfører at perioden 26. januar - 2. februar "dras tilbake"
+            // til 6. januar, siden 8. januar er en mandag og 5. januar (forrige agp-innslag) er en fredag
+            // så regnes lørdag + søndag som del av arbeidsgiverperioden også.
+            // Dermed ble perioden 6. januar - 2. februar regnet som tilstøtende til 1.-5. januar, selv om
+            // de to har forskjellige skjæringstidspunkt.
+            håndterInntektsmelding(
+                listOf(
+                    1.januar til 5.januar,
+                    8.januar til 8.januar,
+                    24.januar til 2.februar
+                )
             )
-        )
-        assertEquals(1.januar til 5.januar, inspektør.periode(1.vedtaksperiode))
-        assertEquals(6.januar til 2.februar, inspektør.periode(2.vedtaksperiode))
-        assertEquals(5.februar til 21.februar, inspektør.periode(3.vedtaksperiode))
+            assertEquals(1.januar til 5.januar, inspektør.periode(1.vedtaksperiode))
+            assertEquals(6.januar til 2.februar, inspektør.periode(2.vedtaksperiode))
+            assertEquals(5.februar til 21.februar, inspektør.periode(3.vedtaksperiode))
 
-        assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
-        assertTilstander(3.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVVENTER_BLOKKERENDE_PERIODE, AVVENTER_VILKÅRSPRØVING)
+            assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(2.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING, AVVENTER_AVSLUTTET_UTEN_UTBETALING, AVSLUTTET_UTEN_UTBETALING)
+            assertTilstander(3.vedtaksperiode, START, AVVENTER_INNTEKTSMELDING, AVVENTER_BLOKKERENDE_PERIODE, AVVENTER_VILKÅRSPRØVING)
+        }
     }
 }
