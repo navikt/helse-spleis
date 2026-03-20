c46f36252318cfa5771647447ffc15a3c49f876c
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2026-03-20 15:50:23 +0100

    Gjør det kneppet enklere å bytte om Behovslytter

--- sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/Behovslytter.kt
+++ sykepenger-mediators/src/main/kotlin/no/nav/helse/spleis/Behovslytter.kt
@@ -4,11 +4,11 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
-import io.ktor.util.logging.Logger
 import java.time.LocalDateTime
 import java.util.UUID
 import kotlin.collections.set
 import no.nav.helse.spleis.EnBehovslytterSomSerOmBehovErLike.UtgåendeBehovsmelding.Companion.utgåendeBehov
+import org.slf4j.LoggerFactory
 
 internal interface Behovslytter {
     fun behovsmeldingFraEventBus(json: String )
@@ -20,8 +20,10 @@ internal object EnIkkeAgerendeBehovslytter: Behovslytter {
     override fun behovsmeldingFraAktivitetslogg(json: String) {}
 }
 
-internal class EnBehovslytterSomSerOmBehovErLike(private val sikkerLogg: Logger) : Behovslytter {
+internal class EnBehovslytterSomSerOmBehovErLike() : Behovslytter {
     private val utgåendeBehovsmeldingerFraEventBus = mutableMapOf<Set<String>, UtgåendeBehovsmelding>()
+    private val påNais = System.getenv("NAIS_CLUSTER_NAME")?.lowercase()?.endsWith("-gcp") == true
+    private val kastException = !påNais
 
     override fun behovsmeldingFraEventBus(json: String) {
         try {
@@ -30,6 +32,7 @@ internal class EnBehovslytterSomSerOmBehovErLike(private val sikkerLogg: Logger)
             utgåendeBehovsmeldingerFraEventBus[utgåendeBehovsmelding.identifikator] = utgåendeBehovsmelding
         } catch (exception: Exception) {
             sikkerLogg.warn("[EnBehovslytterSomSerOmBehovErLike] Feil ved oversetting av behov sendt fra EventBus", exception)
+            if (kastException) throw exception
         }
     }
 
@@ -40,6 +43,7 @@ internal class EnBehovslytterSomSerOmBehovErLike(private val sikkerLogg: Logger)
             check(fraAktivitetslogg.json == fraEventBus.json) { "Disse beohvene er jo ikke like!\n\t${fraAktivitetslogg.json}\n\t${fraEventBus.json}" }
         } catch (exception: Exception) {
             sikkerLogg.warn("[EnBehovslytterSomSerOmBehovErLike] Feil ved sjekking av om behov sendt fra Aktivitetslogg er like behov sendt fra EventBus", exception)
+            if (kastException) throw exception
         }
     }
 
@@ -81,5 +85,8 @@ internal class EnBehovslytterSomSerOmBehovErLike(private val sikkerLogg: Logger)
             private fun JsonNode.fagområdeOrNull() =
                 (path("Utbetaling").path("fagområde").takeUnless { it.isMissingOrNull() } ?: path("Feriepengeutbetaling").path("fagområde").takeUnless { it.isMissingOrNull() })?.asText()
         }
+    }
+    private companion object {
+        private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
     }
 }
