267caecdce9c70252a3332f2fe23c59568248e2b
Linus Johansson <linus.123@hotmail.com>
2026-03-30 15:36:04 +0200

    Endrer fra AbstractEndToEnd -> AbstractDsl

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/brukerutbetaling/BrukerutbetalingerTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/brukerutbetaling/BrukerutbetalingerTest.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.spleis.e2e.brukerutbetaling
 
+import no.nav.helse.dsl.AbstractDslTest
 import no.nav.helse.dsl.INNTEKT
 import no.nav.helse.dsl.a1
 import no.nav.helse.hendelser.Inntektsmelding
@@ -7,53 +8,46 @@ import no.nav.helse.hendelser.til
 import no.nav.helse.januar
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVSLUTTET
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_SIMULERING
-import no.nav.helse.spleis.e2e.AbstractEndToEndTest
-import no.nav.helse.spleis.e2e.assertSisteTilstand
-import no.nav.helse.spleis.e2e.håndterArbeidsgiveropplysninger
-import no.nav.helse.spleis.e2e.håndterSimulering
-import no.nav.helse.spleis.e2e.håndterSykmelding
-import no.nav.helse.spleis.e2e.håndterSøknad
-import no.nav.helse.spleis.e2e.håndterUtbetalingsgodkjenning
-import no.nav.helse.spleis.e2e.håndterUtbetalt
-import no.nav.helse.spleis.e2e.håndterVilkårsgrunnlag
-import no.nav.helse.spleis.e2e.håndterYtelser
-import no.nav.helse.spleis.e2e.nyttVedtak
 import no.nav.helse.økonomi.Inntekt.Companion.månedlig
 import org.junit.jupiter.api.Test
 
-internal class BrukerutbetalingerTest : AbstractEndToEndTest() {
+internal class BrukerutbetalingerTest : AbstractDslTest() {
 
     @Test
     fun `utbetaling med 0 refusjon til arbeidsgiver`() {
-        håndterSykmelding(januar)
-        håndterSøknad(januar)
-        håndterArbeidsgiveropplysninger(
-            arbeidsgiverperioder = listOf(1.januar til 16.januar),
-            refusjon = Inntektsmelding.Refusjon(0.månedlig, null),
-            vedtaksperiodeIdInnhenter = 1.vedtaksperiode,
-        )
-        håndterVilkårsgrunnlag(1.vedtaksperiode)
-        håndterYtelser()
-        håndterSimulering()
-        håndterUtbetalingsgodkjenning()
-        håndterUtbetalt()
+        a1 {
+            håndterSykmelding(januar)
+            håndterSøknad(januar)
+            håndterArbeidsgiveropplysninger(
+                arbeidsgiverperioder = listOf(1.januar til 16.januar),
+                refusjon = Inntektsmelding.Refusjon(0.månedlig, null),
+                vedtaksperiodeId = 1.vedtaksperiode,
+            )
+            håndterVilkårsgrunnlag(1.vedtaksperiode)
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+            håndterUtbetalt()
 
-        assertSisteTilstand(1.vedtaksperiode, AVSLUTTET, orgnummer = a1)
+            assertSisteTilstand(1.vedtaksperiode, AVSLUTTET, orgnummer = a1)
+        }
     }
 
     @Test
     fun `utbetaling med delvis refusjon til arbeidsgiver`() {
-        håndterSykmelding(januar)
-        håndterSøknad(januar)
-        håndterArbeidsgiveropplysninger(
-            arbeidsgiverperioder = listOf(1.januar til 16.januar),
-            refusjon = Inntektsmelding.Refusjon(20000.månedlig, null),
-            vedtaksperiodeIdInnhenter = 1.vedtaksperiode,
-        )
-        håndterVilkårsgrunnlag(1.vedtaksperiode)
-        håndterYtelser()
+        a1 {
+            håndterSykmelding(januar)
+            håndterSøknad(januar)
+            håndterArbeidsgiveropplysninger(
+                arbeidsgiverperioder = listOf(1.januar til 16.januar),
+                refusjon = Inntektsmelding.Refusjon(20000.månedlig, null),
+                vedtaksperiodeId = 1.vedtaksperiode,
+            )
+            håndterVilkårsgrunnlag(1.vedtaksperiode)
+            håndterYtelser(1.vedtaksperiode)
 
-        assertSisteTilstand(1.vedtaksperiode, AVVENTER_SIMULERING, orgnummer = a1)
+            assertSisteTilstand(1.vedtaksperiode, AVVENTER_SIMULERING, orgnummer = a1)
+        }
     }
 
     @Test
@@ -64,15 +58,17 @@ internal class BrukerutbetalingerTest : AbstractEndToEndTest() {
 
     @Test
     fun `utbetaling med delvis refusjon`() {
-        håndterSykmelding(januar)
-        håndterSøknad(januar)
-        håndterArbeidsgiveropplysninger(
-            arbeidsgiverperioder = listOf(1.januar til 16.januar),
-            refusjon = Inntektsmelding.Refusjon(INNTEKT / 2, null),
-            vedtaksperiodeIdInnhenter = 1.vedtaksperiode,
-        )
-        håndterVilkårsgrunnlag(1.vedtaksperiode)
-        håndterYtelser()
-        assertSisteTilstand(1.vedtaksperiode, AVVENTER_SIMULERING, orgnummer = a1)
+        a1 {
+            håndterSykmelding(januar)
+            håndterSøknad(januar)
+            håndterArbeidsgiveropplysninger(
+                arbeidsgiverperioder = listOf(1.januar til 16.januar),
+                refusjon = Inntektsmelding.Refusjon(INNTEKT / 2, null),
+                vedtaksperiodeId = 1.vedtaksperiode,
+            )
+            håndterVilkårsgrunnlag(1.vedtaksperiode)
+            håndterYtelser(1.vedtaksperiode)
+            assertSisteTilstand(1.vedtaksperiode, AVVENTER_SIMULERING, orgnummer = a1)
+        }
     }
 }
