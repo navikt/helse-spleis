7ebfb1e7013f0035d9027fba22898ab2563a6934
Linus Johansson <linus.123@hotmail.com>
2026-03-31 14:47:06 +0200

    Endrer fra AbstractEndToEnd -> AbstractDsl

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/revurdering/RevurderTidslinjeFlereArbeidsgivereTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/revurdering/RevurderTidslinjeFlereArbeidsgivereTest.kt
@@ -1,8 +1,10 @@
 package no.nav.helse.spleis.e2e.revurdering
 
 import java.time.LocalDate
+import no.nav.helse.dsl.AbstractDslTest
 import no.nav.helse.dsl.a1
 import no.nav.helse.dsl.a2
+import no.nav.helse.dsl.nyPeriode
 import no.nav.helse.februar
 import no.nav.helse.hendelser.Dagtype.Feriedag
 import no.nav.helse.hendelser.ManuellOverskrivingDag
@@ -24,26 +26,8 @@ import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_HISTORIKK_REVUR
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_REVURDERING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_SIMULERING_REVURDERING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.TIL_UTBETALING
-import no.nav.helse.spleis.e2e.AbstractEndToEndTest
-import no.nav.helse.spleis.e2e.assertIngenFunksjonelleFeil
-import no.nav.helse.spleis.e2e.assertSisteTilstand
-import no.nav.helse.spleis.e2e.assertTilstander
-import no.nav.helse.spleis.e2e.assertVarsel
-import no.nav.helse.spleis.e2e.assertVarsler
-import no.nav.helse.spleis.e2e.forlengVedtak
-import no.nav.helse.spleis.e2e.håndterInntektsmelding
-import no.nav.helse.spleis.e2e.håndterOverstyrTidslinje
-import no.nav.helse.spleis.e2e.håndterSimulering
-import no.nav.helse.spleis.e2e.håndterSykmelding
-import no.nav.helse.spleis.e2e.håndterSøknad
-import no.nav.helse.spleis.e2e.håndterUtbetalingsgodkjenning
-import no.nav.helse.spleis.e2e.håndterUtbetalt
-import no.nav.helse.spleis.e2e.håndterVilkårsgrunnlagFlereArbeidsgivere
-import no.nav.helse.spleis.e2e.håndterYtelser
+import no.nav.helse.spleis.e2e.AktivitetsloggFilter.Companion.filter
 import no.nav.helse.spleis.e2e.manuellFeriedag
-import no.nav.helse.spleis.e2e.nullstillTilstandsendringer
-import no.nav.helse.spleis.e2e.nyeVedtak
-import no.nav.helse.spleis.e2e.tilGodkjenning
 import no.nav.helse.utbetalingstidslinje.Utbetalingsdag.NavHelgDag
 import no.nav.helse.økonomi.Inntekt
 import no.nav.helse.økonomi.Inntekt.Companion.INGEN
@@ -54,159 +38,192 @@ import no.nav.helse.økonomi.inspectors.inspektør
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class RevurderTidslinjeFlereArbeidsgivereTest : AbstractEndToEndTest() {
+internal class RevurderTidslinjeFlereArbeidsgivereTest : AbstractDslTest() {
 
     @Test
     fun `revurdering for periode som start samme dag som en førstegangsvurdering`() {
-        håndterSykmelding(Sykmeldingsperiode(1.januar, 16.januar), orgnummer = a2)
-        håndterSøknad(Sykdom(1.januar, 16.januar, 100.prosent), orgnummer = a2)
+        a2 {
+            håndterSykmelding(Sykmeldingsperiode(1.januar, 16.januar))
+            håndterSøknad(Sykdom(1.januar, 16.januar, 100.prosent))
+            håndterSykmelding(Sykmeldingsperiode(17.januar, 25.januar))
+            håndterSøknad(Sykdom(17.januar, 25.januar, 100.prosent))
+            håndterInntektsmelding(listOf(1.januar til 16.januar), beregnetInntekt = 20000.månedlig)
+            håndterVilkårsgrunnlagFlereArbeidsgivere(2.vedtaksperiode, a1, a2)
+            håndterYtelser(2.vedtaksperiode)
+            håndterSimulering(2.vedtaksperiode)
+        }
 
-        håndterSykmelding(Sykmeldingsperiode(17.januar, 25.januar), orgnummer = a2)
-        håndterSøknad(Sykdom(17.januar, 25.januar, 100.prosent), orgnummer = a2)
-        håndterInntektsmelding(
-            listOf(1.januar til 16.januar),
-            beregnetInntekt = 20000.månedlig,
-            orgnummer = a2
-        )
-        håndterVilkårsgrunnlagFlereArbeidsgivere(vedtaksperiodeIdInnhenter = 2.vedtaksperiode, a1, a2, orgnummer = a2)
-        håndterYtelser(2.vedtaksperiode, orgnummer = a2)
-        håndterSimulering(2.vedtaksperiode, orgnummer = a2)
+        a1 {
+            håndterSykmelding(Sykmeldingsperiode(10.januar, 16.januar))
+            håndterSøknad(Sykdom(10.januar, 16.januar, 100.prosent))
+            håndterSykmelding(Sykmeldingsperiode(17.januar, 25.januar))
+            håndterSøknad(Sykdom(17.januar, 25.januar, 100.prosent))
+        }
 
-        håndterSykmelding(Sykmeldingsperiode(10.januar, 16.januar), orgnummer = a1)
-        håndterSøknad(Sykdom(10.januar, 16.januar, 100.prosent), orgnummer = a1)
+        a2 {
+            håndterYtelser(2.vedtaksperiode)
+            håndterSimulering(2.vedtaksperiode)
+        }
 
-        håndterSykmelding(Sykmeldingsperiode(17.januar, 25.januar), orgnummer = a1)
-        håndterSøknad(Sykdom(17.januar, 25.januar, 100.prosent), orgnummer = a1)
+        a1 {
+            håndterInntektsmelding(listOf(2.januar til 17.januar), beregnetInntekt = 20000.månedlig)
 
-        håndterYtelser(2.vedtaksperiode, orgnummer = a2)
-        håndterSimulering(2.vedtaksperiode, orgnummer = a2)
 
-        håndterInntektsmelding(
-            listOf(2.januar til 17.januar),
-            beregnetInntekt = 20000.månedlig,
-            orgnummer = a1
-        )
 
-        assertIngenFunksjonelleFeil()
-
-        assertVarsler(listOf(RV_IM_4), 2.vedtaksperiode.filter(orgnummer = a1))
-        assertVarsler(listOf(Varselkode.RV_VV_2), 2.vedtaksperiode.filter(orgnummer = a2))
-        assertSisteTilstand(1.vedtaksperiode, AVSLUTTET_UTEN_UTBETALING, orgnummer = a2)
-        assertSisteTilstand(2.vedtaksperiode, AVVENTER_HISTORIKK, orgnummer = a2)
-        assertSisteTilstand(1.vedtaksperiode, AVSLUTTET_UTEN_UTBETALING, orgnummer = a1)
-        assertSisteTilstand(2.vedtaksperiode, AVVENTER_BLOKKERENDE_PERIODE, orgnummer = a1)
+            assertIngenFunksjonelleFeil()
+            assertVarsler(listOf(RV_IM_4), 2.vedtaksperiode.filter())
+            assertSisteTilstand(1.vedtaksperiode, AVSLUTTET_UTEN_UTBETALING)
+            assertSisteTilstand(2.vedtaksperiode, AVVENTER_BLOKKERENDE_PERIODE)
+        }
+        a2 {
+            assertVarsler(listOf(Varselkode.RV_VV_2), 2.vedtaksperiode.filter())
+            assertSisteTilstand(1.vedtaksperiode, AVSLUTTET_UTEN_UTBETALING)
+            assertSisteTilstand(2.vedtaksperiode, AVVENTER_HISTORIKK)
+        }
     }
 
     @Test
     fun `to AG - to perioder på hver - første periode blir revurdert på én AG og avventer godkjenning`() {
-        nyeVedtak(januar, a2, a1)
-        forlengVedtak(februar, a2, a1)
+        listOf(a2, a1).nyeVedtak(januar)
+        listOf(a2, a1).forlengVedtak(februar)
         nullstillTilstandsendringer()
 
-        håndterOverstyrTidslinje((20.januar til 22.januar).map { manuellFeriedag(it) }, orgnummer = a2)
-        håndterYtelser(1.vedtaksperiode, orgnummer = a2)
-        assertVarsel(RV_UT_23, 1.vedtaksperiode.filter(orgnummer = a2))
-        håndterSimulering(1.vedtaksperiode, orgnummer = a2)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = a2)
-        håndterUtbetalt(orgnummer = a2)
+        a2 {
+            håndterOverstyrTidslinje((20.januar til 22.januar).map { manuellFeriedag(it) })
+            håndterYtelser(1.vedtaksperiode)
+            assertVarsel(RV_UT_23, 1.vedtaksperiode.filter())
+            håndterSimulering(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+            håndterUtbetalt()
+        }
 
-        håndterYtelser(1.vedtaksperiode, orgnummer = a1)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = a1)
+        a1 {
+            håndterYtelser(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+        }
 
-        håndterYtelser(2.vedtaksperiode, orgnummer = a2)
-        håndterUtbetalingsgodkjenning(2.vedtaksperiode, orgnummer = a2)
+        a2 {
+            håndterYtelser(2.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(2.vedtaksperiode)
+        }
 
-        håndterYtelser(2.vedtaksperiode, orgnummer = a1)
-        håndterUtbetalingsgodkjenning(2.vedtaksperiode, orgnummer = a1)
+        a1 {
+            håndterYtelser(2.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(2.vedtaksperiode)
+        }
 
-        inspektør(a2) {
+        a2 {
             assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, AVVENTER_SIMULERING_REVURDERING, AVVENTER_GODKJENNING_REVURDERING, TIL_UTBETALING, AVSLUTTET)
             assertTilstander(2.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, AVVENTER_GODKJENNING_REVURDERING, AVSLUTTET)
             assertIngenFunksjonelleFeil()
-            assertEquals(2, avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
-            assertEquals(2, avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
+            assertEquals(2, inspektør.avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
+            assertEquals(2, inspektør.avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
         }
 
-        inspektør(a1) {
+        a1 {
             assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, AVVENTER_GODKJENNING_REVURDERING, AVSLUTTET)
             assertTilstander(2.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, AVVENTER_GODKJENNING_REVURDERING, AVSLUTTET)
             assertIngenFunksjonelleFeil()
-            assertEquals(2, avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
-            assertEquals(2, avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
+            assertEquals(2, inspektør.avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
+            assertEquals(2, inspektør.avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
         }
     }
 
     @Test
     fun `revurdere en AG når en annen AG er til godkjenning`() {
-        tilGodkjenning(januar, a1, a2)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = a1)
-        håndterUtbetalt(orgnummer = a1)
-
-        håndterYtelser(1.vedtaksperiode, orgnummer = a2)
-        håndterSimulering(1.vedtaksperiode, orgnummer = a2)
+        a1 {
+            nyPeriode(januar)
+            håndterInntektsmelding(listOf(1.januar til 16.januar))
+        }
+        a2 {
+            nyPeriode(januar)
+            håndterInntektsmelding(listOf(1.januar til 16.januar))
+        }
+        a1 {
+            håndterVilkårsgrunnlagFlereArbeidsgivere(1.vedtaksperiode, a1, a2)
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+            håndterUtbetalt()
+        }
+        a2 {
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+        }
         nullstillTilstandsendringer()
 
-        håndterOverstyrTidslinje((29.januar til 29.januar).map { manuellFeriedag(it) }, orgnummer = a1)
-
-        assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, orgnummer = a1)
-        assertTilstander(1.vedtaksperiode, AVVENTER_GODKJENNING, AVVENTER_BLOKKERENDE_PERIODE, orgnummer = a2)
+        a1 {
+            håndterOverstyrTidslinje((29.januar til 29.januar).map { manuellFeriedag(it) })
+            assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING)
+        }
+        a2 {
+            assertTilstander(1.vedtaksperiode, AVVENTER_GODKJENNING, AVVENTER_BLOKKERENDE_PERIODE)
+        }
     }
 
     @Test
     fun `to AG - to perioder på hver - én blir revurdert på én AG`() {
-        nyeVedtak(januar, a2, a1)
-        forlengVedtak(februar, a2, a1)
+        listOf(a2, a1).nyeVedtak(januar)
+        listOf(a2, a1).forlengVedtak(februar)
         nullstillTilstandsendringer()
 
-        håndterOverstyrTidslinje((20.januar til 22.januar).map { manuellFeriedag(it) }, orgnummer = a1)
-        håndterYtelser(1.vedtaksperiode, orgnummer = a2)
-        assertVarsel(RV_UT_23, 1.vedtaksperiode.filter(orgnummer = a1))
-        inspektør(a2) {
+        a1 {
+            håndterOverstyrTidslinje((20.januar til 22.januar).map { manuellFeriedag(it) })
+        }
+        a2 {
+            håndterYtelser(1.vedtaksperiode)
+        }
+        a1 {
+            assertVarsel(RV_UT_23, 1.vedtaksperiode.filter())
+        }
+        a2 {
             assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING, AVVENTER_HISTORIKK_REVURDERING, AVVENTER_GODKJENNING_REVURDERING)
             assertTilstander(2.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING)
             assertIngenFunksjonelleFeil()
-            assertEquals(1, avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
-            assertEquals(1, ikkeUtbetalteUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
-            assertEquals(1, avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
-            assertEquals(0, ikkeUtbetalteUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
+            assertEquals(1, inspektør.avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
+            assertEquals(1, inspektør.ikkeUtbetalteUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
+            assertEquals(1, inspektør.avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
+            assertEquals(0, inspektør.ikkeUtbetalteUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
         }
 
-        inspektør(a1) {
+        a1 {
             assertTilstander(1.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING)
             assertTilstander(2.vedtaksperiode, AVSLUTTET, AVVENTER_REVURDERING)
             assertIngenFunksjonelleFeil()
-            assertEquals(1, avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
-            assertEquals(1, ikkeUtbetalteUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
-            assertEquals(1, avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
-            assertEquals(0, ikkeUtbetalteUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
+            assertEquals(1, inspektør.avsluttedeUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
+            assertEquals(1, inspektør.ikkeUtbetalteUtbetalingerForVedtaksperiode(1.vedtaksperiode).size)
+            assertEquals(1, inspektør.avsluttedeUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
+            assertEquals(0, inspektør.ikkeUtbetalteUtbetalingerForVedtaksperiode(2.vedtaksperiode).size)
         }
     }
 
     @Test
     fun `Revurdering til ferie på a1 skal ikke påvirke utbetalingen til a2`() {
-        nyeVedtak(januar, a1, a2, inntekt = 32000.månedlig)
+        listOf(a1, a2).nyeVedtak(januar, inntekt = 32000.månedlig)
         assertPeriode(17.januar til 31.januar, a1, 1080.daglig)
         assertPeriode(17.januar til 31.januar, a2, 1080.daglig)
 
-        håndterOverstyrTidslinje((17.januar til 21.januar).map { ManuellOverskrivingDag(it, Feriedag) }, orgnummer = a1)
-
-        håndterYtelser(1.vedtaksperiode, orgnummer = a1)
-        assertVarsler(listOf(RV_UT_23), 1.vedtaksperiode.filter(orgnummer = a1))
-        håndterSimulering(1.vedtaksperiode, orgnummer = a1)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = a1)
-        håndterUtbetalt(orgnummer = a1)
-
-        håndterYtelser(1.vedtaksperiode, orgnummer = a2)
-        håndterUtbetalingsgodkjenning(1.vedtaksperiode, orgnummer = a2)
+        a1 {
+            håndterOverstyrTidslinje((17.januar til 21.januar).map { ManuellOverskrivingDag(it, Feriedag) })
+            håndterYtelser(1.vedtaksperiode)
+            assertVarsler(listOf(RV_UT_23), 1.vedtaksperiode.filter())
+            håndterSimulering(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+            håndterUtbetalt()
+        }
+        a2 {
+            håndterYtelser(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode)
+        }
 
         assertPeriode(17.januar til 21.januar, a1, INGEN)
         assertPeriode(22.januar til 31.januar, a1, 1080.daglig)
-
         assertPeriode(17.januar til 31.januar, a2, 1080.daglig)
     }
 
     private fun assertDag(dato: LocalDate, orgnummer: String, arbeidsgiverbeløp: Inntekt, personbeløp: Inntekt) {
-        inspektør(orgnummer).utbetalingstidslinjer(1.vedtaksperiode)[dato].let {
+        val vedtaksperiodeId = orgnummer { 1.vedtaksperiode }
+        inspektør(orgnummer).utbetalingstidslinjer(vedtaksperiodeId)[dato].let {
             if (it is NavHelgDag) return
             assertEquals(arbeidsgiverbeløp, it.økonomi.inspektør.arbeidsgiverbeløp)
             assertEquals(personbeløp, it.økonomi.inspektør.personbeløp)
