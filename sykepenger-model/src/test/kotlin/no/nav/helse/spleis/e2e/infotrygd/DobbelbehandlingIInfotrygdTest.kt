17be6e4a1ffb3a6fb8f9bf21a40a7a22354f5f52
Linus Johansson <linus.123@hotmail.com>
2026-04-01 09:32:57 +0200

    Endrar fra AbstractEndToEnd -> AbstractDsl

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/infotrygd/DobbelbehandlingIInfotrygdTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/infotrygd/DobbelbehandlingIInfotrygdTest.kt
@@ -1,7 +1,9 @@
 package no.nav.helse.spleis.e2e.infotrygd
 
 import java.time.LocalDate
+import no.nav.helse.dsl.AbstractDslTest
 import no.nav.helse.dsl.a1
+import no.nav.helse.dsl.forlengVedtak
 import no.nav.helse.februar
 import no.nav.helse.hendelser.Sykmeldingsperiode
 import no.nav.helse.hendelser.Søknad.Søknadsperiode.Sykdom
@@ -14,61 +16,55 @@ import no.nav.helse.person.tilstandsmaskin.TilstandType.AVSLUTTET
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_INFOTRYGDHISTORIKK
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_INNTEKTSMELDING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.START
-import no.nav.helse.spleis.e2e.AbstractEndToEndTest
+import no.nav.helse.spleis.e2e.AktivitetsloggFilter.Companion.filter
 import no.nav.helse.spleis.e2e.arbeidsgiveropplysninger.TrengerArbeidsgiveropplysningerTest.Companion.assertEtterspurt
-import no.nav.helse.spleis.e2e.assertSisteTilstand
-import no.nav.helse.spleis.e2e.assertTilstander
-import no.nav.helse.spleis.e2e.assertVarsel
-import no.nav.helse.spleis.e2e.forlengVedtak
-import no.nav.helse.spleis.e2e.håndterPåminnelse
-import no.nav.helse.spleis.e2e.håndterSykmelding
-import no.nav.helse.spleis.e2e.håndterSøknad
-import no.nav.helse.spleis.e2e.håndterUtbetalingshistorikkEtterInfotrygdendring
-import no.nav.helse.spleis.e2e.håndterYtelser
-import no.nav.helse.spleis.e2e.nyttVedtak
 import no.nav.helse.utbetalingslinjer.Endringskode
 import no.nav.helse.økonomi.Prosentdel.Companion.prosent
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class DobbelbehandlingIInfotrygdTest : AbstractEndToEndTest() {
+internal class DobbelbehandlingIInfotrygdTest : AbstractDslTest() {
 
     @Test
     fun `avdekker overlapp dobbelbehandlinger i Infotrygd`() {
-        håndterSykmelding(Sykmeldingsperiode(3.januar, 26.januar))
-        håndterSøknad(Sykdom(3.januar, 26.januar, 100.prosent))
-        assertSisteTilstand(1.vedtaksperiode, AVVENTER_INNTEKTSMELDING)
-        val historie1 = arrayOf(
-            ArbeidsgiverUtbetalingsperiode(a1, 3.januar, 26.januar)
-        )
-        håndterUtbetalingshistorikkEtterInfotrygdendring(
-            *historie1,
-            besvart = LocalDate.EPOCH.atStartOfDay()
-        )
-        observatør.assertEtterspurt(1.vedtaksperiode.id(a1), EventSubscription.Inntekt::class, EventSubscription.Refusjon::class)
-        assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING)
+        a1 {
+            håndterSykmelding(Sykmeldingsperiode(3.januar, 26.januar))
+            håndterSøknad(Sykdom(3.januar, 26.januar, 100.prosent))
+            assertSisteTilstand(1.vedtaksperiode, AVVENTER_INNTEKTSMELDING)
+            val historie1 = listOf(
+                ArbeidsgiverUtbetalingsperiode(a1, 3.januar, 26.januar)
+            )
+            håndterUtbetalingshistorikkEtterInfotrygdendring(
+                historie1,
+                besvart = LocalDate.EPOCH.atStartOfDay()
+            )
+            observatør.assertEtterspurt(1.vedtaksperiode, EventSubscription.Inntekt::class, EventSubscription.Refusjon::class)
+            assertTilstander(1.vedtaksperiode, START, AVVENTER_INFOTRYGDHISTORIKK, AVVENTER_INNTEKTSMELDING)
+        }
     }
 
     @Test
     fun `utbetaling i infotrygd etterpå`() {
-        nyttVedtak(januar)
-        forlengVedtak(februar)
+        a1 {
+            nyttVedtak(januar)
+            forlengVedtak(februar)
 
-        val historie1 = arrayOf(
-            ArbeidsgiverUtbetalingsperiode(a1, 17.januar, 26.januar)
-        )
-        håndterUtbetalingshistorikkEtterInfotrygdendring(
-            *historie1,
-            besvart = LocalDate.EPOCH.atStartOfDay()
-        )
+            val historie1 = listOf(
+                ArbeidsgiverUtbetalingsperiode(a1, 17.januar, 26.januar)
+            )
+            håndterUtbetalingshistorikkEtterInfotrygdendring(
+                historie1,
+                besvart = LocalDate.EPOCH.atStartOfDay()
+            )
 
-        håndterPåminnelse(1.vedtaksperiode, AVSLUTTET, flagg = setOf("ønskerReberegning"))
-        håndterYtelser(1.vedtaksperiode)
+            håndterPåminnelse(1.vedtaksperiode, AVSLUTTET, flagg = setOf("ønskerReberegning"))
+            håndterYtelser(1.vedtaksperiode)
 
-        assertVarsel(RV_IT_3, 1.vedtaksperiode.filter())
-        inspektør.utbetalinger(1.vedtaksperiode).last().inspektør.also { utbetalingInspektør ->
-            assertEquals(Endringskode.UEND, utbetalingInspektør.arbeidsgiverOppdrag.inspektør.endringskode)
-            assertEquals(0, utbetalingInspektør.personOppdrag.inspektør.antallLinjer())
+            assertVarsel(RV_IT_3, 1.vedtaksperiode.filter())
+            inspektør.utbetalinger(1.vedtaksperiode).last().inspektør.also { utbetalingInspektør ->
+                assertEquals(Endringskode.UEND, utbetalingInspektør.arbeidsgiverOppdrag.inspektør.endringskode)
+                assertEquals(0, utbetalingInspektør.personOppdrag.inspektør.antallLinjer())
+            }
         }
     }
 }
