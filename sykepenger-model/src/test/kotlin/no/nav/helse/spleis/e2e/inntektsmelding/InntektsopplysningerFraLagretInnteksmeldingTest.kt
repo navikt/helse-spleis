806b6d0a940cc7360d6ccd1826471e18e3bd7826
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2026-05-26 13:18:36 +0200

    Scenario som gjenskaper unødvendig behov for påminnelse
    - Når en annen periode enn den som er til behandling mangler inntektsmelding må perioden som er til behandling manuelt påminnes med reberegning.
    
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>

--- sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/InntektsopplysningerFraLagretInnteksmeldingTest.kt
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/InntektsopplysningerFraLagretInnteksmeldingTest.kt
@@ -2,16 +2,24 @@ package no.nav.helse.spleis.e2e.inntektsmelding
 
 import java.time.LocalDateTime
 import java.util.UUID
+import no.nav.helse.assertForventetFeil
 import no.nav.helse.dsl.AbstractDslTest
 import no.nav.helse.dsl.INNTEKT
 import no.nav.helse.dsl.a1
+import no.nav.helse.dsl.a2
 import no.nav.helse.dsl.nyttVedtak
+import no.nav.helse.februar
 import no.nav.helse.hendelser.Avsender.ARBEIDSGIVER
+import no.nav.helse.hendelser.Dagtype
+import no.nav.helse.hendelser.ManuellOverskrivingDag
 import no.nav.helse.hendelser.MeldingsreferanseId
+import no.nav.helse.hendelser.til
 import no.nav.helse.januar
+import no.nav.helse.person.aktivitetslogg.Varselkode
 import no.nav.helse.person.aktivitetslogg.Varselkode.RV_IM_27
 import no.nav.helse.person.beløp.Beløpstidslinje
 import no.nav.helse.person.beløp.Kilde
+import no.nav.helse.person.tilstandsmaskin.TilstandType
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVSLUTTET
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_INNTEKTSMELDING
 import no.nav.helse.person.tilstandsmaskin.TilstandType.AVVENTER_VILKÅRSPRØVING
@@ -73,6 +81,49 @@ internal class InntektsopplysningerFraLagretInnteksmeldingTest: AbstractDslTest(
 
             assertTilstander(1.vedtaksperiode, AVSLUTTET)
             assertInfo("Håndterer ikke inntektsopplysninger fra lagret inntektsmelding i tilstand Avsluttet")
+        }
+    }
+
+    @Test
+    fun `annen periode enn den som er til behandling mangler inntektsmelding`() {
+        a1 {
+            nyttVedtak(januar, ghosts = listOf(a2))
+            håndterOverstyrTidslinje((1.januar til 31.januar).map { ManuellOverskrivingDag (it, Dagtype.Svangerskapspengerdag)})
+            håndterYtelser(1.vedtaksperiode)
+            håndterSimulering(1.vedtaksperiode)
+            håndterUtbetalingsgodkjenning(1.vedtaksperiode, true)
+            håndterUtbetalt()
+            håndterSøknad(februar)
+
+
+            assertVarsler(1.vedtaksperiode, Varselkode.RV_VV_2, Varselkode.RV_UT_23)
+
+        }
+
+        a2 {
+            håndterSøknad(1.januar til 9.februar)
+            håndterInntektsmelding(listOf(1.januar til 16.januar))
+            assertVarsler(1.vedtaksperiode, Varselkode.RV_IM_4) //Hvorfor får vi varsel når dette er første inntektsmelding på a2????
+        }
+
+        a1 {
+            håndterInntektsopplysningerFraLagretInnteksmelding(
+                vedtaksperiodeId = 2.vedtaksperiode,
+                inntekt = INNTEKT,
+                refusjon = INNTEKT
+            )
+            assertVarsler(2.vedtaksperiode, RV_IM_27)
+            assertForventetFeil(
+                forklaring = "Ettersom det er en annen periode som skal videre først må man påminne den som er neste til behandling",
+                nå = {
+                    assertSisteTilstand(1.vedtaksperiode, TilstandType.AVVENTER_REVURDERING)
+                },
+                ønsket = {
+                    assertSisteTilstand(1.vedtaksperiode, TilstandType.AVVENTER_HISTORIKK_REVURDERING)
+                }
+            )
+            håndterPåminnelse(1.vedtaksperiode, TilstandType.AVVENTER_REVURDERING, flagg = setOf("ønskerReberegning"))
+            assertSisteTilstand(1.vedtaksperiode, TilstandType.AVVENTER_HISTORIKK_REVURDERING)
         }
     }
 }
