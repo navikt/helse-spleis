60ff77aa75021c950e43a74a685dc0a366bc3e25
Roy A G <43439497+royag-nav@users.noreply.github.com>
2026-06-08 16:03:50 +0200

    Test på at vi håndterer ikke korrigerte refusjonsopplysinger frem i tid som sier at det er refusjon allikevel
    Co-authored-by: Erik Maximilian Forsman <erik.maximilian.forsman@nav.no>
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

---
+++ sykepenger-model/src/test/kotlin/no/nav/helse/spleis/e2e/inntektsmelding/InntektsmeldingMedRefusjonsopplForPerioderViEnnåIkkeHarMottatTest.kt
@@ -0,0 +1,50 @@
+package no.nav.helse.spleis.e2e.inntektsmelding
+
+import no.nav.helse.assertForventetFeil
+import no.nav.helse.dsl.AbstractDslTest
+import no.nav.helse.dsl.INNTEKT
+import no.nav.helse.dsl.a1
+import no.nav.helse.dsl.nyttVedtak
+import no.nav.helse.februar
+import no.nav.helse.hendelser.Inntektsmelding
+import no.nav.helse.hendelser.somPeriode
+import no.nav.helse.hendelser.til
+import no.nav.helse.januar
+import no.nav.helse.person.beløp.BeløpstidslinjeTest.Companion.assertBeløpstidslinje
+import no.nav.helse.økonomi.Inntekt.Companion.INGEN
+import org.junit.jupiter.api.Test
+
+internal class InntektsmeldingMedRefusjonsopplForPerioderViEnnåIkkeHarMottatTest : AbstractDslTest() {
+
+    @Test
+    fun `håndterer ikke korrigerte refusjonsopplysinger frem i tid som sier at det er refusjon allikevel`() {
+        a1 {
+            nyttVedtak(
+                periode = januar,
+                refusjon = Inntektsmelding.Refusjon(
+                    beløp = INNTEKT,
+                    opphørsdato = 31.januar,
+                )
+            )
+            val fremtidigeRefusjonsopplysninger = inspektør.ubrukteRefusjonsopplysninger.refusjonstidslinjer.getValue(1.januar)
+            assertBeløpstidslinje(fremtidigeRefusjonsopplysninger, 1.februar.somPeriode(), INGEN)
+            håndterInntektsmelding(
+                arbeidsgiverperioder = listOf(1.januar til 16.januar),
+                refusjon = Inntektsmelding.Refusjon(
+                    beløp = INNTEKT,
+                    opphørsdato = null,
+                )
+            )
+            val reviderteOpplysninger = inspektør.ubrukteRefusjonsopplysninger.refusjonstidslinjer.getValue(1.januar)
+            assertForventetFeil(
+                forklaring = "vi håndterer ikke korrigerte refusjonsopplysinger frem i tid som sier at det er refusjon allikevel",
+                nå = {
+                    assertBeløpstidslinje(reviderteOpplysninger, 1.februar.somPeriode(), INGEN)
+                },
+                ønsket = {
+                    assertBeløpstidslinje(reviderteOpplysninger, 1.februar.somPeriode(), INNTEKT)
+                }
+            )
+        }
+    }
+}
